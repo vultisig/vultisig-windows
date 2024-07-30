92665f51cddcf049c46cf388dd97f29b1865bbd5
Johnny Luo <johnnyluo1980@gmail.com>
2024-07-30 21:47:00 +1000

    fix db

--- storage/migrate/000001_initial.up.sql
+++ storage/migrate/000001_initial.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS vaults (
     reshare_prefix TEXT NOT NULL DEFAULT '',
     signers TEXT NOT NULL,
     is_backedup INTEGER NOT NULL DEFAULT 0,
-    order INTEGER NOT NULL DEFAULT 0,
+    listorder INTEGER NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
