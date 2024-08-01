79102d075d66324d67f470dc1a9a409062e85174
zhmolly11 <zhmolly11@gmail.com>
2024-08-01 12:26:03 +0900

    import vault ui workflow done

--- frontend/src/pages/importVault/ImportVaultView.tsx
+++ frontend/src/pages/importVault/ImportVaultView.tsx
@@ -1,41 +1,92 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import ImportVaultDialog from "../../components/dialog/ImportVaultDialog";
 
 const ImportVaultView: React.FC = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [fileContent, setFileContent] = useState("");
-  const [passwd, setPasswd] = useState("");
   const [isContinue, setContinue] = useState(false);
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [dialogTitle, setDialogTitle] = useState("");
+  const [dialogContent, setDialogContent] = useState("");
 
-  const onUpload = () => {
-    const fileInput = document.createElement("input");
-    fileInput.type = "file";
-    fileInput.style.display = "none";
+  const handleUpload = () => {
+    const fileInput = document.getElementById("file_upload");
+    if (fileInput) {
+      fileInput.click();
+    }
+  };
 
-    fileInput.addEventListener("change", (event) => {
-      if (event.target) {
-        setSelectedFile(event.target.files[0]);
-        if (event.target.files[0] !== "") {
-          return;
-        }
-        const reader = new FileReader();
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setSelectedFile(event.target.files[0]);
+      const reader = new FileReader();
 
-        reader.onload = () => {
-          const data = reader.result;
-          if (data) {
-            // verify if data is valid
+      reader.onload = () => {
+        const data = reader.result;
+        setFileContent("");
+        setContinue(false);
+        if (data) {
+          // basic verify
+          const isVerified = true;
+          if (isVerified) {
             setFileContent(data.toString());
+            setDialogTitle("Enter Password");
+            setDialogContent("");
+          } else {
+            setDialogTitle("Invalid File Format");
+            setDialogContent(
+              "Please make sure that you selected the correct backup file with .bak extension."
+            );
           }
-        };
+          setDialogOpen(true);
+        }
+      };
 
-        reader.readAsText(event.target.files[0]);
+      reader.readAsText(event.target.files[0]);
+      if (fileInputRef.current) {
+        fileInputRef.current.value = "";
       }
-    });
+    }
+  };
 
-    fileInput.click();
+  const handleCancel = () => {
+    setSelectedFile(null);
+    setFileContent("");
+    setContinue(false);
+  };
+
+  const handleCloseDialog = () => setDialogOpen(false);
+
+  const handleOk = (passwd: string) => {
+    // verify vault
+    console.log(passwd);
+    const isVerified = false;
+    if (isVerified) {
+      setContinue(true);
+    } else {
+      setFileContent("");
+      setDialogTitle("Incorrect Password");
+      setDialogContent("Failed to decrypt the backup");
+      setTimeout(() => {
+        setDialogOpen(true);
+      }, 0);
+    }
+  };
+
+  const handleContinue = () => {
+    // save vault
   };
 
   return (
     <>
+      <input
+        id="file_upload"
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        className="hidden"
+      />
       <div className="text-white pt-[10vh]">
         <div className="text-center px-20">
           <h6 className="text-lg mb-8">
@@ -43,7 +94,7 @@ const ImportVaultView: React.FC = () => {
           </h6>
           <div
             className="w-full bg-[#33E6BF]/[.14] h-[250px] border-2 border-dashed border-[#33E6BF] rounded-lg font-bold cursor-pointer"
-            onClick={onUpload}
+            onClick={handleUpload}
           >
             <img
               src="/assets/images/file.svg"
@@ -52,7 +103,7 @@ const ImportVaultView: React.FC = () => {
             />
             Upload backup file
           </div>
-          {selectedFile && fileContent && (
+          {selectedFile && fileContent && isContinue && (
             <div className="flex justify-between mt-8">
               <div className="flex">
                 <img
@@ -66,6 +117,7 @@ const ImportVaultView: React.FC = () => {
                 src="/assets/images/cancel.svg"
                 className="mr-4 w-4"
                 alt="cancel"
+                onClick={handleCancel}
               />
             </div>
           )}
@@ -76,14 +128,19 @@ const ImportVaultView: React.FC = () => {
                 : "text-[#BDBDBD] bg-white/[.10]"
             }`}
             disabled={!isContinue}
-            onClick={() => {
-              console.log("test");
-            }}
+            onClick={handleContinue}
           >
             Continue
           </button>
         </div>
       </div>
+      <ImportVaultDialog
+        title={dialogTitle}
+        content={dialogContent}
+        isOpen={isDialogOpen}
+        onClose={handleCloseDialog}
+        onOk={handleOk}
+      />
     </>
   );
 };
