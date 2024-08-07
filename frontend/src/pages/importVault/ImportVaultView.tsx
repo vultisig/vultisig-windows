4424eca4d1526cc15ef5ed96ad6c5eb873430cc6
0xkobe <dev_star11@outlook.com>
2024-08-07 21:11:27 +0800

    test

--- frontend/src/pages/importVault/ImportVaultView.tsx
+++ frontend/src/pages/importVault/ImportVaultView.tsx
@@ -129,7 +129,7 @@ const ImportVaultView: React.FC = () => {
         <div className="text-center px-20">
           <h6 className="text-lg mb-8">{t('enter_previous_vault')}</h6>
           <div
-            className="w-full bg-[#33E6BF]/[.14] h-[250px] border-2 border-dashed border-[#33E6BF] rounded-lg font-bold cursor-pointer"
+            className="bg-[#33E6BF]/[.14] h-[250px] border-2 border-dashed border-[#33E6BF] rounded-lg font-bold cursor-pointer"
             onClick={handleUpload}
           >
             {isContinue && decryptedContent && (
