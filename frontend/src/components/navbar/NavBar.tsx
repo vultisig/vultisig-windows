b3dee8d30e17c77bb4779e140ba0ea75da99db5b
ehsan-saradar <ehsan.saradar@gmail.com>
2024-08-10 15:13:25 +0330

    fix navbar lint

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -6,6 +6,7 @@ interface NavBarProps {
   title: string;
   questionLink?: string;
   handleBack?: () => void;
+  test?: string;
 }
 
 const NavBar: React.FC<NavBarProps> = ({ title, questionLink, handleBack }) => {
@@ -19,9 +20,8 @@ const NavBar: React.FC<NavBarProps> = ({ title, questionLink, handleBack }) => {
 
   return (
     <nav className="bg-primary text-white p-4 flex justify-between items-center border-b border-gray-400">
-      <img
-        src="/assets/images/icons/caretLeft.svg"
-        alt="Back"
+      <button
+        className="bg-transparent"
         onClick={() => {
           if (handleBack) {
             handleBack();
@@ -29,15 +29,22 @@ const NavBar: React.FC<NavBarProps> = ({ title, questionLink, handleBack }) => {
             navigate(-1);
           }
         }}
-        className="cursor-pointer"
-      />
+      >
+        <img
+          src="/assets/images/icons/caretLeft.svg"
+          alt="Back"
+          className="cursor-pointer"
+        />
+      </button>
+
       <h1 className="text-xl font-bold">{title}</h1>
-      <img
-        src="/assets/images/icons/question.svg"
-        alt="Question"
-        onClick={openHelp}
-        className={questionLink ? 'cursor-pointer' : 'invisible'}
-      />
+      <button className="bg-transparent" onClick={openHelp}>
+        <img
+          src="/assets/images/icons/question.svg"
+          alt="Question"
+          className={questionLink ? 'cursor-pointer' : 'invisible'}
+        />
+      </button>
     </nav>
   );
 };
