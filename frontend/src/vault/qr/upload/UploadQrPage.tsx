d746a8fe52bce0ea045b54f1abdf51c9da69af89
rcoderdev <radzionch@gmail.com>
2024-08-23 15:16:43 +0400

    feat: Add styled-components utility for horizontal padding
    This commit adds a new utility function, `horizontalPadding`, to the `styled-components` CSS library. The function takes a value (either a string or number) and generates CSS code for setting the left and right padding of an element. This utility can be used to easily apply consistent horizontal padding to elements in the UI, improving the overall layout and spacing.

--- frontend/src/vault/qr/upload/UploadQrPage.tsx
+++ frontend/src/vault/qr/upload/UploadQrPage.tsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components';
+import { VStack } from '../../../lib/ui/layout/Stack';
+import { horizontalPadding } from '../../../lib/ui/css/horizontalPadding';
+import { UploadQrPageHeader } from './UploadQrPageHeader';
+
+const Container = styled(VStack)`
+  flex: 1;
+  ${horizontalPadding(40)};
+  padding-bottom: 40px;
+`;
+
 export const UploadQrPage = () => {
-  return <p>upload qr page will be here</p>;
+  return (
+    <Container>
+      <UploadQrPageHeader />
+    </Container>
+  );
 };
