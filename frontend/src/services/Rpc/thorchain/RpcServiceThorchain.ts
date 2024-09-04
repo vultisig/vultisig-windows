9debf56fc6dbb46b93a1114ecfb8346f63e5b66b
Enrique Souza <enriquesouza@live.com>
2024-09-04 02:24:32 -0300

    thorchain

--- frontend/src/services/Rpc/thorchain/RpcServiceThorchain.ts
+++ frontend/src/services/Rpc/thorchain/RpcServiceThorchain.ts
@@ -1,70 +1,159 @@
 /* eslint-disable */
 import { Coin } from '../../../gen/vultisig/keysign/v1/coin_pb';
+import { Chain } from '../../../model/chain';
 import { CoinMeta } from '../../../model/coin-meta';
-import { SpecificGasInfo } from '../../../model/gas-info';
+import { SpecificGasInfo, SpecificThorchain } from '../../../model/gas-info';
+import { Endpoint } from '../../Endpoint';
 import { IRpcService } from '../IRpcService';
 
 export class RpcServiceThorchain implements IRpcService {
-  calculateFee(coin: Coin): number {
-    throw new Error('Method not implemented.');
+  async calculateFee(_coin?: Coin): Promise<number> {
+    let urlString = Endpoint.fetchThorchainNetworkInfoNineRealms;
+    const response = await fetch(urlString);
+    const data = await response.json();
+
+    const fee = Number(data.native_tx_fee_rune);
+
+    return fee;
   }
-  sendTransaction(encodedTransaction: string): Promise<string> {
-    throw new Error('Method not implemented.');
+
+  async sendTransaction(encodedTransaction: string): Promise<string> {
+    return await this.broadcastTransaction(encodedTransaction);
   }
-  getBalance(coin: Coin): Promise<string> {
-    throw new Error('Method not implemented.');
+
+  async getBalance(coin: Coin): Promise<string> {
+    let url = Endpoint.fetchAccountBalanceThorchainNineRealms(coin.address);
+    const response = await fetch(url, {
+      headers: {
+        'X-Client-ID': 'vultisig',
+      },
+    });
+
+    const data = await response.json();
+
+    return (
+      data.balances.find(
+        (b: any) => b.denom.toLowerCase() === coin.ticker.toLowerCase()
+      ).amount ?? 0
+    );
   }
-  broadcastTransaction(hex: string): Promise<string> {
-    throw new Error('Method not implemented.');
+
+  async broadcastTransaction(hex: string): Promise<string> {
+    const url = Endpoint.broadcastTransactionThorchainNineRealms;
+
+    // fetch to post the transaction
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: hex,
+    });
+
+    const data = await response.json();
+    return data.txResponse?.txhash;
   }
-  resolveENS?(ensName: string): Promise<string> {
-    throw new Error('Method not implemented.');
+
+  async resolveENS?(ensName: string): Promise<string> {
+    let url = Endpoint.resolveTNS(ensName);
+    const response = await fetch(url);
+    const data = await response.json();
+    const entry = data.entries.find(
+      (e: any) =>
+        e.chain.toLowerCase() === Chain.THORChain.toString().toLowerCase()
+    );
+
+    if (!entry) {
+      throw new Error('TNS entry not found');
+    }
+    return entry.address;
   }
-  getGasInfo(coin: Coin): Promise<SpecificGasInfo> {
-    throw new Error('Method not implemented.');
+
+  async getGasInfo(coin: Coin): Promise<SpecificThorchain> {
+    const account = await this.fetchAccountNumber(coin.address);
+    const fee = await this.calculateFee(coin);
+
+    const specificThorchain = {
+      fee, // sometimes the fee is calculated like EVMs, so we need to add it here
+      gasPrice: fee, //The gas price is the price per byte of the transaction
+      accountNumber: account?.accountNumber,
+      sequence: account?.sequence ?? 0,
+      isDeposit: false,
+    };
+
+    return specificThorchain;
   }
+
+  async fetchAccountNumber(address: string) {
+    const url = Endpoint.fetchAccountNumberThorchainNineRealms(address);
+    const response = await fetch(url, {
+      headers: {
+        'X-Client-ID': 'vultisig',
+      },
+    });
+
+    const data = await response.json();
+    return data.result.value;
+  }
+
+  /*
+  
+  Not sure why we have to get the network id
+
+  func getTHORChainChainID() async throws -> String  {
+        if !network.isEmpty {
+            print("network id\(network)")
+            return network
+        }
+        let (data, _) = try await URLSession.shared.data(from: Endpoint.thorchainNetworkInfo)
+        let response = try JSONDecoder().decode(THORChainNetworkStatus.self, from: data)
+        network = response.result.node_info.network
+        return response.result.node_info.network
+    }
+  */
+
   estimateGas?(
-    senderAddress: string,
-    recipientAddress: string,
-    value: bigint,
-    memo?: string
+    _senderAddress: string,
+    _recipientAddress: string,
+    _value: bigint,
+    _memo?: string
   ): Promise<bigint> {
     throw new Error('Method not implemented.');
   }
   fetchTokenBalance?(
-    contractAddress: string,
-    walletAddress: string
+    _contractAddress: string,
+    _walletAddress: string
   ): Promise<bigint> {
     throw new Error('Method not implemented.');
   }
   fetchAllowance?(
-    contractAddress: string,
-    owner: string,
-    spender: string
+    _contractAddress: string,
+    _owner: string,
+    _spender: string
   ): Promise<bigint> {
     throw new Error('Method not implemented.');
   }
   getTokenInfo?(
-    contractAddress: string
+    _contractAddress: string
   ): Promise<{ name: string; symbol: string; decimals: number }> {
     throw new Error('Method not implemented.');
   }
-  fetchTokens?(nativeToken: Coin): Promise<CoinMeta[]> {
+  fetchTokens?(_nativeToken: Coin): Promise<CoinMeta[]> {
     throw new Error('Method not implemented.');
   }
   fetchRecentBlockhash?(): Promise<string> {
     throw new Error('Method not implemented.');
   }
   fetchTokenAssociatedAccountByOwner?(
-    walletAddress: string,
-    mintAddress: string
+    _walletAddress: string,
+    _mintAddress: string
   ): Promise<string> {
     throw new Error('Method not implemented.');
   }
-  fetchTokenAccountsByOwner?(walletAddress: string): Promise<[]> {
+  fetchTokenAccountsByOwner?(_walletAddress: string): Promise<[]> {
     throw new Error('Method not implemented.');
   }
-  fetchHighPriorityFee?(account: string): Promise<number> {
+  fetchHighPriorityFee?(_account: string): Promise<number> {
     throw new Error('Method not implemented.');
   }
 }
