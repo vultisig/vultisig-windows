0ea65324b2f84c5791d0cb76cdec9c26c9bfebfd
rcoderdev <radzionch@gmail.com>
2024-09-06 11:23:58 +0400

    chore: Add deprecated send coin prompt component and related files

--- frontend/src/vault/components/VaultPrimaryActions.tsx
+++ frontend/src/vault/components/VaultPrimaryActions.tsx
@@ -1,11 +1,12 @@
 import { Button } from '../../lib/ui/buttons/Button';
 import { UniformColumnGrid } from '../../lib/ui/css/uniformColumnGrid';
 import { Text } from '../../lib/ui/text';
+import { SendCoinPromptDeprecated } from './sendDeprecated/SendCoinPromptDeprecated';
 
 export const VaultPrimaryActions: React.FC = () => {
   return (
     <UniformColumnGrid fullWidth gap={12}>
-      <Button kind="outlined">SEND</Button>
+      <SendCoinPromptDeprecated />
       <Button kind="outlined">
         <Text color="primarAlt">SWAP</Text>
       </Button>
