494a67743e86bdd659d845ab2c557fb0ac64184d
Enrique Souza <enriquesouza@live.com>
2024-09-12 22:33:48 -0300

    logs

--- frontend/src/services/Blockchain/solana/BlockchainServiceSolana.ts
+++ frontend/src/services/Blockchain/solana/BlockchainServiceSolana.ts
@@ -11,6 +11,10 @@ import {
 } from '../../../model/transaction';
 import { BlockchainService } from '../BlockchainService';
 import { SpecificSolana } from '../../../model/gas-info';
+import { SignedTransactionResult } from '../signed-transaction-result';
+import { tss } from '../../../../wailsjs/go/models';
+import { AddressServiceFactory } from '../../Address/AddressServiceFactory';
+import SignatureProvider from '../signature-provider';
 
 export class BlockchainServiceSolana
     extends BlockchainService
@@ -77,12 +81,12 @@ export class BlockchainServiceSolana
         const { priorityFee, recentBlockHash, fromTokenAssociatedAddress, toTokenAssociatedAddress } = specific;
 
         if (keysignPayload.coin.isNativeToken) {
-
-            let input = TW.Solana.Proto.SigningInput.create({
+            // Native token transfer
+            const input = TW.Solana.Proto.SigningInput.create({
                 transferTransaction: TW.Solana.Proto.Transfer.create({
                     recipient: keysignPayload.toAddress,
                     value: BigInt(keysignPayload.toAmount),
-                    memo: keysignPayload.memo,
+                    memo: keysignPayload.memo ?? '', // Memo could be optional
                 }),
                 recentBlockhash: recentBlockHash,
                 sender: keysignPayload.coin.address,
@@ -94,10 +98,10 @@ export class BlockchainServiceSolana
             return TW.Solana.Proto.SigningInput.encode(input).finish();
 
         } else {
-
-            if (!fromTokenAssociatedAddress && !toTokenAssociatedAddress) {
-
-                let tokenTransferMessage = TW.Solana.Proto.TokenTransfer.create({
+            // Token transfer
+            if (fromTokenAssociatedAddress && toTokenAssociatedAddress) {
+                // Both addresses are available for token transfer
+                const tokenTransferMessage = TW.Solana.Proto.TokenTransfer.create({
                     tokenMintAddress: keysignPayload.coin.contractAddress,
                     senderTokenAddress: fromTokenAssociatedAddress,
                     recipientTokenAddress: toTokenAssociatedAddress,
@@ -105,7 +109,7 @@ export class BlockchainServiceSolana
                     decimals: keysignPayload.coin.decimals,
                 });
 
-                let input = TW.Solana.Proto.SigningInput.create({
+                const input = TW.Solana.Proto.SigningInput.create({
                     tokenTransferTransaction: tokenTransferMessage,
                     recentBlockhash: recentBlockHash,
                     sender: keysignPayload.coin.address,
@@ -117,16 +121,15 @@ export class BlockchainServiceSolana
                 return TW.Solana.Proto.SigningInput.encode(input).finish();
 
             } else if (fromTokenAssociatedAddress && !toTokenAssociatedAddress) {
-
-                let receiverAddress = this.walletCore.SolanaAddress.createWithString(keysignPayload.toAddress);
-                let generatedAssociatedAddress = receiverAddress.defaultTokenAddress(keysignPayload.coin.contractAddress);
-
+                // Generate the associated address if `toTokenAssociatedAddress` is missing
+                const receiverAddress = this.walletCore.SolanaAddress.createWithString(keysignPayload.toAddress);
+                const generatedAssociatedAddress = receiverAddress.defaultTokenAddress(keysignPayload.coin.contractAddress);
 
                 if (!generatedAssociatedAddress) {
-                    throw new Error("We must have the association between the minted token and the TO address");
+                    throw new Error('We must have the association between the minted token and the TO address');
                 }
 
-                let createAndTransferTokenMessage = TW.Solana.Proto.CreateAndTransferToken.create({
+                const createAndTransferTokenMessage = TW.Solana.Proto.CreateAndTransferToken.create({
                     recipientMainAddress: keysignPayload.toAddress,
                     tokenMintAddress: keysignPayload.coin.contractAddress,
                     recipientTokenAddress: generatedAssociatedAddress,
@@ -135,7 +138,7 @@ export class BlockchainServiceSolana
                     decimals: keysignPayload.coin.decimals,
                 });
 
-                let input = TW.Solana.Proto.SigningInput.create({
+                const input = TW.Solana.Proto.SigningInput.create({
                     createAndTransferTokenTransaction: createAndTransferTokenMessage,
                     recentBlockhash: recentBlockHash,
                     sender: keysignPayload.coin.address,
@@ -146,34 +149,122 @@ export class BlockchainServiceSolana
 
                 return TW.Solana.Proto.SigningInput.encode(input).finish();
 
+            } else {
+                throw new Error('To send tokens we must have the association between the minted token and the TO address');
             }
-
         }
-
-        throw new Error('To send tokens we must have the association between the minted token and the TO address');
     }
 
     public async getPreSignedImageHash(
         keysignPayload: KeysignPayload
     ): Promise<string[]> {
+        // Get the pre-signed input data
         const input = await this.getPreSignedInputData(keysignPayload);
+
+        // Compile pre-image hashes using TransactionCompiler
         const preHashes = this.walletCore.TransactionCompiler.preImageHashes(
-            this.coinType as any,
+            this.coinType as any, // Ensure this.coinType is correct for Solana
             input
         );
 
-        const preSigningOutput =
-            TW.Solana.Proto.PreSigningOutput.decode(preHashes);
+        // Decode the result into a Solana-specific PreSigningOutput
+        const preSigningOutput = TW.Solana.Proto.PreSigningOutput.decode(preHashes);
+
+        console.log(preSigningOutput.errorMessage);
+
+        // Check for any error messages
         if (preSigningOutput.errorMessage !== '') {
             console.error('preSigningOutput error:', preSigningOutput.errorMessage);
             throw new Error(preSigningOutput.errorMessage);
         }
 
+        console.log('preSigningOutput:', preSigningOutput);
+
+        // Convert the result data to hex, and ensure consistency with Swift output
         const imageHashes = [
-            this.walletCore.HexCoding.encode(
-                preSigningOutput.data
-            ).stripHexPrefix(),
+            this.walletCore.HexCoding.encode(preSigningOutput.data).stripHexPrefix(),
         ];
+
+        console.log('imageHashes:', imageHashes[0].toString());
+
         return imageHashes;
+    }
+
+    public async getSignedTransaction(
+        vaultHexPublicKey: string,
+        vaultHexChainCode: string,
+        data: KeysignPayload | Uint8Array,
+        signatures: { [key: string]: tss.KeysignResponse }
+    ): Promise<SignedTransactionResult> {
+        let inputData: Uint8Array;
+        if (data instanceof Uint8Array) {
+            inputData = data;
+        } else {
+            inputData = await this.getPreSignedInputData(data);
+        }
+
+        const addressService = AddressServiceFactory.createAddressService(
+            this.chain,
+            this.walletCore
+        );
+
+        const solanaPublicKey = await addressService.getDerivedPubKey(
+            vaultHexPublicKey,
+            vaultHexChainCode,
+            this.walletCore.CoinTypeExt.derivationPath(this.coinType)
+        );
+        const publicKeyData = Buffer.from(solanaPublicKey, 'hex');
+        const publicKey = this.walletCore.PublicKey.createWithData(
+            publicKeyData,
+            this.walletCore.PublicKeyType.ed25519
+        );
+
+        const preHashes = this.walletCore.TransactionCompiler.preImageHashes(
+            this.coinType,
+            inputData
+        );
+
+        const publicKeys = this.walletCore.DataVector.create();
+        const preSigningOutput = TW.Solana.Proto.PreSigningOutput.decode(preHashes);
+
+        if (preSigningOutput.errorMessage !== '') {
+            throw new Error(preSigningOutput.errorMessage);
+        }
+
+        const allSignatures = this.walletCore.DataVector.create();
+        const signatureProvider = new SignatureProvider(
+            this.walletCore,
+            signatures
+        );
+        const signature = signatureProvider.getSignatureWithRecoveryId(
+            preSigningOutput.data
+        );
+
+        if (!publicKey.verify(signature, preSigningOutput.data)) {
+            throw new Error('Failed to verify signature');
+        }
+
+        allSignatures.add(signature);
+        publicKeys.add(publicKeyData);
+
+        const compiled = this.walletCore.TransactionCompiler.compileWithSignaturesAndPubKeyType(
+            this.coinType,
+            inputData,
+            allSignatures,
+            publicKeys,
+            this.walletCore.PublicKeyType.ed25519
+        );
+
+        const output = TW.Ethereum.Proto.SigningOutput.decode(compiled);
+
+        if (output.errorMessage !== '') {
+            throw new Error(output.errorMessage);
+        }
+
+        const result = new SignedTransactionResult(
+            this.walletCore.HexCoding.encode(output.encoded).stripHexPrefix(),
+            this.walletCore.HexCoding.encode(output.encoded)
+        );
+        return result;
     }
 }
