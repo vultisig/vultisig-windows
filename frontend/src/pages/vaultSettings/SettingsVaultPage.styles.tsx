1329256fcf4a77c902a4fd64627dd03af6ee60eb
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-09-16 19:42:18 +0300

    settings footer

--- frontend/src/pages/vaultSettings/SettingsVaultPage.styles.tsx
+++ frontend/src/pages/vaultSettings/SettingsVaultPage.styles.tsx
@@ -4,6 +4,7 @@ import { VStack } from '../../lib/ui/layout/Stack';
 import { Panel } from '../../lib/ui/panel/Panel';
 import { getColor } from '../../lib/ui/theme/getters';
 import { pageConfig } from '../../ui/page/config';
+import { PageSlice } from '../../ui/page/PageSlice';
 
 export const ListItemPanel = styled(Panel)`
   font-weight: 400;
@@ -14,10 +15,27 @@ export const ListItemPanel = styled(Panel)`
 
 export const Container = styled(VStack)`
   margin-bottom: ${pageConfig.verticalPadding}px;
+  justify-content: space-between;
 `;
 
 export const StyledVStack = styled(VStack)`
   &:not(:last-child) {
     margin-bottom: 18px;
   }
+`;
+
+export const Footer = styled.footer`
+  width: fit-content;
+  margin-inline: auto;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const StyledPageSlice = styled(PageSlice)`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
 `;
