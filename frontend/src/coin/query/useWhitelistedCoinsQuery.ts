b2cddd591fe486bfae8faf8b54c468718f8025a8
rcoderdev <radzionch@gmail.com>
2024-09-17 15:22:57 +0400

    Refactor useWhitelistedCoinsQuery and VaultChainCoinOptions
    This commit refactors the useWhitelistedCoinsQuery and VaultChainCoinOptions functions in the codebase. The changes include removing unused imports, simplifying the code structure, and improving the search functionality. The refactoring improves code readability and maintainability.

--- frontend/src/coin/query/useWhitelistedCoinsQuery.ts
+++ frontend/src/coin/query/useWhitelistedCoinsQuery.ts
@@ -1,25 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
-import { useMemo } from 'react';
 
 import { Fetch } from '../../../wailsjs/go/utils/GoHttp';
 import { EvmChain, evmChainIds } from '../../chain/evm/EvmChain';
-import { EagerQuery } from '../../lib/ui/query/Query';
-import { withoutDuplicates } from '../../lib/utils/array/withoutDuplicates';
 import { Chain } from '../../model/chain';
-import { CoinMeta } from '../../model/coin-meta';
-import { TokensStore } from '../../services/Coin/CoinList';
 import { Endpoint } from '../../services/Endpoint';
-import { areEqualCoins } from '../Coin';
 import {
   OneInchTokensResponse,
   oneInchTokenToCoinMeta,
 } from '../oneInch/token';
-import { getCoinMetaKey } from '../utils/coinMeta';
 
-export const useWhitelistedCoinsQuery = (
-  chain: Chain
-): EagerQuery<CoinMeta[]> => {
-  const query = useQuery({
+export const useWhitelistedCoinsQuery = (chain: Chain) => {
+  return useQuery({
     queryKey: ['whitelistedCoins', chain],
     queryFn: async () => {
       const evmChainId = evmChainIds[chain as EvmChain];
@@ -41,22 +32,4 @@ export const useWhitelistedCoinsQuery = (
       return [];
     },
   });
-
-  return useMemo(() => {
-    const initialData = TokensStore.TokenSelectionAssets.filter(
-      token => token.chain === chain && !token.isNativeToken
-    );
-
-    const data = withoutDuplicates(
-      [...initialData, ...(query.data ?? [])],
-      (one, another) =>
-        areEqualCoins(getCoinMetaKey(one), getCoinMetaKey(another))
-    );
-
-    return {
-      data,
-      isPending: query.isPending,
-      errors: query.error ? [query.error] : [],
-    };
-  }, [chain]);
 };
--- frontend/src/vault/chain/manage/coin/VaultChainCoinOptions.tsx
+++ frontend/src/vault/chain/manage/coin/VaultChainCoinOptions.tsx
@@ -1,9 +1,17 @@
+import { useMemo } from 'react';
+
+import { areEqualCoins } from '../../../../coin/Coin';
 import { useWhitelistedCoinsQuery } from '../../../../coin/query/useWhitelistedCoinsQuery';
-import { getCoinMetaSearchStrings } from '../../../../coin/utils/coinMeta';
+import {
+  getCoinMetaKey,
+  getCoinMetaSearchStrings,
+} from '../../../../coin/utils/coinMeta';
 import { VStack } from '../../../../lib/ui/layout/Stack';
 import { useCurrentSearch } from '../../../../lib/ui/search/CurrentSearchProvider';
 import { useSearchFilter } from '../../../../lib/ui/search/hooks/useSearchFilter';
 import { Text } from '../../../../lib/ui/text';
+import { withoutDuplicates } from '../../../../lib/utils/array/withoutDuplicates';
+import { TokensStore } from '../../../../services/Coin/CoinList';
 import { useCurrentVaultChainId } from '../../useCurrentVaultChainId';
 import { ManageVaultChainCoin } from './ManageVaultChainCoin';
 
@@ -12,10 +20,31 @@ export const VaultChainCoinOptions = () => {
 
   const query = useWhitelistedCoinsQuery(chainId);
 
+  const initialItems = useMemo(
+    () =>
+      TokensStore.TokenSelectionAssets.filter(
+        token => token.chain === chainId && !token.isNativeToken
+      ),
+    [chainId]
+  );
+
   const [searchQuery] = useCurrentSearch();
+
+  const items = useMemo(() => {
+    if (!searchQuery) {
+      return initialItems;
+    }
+
+    return withoutDuplicates(
+      [...initialItems, ...(query.data ?? [])],
+      (one, another) =>
+        areEqualCoins(getCoinMetaKey(one), getCoinMetaKey(another))
+    );
+  }, [searchQuery, initialItems, query]);
+
   const options = useSearchFilter({
     searchQuery,
-    items: query.data ?? [],
+    items,
     getSearchStrings: getCoinMetaSearchStrings,
   });
 
@@ -24,9 +53,9 @@ export const VaultChainCoinOptions = () => {
       {options.map(option => (
         <ManageVaultChainCoin key={option.ticker} value={option} />
       ))}
-      {query.isPending && (
+      {searchQuery && query.isPending && (
         <VStack fullWidth alignItems="center">
-          <Text>Loading ...</Text>
+          <Text>Searching ...</Text>
         </VStack>
       )}
     </>
