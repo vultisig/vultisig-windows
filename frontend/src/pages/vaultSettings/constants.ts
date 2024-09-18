bfd4cba2783192967d6fcc5a5cfd00b68a0d106a
Johnny Luo <johnnyluo1980@gmail.com>
2024-09-19 09:26:28 +1000

    Fix links

--- frontend/src/pages/vaultSettings/constants.ts
+++ frontend/src/pages/vaultSettings/constants.ts
@@ -16,6 +16,9 @@ export const VULTISIG_GITHUB_LINK =
   'https://github.com/vultisig/vultisig-windows';
 export const VULTISIG_TWITTER_LINK = 'https://x.com/vultisig';
 export const VULTISIG_DISCORD_LINK = 'https://discord.gg/ngvW8tRRfB';
+export const VULTISIG_PRIVACY_POLICY_LINK = 'https://vultisig.com/privacy';
+export const VULTISIG_TERMS_OF_SERVICE_LINK =
+  'https://vultisig.com/termofservice';
 
 type SettingItem = {
   id: string;
