f3487bcf51ba9e4fb2ffc831052d91a9fe09c9df
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-09-19 22:03:11 +0300

    feat: layout and animations

--- frontend/src/pages/vaultSettings/vaulFaq/FaqVaultPage.styles.tsx
+++ frontend/src/pages/vaultSettings/vaulFaq/FaqVaultPage.styles.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import { UnstyledButton } from '../../../lib/ui/buttons/UnstyledButton';
 import { borderRadius } from '../../../lib/ui/css/borderRadius';
+import { HStack } from '../../../lib/ui/layout/Stack';
 import { getColor } from '../../../lib/ui/theme/getters';
 
 export const FaqBox = styled.div`
@@ -14,7 +15,7 @@ export const FaqButton = styled(UnstyledButton)`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 8px;
+  gap: 12px;
 
   padding: 16px;
   ${borderRadius.m};
@@ -30,4 +31,15 @@ export const HorizontalLine = styled.div`
   width: 100%;
   height: 1px;
   background-color: ${getColor('textShy')};
+`;
+
+export const Row = styled(HStack)`
+  align-self: stretch;
+`;
+
+export const FaqContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12px;
 `;
