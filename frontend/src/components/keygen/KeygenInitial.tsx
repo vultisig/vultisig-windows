5e55637527de4eacbd67410b1dffaf35f19e75d7
rcoderdev <radzionch@gmail.com>
2024-09-23 10:54:18 +0400

    wip

--- frontend/src/components/keygen/KeygenInitial.tsx
+++ frontend/src/components/keygen/KeygenInitial.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+import { Button } from '../../lib/ui/buttons/Button';
+import { VStack } from '../../lib/ui/layout/Stack';
+import { makeAppPath } from '../../navigation';
 
 interface KeygenInitialProps {
   onContinue: () => void;
@@ -17,28 +22,24 @@ const KeygenInitial: React.FC<KeygenInitialProps> = ({ onContinue }) => {
           className="mt-12"
         />
         <p className="mt-4 text-sm text-[#BBC1C7]">{t('this_device_is_the')}</p>
-        <p className="mt-2 text-xl font-bold text-white">
-          {t('initiating_device')}
-        </p>
-        <button
-          className="text-lg rounded-full w-full font-bold py-2 mt-12 text-sm text-btn-primary bg-secondary"
-          onClick={onContinue}
-        >
-          {t('create_qr')}
-        </button>
+        <VStack alignItems="center" gap={40}>
+          <p className="mt-2 text-xl font-bold text-white">
+            {t('initiating_device')}
+          </p>
+          <Button onClick={onContinue}>{t('create_qr')}</Button>
+        </VStack>
       </div>
       <div className="w-80 h-80 bg-btn-primary rounded-2xl p-8 flex flex-col items-center justify-center">
         <img src="/assets/icons/pair.svg" alt="scan_qr" className="mt-12" />
         <p className="mt-4 text-sm text-[#BBC1C7]">{t('this_device_is_the')}</p>
-        <p className="mt-2 text-xl font-bold text-white">
-          {t('pairing_device')}
-        </p>
-        <button
-          className="text-lg rounded-full w-full font-bold py-2 mt-12 text-sm text-btn-primary bg-secondary"
-          onClick={() => {}}
-        >
-          {t('scan_qr')}
-        </button>
+        <VStack alignItems="center" gap={40}>
+          <p className="mt-2 text-xl font-bold text-white">
+            {t('pairing_device')}
+          </p>
+          <Link to={makeAppPath('importVault')}>
+            <Button as="div">{t('scan_qr')}</Button>
+          </Link>
+        </VStack>
       </div>
     </div>
   );
