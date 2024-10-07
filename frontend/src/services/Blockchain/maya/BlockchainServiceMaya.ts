aded09725d05d351ab4f017564e0cf990767a27e
Enrique Souza <enriquesouza@live.com>
2024-10-07 02:36:03 -0300

    cosmos

--- frontend/src/services/Blockchain/maya/BlockchainServiceMaya.ts
+++ frontend/src/services/Blockchain/maya/BlockchainServiceMaya.ts
@@ -248,7 +248,9 @@ export class BlockchainServiceMaya
       const parsedData = JSON.parse(serializedData);
       const txBytes = parsedData.tx_bytes;
       const decodedTxBytes = Buffer.from(txBytes, 'base64');
-      const hash = createHash('sha256').update(decodedTxBytes).digest('hex');
+      const hash = createHash('sha256')
+        .update(decodedTxBytes as any)
+        .digest('hex');
       const result = new SignedTransactionResult(
         serializedData,
         hash,
