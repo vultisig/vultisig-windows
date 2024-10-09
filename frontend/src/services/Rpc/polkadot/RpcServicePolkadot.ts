b0c450828edfd8d6e9e99c473901fe7548304d9b
Enrique Souza <enriquesouza@live.com>
2024-10-08 23:32:49 -0300

    remove unused methods

--- frontend/src/services/Rpc/polkadot/RpcServicePolkadot.ts
+++ frontend/src/services/Rpc/polkadot/RpcServicePolkadot.ts
@@ -2,6 +2,7 @@
 import { Coin } from '../../../gen/vultisig/keysign/v1/coin_pb';
 import { CoinMeta } from '../../../model/coin-meta';
 import { SpecificTransactionInfo } from '../../../model/specific-transaction-info';
+import { Endpoint } from '../../Endpoint';
 import { IRpcService } from '../IRpcService';
 
 export class RpcServicePolkadot implements IRpcService {
@@ -23,33 +24,42 @@ export class RpcServicePolkadot implements IRpcService {
   getSpecificTransactionInfo(coin: Coin): Promise<SpecificTransactionInfo> {
     throw new Error('Method not implemented.');
   }
-  estimateGas?(
-    senderAddress: string,
-    recipientAddress: string,
-    value: bigint,
-    memo?: string
-  ): Promise<bigint> {
-    throw new Error('Method not implemented.');
-  }
-  fetchTokenBalance?(
-    contractAddress: string,
-    walletAddress: string
-  ): Promise<bigint> {
-    throw new Error('Method not implemented.');
-  }
-  fetchAllowance?(
-    contractAddress: string,
-    owner: string,
-    spender: string
-  ): Promise<bigint> {
-    throw new Error('Method not implemented.');
-  }
-  getTokenInfo?(
-    contractAddress: string
-  ): Promise<{ name: string; symbol: string; decimals: number }> {
-    throw new Error('Method not implemented.');
-  }
-  fetchTokens?(nativeToken: Coin): Promise<CoinMeta[]> {
-    throw new Error('Method not implemented.');
+
+  private async fetchBalance(address: string): Promise<BigInt> {
+    const body = { key: address };
+    const maxRetries = 3;
+    const retryDelay = 1000;
+
+    for (let attempt = 1; attempt <= maxRetries; attempt++) {
+      try {
+        const requestBody = JSON.stringify(body);
+
+        const response = await fetch(Endpoint.polkadotServiceBalance, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: requestBody,
+        });
+
+        const data = await response.json();
+
+        if (data.data?.account?.balance) {
+          const balance = BigInt(data.data.account.balance) * BigInt(10 ** 10);
+          return balance;
+        }
+      } catch (error) {
+        console.log(
+          `PolkadotService > fetchBalance > ${error}, Attempt: ${attempt} of ${maxRetries}`
+        );
+        if (attempt < maxRetries) {
+          await new Promise(resolve => setTimeout(resolve, retryDelay));
+        } else {
+          return BigInt(0);
+        }
+      }
+    }
+
+    return BigInt(0);
   }
 }
--- frontend/src/services/Rpc/sui/RpcServiceSui.ts
+++ frontend/src/services/Rpc/sui/RpcServiceSui.ts
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import { Coin } from '../../../gen/vultisig/keysign/v1/coin_pb';
-import { CoinMeta } from '../../../model/coin-meta';
 import { SpecificTransactionInfo } from '../../../model/specific-transaction-info';
 import { IRpcService } from '../IRpcService';
 
@@ -21,35 +20,6 @@ export class RpcServiceSui implements IRpcService {
     throw new Error('Method not implemented.');
   }
   getSpecificTransactionInfo(coin: Coin): Promise<SpecificTransactionInfo> {
-    throw new Error('Method not implemented.');
-  }
-  estimateGas?(
-    senderAddress: string,
-    recipientAddress: string,
-    value: bigint,
-    memo?: string
-  ): Promise<bigint> {
-    throw new Error('Method not implemented.');
-  }
-  fetchTokenBalance?(
-    contractAddress: string,
-    walletAddress: string
-  ): Promise<bigint> {
-    throw new Error('Method not implemented.');
-  }
-  fetchAllowance?(
-    contractAddress: string,
-    owner: string,
-    spender: string
-  ): Promise<bigint> {
-    throw new Error('Method not implemented.');
-  }
-  getTokenInfo?(
-    contractAddress: string
-  ): Promise<{ name: string; symbol: string; decimals: number }> {
-    throw new Error('Method not implemented.');
-  }
-  fetchTokens?(nativeToken: Coin): Promise<CoinMeta[]> {
     throw new Error('Method not implemented.');
   }
 }
