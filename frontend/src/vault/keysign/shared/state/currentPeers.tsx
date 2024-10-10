b88b68834975bb5be041da285e4aa58e2101a6e1
rcoderdev <radzionch@gmail.com>
2024-10-10 15:29:22 +0400

    Refactor code to add omit utility function for omitting properties from an object

--- frontend/src/vault/keysign/shared/state/currentPeers.tsx
+++
@@ -1,4 +0,0 @@
-import { getStateProviderSetup } from '../../../../lib/ui/state/getStateProviderSetup';
-
-export const { useState: useCurrentPeers, provider: CurrentPeersProvider } =
-  getStateProviderSetup<string[]>('CurrentPeers');
