4cb1fe14e68ae710dda5a3fdf058b2a0d78dd69f
Enrique Souza <enriquesouza@live.com>
2024-10-11 00:59:05 -0300

    sui

--- frontend/src/services/Fee/sui/FeeServiceSui.ts
+++ frontend/src/services/Fee/sui/FeeServiceSui.ts
@@ -1,11 +1,20 @@
 /* eslint-disable */
 import { Coin } from '../../../gen/vultisig/keysign/v1/coin_pb';
 import { SpecificSui } from '../../../model/specific-transaction-info';
+import { RpcServiceFactory } from '../../Rpc/RpcServiceFactory';
 import { FeeService } from '../FeeService';
 import { IFeeService } from '../IFeeService';
 
 export class FeeServiceSui extends FeeService implements IFeeService {
   async getFee(coin: Coin): Promise<SpecificSui> {
-    return {} as SpecificSui;
+    const rpcService = RpcServiceFactory.createRpcService(this.chain);
+    try {
+      const specificTransactionInfo =
+        (await rpcService.getSpecificTransactionInfo(coin)) as SpecificSui;
+      return specificTransactionInfo;
+    } catch (ex) {
+      console.error('Failed to get SUI fee, error: ', ex);
+      return {} as SpecificSui;
+    }
   }
 }
