b797b3e1201d8b96af989fad1f1b54218445de8c
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-10-15 09:50:53 +0300

    fix: validate address onBlur

--- frontend/src/pages/vaultSettings/vaultAddressBook/schemas/addressSchema.ts
+++ frontend/src/pages/vaultSettings/vaultAddressBook/schemas/addressSchema.ts
@@ -1,14 +1,55 @@
+import { WalletCore } from '@trustwallet/wallet-core';
 import { z } from 'zod';
 
-export const addressSchema = z.object({
-  title: z
-    .string()
-    .min(1, 'vault_settings_address_book_title_min_length_error')
-    .max(50, 'vault_settings_address_book_title_max_length_error'),
-  address: z
-    .string()
-    .min(1, 'vault_settings_address_book_address_min_length_error'),
-  chain: z.string(),
-});
+import { AddressBookItem } from '../../../../lib/types/address-book';
+import { Chain } from '../../../../model/chain';
+import { AddressServiceFactory } from '../../../../services/Address/AddressServiceFactory';
 
-export type AddressFormValues = z.infer<typeof addressSchema>;
+export const getAddressSchema = ({
+  walletCore,
+  addressBookItems,
+}: {
+  walletCore: WalletCore | null;
+  addressBookItems: AddressBookItem[];
+}) =>
+  z
+    .object({
+      title: z
+        .string()
+        .min(1, 'vault_settings_address_book_title_min_length_error')
+        .max(50, 'vault_settings_address_book_title_max_length_error'),
+      address: z
+        .string()
+        .min(1, 'vault_settings_address_book_address_min_length_error'),
+      chain: z.string(),
+    })
+    .superRefine(async (data, ctx) => {
+      const { address, chain } = data;
+
+      const addressService = AddressServiceFactory.createAddressService(
+        chain as Chain,
+        walletCore
+      );
+
+      const isValidAddress = await addressService.validateAddress(address);
+
+      if (!isValidAddress) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ['address'],
+          message: 'vault_settings_address_book_invalid_address_error',
+        });
+      }
+
+      const isAddressAlreadyAdded = addressBookItems.some(
+        item => item.address === address
+      );
+
+      if (isAddressAlreadyAdded) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ['address'],
+          message: 'vault_settings_address_book_repeated_address_error',
+        });
+      }
+    });
