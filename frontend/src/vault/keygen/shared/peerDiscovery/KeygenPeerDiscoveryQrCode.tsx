a11767fe0c8ea8bfc6ec10cc2574d93b97b2ed07
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-10-17 08:55:58 +0300

    fix: QR code

--- frontend/src/vault/keygen/shared/peerDiscovery/KeygenPeerDiscoveryQrCode.tsx
+++ frontend/src/vault/keygen/shared/peerDiscovery/KeygenPeerDiscoveryQrCode.tsx
@@ -1,21 +1,8 @@
-import { ElementSizeAware } from '../../../../lib/ui/base/ElementSizeAware';
-import { TakeWholeSpaceCenterContent } from '../../../../lib/ui/layout/TakeWholeSpaceCenterContent';
 import { ComponentWithValueProps } from '../../../../lib/ui/props';
 import { FramedQrCode } from '../../../../lib/ui/qr/FramedQrCode';
 
 export const KeygenPeerDiscoveryQrCode = ({
   value,
 }: ComponentWithValueProps<string>) => {
-  return (
-    <ElementSizeAware
-      render={({ setElement, size }) => (
-        <TakeWholeSpaceCenterContent ref={setElement}>
-          <FramedQrCode
-            value={value}
-            size={size ? Math.min(size.width, size.height) : 0}
-          />
-        </TakeWholeSpaceCenterContent>
-      )}
-    />
-  );
+  return <FramedQrCode value={value} size={365} />;
 };
