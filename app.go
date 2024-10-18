a2cade6808225aea2fefea211b7ea36dd38b2c9d
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-10-18 08:48:05 +0300

    fix: vault qr being saved with wrong extension

--- app.go
+++ app.go
@@ -48,7 +48,9 @@ func (a *App) SaveFileBkp(suggestedFilename string, base64Data string) (string, 
 func (a *App) SaveFile(suggestedFilename string, base64Data string) (string, error) {
 	filename, err := runtime.SaveFileDialog(a.ctx, runtime.SaveDialogOptions{
 		Title:           "Save File",
-		Filters:         []runtime.FileFilter{{DisplayName: "All Files(*)", Pattern: "*.bak;*.dat;*.vult"}},
+		Filters:         []runtime.FileFilter{
+            {DisplayName: "All Supported Files (*.png;*.bak;*.dat;*.vult)", Pattern: "*.png;*.bak;*.dat;*.vult"},
+        },
 		DefaultFilename: suggestedFilename,
 	})
 	if err != nil {
