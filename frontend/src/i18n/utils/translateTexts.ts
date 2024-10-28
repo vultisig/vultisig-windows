6d7659c53e980d5b970cefdc4ef88cfc3b0db20f
rcoderdev <radzionch@gmail.com>
2024-10-28 16:26:23 +0400

    Add Language module and translation scripts

---
+++ frontend/src/i18n/utils/translateTexts.ts
@@ -0,0 +1,60 @@
+import { TranslationServiceClient } from '@google-cloud/translate';
+
+import { toBatches } from '../../lib/utils/array/toBatches';
+import { Language } from '../Language';
+
+const batchSize = 600;
+
+interface TranslateTextsParams {
+  texts: string[];
+  from: Language;
+  to: Language;
+}
+
+export const translateTexts = async ({
+  texts,
+  from,
+  to,
+}: TranslateTextsParams): Promise<string[]> => {
+  console.log(`Translating ${texts.length} texts from ${from} to ${to}`);
+  if (texts.length === 0) {
+    return [];
+  }
+
+  const translationClient = new TranslationServiceClient();
+
+  const batches = toBatches(texts, batchSize);
+
+  const result = [];
+  for (const contents of batches) {
+    const request = {
+      parent: `projects/${process.env.GOOGLE_TRANSLATE_PROJECT_ID}/locations/global`,
+      contents,
+      mimeType: 'text/plain',
+      sourceLanguageCode: from,
+      targetLanguageCode: to,
+    };
+
+    const [{ translations }] = await translationClient.translateText(request);
+    if (!translations) {
+      throw new Error('No translations');
+    }
+
+    result.push(
+      ...translations.map((translation, index) => {
+        let { translatedText } = translation;
+        if (!translatedText) {
+          throw new Error('No translatedText');
+        }
+
+        if (contents[index].endsWith('?') && !translatedText.endsWith('?')) {
+          translatedText += '?';
+        }
+
+        return translatedText;
+      })
+    );
+  }
+
+  return result;
+};
---
+++ frontend/src/lib/codegen/utils/createJsonFile.ts
@@ -0,0 +1,28 @@
+import { createFile } from './createFile';
+import { formatCode } from './formatCode';
+
+interface CreateJsonFileParams {
+  directory: string;
+  fileName: string;
+  content: string;
+}
+
+export const createJsonFile = async ({
+  directory,
+  fileName,
+  content,
+}: CreateJsonFileParams) => {
+  const extension = 'json';
+
+  const code = await formatCode({
+    extension,
+    content,
+  });
+
+  createFile({
+    directory,
+    fileName,
+    content: code,
+    extension,
+  });
+};
