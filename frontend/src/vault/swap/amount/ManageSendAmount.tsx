a97aa08b6a1a7c3f48b3f7248b386fbf6db18d59
atarapata <oleksandr@limitless.network>
2024-11-05 20:23:48 +0800

    added max button for amount input at swap page

--- frontend/src/vault/swap/amount/ManageSendAmount.tsx
+++ frontend/src/vault/swap/amount/ManageSendAmount.tsx
@@ -1,7 +1,45 @@
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 
+import { fromChainAmount } from '../../../chain/utils/fromChainAmount';
+import { ActionInsideInteractiveElement } from '../../../lib/ui/base/ActionInsideInteractiveElement';
+import { UnstyledButton } from '../../../lib/ui/buttons/UnstyledButton';
+import { borderRadius } from '../../../lib/ui/css/borderRadius';
+import { centerContent } from '../../../lib/ui/css/centerContent';
+import { horizontalPadding } from '../../../lib/ui/css/horizontalPadding';
+import { textInputHeight } from '../../../lib/ui/css/textInput';
+import { toSizeUnit } from '../../../lib/ui/css/toSizeUnit';
 import { AmountTextInput } from '../../../lib/ui/inputs/AmountTextInput';
+import { HStack } from '../../../lib/ui/layout/Stack';
+import { text } from '../../../lib/ui/text';
+import { getColor } from '../../../lib/ui/theme/getters';
+import { AmountSuggestion } from '../../send/amount/AmountSuggestion';
+import { SendCoinBalanceDependant } from '../../send/coin/balance/SendCoinBalanceDependant';
 import { useSwapAmount } from '../state/amount';
+
+const suggestions = [0.25, 0.5];
+
+const maxButtonOffset = 8;
+const maxButtonHeight = textInputHeight - maxButtonOffset * 2;
+
+const MaxButton = styled(UnstyledButton)`
+  ${horizontalPadding(8)};
+  ${borderRadius.s};
+  ${centerContent};
+  height: ${toSizeUnit(maxButtonHeight)};
+
+  ${text({
+    weight: 600,
+    size: 16,
+    color: 'contrast',
+  })}
+
+  &:hover {
+    background: ${getColor('mist')};
+  }
+
+  text-transform: uppercase;
+`;
 
 export const ManageAmount = () => {
   const [value, setValue] = useSwapAmount();
@@ -9,15 +47,54 @@ export const ManageAmount = () => {
   const { t } = useTranslation();
 
   return (
-    <AmountTextInput
-      label={t('amount')}
-      placeholder={t('enter_amount')}
-      value={value}
-      onValueChange={setValue}
-      onKeyDown={e => {
-        if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
-          e.preventDefault();
-        }
+    <ActionInsideInteractiveElement
+      render={() => (
+        <AmountTextInput
+          label={t('amount')}
+          suggestion={
+            <SendCoinBalanceDependant
+              pending={() => null}
+              error={() => null}
+              success={({ amount, decimals }) => (
+                <HStack alignItems="center" gap={4}>
+                  {suggestions.map(suggestion => (
+                    <AmountSuggestion
+                      onClick={() => {
+                        setValue(
+                          fromChainAmount(amount, decimals) * suggestion
+                        );
+                      }}
+                      key={suggestion}
+                      value={suggestion}
+                    />
+                  ))}
+                </HStack>
+              )}
+            />
+          }
+          placeholder={t('enter_amount')}
+          value={value}
+          onValueChange={setValue}
+        />
+      )}
+      action={
+        <SendCoinBalanceDependant
+          pending={() => null}
+          error={() => null}
+          success={({ amount, decimals }) => (
+            <MaxButton
+              onClick={() => {
+                setValue(fromChainAmount(amount, decimals));
+              }}
+            >
+              {t('max')}
+            </MaxButton>
+          )}
+        />
+      }
+      actionPlacerStyles={{
+        right: maxButtonOffset,
+        bottom: maxButtonOffset,
       }}
     />
   );
