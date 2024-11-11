f6903076e187fd3ff297ee964841c08c7b02cb32
Enrique Souza <enriquesouza@live.com>
2024-11-11 05:11:19 -0300

    clean up

--- frontend/src/services/Send/SendServiceFactory.ts
+++ frontend/src/services/Send/SendServiceFactory.ts
@@ -65,7 +65,7 @@ export class SendServiceFactory {
       case Chain.TerraClassic:
         return new SendServiceCosmos(chain, walletCore);
       case Chain.Polkadot:
-        return new SendService(chain, walletCore); // TODO: implement Polkadot
+        return new SendService(chain, walletCore);
       default:
         throw new Error('Chain not supported');
     }
