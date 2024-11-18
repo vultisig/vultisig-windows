2372fb60c22b7d593ced49c0eadf1e66a1c126fc
rcoderdev <radzionch@gmail.com>
2024-11-18 15:54:50 +0400

    refactor: simplify peer options query by removing unused signers logic

--- frontend/src/vault/keygen/shared/peerDiscovery/queries/usePeerOptionsQuery.ts
+++ frontend/src/vault/keygen/shared/peerDiscovery/queries/usePeerOptionsQuery.ts
@@ -1,11 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { pollingQueryOptions } from '../../../../../lib/ui/query/utils/options';
-import { intersection } from '../../../../../lib/utils/array/intersection';
 import { without } from '../../../../../lib/utils/array/without';
 import { withoutDuplicates } from '../../../../../lib/utils/array/withoutDuplicates';
 import { queryUrl } from '../../../../../lib/utils/query/queryUrl';
-import { useCurrentVault } from '../../../../state/currentVault';
 import { useCurrentLocalPartyId } from '../../../state/currentLocalPartyId';
 import { useCurrentServerUrl } from '../../../state/currentServerUrl';
 import { useCurrentSessionId } from '../../state/currentSessionId';
@@ -14,17 +12,13 @@ export const usePeerOptionsQuery = () => {
   const sessionId = useCurrentSessionId();
   const localPartyId = useCurrentLocalPartyId();
   const serverUrl = useCurrentServerUrl();
-  const { signers } = useCurrentVault();
 
   return useQuery({
     queryKey: ['peerOptions', sessionId, serverUrl],
     queryFn: async () => {
       const response = await queryUrl<string[]>(`${serverUrl}/${sessionId}`);
 
-      return intersection(
-        without(withoutDuplicates(response), localPartyId),
-        signers
-      );
+      return without(withoutDuplicates(response), localPartyId);
     },
     ...pollingQueryOptions(2000),
   });
