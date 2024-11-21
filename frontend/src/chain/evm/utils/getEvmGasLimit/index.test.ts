2b746865448b215e7d814888ba7b0cabc1ecef24
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-11-21 22:09:18 +0200

    feat: add tests for get evm gas limit

---
+++ frontend/src/chain/evm/utils/getEvmGasLimit/index.test.ts
@@ -0,0 +1,105 @@
+import { describe, expect, it, vi } from 'vitest';
+
+import { EvmChain } from '../../../../model/chain';
+import { getEvmGasLimit } from '../getEvmGasLimit';
+
+vi.mock('../evmGasLimit', () => ({
+  evmNativeTokenGasLimit: {
+    [EvmChain.Ethereum]: 23000,
+    [EvmChain.Base]: 40000,
+    [EvmChain.Arbitrum]: 120000,
+    [EvmChain.Polygon]: 23000,
+    [EvmChain.Optimism]: 40000,
+    [EvmChain.CronosChain]: 40000,
+    [EvmChain.Blast]: 40000,
+    [EvmChain.BSC]: 40000,
+    [EvmChain.Avalanche]: 23000,
+    [EvmChain.Zksync]: 200000,
+  },
+  evmTokenGasLimit: {
+    [EvmChain.Ethereum]: 120000,
+    [EvmChain.Base]: 120000,
+    [EvmChain.Arbitrum]: 120000,
+    [EvmChain.Polygon]: 120000,
+    [EvmChain.Optimism]: 120000,
+    [EvmChain.CronosChain]: 120000,
+    [EvmChain.Blast]: 120000,
+    [EvmChain.BSC]: 120000,
+    [EvmChain.Avalanche]: 120000,
+    [EvmChain.Zksync]: 200000,
+  },
+}));
+
+describe('getEvmGasLimit', () => {
+  it('should return the native token gas limit for Ethereum', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: EvmChain.Ethereum,
+      isNativeToken: true,
+    });
+    expect(gasLimit).toBe(23000);
+  });
+
+  it('should return the token gas limit for Ethereum', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: EvmChain.Ethereum,
+      isNativeToken: false,
+    });
+    expect(gasLimit).toBe(120000);
+  });
+
+  it('should return the native token gas limit for Base', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: EvmChain.Base,
+      isNativeToken: true,
+    });
+    expect(gasLimit).toBe(40000);
+  });
+
+  it('should return the token gas limit for Base', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: EvmChain.Base,
+      isNativeToken: false,
+    });
+    expect(gasLimit).toBe(120000);
+  });
+
+  it('should return the native token gas limit for Arbitrum', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: EvmChain.Arbitrum,
+      isNativeToken: true,
+    });
+    expect(gasLimit).toBe(120000);
+  });
+
+  it('should return the token gas limit for Arbitrum', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: EvmChain.Arbitrum,
+      isNativeToken: false,
+    });
+    expect(gasLimit).toBe(120000);
+  });
+
+  it('should return the native token gas limit for Zksync', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: EvmChain.Zksync,
+      isNativeToken: true,
+    });
+    expect(gasLimit).toBe(200000);
+  });
+
+  it('should return the token gas limit for Zksync', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: EvmChain.Zksync,
+      isNativeToken: false,
+    });
+    expect(gasLimit).toBe(200000);
+  });
+
+  it('should return undefined if the chain ID does not exist in the record', () => {
+    const gasLimit = getEvmGasLimit({
+      chainId: 'UNKNOWN_CHAIN' as EvmChain,
+      isNativeToken: true,
+    });
+    expect(gasLimit).toBeUndefined();
+  });
+});
