7a400c664c4e8e001e10c3900d35157f0c14de6f
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-11-26 18:03:18 +0200

    feat: add keygen tests

---
+++ frontend/src/vault/keygen/utils/joinSession/index.ts
@@ -0,0 +1,25 @@
+import { assertFetchResponse } from '../../../../lib/utils/fetch/assertFetchResponse';
+
+type JoinSessionInput = {
+  serverUrl: string;
+  sessionId: string;
+  localPartyId: string;
+};
+
+export const joinSession = async ({
+  serverUrl,
+  sessionId,
+  localPartyId,
+}: JoinSessionInput) => {
+  const response = await fetch(`${serverUrl}/${sessionId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify([localPartyId]),
+  });
+
+  await assertFetchResponse(response);
+
+  return response;
+};
