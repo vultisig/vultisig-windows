e72093f1464977a10e104e786aa8ec1f22a5c28c
rcoderdev <radzionch@gmail.com>
2024-11-28 11:21:01 +0400

    refactor: simplify toStorageVault function by using object destructuring and returning an object directly

--- frontend/src/vault/utils/storageVault.ts
+++ frontend/src/vault/utils/storageVault.ts
@@ -4,21 +4,31 @@ import { Vault } from '../../gen/vultisig/vault/v1/vault_pb';
 export const getStorageVaultId = (vault: storage.Vault): string =>
   vault.public_key_ecdsa;
 
-export const toStorageVault = (vault: Vault): storage.Vault => {
-  return storage.Vault.createFrom({
-    name: vault.name,
-    public_key_ecdsa: vault.publicKeyEcdsa,
-    public_key_eddsa: vault.publicKeyEddsa,
-    signers: vault.signers,
-    created_at: vault.createdAt,
-    hex_chain_code: vault.hexChainCode,
-    keyshares: vault.keyShares.map(share => ({
-      public_key: share.publicKey,
-      keyshare: share.keyshare,
-    })),
-    local_party_id: vault.localPartyId,
-    reshare_prefix: vault.resharePrefix,
-    is_backed_up: true,
-    coins: [],
-  });
-};
+export const toStorageVault = ({
+  name,
+  publicKeyEcdsa,
+  publicKeyEddsa,
+  signers,
+  createdAt,
+  hexChainCode,
+  keyShares,
+  localPartyId,
+  resharePrefix,
+}: Vault): storage.Vault => ({
+  name: name,
+  public_key_ecdsa: publicKeyEcdsa,
+  public_key_eddsa: publicKeyEddsa,
+  signers: signers,
+  created_at: createdAt,
+  hex_chain_code: hexChainCode,
+  keyshares: keyShares.map(share => ({
+    public_key: share.publicKey,
+    keyshare: share.keyshare,
+  })),
+  local_party_id: localPartyId,
+  reshare_prefix: resharePrefix,
+  order: 0,
+  is_backed_up: true,
+  coins: [],
+  convertValues: () => {},
+});
