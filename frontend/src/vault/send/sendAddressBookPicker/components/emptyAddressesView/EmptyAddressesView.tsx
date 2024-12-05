d5bcc4e4aaad945fd0e025ebc557af1ad773e466
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-12-05 10:38:16 +0200

    feat: empty address view componnets

---
+++ frontend/src/vault/send/sendAddressBookPicker/components/emptyAddressesView/EmptyAddressesView.tsx
@@ -0,0 +1,52 @@
+import { useTranslation } from 'react-i18next';
+
+import { Button } from '../../../../../lib/ui/buttons/Button';
+import { PageHeaderBackButton } from '../../../../../ui/page/PageHeaderBackButton';
+import { PageHeaderTitle } from '../../../../../ui/page/PageHeaderTitle';
+import { AddressBookPageHeader } from '../../SendAddressBookManager.styles';
+import {
+  CenteredBox,
+  Container,
+  ResponsiveImage,
+  ResponsiveText,
+} from './EmptyAddressView.styles';
+
+type EmptyAddressesViewProps = {
+  onOpenAddAddressView: () => void;
+};
+
+const EmptyAddressesView = ({
+  onOpenAddAddressView,
+}: EmptyAddressesViewProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <AddressBookPageHeader
+        data-testid="EmptyAddressesView-AddressBookPageHeader"
+        primaryControls={<PageHeaderBackButton />}
+        title={
+          <PageHeaderTitle>{t('vault_settings_address_book')}</PageHeaderTitle>
+        }
+      />
+      <Container>
+        <CenteredBox>
+          <ResponsiveImage
+            src="/assets/images/warningYellow.svg"
+            alt="warning"
+            width={120}
+            height={120}
+          />
+          <ResponsiveText weight={700} color="contrast">
+            {t('vault_settings_address_book_no_addresses_title')}
+          </ResponsiveText>
+        </CenteredBox>
+        <Button onClick={onOpenAddAddressView}>
+          {t('vault_settings_address_book_add_addresses_button')}
+        </Button>
+      </Container>
+    </>
+  );
+};
+
+export default EmptyAddressesView;
