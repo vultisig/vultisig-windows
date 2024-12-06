58398f67ae01699c30ae1bdd052bd216114031a0
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-12-06 20:42:04 +0200

    fix: SendFeeValue

--- frontend/src/vault/send/fee/SendFeeValue.tsx
+++ frontend/src/vault/send/fee/SendFeeValue.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
+
 import { formatFee } from '../../../chain/tx/fee/utils/formatFee';
 import { useCurrentSendCoin } from '../state/sendCoin';
+import { useSendFees } from '../state/sendFees';
 import { useSendSpecificTxInfo } from './SendSpecificTxInfoProvider';
 
 export const SendFeeValue = () => {
   const txInfo = useSendSpecificTxInfo();
   const [coinKey] = useCurrentSendCoin();
-  const { chain } = coinKey;
 
-  return <>{formatFee({ chain: chain, txInfo })}</>;
+  const { chain } = coinKey;
+  const fees = formatFee({ chain: chain, txInfo });
+  const [, setFees] = useSendFees();
+
+  useEffect(() => {
+    setFees({
+      totalFeesFormatted: fees,
+    });
+  }, [fees, setFees]);
+
+  return fees;
 };
