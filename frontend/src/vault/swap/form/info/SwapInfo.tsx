e40c4dc7d4a28ca634fcfa44608b11a9b6455222
rcoderdev <radzionch@gmail.com>
2024-12-06 12:10:03 +0400

    feat: improve code formatting and consistency in TypeScript definitions and service files

--- frontend/src/vault/swap/form/info/SwapInfo.tsx
+++ frontend/src/vault/swap/form/info/SwapInfo.tsx
@@ -1,11 +1,17 @@
 import { StrictInfoRow } from '../../../../lib/ui/layout/StrictInfoRow';
 import { ActiveQueryOnly } from '../../../../lib/ui/query/components/ActiveQueryOnly';
+import { WarningBlock } from '../../../../lib/ui/status/WarningBlock';
+import { extractErrorMsg } from '../../../../lib/utils/error/extractErrorMsg';
 import { useSwapQuoteQuery } from '../../queries/useSwapQuoteQuery';
 import { SwapProvider } from './SwapProvider';
 import { SwapTotalFee } from './SwapTotalFee';
 
 export const SwapInfo = () => {
   const query = useSwapQuoteQuery();
+
+  if (query.error) {
+    return <WarningBlock>{extractErrorMsg(query.error)}</WarningBlock>;
+  }
 
   return (
     <ActiveQueryOnly value={query}>
