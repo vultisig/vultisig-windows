68c33c1b57b3f10710461f86c14980feff365049
rcoderdev <radzionch@gmail.com>
2024-12-09 14:20:34 +0400

    refactor: remove KeysignPayloadUtils and replace with useKeysignMsgsQuery for improved query handling

---
+++ frontend/src/vault/keysign/join/queries/useKeysignMsgsQuery.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { Chain } from '../../../../model/chain';
+import { useAssertWalletCore } from '../../../../providers/WalletCoreProvider';
+import { BlockchainServiceFactory } from '../../../../services/Blockchain/BlockchainServiceFactory';
+import { useKeysignPayload } from '../../shared/state/keysignPayload';
+
+export const useKeysignMsgsQuery = () => {
+  const walletCore = useAssertWalletCore();
+
+  const payload = useKeysignPayload();
+
+  return useQuery({
+    queryKey: ['keysignMsgs', payload],
+    queryFn: () => {
+      const { coin } = payload;
+      if (!coin) {
+        return [];
+      }
+
+      const service = BlockchainServiceFactory.createService(
+        coin.chain as Chain,
+        walletCore!
+      );
+
+      return service.getPreSignedImageHash(payload);
+    },
+    meta: {
+      disablePersist: true,
+    },
+  });
+};
