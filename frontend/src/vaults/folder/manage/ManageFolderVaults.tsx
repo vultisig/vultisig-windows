62301314b472bf178ee2aebb582c28892c7671fd
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2024-12-10 22:06:38 +0200

    feat: improve manage folder vaults

--- frontend/src/vaults/folder/manage/ManageFolderVaults.tsx
+++ frontend/src/vaults/folder/manage/ManageFolderVaults.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { storage } from '../../../../wailsjs/go/models';
-import { DnDList } from '../../../lib/dnd/DnDList';
+import { DnDList, ItemChangeParams } from '../../../lib/dnd/DnDList';
 import { VStack } from '../../../lib/ui/layout/Stack';
 import {
   DnDItemContainer,
   DnDItemHighlight,
 } from '../../../lib/ui/list/item/DnDItemContainer';
+import { Text } from '../../../lib/ui/text';
 import { isEmpty } from '../../../lib/utils/array/isEmpty';
 import { sortEntitiesWithOrder } from '../../../lib/utils/entities/EntityWithOrder';
 import { getNewOrder } from '../../../lib/utils/order/getNewOrder';
@@ -20,74 +22,78 @@ import { useCurrentVaultFolder } from '../state/currentVaultFolder';
 
 export const ManageFolderVaults = () => {
   const { id } = useCurrentVaultFolder();
-
+  const { t } = useTranslation();
   const vaults = useFolderVaults(id);
-
   const [items, setItems] = useState(() => sortEntitiesWithOrder(vaults));
+  const { mutate: remove } = useRemoveVaultFromFolderMutation();
+  const { mutate: updateOrder } = useUpdateVaultOrderMutation();
 
   useEffect(() => {
     setItems(sortEntitiesWithOrder(vaults));
   }, [vaults]);
 
-  const { mutate: remove } = useRemoveVaultFromFolderMutation();
+  const handleOnItemChange = (id: string, { index }: ItemChangeParams) => {
+    const order = getNewOrder({
+      orders: items.map(item => item.order),
+      sourceIndex: items.findIndex(item => getStorageVaultId(item) === id),
+      destinationIndex: index,
+    });
 
-  const { mutate: updateOrder } = useUpdateVaultOrderMutation();
+    updateOrder({
+      id,
+      order,
+    });
+
+    setItems(prev =>
+      sortEntitiesWithOrder(
+        prev.map(
+          item =>
+            (getStorageVaultId(item) === id
+              ? { ...item, order }
+              : item) as storage.Vault
+        )
+      )
+    );
+  };
 
   if (isEmpty(items)) return null;
 
   return (
-    <DnDList
-      items={items}
-      getItemId={getStorageVaultId}
-      onChange={(id, { index }) => {
-        const order = getNewOrder({
-          orders: items.map(item => item.order),
-          sourceIndex: items.findIndex(item => getStorageVaultId(item) === id),
-          destinationIndex: index,
-        });
+    <VStack gap={8}>
+      <Text weight="500" color="supporting" size={14}>
+        {t('current_vaults')}
+      </Text>
+      <DnDList
+        items={items}
+        getItemId={getStorageVaultId}
+        onChange={handleOnItemChange}
+        renderList={({ props }) => <VStack gap={8} {...props} />}
+        renderItem={({ item, draggableProps, dragHandleProps, status }) => {
+          const vaultId = getStorageVaultId(item);
 
-        updateOrder({
-          id,
-          order,
-        });
-
-        setItems(prev =>
-          sortEntitiesWithOrder(
-            prev.map(
-              item =>
-                (getStorageVaultId(item) === id
-                  ? { ...item, order }
-                  : item) as storage.Vault
-            )
-          )
-        );
-      }}
-      renderList={({ props }) => <VStack gap={8} {...props} />}
-      renderItem={({ item, draggableProps, dragHandleProps, status }) => {
-        const vaultId = getStorageVaultId(item);
-
-        return (
-          <DnDItemContainer
-            key={vaultId}
-            {...draggableProps}
-            {...dragHandleProps}
-            status={status}
-          >
-            <CurrentVaultProvider value={item}>
-              <FolderVaultOption
-                isDraggable
-                value={true}
-                onChange={() => {
-                  remove({
-                    vaultId,
-                  });
-                }}
-              />
-            </CurrentVaultProvider>
-            {status === 'overlay' && <DnDItemHighlight />}
-          </DnDItemContainer>
-        );
-      }}
-    />
+          return (
+            <DnDItemContainer
+              key={vaultId}
+              {...draggableProps}
+              {...dragHandleProps}
+              status={status}
+            >
+              <CurrentVaultProvider value={item}>
+                <FolderVaultOption
+                  isDraggable
+                  value={true}
+                  onChange={() => {
+                    remove({
+                      vaultId,
+                    });
+                  }}
+                />
+              </CurrentVaultProvider>
+              {status === 'overlay' && <DnDItemHighlight />}
+            </DnDItemContainer>
+          );
+        }}
+      />
+    </VStack>
   );
 };
