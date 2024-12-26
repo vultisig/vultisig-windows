3cda4902665c2b1b5997739646a41b55bd55f76d
rcoderdev <radzionch@gmail.com>
2024-12-26 12:58:48 +0400

    refactor: remove walletCorePublicKeyType and utilize match utility for public key type handling

--- frontend/src/chain/keysign/TssKeysignType.ts
+++ frontend/src/chain/keysign/TssKeysignType.ts
@@ -1,13 +1,6 @@
-import { PublicKeyType } from '@trustwallet/wallet-core/dist/src/wallet-core';
-
 import { Chain } from '../../model/chain';
 
 export type TssKeysignType = 'ecdsa' | 'eddsa';
-
-export const walletCorePublicKeyType: Record<TssKeysignType, PublicKeyType> = {
-  ecdsa: PublicKeyType.secp256k1,
-  eddsa: PublicKeyType.ed25519,
-};
 
 export const edDsaChains = [
   Chain.Solana,
--- frontend/src/vault/publicKey/toWalletCorePublicKey.ts
+++ frontend/src/vault/publicKey/toWalletCorePublicKey.ts
@@ -1,8 +1,8 @@
 import { WalletCore } from '@trustwallet/wallet-core';
 
 import { GetDerivedPubKey } from '../../../wailsjs/go/tss/TssService';
-import { walletCorePublicKeyType } from '../../chain/keysign/TssKeysignType';
 import { getCoinType } from '../../chain/walletCore/getCoinType';
+import { match } from '../../lib/utils/match';
 import { Chain } from '../../model/chain';
 import { VaultPublicKey } from './VaultPublicKey';
 
@@ -31,7 +31,10 @@ export const toWalletCorePublicKey = async ({
     isEdDSA
   );
 
-  const publicKeyType = walletCorePublicKeyType[value.type];
+  const publicKeyType = match(value.type, {
+    ecdsa: () => walletCore.PublicKeyType.secp256k1,
+    eddsa: () => walletCore.PublicKeyType.ed25519,
+  });
 
   return walletCore.PublicKey.createWithData(
     Buffer.from(derivedPublicKey, 'hex'),
