1d169bad8324c39a35b9211d5ff0df78181da874
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-07 21:23:25 +0200

    feat: hook for auto discovering tokens

---
+++ frontend/src/coin/query/useAutoDiscoverTokensForNativeToken.ts
@@ -0,0 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { Coin } from '../../gen/vultisig/keysign/v1/coin_pb';
+import { Chain } from '../../model/chain';
+import { IRpcService } from '../../services/Rpc/IRpcService';
+import { RpcServiceFactory } from '../../services/Rpc/RpcServiceFactory';
+import { ITokenService } from '../../services/Tokens/ITokenService';
+
+export const useAutoDiscoverTokensForNativeToken = ({
+  chain,
+  coin,
+}: {
+  chain: Chain;
+  coin: Coin;
+}) => {
+  const factory = RpcServiceFactory.createRpcService(chain) as IRpcService &
+    ITokenService;
+
+  return useQuery({
+    queryKey: ['autoDiscoverTokens', coin.address],
+    queryFn: async () => {
+      console.log('## running');
+      if (!coin.isNativeToken) {
+        throw new Error('Coin is not native token');
+      }
+
+      if (!coin) {
+        throw new Error('Coin is undefined');
+      }
+
+      const result = await factory.getTokens(coin);
+
+      if (!result.length) {
+        return [];
+      }
+
+      return result;
+    },
+  });
+};
