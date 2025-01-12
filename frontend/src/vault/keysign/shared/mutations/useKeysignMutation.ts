48fe84ef94a90d0999e79133795b63f0ad9041c1
rcoderdev <radzionch@gmail.com>
2025-01-12 11:14:59 +0400

    feat: refactor useKeysignMutation to integrate join keysign message payload handling
    - Replaced the previous payload retrieval with the new `useJoinKeysignMessagePayload` hook for improved clarity and maintainability.
    - Enhanced the mutation function to utilize `matchRecordUnion` for dynamic handling of keysign and custom message types.
    - Streamlined transaction input data processing and signature retrieval, ensuring better organization and readability of the code.
    - Updated the overall structure to support the new payload handling logic while maintaining existing functionality.

--- frontend/src/vault/keysign/shared/mutations/useKeysignMutation.ts
+++ frontend/src/vault/keysign/shared/mutations/useKeysignMutation.ts
@@ -5,6 +5,7 @@ import { getPreSigningHashes } from '../../../../chain/tx/utils/getPreSigningHas
 import { getCoinType } from '../../../../chain/walletCore/getCoinType';
 import { hexEncode } from '../../../../chain/walletCore/hexEncode';
 import { getLastItem } from '../../../../lib/utils/array/getLastItem';
+import { matchRecordUnion } from '../../../../lib/utils/matchRecordUnion';
 import { chainPromises } from '../../../../lib/utils/promise/chainPromises';
 import { pick } from '../../../../lib/utils/record/pick';
 import { recordFromItems } from '../../../../lib/utils/record/recordFromItems';
@@ -17,102 +18,125 @@ import { useCurrentServerUrl } from '../../../keygen/state/currentServerUrl';
 import { getVaultPublicKey } from '../../../publicKey/getVaultPublicKey';
 import { useCurrentHexEncryptionKey } from '../../../setup/state/currentHexEncryptionKey';
 import { useCurrentVault } from '../../../state/currentVault';
+import { useJoinKeysignMessagePayload } from '../../join/state/joinKeysignMessagePayload';
 import { getKeysignChain } from '../../utils/getKeysignChain';
 import { getTssKeysignType } from '../../utils/getTssKeysignType';
 import { getTxInputData } from '../../utils/getTxInputData';
-import { useKeysignPayload } from '../state/keysignPayload';
 
 export const useKeysignMutation = () => {
-  const payload = useKeysignPayload();
   const walletCore = useAssertWalletCore();
   const vault = useCurrentVault();
   const sessionId = useCurrentSessionId();
   const encryptionKeyHex = useCurrentHexEncryptionKey();
   const serverUrl = useCurrentServerUrl();
 
+  const payload = useJoinKeysignMessagePayload();
+
   return useMutation({
     mutationFn: async () => {
-      const chain = getKeysignChain(payload);
+      return matchRecordUnion(payload, {
+        keysign: async payload => {
+          const chain = getKeysignChain(payload);
 
-      const inputs = await getTxInputData({
-        keysignPayload: payload,
-        walletCore,
-      });
-
-      const groupedMsgs = inputs.map(txInputData =>
-        getPreSigningHashes({
-          txInputData,
-          walletCore,
-          chain,
-        }).map(value =>
-          hexEncode({
-            value,
+          const inputs = await getTxInputData({
+            keysignPayload: payload,
             walletCore,
-          })
-        )
-      );
+          });
 
-      const msgs = groupedMsgs.flat().sort();
-
-      const blockchainService = BlockchainServiceFactory.createService(
-        chain as Chain,
-        walletCore
-      );
-
-      const tssType = getTssKeysignType(chain);
-
-      const coinType = getCoinType({ walletCore, chain });
-
-      const signatures = await Keysign(
-        vault,
-        msgs,
-        vault.local_party_id,
-        walletCore.CoinTypeExt.derivationPath(coinType),
-        sessionId,
-        encryptionKeyHex,
-        serverUrl,
-        tssType
-      );
-
-      const signaturesRecord = recordFromItems(signatures, ({ msg }) =>
-        Buffer.from(msg, 'base64').toString('hex')
-      );
-
-      const publicKey = await getVaultPublicKey({
-        vault,
-        chain,
-        walletCore,
-      });
-
-      const hashes = await chainPromises(
-        inputs.map(async (txInputData, index) => {
-          const msgs = groupedMsgs[index];
-
-          const { rawTransaction, signature } =
-            await blockchainService.getSignedTransaction(
-              publicKey,
+          const groupedMsgs = inputs.map(txInputData =>
+            getPreSigningHashes({
               txInputData,
-              pick(signaturesRecord, msgs)
-            );
-
-          const rpcService = RpcServiceFactory.createRpcService(chain);
-
-          // TODO: Transaction broadcasting interface should be redesigned
-          // It should receive a typed input instead of a string
-          if (chain === Chain.Sui) {
-            return rpcService.broadcastTransaction(
-              JSON.stringify({
-                unsignedTransaction: rawTransaction,
-                signature: signature,
+              walletCore,
+              chain,
+            }).map(value =>
+              hexEncode({
+                value,
+                walletCore,
               })
-            );
-          }
+            )
+          );
 
-          return rpcService.broadcastTransaction(rawTransaction);
-        })
-      );
+          const msgs = groupedMsgs.flat().sort();
 
-      return getLastItem(hashes);
+          const blockchainService = BlockchainServiceFactory.createService(
+            chain as Chain,
+            walletCore
+          );
+
+          const tssType = getTssKeysignType(chain);
+
+          const coinType = getCoinType({ walletCore, chain });
+
+          const signatures = await Keysign(
+            vault,
+            msgs,
+            vault.local_party_id,
+            walletCore.CoinTypeExt.derivationPath(coinType),
+            sessionId,
+            encryptionKeyHex,
+            serverUrl,
+            tssType
+          );
+
+          const signaturesRecord = recordFromItems(signatures, ({ msg }) =>
+            Buffer.from(msg, 'base64').toString('hex')
+          );
+
+          const publicKey = await getVaultPublicKey({
+            vault,
+            chain,
+            walletCore,
+          });
+
+          const hashes = await chainPromises(
+            inputs.map(async (txInputData, index) => {
+              const msgs = groupedMsgs[index];
+
+              const { rawTransaction, signature } =
+                await blockchainService.getSignedTransaction(
+                  publicKey,
+                  txInputData,
+                  pick(signaturesRecord, msgs)
+                );
+
+              const rpcService = RpcServiceFactory.createRpcService(chain);
+
+              // TODO: Transaction broadcasting interface should be redesigned
+              // It should receive a typed input instead of a string
+              if (chain === Chain.Sui) {
+                return rpcService.broadcastTransaction(
+                  JSON.stringify({
+                    unsignedTransaction: rawTransaction,
+                    signature: signature,
+                  })
+                );
+              }
+
+              return rpcService.broadcastTransaction(rawTransaction);
+            })
+          );
+
+          return getLastItem(hashes);
+        },
+        custom: async ({ message, method }) => {
+          const [signature] = await Keysign(
+            vault,
+            // TODO: which argument here?
+            [message],
+            vault.local_party_id,
+            // TODO: which argument here?
+            method,
+            sessionId,
+            encryptionKeyHex,
+            serverUrl,
+            // TODO: which argument here?
+            'ecdsa'
+          );
+
+          // TODO: what should it return?
+          return signature.msg;
+        },
+      });
     },
   });
 };
