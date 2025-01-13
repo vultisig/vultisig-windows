cb4867807e943b81cb360867160e7f9fbb49cb66
rcoderdev <radzionch@gmail.com>
2025-01-13 11:33:48 +0400

    fix: update SaveFileBkp return type to Promise<boolean>
    - Changed the return type of the SaveFileBkp function from Promise<string> to Promise<boolean> to better reflect the success status of the backup operation.
    - This modification enhances type safety and improves clarity regarding the function's output.

--- frontend/wailsjs/go/main/App.d.ts
+++ frontend/wailsjs/go/main/App.d.ts
@@ -7,4 +7,4 @@ export function ReadTextFile(arg1: string): Promise<string>;
 
 export function SaveFile(arg1: string, arg2: string): Promise<string>;
 
-export function SaveFileBkp(arg1: string, arg2: string): Promise<string>;
+export function SaveFileBkp(arg1: string, arg2: string): Promise<boolean>;
