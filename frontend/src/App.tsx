36656c7b38d531d9469107889a8a23b7e300da0d
rcoderdev <radzionch@gmail.com>
2025-01-13 14:57:53 +0400

    feat: add ErrorBoundary component for improved error handling in the app
    - Introduced an ErrorBoundary component to catch and handle errors in the application.
    - Wrapped the main application structure in ErrorBoundary to provide fallback UI in case of errors.
    - This enhancement improves the user experience by preventing the app from crashing and allowing for graceful error management.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 
 import { InitializedWalletOnly } from './components/wallet/InitializedWalletOnly';
+import { ErrorBoundary } from './errors/components/ErrorBoundary';
 import { GlobalStyle } from './lib/ui/css/GlobalStyle';
 import { darkTheme } from './lib/ui/theme/darkTheme';
 import { ThemeProvider } from './lib/ui/theme/ThemeProvider';
@@ -22,17 +23,19 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={darkTheme}>
           <GlobalStyle />
-          <div className="w-full min-h-screen bg-primary h-full flex flex-col">
-            <RemoteStateDependant>
-              <InitializedWalletOnly>
-                <OnboardingResetter>
-                  <ToastProvider>
-                    <RouterProvider router={router} />
-                  </ToastProvider>
-                </OnboardingResetter>
-              </InitializedWalletOnly>
-            </RemoteStateDependant>
-          </div>
+          <ErrorBoundary>
+            <div className="w-full min-h-screen bg-primary h-full flex flex-col">
+              <RemoteStateDependant>
+                <InitializedWalletOnly>
+                  <OnboardingResetter>
+                    <ToastProvider>
+                      <RouterProvider router={router} />
+                    </ToastProvider>
+                  </OnboardingResetter>
+                </InitializedWalletOnly>
+              </RemoteStateDependant>
+            </div>
+          </ErrorBoundary>
         </ThemeProvider>
       </QueryClientProvider>
     </WalletCoreProvider>
