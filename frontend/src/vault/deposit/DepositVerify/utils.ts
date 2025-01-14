a32c2f05ae004cab76d453b9d56cb4bbe8b7a815
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-14 10:30:13 +0700

    feat: adjust formatting form data for deposit verify screen utility

--- frontend/src/vault/deposit/DepositVerify/utils.ts
+++ frontend/src/vault/deposit/DepositVerify/utils.ts
@@ -14,6 +14,7 @@ export const getFormattedFormData = (
 
     // For THORChain unbond we need to hardcode the amount to 0.00000001 RUNE on the UI
     if (key === 'amount' && chainAction === 'unbond') {
+      formattedFormData['unbondAmount'] = formData[key];
       formattedFormData[key] = `${AMOUNT_FOR_THORCHAIN_UNBOND.toFixed(8)} RUNE`;
     } else {
       formattedFormData[key] = value;
