b3fc0ebc0c43ab479de5ae02992d84fbd25d698c
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-15 14:13:01 +0700

    fix: comemnt

--- frontend/src/vault/deposit/DepositVerify/utils.ts
+++ frontend/src/vault/deposit/DepositVerify/utils.ts
@@ -1,5 +1,5 @@
+import { storage } from '../../../../wailsjs/go/models';
 import { getChainFeeCoin } from '../../../chain/tx/fee/utils/getChainFeeCoin';
-import { CoinKey } from '../../../coin/Coin';
 import { Chain } from '../../../model/chain';
 import { ChainAction } from '../ChainAction';
 
@@ -10,32 +10,31 @@ function getDustDepositAmountString(decimals: number) {
 export const getFormattedFormData = (
   formData: Record<string, unknown>,
   chainAction: ChainAction,
-  coin: CoinKey
+  coin: storage.Coin
 ) => {
   const formattedFormData = { ...formData };
-  const decimals = getChainFeeCoin(
-    coin.id === 'RUNE' ? Chain.THORChain : Chain.MayaChain
-  )?.decimals;
+  const decimals = getChainFeeCoin(coin.chain as Chain)?.decimals;
   const dustAmount = getDustDepositAmountString(decimals);
 
   // For THORChain / MayaChain LEAVE we need to hardcode the amount for the transaction
-  if (chainAction === 'leave' && (coin.id === 'RUNE' || coin.id === 'CACAO')) {
+  if (
+    chainAction === 'leave' &&
+    (coin.ticker === 'RUNE' || coin.ticker === 'CACAO')
+  ) {
     formattedFormData.amount = dustAmount;
   }
 
   // For THORChain unbond and MayaChain unbond_with_lp we need to hardcode the amount on the UI
   if (
-    ('amount' in formData && chainAction === 'unbond' && coin.id === 'RUNE') ||
-    ('amount' in formData &&
-      chainAction === 'unbond_with_lp' &&
-      coin.id === 'CACAO')
+    ('amount' in formData && chainAction === 'unbond') ||
+    ('amount' in formData && chainAction === 'unbond_with_lp')
   ) {
     formattedFormData.unbondAmount = formData.amount;
     formattedFormData.amount = dustAmount;
   }
 
   // If 'amount' doesn't exist in the current formData as it's not mandatory for MayaChain unbond_with_lp Form, we need to hardcode the amount to 0.00000001 for the transaction
-  if (chainAction === 'unbond_with_lp' && coin.id === 'CACAO') {
+  if (chainAction === 'unbond_with_lp') {
     formattedFormData.amount = dustAmount;
   }
 
