c7e19b4d7489b11bf75b04a33455995d55ed8edb
rcoderdev <radzionch@gmail.com>
2025-01-17 16:14:19 +0400

    docs: update README and remove unused service interfaces
    - Enhanced the README.md with additional installation instructions and tips for generating protobuf files for TypeScript.
    - Removed obsolete service-related files (IService.ts, Service.ts, ServiceFactory.ts) to streamline the codebase and eliminate redundancy.
    - Updated Linux installation instructions for `libwebkit2gtk-4.0-dev` to improve clarity.

--- frontend/src/services/IService.ts
+++
@@ -1,9 +0,0 @@
-import { ICoinService } from './Coin/ICoinService';
-import { IPriceService } from './Price/IPriceService';
-import { IRpcService } from './Rpc/IRpcService';
-
-export interface IService {
-  rpcService: IRpcService;
-  coinService: ICoinService;
-  priceService: IPriceService;
-}
