6c7273c356c4531063f5c29bb269820fdeec0eff
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-21 02:15:53 +0800

    feat: retreive all chain tokens from local storage

--- frontend/src/vault/chain/manage/coin/VaultChainCoinOptions.tsx
+++ frontend/src/vault/chain/manage/coin/VaultChainCoinOptions.tsx
@@ -1,36 +1,48 @@
 import { useMemo } from 'react';
 
+import { storage } from '../../../../../wailsjs/go/models';
 import { areEqualCoins, coinKeyToString } from '../../../../coin/Coin';
 import { useWhitelistedCoinsQuery } from '../../../../coin/query/useWhitelistedCoinsQuery';
 import {
   getCoinMetaKey,
   getCoinMetaSearchStrings,
 } from '../../../../coin/utils/coinMeta';
+import { storageCoinToCoin } from '../../../../coin/utils/storageCoin';
 import { VStack } from '../../../../lib/ui/layout/Stack';
 import { useCurrentSearch } from '../../../../lib/ui/search/CurrentSearchProvider';
 import { useSearchFilter } from '../../../../lib/ui/search/hooks/useSearchFilter';
 import { Text } from '../../../../lib/ui/text';
 import { withoutDuplicates } from '../../../../lib/utils/array/withoutDuplicates';
-import { TokensStore } from '../../../../services/Coin/CoinList';
+import { CoinMeta } from '../../../../model/coin-meta';
+import {
+  PersistentStateKey,
+  usePersistentState,
+} from '../../../../state/persistentState';
 import { useCurrentVaultChain } from '../../useCurrentVaultChain';
 import { ManageVaultChainCoin } from './ManageVaultChainCoin';
 
 export const VaultChainCoinOptions = () => {
   const chain = useCurrentVaultChain();
-
+  const [chainToCoinsMap] = usePersistentState<Record<string, storage.Coin[]>>(
+    PersistentStateKey.ChainAllTokens,
+    {}
+  );
+  const coins = useMemo(
+    () => chainToCoinsMap[chain] ?? ([] as storage.Coin[]),
+    [chain, chainToCoinsMap]
+  );
   const query = useWhitelistedCoinsQuery(chain);
+  const [searchQuery] = useCurrentSearch();
 
   const initialItems = useMemo(() => {
-    const suggestedItems = TokensStore.TokenSelectionAssets.filter(
-      token => token.chain === chain
-    );
+    const suggestedItems = coins
+      .filter(token => token.chain === chain)
+      .map(coin => CoinMeta.fromCoin(storageCoinToCoin(coin)));
 
     return withoutDuplicates(suggestedItems, (one, another) =>
       areEqualCoins(getCoinMetaKey(one), getCoinMetaKey(another))
     ).filter(({ isNativeToken }) => !isNativeToken);
-  }, [chain]);
-
-  const [searchQuery] = useCurrentSearch();
+  }, [chain, coins]);
 
   const allUniqueItems = useMemo(() => {
     return withoutDuplicates(
