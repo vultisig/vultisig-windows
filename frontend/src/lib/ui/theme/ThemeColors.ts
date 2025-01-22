7be0b9d2138183aac26a2a6e0199b31891f88721
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-22 16:33:38 +0800

    feat: extend getHoverVariant and ThemeColors

--- frontend/src/lib/ui/theme/ThemeColors.ts
+++ frontend/src/lib/ui/theme/ThemeColors.ts
@@ -4,23 +4,28 @@ export type ThemeColors = {
   text: HSLA;
   textSupporting: HSLA;
   textShy: HSLA;
-
+  textDark: HSLA;
   primary: HSLA;
   primaryAlt: HSLA;
-
   idle: HSLA;
-  success: HSLA;
-
   background: HSLA;
   foreground: HSLA;
   foregroundExtra: HSLA;
   foregroundSuper: HSLA;
   overlay: HSLA;
-
   contrast: HSLA;
+  success: HSLA;
   mist: HSLA;
   mistExtra: HSLA;
   transparent: HSLA;
   white: HSLA;
   danger: HSLA;
+  buttonBackgroundDisabled: HSLA;
+};
+
+export type ThemeGradients = {
+  primary: string;
+  button: string;
+  alert: string;
+  overlay: string;
 };
