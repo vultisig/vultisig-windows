d1d0259c36803ed985d53f0a02f20c8230f881bc
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-27 10:41:45 +0700

    feat: adjust utils

--- frontend/src/vault/chain/utils/index.ts
+++ frontend/src/vault/chain/utils/index.ts
@@ -26,17 +26,3 @@ export const shouldDisplayChainLogo = ({
       chainsWithDifferentGovernanceAndGasTokens.includes(chain))
   );
 };
-
-// Not a clean solution but needed because the 'ETH' token comes with the wrong logo for some EVM chains, where the logo is the governance token logo, whereas it still has to be the ETH logo.
-export const isETHGasTokenAndNotGovernanceToken = ({
-  chain,
-  ticker,
-}: {
-  chain: Chain;
-  ticker: string;
-}) => {
-  return (
-    ticker === ETHTicker &&
-    chainsWithDifferentGovernanceAndGasTokens.includes(chain)
-  );
-};
