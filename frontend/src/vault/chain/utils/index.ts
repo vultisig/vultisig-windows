9f75f8b0e86e2cbdd9994f4eda3733f383f92315
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-27 14:41:13 +0700

    feat: adjust deriving gas token is different form gov token

--- frontend/src/vault/chain/utils/index.ts
+++ frontend/src/vault/chain/utils/index.ts
@@ -1,16 +1,7 @@
 import { chainFeeCoin } from '../../../coin/chainFeeCoin';
 import { Chain } from '../../../model/chain';
 
-const chainsWithDifferentGovernanceAndGasTokens: Chain[] = [
-  Chain.Arbitrum,
-  Chain.Optimism,
-  Chain.Base,
-  Chain.Zksync,
-  Chain.Blast,
-];
-
 const ETHTicker = chainFeeCoin[Chain.Ethereum].ticker;
-
 export const shouldDisplayChainLogo = ({
   ticker,
   chain,
@@ -20,9 +11,5 @@ export const shouldDisplayChainLogo = ({
   chain: Chain;
   isNative: boolean;
 }): boolean => {
-  return (
-    !isNative ||
-    (ticker === ETHTicker &&
-      chainsWithDifferentGovernanceAndGasTokens.includes(chain))
-  );
+  return !isNative || (ticker === ETHTicker && chain !== Chain.Ethereum);
 };
