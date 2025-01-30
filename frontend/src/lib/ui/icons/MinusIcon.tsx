3177ffa88d6c844b36723abdeed13d0c987586e6
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-30 23:20:48 +0700

    feat: add icons needed for onboarding flow

---
+++ frontend/src/lib/ui/icons/MinusIcon.tsx
@@ -0,0 +1,10 @@
+export const MinusIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={12} height={2} fill="none">
+    <path
+      stroke="#FFC25C"
+      strokeLinecap="square"
+      strokeWidth={1.5}
+      d="M.75 1h10.5"
+    />
+  </svg>
+);
--- frontend/src/lib/ui/icons/ShieldCheckIcon.tsx
+++ frontend/src/lib/ui/icons/ShieldCheckIcon.tsx
@@ -1,4 +1,4 @@
-const ShieldCheckIcon = () => (
+const ShieldCheckIcon = ({ color = '#fff' }: { color?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={24}
@@ -7,14 +7,14 @@ const ShieldCheckIcon = () => (
     viewBox="0 0 20 20"
   >
     <path
-      stroke="#fff"
+      stroke={color}
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={1.5}
       d="M3.125 8.333V3.75a.625.625 0 0 1 .625-.625h12.5a.625.625 0 0 1 .625.625v4.583c0 6.564-5.57 8.739-6.683 9.107a.59.59 0 0 1-.384 0c-1.112-.368-6.683-2.543-6.683-9.107Z"
     />
     <path
-      stroke="#fff"
+      stroke={color}
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={1.5}
