3f497550ee665e06956b5c1823a29860f0b53045
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-31 20:10:34 +0700

    feat: optimize hooks

---
+++ frontend/src/vault/setup/fast/hooks/useVaultPeersSetup.ts
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+
+import { isEmpty } from '../../../../lib/utils/array/isEmpty';
+import { recordFromKeys } from '../../../../lib/utils/record/recordFromKeys';
+import { usePeerOptionsQuery } from '../../../keygen/shared/peerDiscovery/queries/usePeerOptionsQuery';
+import { usePeersSelectionRecord } from '../../../keysign/shared/state/selectedPeers';
+
+export const useVaultPeersSetup = (isServerSetupCompleted: boolean) => {
+  const peerOptionsQuery = usePeerOptionsQuery({
+    enabled: isServerSetupCompleted,
+  });
+
+  const [peers, setPeers] = usePeersSelectionRecord();
+  const numberOfPeers = Object.values(peers).length;
+
+  useEffect(() => {
+    if (
+      isServerSetupCompleted &&
+      peerOptionsQuery.data &&
+      !isEmpty(peerOptionsQuery.data)
+    ) {
+      setPeers(recordFromKeys(peerOptionsQuery.data, () => true));
+    }
+  }, [isServerSetupCompleted, peerOptionsQuery.data, setPeers]);
+
+  return {
+    isPending:
+      !isServerSetupCompleted ||
+      numberOfPeers === 0 ||
+      peerOptionsQuery.isPending,
+    error: peerOptionsQuery.error,
+    hasPeers: numberOfPeers > 0,
+  };
+};
