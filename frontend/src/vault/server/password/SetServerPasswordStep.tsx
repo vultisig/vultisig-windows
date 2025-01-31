d834cf9f6c4ab55f1ce7cd5a897ae3290615541d
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-01-31 13:33:04 +0700

    feat: SetServerPasswordStep component

--- frontend/src/vault/server/password/SetServerPasswordStep.tsx
+++ frontend/src/vault/server/password/SetServerPasswordStep.tsx
@@ -1,77 +1,120 @@
-import { useMemo, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import { z } from 'zod';
 
 import { Button } from '../../../lib/ui/buttons/Button';
-import { getFormProps } from '../../../lib/ui/form/utils/getFormProps';
 import { PasswordInput } from '../../../lib/ui/inputs/PasswordInput';
 import { VStack } from '../../../lib/ui/layout/Stack';
 import {
   ComponentWithBackActionProps,
   ComponentWithForwardActionProps,
 } from '../../../lib/ui/props';
-import { InfoBlock } from '../../../lib/ui/status/InfoBlock';
+import { WarningBlock } from '../../../lib/ui/status/WarningBlock';
 import { Text } from '../../../lib/ui/text';
 import { PageContent } from '../../../ui/page/PageContent';
 import { PageHeader } from '../../../ui/page/PageHeader';
 import { PageHeaderBackButton } from '../../../ui/page/PageHeaderBackButton';
-import { PageHeaderTitle } from '../../../ui/page/PageHeaderTitle';
 import { KeygenEducationPrompt } from '../../keygen/shared/KeygenEducationPrompt';
 import { useVaultPassword } from './state/password';
+
+const PasswordWarningBlock = styled(WarningBlock)`
+  max-width: max-content;
+  font-size: 13px;
+`;
+
+// TODO: translations
+const passwordSchema = z
+  .object({
+    password: z.string().min(1, 'Password is required'),
+    confirmPassword: z.string().min(1, 'Confirm password is required'),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
+
+type PasswordSchema = z.infer<typeof passwordSchema>;
 
 export const SetServerPasswordStep = ({
   onForward,
   onBack,
 }: ComponentWithForwardActionProps & Partial<ComponentWithBackActionProps>) => {
   const { t } = useTranslation();
-  const [value, setValue] = useVaultPassword();
-  const [repeatedValue, setRepeatedValue] = useState('');
+  const [storedPassword, setStoredPassword] = useVaultPassword();
 
-  const isDisabled = useMemo(() => {
-    if (!value) {
-      return t('password_required');
-    }
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: zodResolver(passwordSchema),
+    mode: 'onBlur',
+    defaultValues: {
+      password: storedPassword || '',
+      confirmPassword: '',
+    },
+  });
 
-    if (value !== repeatedValue) {
-      return t('passwords_dont_match');
-    }
-  }, [repeatedValue, t, value]);
+  const onSubmit = (data: PasswordSchema) => {
+    setStoredPassword(data.password);
+    onForward();
+  };
 
   return (
     <>
       <PageHeader
-        title={<PageHeaderTitle>{t('password')}</PageHeaderTitle>}
         primaryControls={<PageHeaderBackButton onClick={onBack} />}
         secondaryControls={<KeygenEducationPrompt />}
       />
-      <PageContent
-        as="form"
-        {...getFormProps({
-          isDisabled,
-          onSubmit: onForward,
-        })}
-      >
-        <VStack flexGrow gap={12}>
-          <Text size={14} weight="600" color="contrast">
-            {t('password_backup')}
-          </Text>
-          <PasswordInput
-            placeholder={t('enter_password')}
-            value={value}
-            onValueChange={setValue}
-            autoFocus
-          />
-          <PasswordInput
-            placeholder={t('verify_password')}
-            value={repeatedValue}
-            onValueChange={setRepeatedValue}
-          />
+      <PageContent as="form" onSubmit={handleSubmit(onSubmit)}>
+        <VStack flexGrow gap={16}>
+          <VStack gap={8}>
+            {
+              // TODO: translations
+            }
+            <Text variant="h1Regular">Password</Text>
+            <PasswordWarningBlock>
+              Password cannot be reset or recovered
+            </PasswordWarningBlock>
+          </VStack>
+          <VStack gap={8}>
+            <VStack gap={4}>
+              <PasswordInput
+                {...register('password')}
+                isValid={isValid}
+                isInvalid={!!errors.password}
+                placeholder={t('enter_password')}
+                onValueChange={value => setValue('password', value)}
+                autoFocus
+              />
+              {errors.password && (
+                <Text color="danger" size={12}>
+                  {errors.password.message}
+                </Text>
+              )}
+            </VStack>
+            <VStack gap={4}>
+              <PasswordInput
+                {...register('confirmPassword')}
+                isValid={isValid}
+                isInvalid={!!errors.confirmPassword}
+                placeholder={t('verify_password')}
+                onValueChange={value => setValue('confirmPassword', value)}
+              />
+              {errors.confirmPassword && (
+                <Text color="danger" size={12}>
+                  {errors.confirmPassword.message}
+                </Text>
+              )}
+            </VStack>
+          </VStack>
         </VStack>
-        <VStack gap={20}>
-          <InfoBlock>{t('server_password_reason')}</InfoBlock>
-          <Button type="submit" isDisabled={isDisabled}>
-            {t('continue')}
-          </Button>
-        </VStack>
+        <Button type="submit" isDisabled={Boolean(errors)}>
+          {t('next')}
+        </Button>
       </PageContent>
     </>
   );
