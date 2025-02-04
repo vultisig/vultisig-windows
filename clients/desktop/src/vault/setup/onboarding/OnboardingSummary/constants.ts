71fe650d177b25348e623a4e2193c05125aeafa0
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-02-04 09:30:27 +0700

    feat: add constants file for new summary items

---
+++ clients/desktop/src/vault/setup/onboarding/OnboardingSummary/constants.ts
@@ -0,0 +1,23 @@
+import { ArrowSplitIcon } from '../../../../lib/ui/icons/ArrowSplitIcon';
+import { CloudStackIcon } from '../../../../lib/ui/icons/CloudStackIcon';
+import { CloudWithToolkeyIcon } from '../../../../lib/ui/icons/CloudWithToolkeyIcon';
+import { TriangleExclamationIcon } from '../../../../lib/ui/icons/TriangleExclamationIcon';
+
+export const SUMMARY_ITEMS = [
+  {
+    title: 'summaryItemOneTitle',
+    icon: CloudWithToolkeyIcon,
+  },
+  {
+    title: 'summaryItemTwoTitle',
+    icon: ArrowSplitIcon,
+  },
+  {
+    title: 'summaryItemThreeTitle',
+    icon: CloudStackIcon,
+  },
+  {
+    title: 'summaryItemFourTitle',
+    icon: TriangleExclamationIcon,
+  },
+];
