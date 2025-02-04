e94176bb2a24178249a038529fff66474476b403
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-02-04 09:28:22 +0700

    feat: add icons

---
+++ clients/desktop/src/lib/ui/icons/CloudWithToolkeyIcon.tsx
@@ -0,0 +1,16 @@
+export const CloudWithToolkeyIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none">
+    <path
+      stroke="#4879FD"
+      strokeLinecap="square"
+      strokeWidth={1.5}
+      d="M4.97 19.39a5.184 5.184 0 0 1-3.22-4.812c0-2.625 1.93-4.794 4.434-5.136C7.014 7.002 9.305 5.25 12 5.25s4.985 1.753 5.816 4.192c2.504.342 4.434 2.51 4.434 5.136a5.184 5.184 0 0 1-3.22 4.812"
+    />
+    <path
+      stroke="#4879FD"
+      strokeLinecap="square"
+      strokeWidth={1.5}
+      d="m8.75 17.5 2.5 2.75 5-5.5"
+    />
+  </svg>
+);
