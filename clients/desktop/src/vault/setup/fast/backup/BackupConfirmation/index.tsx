5fdd1ea9044ec7758d2b10066f18b81e7fd0daa1
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-02-06 14:34:56 +0700

    feat: backup slides

--- clients/desktop/src/vault/setup/fast/backup/BackupConfirmation/index.tsx
+++ clients/desktop/src/vault/setup/fast/backup/BackupConfirmation/index.tsx
@@ -1,1 +1,143 @@
-export const BackupConfirmation = () => <></>;
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+import { storage } from '../../../../../../wailsjs/go/models';
+import { Button } from '../../../../../lib/ui/buttons/Button';
+import { UnstyledButton } from '../../../../../lib/ui/buttons/UnstyledButton';
+import DownloadIcon from '../../../../../lib/ui/icons/DownloadIcon';
+import { VStack } from '../../../../../lib/ui/layout/Stack';
+import { useInvalidateQueries } from '../../../../../lib/ui/query/hooks/useInvalidateQueries';
+import { Text } from '../../../../../lib/ui/text';
+import { PageContent } from '../../../../../ui/page/PageContent';
+import { PageHeader } from '../../../../../ui/page/PageHeader';
+import { AnimatedLoader } from '../../../../../ui/pending/AnimatedLoader';
+import { useBackupVaultMutation } from '../../../../mutations/useBackupVaultMutation';
+import { vaultsQueryKey } from '../../../../queries/useVaultsQuery';
+
+export const steps = [
+  'multiFactor',
+  'selfCustodial',
+  'crossChain',
+  'over30Chains',
+  'availablePlatforms',
+  'seedlessWallet',
+] as const;
+
+export type SetupFastVaultEducationSlidesStep = (typeof steps)[number];
+
+const Wrapper = styled(VStack)`
+  max-width: 500px;
+  align-self: center;
+`;
+
+const Content = styled(VStack)`
+  flex: 1;
+`;
+
+const MissingRiveAnimationPlaceholder = styled(VStack)`
+  flex: 1;
+  background-color: purple;
+  width: 100%;
+`;
+
+type BackupConfirmationProps = {
+  vault: storage.Vault;
+  onCompleted: () => void;
+};
+
+export const BackupConfirmation: FC<BackupConfirmationProps> = ({
+  vault,
+  onCompleted,
+}) => {
+  const { t } = useTranslation();
+  const invalidateQueries = useInvalidateQueries();
+  const { mutate: backupVault, isPending, error } = useBackupVaultMutation();
+
+  const handleBackup = async () => {
+    if (!vault) return;
+
+    backupVault(
+      { vault, password: '' },
+      {
+        onSuccess: () => {
+          onCompleted();
+          invalidateQueries(vaultsQueryKey);
+        },
+      }
+    );
+  };
+
+  return (
+    <PageContent>
+      <PageHeader
+        title={
+          <Text size={16} weight={500}>
+            {t('fastVaultSetup.backup.backupVault')}
+          </Text>
+        }
+      />
+      <Wrapper
+        flexGrow
+        alignItems="center"
+        justifyContent="space-between"
+        gap={48}
+      >
+        <Content alignItems="center" justifyContent="space-between" gap={40}>
+          <MissingRiveAnimationPlaceholder
+            alignItems="center"
+            justifyContent="center"
+          >
+            Placeholder (Missing Rive Animation Component)
+          </MissingRiveAnimationPlaceholder>
+          <VStack gap={16}>
+            <Text centerHorizontally variant="h1Regular">
+              {t('fastVaultSetup.backup.backupConfirmationDescription')}
+            </Text>
+            <Text centerHorizontally color="shy" size={14}>
+              {t('fastVaultSetup.backup.onlineStorageDescription')}{' '}
+              <UnstyledButton
+                onClick={() => {
+                  // TODO: Tony to add learn more link when available
+                }}
+              >
+                <Text centerHorizontally color="supporting" as="span">
+                  {t('learnMore')}
+                </Text>
+              </UnstyledButton>
+            </Text>
+          </VStack>
+        </Content>
+        {isPending ? (
+          <LoaderWrapper>
+            <AnimatedLoader />
+          </LoaderWrapper>
+        ) : (
+          <VStack gap={4}>
+            <BackupButton onClick={handleBackup} size="m">
+              <DownloadIcon />
+              <Text as="span" size={14}>
+                {t('fastVaultSetup.backup.backUpNow')}
+              </Text>
+            </BackupButton>
+            {error && (
+              <Text size={12} color="danger">
+                {error.message}
+              </Text>
+            )}
+          </VStack>
+        )}
+      </Wrapper>
+    </PageContent>
+  );
+};
+
+const BackupButton = styled(Button)`
+  font-size: 20px;
+  gap: 8px;
+`;
+
+const LoaderWrapper = styled(VStack)`
+  height: 24px;
+  width: 24px;
+`;
--- clients/desktop/src/vault/setup/fast/backup/BackupOverviewSlidesPartOne/AnimationDescription.tsx
+++ clients/desktop/src/vault/setup/fast/backup/BackupOverviewSlidesPartOne/AnimationDescription.tsx
@@ -1,43 +1,54 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
 import { AnimatedVisibility } from '../../../../../lib/ui/layout/AnimatedVisibility';
 import { GradientText, Text } from '../../../../../lib/ui/text';
 import { BACKUP_VAULT_ANIMATIONS } from './hooks/useBackupOverviewStepsAnimations';
 
-const stepToAnimationDescription = {
-  1: () => (
-    <Text size={48}>
-      Your vault holds 2 shares,{' '}
-      <GradientText as="span">back them up now</GradientText>
-    </Text>
-  ),
-  2: () => (
-    <Text size={48}>
-      Part 1 of the vault shares will be{' '}
-      <GradientText as="span">held by the server</GradientText>.
-    </Text>
-  ),
-  3: () => (
-    <Text size={48}>
-      It is sent to you for complete self-custody!{' '}
-      <GradientText as="span">Check your e-mail to verify</GradientText>
-    </Text>
-  ),
-};
-
 type AnimationDescriptionProps = {
   animation: (typeof BACKUP_VAULT_ANIMATIONS)[number];
 };
 export const AnimationDescription: FC<AnimationDescriptionProps> = ({
   animation,
-}) => (
-  <Wrapper>
-    <AnimatedVisibility>
-      <TextWrapper>{stepToAnimationDescription[animation]()}</TextWrapper>
-    </AnimatedVisibility>
-  </Wrapper>
-);
+}) => {
+  const { t } = useTranslation();
+  const stepToAnimationDescription = {
+    1: () => (
+      <Text size={48}>
+        {t('fastVaultSetup.backup.vaultShares')}{' '}
+        <GradientText as="span">
+          {t('fastVaultSetup.backup.backUpNow')}
+        </GradientText>
+      </Text>
+    ),
+    2: () => (
+      <Text size={48}>
+        {t('fastVaultSetup.backup.part1')}{' '}
+        <GradientText as="span">
+          {t('fastVaultSetup.backup.heldByServer')}
+        </GradientText>
+        .
+      </Text>
+    ),
+    3: () => (
+      <Text size={48}>
+        {t('fastVaultSetup.backup.completeCustody')}{' '}
+        <GradientText as="span">
+          {t('fastVaultSetup.backup.checkEmail')}
+        </GradientText>
+      </Text>
+    ),
+  };
+
+  return (
+    <Wrapper>
+      <AnimatedVisibility>
+        <TextWrapper>{stepToAnimationDescription[animation]()}</TextWrapper>
+      </AnimatedVisibility>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   height: 144px;
