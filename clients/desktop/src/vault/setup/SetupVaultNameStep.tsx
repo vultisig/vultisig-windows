f472dbeb318b63a264b99d5c7b788aeb96a9db95
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-02-12 16:33:15 +0300

    faet: refactor how we handle setup name

--- clients/desktop/src/vault/setup/SetupVaultNameStep.tsx
+++ clients/desktop/src/vault/setup/SetupVaultNameStep.tsx
@@ -1,4 +1,7 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
 
 import { ActionInsideInteractiveElement } from '../../lib/ui/base/ActionInsideInteractiveElement';
 import { Button } from '../../lib/ui/buttons/Button';
@@ -8,7 +11,6 @@ import {
   textInputHeight,
   textInputHorizontalPadding,
 } from '../../lib/ui/css/textInput';
-import { getFormProps } from '../../lib/ui/form/utils/getFormProps';
 import { CircledCloseIcon } from '../../lib/ui/icons/CircledCloseIcon';
 import { TextInput } from '../../lib/ui/inputs/TextInput';
 import { VStack } from '../../lib/ui/layout/Stack';
@@ -17,17 +19,47 @@ import { Text } from '../../lib/ui/text';
 import { PageContent } from '../../ui/page/PageContent';
 import { PageHeader } from '../../ui/page/PageHeader';
 import { PageHeaderBackButton } from '../../ui/page/PageHeaderBackButton';
+import { useVaultNames } from '../hooks/useVaultNames';
 import { KeygenEducationPrompt } from '../keygen/shared/KeygenEducationPrompt';
-import { useValidateVaultName } from './shared/hooks';
+import { MAX_VAULT_NAME_LENGTH } from './shared/constants';
 import { useVaultName } from './state/vaultName';
+
+export const vaultNameSchema = (existingNames: string[]) =>
+  z.object({
+    vaultName: z
+      .string()
+      .min(1, 'vault_name_required')
+      .max(MAX_VAULT_NAME_LENGTH, 'vault_name_max_length_error')
+      .refine(name => !existingNames.includes(name), {
+        message: 'vault_name_already_exists',
+      }),
+  });
+
+type VaultNameSchemaType = z.infer<ReturnType<typeof vaultNameSchema>>;
 
 export const SetupVaultNameStep = ({
   onForward,
   onBack,
 }: OnForwardProp & Partial<OnBackProp>) => {
   const { t } = useTranslation();
-  const [value, setValue] = useVaultName();
-  const errorMessage = useValidateVaultName();
+  const [_, setName] = useVaultName();
+  const existingVaultNames = useVaultNames();
+
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<VaultNameSchemaType>({
+    resolver: zodResolver(vaultNameSchema(existingVaultNames)),
+    defaultValues: { vaultName: '' },
+    mode: 'all',
+  });
+
+  const onSubmit: SubmitHandler<VaultNameSchemaType> = ({ vaultName }) => {
+    setName(vaultName);
+    onForward();
+  };
 
   return (
     <>
@@ -35,13 +67,7 @@ export const SetupVaultNameStep = ({
         primaryControls={<PageHeaderBackButton onClick={onBack} />}
         secondaryControls={<KeygenEducationPrompt />}
       />
-      <PageContent
-        as="form"
-        {...getFormProps({
-          onSubmit: onForward,
-        })}
-        gap={16}
-      >
+      <PageContent as="form" onSubmit={handleSubmit(onSubmit)} gap={16}>
         <VStack>
           <Text variant="h1Regular">Name your vault</Text>
           <Text size={14} color="shy">
@@ -51,15 +77,22 @@ export const SetupVaultNameStep = ({
         <VStack flexGrow gap={4}>
           <ActionInsideInteractiveElement
             render={() => (
-              <TextInput
-                placeholder={t('enter_vault_name')}
-                value={value}
-                onValueChange={setValue}
-                autoFocus
+              <Controller
+                name="vaultName"
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    {...field}
+                    placeholder={t('enter_vault_name')}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                    autoFocus
+                  />
+                )}
               />
             )}
             action={
-              <UnstyledButton onClick={() => setValue('')}>
+              <UnstyledButton onClick={() => setValue('vaultName', '')}>
                 <CircledCloseIcon />
               </UnstyledButton>
             }
@@ -68,13 +101,13 @@ export const SetupVaultNameStep = ({
               bottom: (textInputHeight - iconButtonIconSizeRecord.l) / 2,
             }}
           />
-          {errorMessage && (
+          {errors.vaultName && errors.vaultName.message && (
             <Text color="danger" size={12}>
-              {errorMessage}
+              {t(errors.vaultName.message)}
             </Text>
           )}
         </VStack>
-        <Button type="submit" isDisabled={Boolean(errorMessage)}>
+        <Button type="submit" isDisabled={Boolean(errors.vaultName?.message)}>
           {t('next')}
         </Button>
       </PageContent>
