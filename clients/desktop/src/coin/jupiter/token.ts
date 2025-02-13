e42e0ac69b1720ad7e248875f455a71b117efa1d
Enrique <enriquesouza@live.com>
2025-02-13 16:11:46 -0300

    Jupiter white list tokens

---
+++ clients/desktop/src/coin/jupiter/token.ts
@@ -0,0 +1,33 @@
+import { Chain } from '@core/chain/Chain';
+import { Coin } from '@core/chain/coin/Coin';
+
+export type SolanaJupiterToken = {
+  address: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  logoURI?: string;
+  extensions?: {
+    coingeckoId?: string;
+  };
+};
+
+export type FromSolanaJupiterTokensInput = {
+  tokens: SolanaJupiterToken[];
+  chain: Chain;
+};
+
+export const fromSolanaJupiterTokens = ({
+  tokens,
+  chain,
+}: FromSolanaJupiterTokensInput): Coin[] => {
+  return tokens
+    .filter(token => !!token.logoURI) // Ensure only tokens with logos are included
+    .map(token => ({
+      chain,
+      id: token.address,
+      decimals: token.decimals,
+      logo: token.logoURI || '',
+      ticker: token.symbol,
+    }));
+};
