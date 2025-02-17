2b7d1993441c8e2d589c00cb8f42ba3e22d5935d
rcoderdev <radzionch@gmail.com>
2025-02-17 11:10:52 +0400

    feat: improve native coin identification and token management

--- clients/desktop/src/chain/swap/native/asset/toNativeSwapAsset.ts
+++ clients/desktop/src/chain/swap/native/asset/toNativeSwapAsset.ts
@@ -1,23 +1,19 @@
 import { CoinKey } from '@core/chain/coin/Coin';
-import { isFeeCoin } from '@core/chain/coin/utils/isFeeCoin';
+import { isNativeCoin } from '@core/chain/coin/utils/isNativeCoin';
 import { EntityWithTicker } from '@lib/utils/entities/EntityWithTicker';
 
-import { NativeSwapChain, nativeSwapChainIds } from '../NativeSwapChain';
+import { nativeSwapChainIds, NativeSwapEnabledChain } from '../NativeSwapChain';
 
 export const toNativeSwapAsset = ({
   chain,
   id,
   ticker,
-}: CoinKey & EntityWithTicker): string => {
-  const swapChainId = nativeSwapChainIds[chain as NativeSwapChain];
-
-  if (!swapChainId) {
-    throw new Error(`No swap chain id found for ${chain}`);
-  }
+}: CoinKey<NativeSwapEnabledChain> & EntityWithTicker): string => {
+  const swapChainId = nativeSwapChainIds[chain];
 
   const key = `${swapChainId}.${ticker}`;
 
-  if (isFeeCoin({ chain, id })) {
+  if (isNativeCoin({ chain, id })) {
     return key;
   }
 
