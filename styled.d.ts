dcfcaf1a7c675259f9e7b8dbd6accc702dac9ec2
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-02-26 23:26:03 +0200

    chore: adjust configurations and packages

--- styled.d.ts
+++ styled.d.ts
@@ -4,7 +4,7 @@ import { Theme as DesktopTheme } from './clients/desktop/src/lib/ui/theme/Theme'
 import { Theme as MobileTheme } from './clients/mobile/src/lib/ui/theme'
 
 declare module 'styled-components' {
-  type ThemeType = typeof DesktopTheme | typeof MobileTheme
+  type ThemeType = typeof MobileTheme | typeof DesktopTheme
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends ThemeType {}
 }
