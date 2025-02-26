fb922e3a924c2f42bc276650a19336091abdf45b
Johnny Luo <johnnyluo1980@gmail.com>
2025-02-26 12:21:47 +1100

    Fix compile error on mobile

--- clients/mobile/modules/mobile-tss/index.ts
+++ clients/mobile/modules/mobile-tss/index.ts
@@ -1,4 +1,4 @@
 // Reexport the native module. On web, it will be resolved to MobileTssModule.web.ts
 // and on native platforms to MobileTssModule.ts
-export { default } from './src/MobileTssModule';
-export * from  './src/MobileTss.types';
+export * from './src/MobileTss.types'
+export { default } from './src/MobileTssModule'
