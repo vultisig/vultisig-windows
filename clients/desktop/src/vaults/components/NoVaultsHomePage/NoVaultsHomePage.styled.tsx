9350374938f62b3a9d42eb237acaf4d4a05a96b7
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-04 14:03:35 +0200

    feat: update NoVaultsHomePage component

--- clients/desktop/src/vaults/components/NoVaultsHomePage/NoVaultsHomePage.styled.tsx
+++ clients/desktop/src/vaults/components/NoVaultsHomePage/NoVaultsHomePage.styled.tsx
@@ -1,7 +1,19 @@
 import styled from 'styled-components'
 
+<<<<<<< Updated upstream
 import { PageContent } from '../../../ui/page/PageContent'
+=======
+import { AnimatedVisibility } from '../../../lib/ui/layout/AnimatedVisibility'
+import { VStack } from '../../../lib/ui/layout/Stack'
+import { getColor } from '../../../lib/ui/theme/getters'
+>>>>>>> Stashed changes
 
 export const Wrapper = styled(PageContent)`
   overflow-y: hidden;
+`
+
+export const HorizontalLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${getColor('foregroundSuper')};
 `
--- clients/desktop/src/vaults/components/NoVaultsHomePage/index.tsx
+++ clients/desktop/src/vaults/components/NoVaultsHomePage/index.tsx
@@ -5,13 +5,21 @@ import { AnimatedVisibility } from '../../../lib/ui/layout/AnimatedVisibility'
 import { VStack } from '../../../lib/ui/layout/Stack'
 import { useAppNavigate } from '../../../navigation/hooks/useAppNavigate'
 import { ProductLogoBlock } from '../../../ui/logo/ProductLogoBlock'
+<<<<<<< Updated upstream
 import { Wrapper } from './NoVaultsHomePage.styled'
+=======
+import { PageContent } from '../../../ui/page/PageContent'
+import { PageHeader } from '../../../ui/page/PageHeader'
+import { PageHeaderBackButton } from '../../../ui/page/PageHeaderBackButton'
+import { HorizontalLine, Wrapper } from './NoVaultsHomePage.styled'
+>>>>>>> Stashed changes
 
 export const NoVaultsHomePage = () => {
   const { t } = useTranslation()
   const navigate = useAppNavigate()
 
   return (
+<<<<<<< Updated upstream
     <Wrapper>
       <VStack flexGrow alignItems="center" justifyContent="center">
         <AnimatedVisibility delay={500}>
@@ -19,15 +27,39 @@ export const NoVaultsHomePage = () => {
         </AnimatedVisibility>
       </VStack>
       <AnimatedVisibility animationConfig="bottomToTop" delay={800}>
+=======
+    <Wrapper delay={200}>
+      {withBackButton && (
+        <PageHeader primaryControls={<PageHeaderBackButton />} />
+      )}
+      <PageContent>
+        <VStack flexGrow alignItems="center" justifyContent="center">
+          <ProductLogoBlock />
+        </VStack>
+>>>>>>> Stashed changes
         <VStack gap={20}>
           <Button onClick={() => navigate('setupVault', { params: {} })}>
             {t('create_new_vault')}
           </Button>
+<<<<<<< Updated upstream
           <Button onClick={() => navigate('importVault')} kind="secondary">
             {t('import_vault')}
           </Button>
         </VStack>
       </AnimatedVisibility>
+=======
+          <HorizontalLine />
+          <VStack gap={12}>
+            <Button onClick={() => navigate('importVault')} kind="secondary">
+              {t('import_vault')}
+            </Button>
+            <Button onClick={() => navigate('importVault')} kind="secondary">
+              {t('scan_qr')}
+            </Button>
+          </VStack>
+        </VStack>
+      </PageContent>
+>>>>>>> Stashed changes
     </Wrapper>
   )
 }
