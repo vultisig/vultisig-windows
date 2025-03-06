6d9148a596af2bda2da6126f15a85252033d4f5f
Johnny Luo <johnnyluo1980@gmail.com>
2025-03-07 08:55:03 +1100

    set DKLS on by default

--- clients/desktop/src/mpc/state/isDklsLibEnabled.ts
+++ clients/desktop/src/mpc/state/isDklsLibEnabled.ts
@@ -2,5 +2,5 @@ import { usePersistentState } from '../../state/persistentState'
 import { PersistentStateKey } from '../../state/persistentState'
 
 export const useIsDklsLibEnabled = () => {
-  return usePersistentState(PersistentStateKey.IsDklsLibEnabled, false)
+  return usePersistentState(PersistentStateKey.IsDklsLibEnabled, true)
 }
