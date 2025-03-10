0d534a0032add07e797981f54f36b2c1c3581564
rcoderdev <radzionch@gmail.com>
2025-03-10 09:50:17 +0400

    refactor: improve KeygenPeerDiscoveryEducation with useBoolean hook
    Replace useState with useBoolean hook for more concise overlay state management in KeygenPeerDiscoveryEducation component

--- clients/desktop/src/vault/keygen/shared/peerDiscovery/KeygenPeerDiscoveryEducation.tsx
+++ clients/desktop/src/vault/keygen/shared/peerDiscovery/KeygenPeerDiscoveryEducation.tsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-
+import { useBoolean } from '../../../../lib/ui/hooks/useBoolean'
 import { SecureVaultKeygenOverlay } from '../../../setup/secure/components/SecureVaultKeygenOverlay'
 import { KeygenType } from '../../KeygenType'
 import { useCurrentKeygenType } from '../../state/currentKeygenType'
 
 export const KeygenPeerDiscoveryEducation = () => {
-  const [overlayShown, setHasShownOverlay] = useState(true)
+  const [overlayShown, { unset: closeOverlay }] = useBoolean(false)
 
   const [keygenType] = useCurrentKeygenType()
 
   if (keygenType === KeygenType.Keygen && !overlayShown) {
-    return (
-      <SecureVaultKeygenOverlay onCompleted={() => setHasShownOverlay(false)} />
-    )
+    return <SecureVaultKeygenOverlay onCompleted={closeOverlay} />
   }
 
   return null
