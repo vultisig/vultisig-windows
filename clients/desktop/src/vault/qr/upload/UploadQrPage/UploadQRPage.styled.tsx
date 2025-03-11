e9ef407062b76a9824cd75e7e0dce563e25a81e9
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-11 15:19:05 +0200

    feat: upload pages refactoring

---
+++ clients/desktop/src/vault/qr/upload/UploadQrPage/UploadQRPage.styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import { PageContent } from '../../../../ui/page/PageContent'
+
+export const StyledPageContent = styled(PageContent)`
+  background-image: url('/assets/images/scanQRCodeBackground.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 0% 40%;
+`
