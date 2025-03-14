198c1c1ef98bb3da4f260a112387fb48a35f7fa0
rcoderdev <radzionch@gmail.com>
2025-03-14 11:07:43 +0400

    Refactor vault hooks to utilize MPC peers
    - Replaced `useVaultKeygenDevices` with `useMpcPeers` in `useStartFastVaultKeygenSessionMutation` for improved MPC peers management.
    - Updated `useVaultPeersSetup` to use `useMpcPeersSelectionRecord` instead of `usePeersSelectionRecord`, aligning with the new MPC state management structure.

--- clients/desktop/src/vault/setup/fast/hooks/useStartFastVaultKeygenSessionMutation.ts
+++ clients/desktop/src/vault/setup/fast/hooks/useStartFastVaultKeygenSessionMutation.ts
@@ -1,15 +1,15 @@
 import { useMutation } from '@tanstack/react-query'
 import { useEffect } from 'react'
 
+import { useMpcPeers } from '../../../../mpc/peers/state/mpcPeers'
 import { useMpcServerUrl } from '../../../../mpc/serverType/state/mpcServerUrl'
 import { useMpcSessionId } from '../../../../mpc/session/state/mpcSession'
 import { startSession } from '../../../keygen/utils/startSession'
-import { useVaultKeygenDevices } from '../../hooks/useVaultKegenDevices'
 
 export const useStartFastVaultKeygenSessionMutation = (enabled: boolean) => {
   const sessionId = useMpcSessionId()
   const serverUrl = useMpcServerUrl()
-  const devices = useVaultKeygenDevices()
+  const devices = useMpcPeers()
 
   const { mutate: start, ...state } = useMutation({
     mutationFn: () => {
