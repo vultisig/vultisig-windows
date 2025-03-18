9441db7c2c6f32ce8c8e6993ef4fb50c51ab329d
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-18 09:08:23 +0200

    fix: adapt SwapCoinInput with new interface

--- clients/desktop/src/vault/swap/form/SwapCoinInput.tsx
+++ clients/desktop/src/vault/swap/form/SwapCoinInput.tsx
@@ -1,27 +1,30 @@
 import { CoinKey } from '@core/chain/coin/Coin'
 import { isOneOf } from '@lib/utils/array/isOneOf'
 import { pick } from '@lib/utils/record/pick'
-import { useMemo } from 'react'
+import { FC, useMemo } from 'react'
 
 import { swapEnabledChains } from '../../../chain/swap/swapEnabledChains'
-import { CoinInputContainer } from '../../../coin/ui/inputs/CoinInputContainer'
 import { SelectCoinOverlay } from '../../../coin/ui/inputs/SelectCoinOverlay'
+import { SwapCoinInputField } from '../../../coin/ui/inputs/SwapCoinInputField'
 import { Opener } from '../../../lib/ui/base/Opener'
 import { InputProps } from '../../../lib/ui/props'
 import {
   useCurrentVaultCoin,
   useCurrentVaultCoins,
 } from '../../state/currentVault'
-import { SwapCoinBalance } from './SwapCoinBalance'
 
-export const SwapCoinInput: React.FC<InputProps<CoinKey>> = ({
+export type SwapSide = 'from' | 'to'
+type SwapCoinInputProps = InputProps<CoinKey> & {
+  side: SwapSide
+}
+
+export const SwapCoinInput: FC<SwapCoinInputProps> = ({
   value,
   onChange,
+  side,
 }) => {
   const coin = useCurrentVaultCoin(value)
-
   const coins = useCurrentVaultCoins()
-
   const options = useMemo(
     () => coins.filter(coin => isOneOf(coin.chain, swapEnabledChains)),
     [coins]
@@ -30,12 +33,11 @@ export const SwapCoinInput: React.FC<InputProps<CoinKey>> = ({
   return (
     <Opener
       renderOpener={({ onOpen }) => (
-        <CoinInputContainer
+        <SwapCoinInputField
           value={{ ...value, ...pick(coin, ['logo', 'ticker']) }}
           onClick={onOpen}
-        >
-          <SwapCoinBalance value={value} />
-        </CoinInputContainer>
+          side={side}
+        />
       )}
       renderContent={({ onClose }) => (
         <SelectCoinOverlay
