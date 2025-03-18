edfe12caf5f1798f1fa3448e08144f3a8bbaeaf3
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-18 09:08:03 +0200

    fix: adapt MangeFromCoin and ManageToCoin with new interface

--- clients/desktop/src/vault/swap/form/ManageFromCoin.tsx
+++ clients/desktop/src/vault/swap/form/ManageFromCoin.tsx
@@ -4,5 +4,5 @@ import { SwapCoinInput } from './SwapCoinInput'
 export const ManageFromCoin = () => {
   const [value, setValue] = useFromCoin()
 
-  return <SwapCoinInput value={value} onChange={setValue} />
+  return <SwapCoinInput side="from" value={value} onChange={setValue} />
 }
