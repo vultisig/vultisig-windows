75cd6e7ce4fffdcba0ddc592f2b2a5161b4497b8
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-18 09:33:39 +0200

    fix: ui issues

--- clients/desktop/src/vault/swap/form/amount/ManageFromAmount.tsx
+++ clients/desktop/src/vault/swap/form/amount/ManageFromAmount.tsx
@@ -1,33 +1,24 @@
 import { useCallback, useState } from 'react'
-import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
-import { horizontalPadding } from '../../../../lib/ui/css/horizontalPadding'
-import { takeWholeSpace } from '../../../../lib/ui/css/takeWholeSpace'
-import {
-  interactiveTextInput,
-  textInputBorderRadius,
-} from '../../../../lib/ui/css/textInput'
-import { toSizeUnit } from '../../../../lib/ui/css/toSizeUnit'
+import { textInputBorderRadius } from '../../../../lib/ui/css/textInput'
 import { InputDebounce } from '../../../../lib/ui/inputs/InputDebounce'
 import { text } from '../../../../lib/ui/text'
 import { useFromAmount } from '../../state/fromAmount'
 import { useFromCoin } from '../../state/fromCoin'
 import { AmountContainer } from './AmountContainer'
-import { AmountLabel } from './AmountLabel'
-import { amountConfig } from './config'
 import { SwapFiatAmount } from './SwapFiatAmount'
 
 const Input = styled.input`
-  ${takeWholeSpace};
-  padding-top: ${toSizeUnit(amountConfig.inputPaddingTop)};
-  ${horizontalPadding(amountConfig.horizontalPadding)}
+  outline: none;
+  background: transparent;
+  ${textInputBorderRadius};
+  text-align: right;
 
   ${text({
-    weight: 700,
-    size: 20,
-    family: 'mono',
-    color: 'contrast',
+    weight: 500,
+    size: 22,
+    color: 'supporting',
   })}
 
   &::placeholder {
@@ -35,12 +26,6 @@ const Input = styled.input`
       color: 'shy',
     })}
   }
-
-  background: transparent;
-
-  ${textInputBorderRadius};
-
-  ${interactiveTextInput};
 `
 
 export const ManageFromAmount = () => {
@@ -48,7 +33,6 @@ export const ManageFromAmount = () => {
   const [fromCoin] = useFromCoin()
   const valueAsString = value?.toString() ?? ''
   const [inputValue, setInputValue] = useState<string>(valueAsString)
-  const { t } = useTranslation()
 
   const handleInputValueChange = useCallback(
     (value: string) => {
@@ -74,11 +58,7 @@ export const ManageFromAmount = () => {
   )
 
   return (
-    <AmountContainer>
-      <AmountLabel>{t('from')}</AmountLabel>
-      {value !== null && (
-        <SwapFiatAmount value={{ amount: value, ...fromCoin }} />
-      )}
+    <AmountContainer gap={6} alignItems="flex-end">
       <InputDebounce
         value={
           Number(valueAsString) === Number(inputValue)
@@ -89,13 +69,16 @@ export const ManageFromAmount = () => {
         render={({ value, onChange }) => (
           <Input
             type="number"
-            placeholder={t('enter_amount')}
+            placeholder={'0'}
             onWheel={event => event.currentTarget.blur()}
             value={value}
             onChange={({ currentTarget: { value } }) => onChange(value)}
           />
         )}
       />
+      {value !== null && (
+        <SwapFiatAmount value={{ amount: value, ...fromCoin }} />
+      )}
     </AmountContainer>
   )
 }
--- clients/desktop/src/vault/swap/form/amount/SwapFiatAmount.tsx
+++ clients/desktop/src/vault/swap/form/amount/SwapFiatAmount.tsx
@@ -4,25 +4,20 @@ import styled from 'styled-components'
 
 import { useFormatFiatAmount } from '../../../../chain/ui/hooks/useFormatFiatAmount'
 import { useCoinPriceQuery } from '../../../../coin/query/useCoinPriceQuery'
-import { centerContent } from '../../../../lib/ui/css/centerContent'
-import { toSizeUnit } from '../../../../lib/ui/css/toSizeUnit'
 import { Spinner } from '../../../../lib/ui/loaders/Spinner'
 import { ValueProp } from '../../../../lib/ui/props'
 import { MatchQuery } from '../../../../lib/ui/query/components/MatchQuery'
 import { text } from '../../../../lib/ui/text'
 import { useCurrentVaultCoin } from '../../../state/currentVault'
-import { amountConfig } from './config'
 
 const Container = styled.div`
-  position: absolute;
   pointer-events: none;
-  right: ${toSizeUnit(amountConfig.horizontalPadding)};
   height: 100%;
-  ${centerContent};
+
   ${text({
-    color: 'supporting',
-    weight: 700,
-    size: 16,
+    color: 'shy',
+    weight: 500,
+    size: 12,
   })};
 `
 
@@ -30,7 +25,6 @@ export const SwapFiatAmount = ({
   value,
 }: ValueProp<CoinKey & EntityWithAmount>) => {
   const coin = useCurrentVaultCoin(value)
-
   const query = useCoinPriceQuery({
     coin,
   })
