c5b2933e6ade68413478d8c673353ac212b2ea36
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-18 19:08:39 +0200

    fix: font weight

--- clients/desktop/src/lib/ui/modal/ModalTitleText.tsx
+++ clients/desktop/src/lib/ui/modal/ModalTitleText.tsx
@@ -2,5 +2,5 @@ import { ChildrenProp } from '../props'
 import { Text, TextProps } from '../text'
 
 export const ModalTitleText = (props: TextProps & ChildrenProp) => (
-  <Text color="contrast" as="div" weight="600" size={18} {...props} />
+  <Text color="contrast" as="div" weight="500" size={18} {...props} />
 )
