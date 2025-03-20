342659c751876fab59c27767657348b8aa01b388
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-20 13:34:37 +0200

    feat: create new hook for price query

--- clients/desktop/src/vault/swap/form/info/SwapTotalFeeFiatValue.tsx
+++ clients/desktop/src/vault/swap/form/info/SwapTotalFeeFiatValue.tsx
@@ -1,61 +1,12 @@
-import { fromChainAmount } from '@core/chain/amount/fromChainAmount'
-import { areEqualCoins, coinKeyToString } from '@core/chain/coin/Coin'
 import { ValueProp } from '@lib/ui/props'
-import { sum } from '@lib/utils/array/sum'
-import { withoutDuplicates } from '@lib/utils/array/withoutDuplicates'
-import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
-import { getRecordSize } from '@lib/utils/record/getRecordSize'
-import { useCallback, useMemo } from 'react'
 
-import { useFormatFiatAmount } from '../../../../chain/ui/hooks/useFormatFiatAmount'
-import { useCoinPricesQuery } from '../../../../coin/query/useCoinPricesQuery'
 import { Skeleton } from '../../../../components/skeleton'
 import { MatchEagerQuery } from '../../../../lib/ui/query/components/MatchEagerQuery'
-import { useTransformQueryData } from '../../../../lib/ui/query/hooks/useTransformQueryData'
-import { useCurrentVaultCoins } from '../../../state/currentVault'
+import { useSwapFiatFeesQuery } from '../../queries/useSwapFiatFeesQuery'
 import { SwapFee } from '../../types/SwapFee'
 
 export const SwapFeeFiatValue = ({ value }: ValueProp<SwapFee[]>) => {
-  const vaultCoins = useCurrentVaultCoins()
-  const coins = useMemo(
-    () =>
-      withoutDuplicates(
-        value.map(key => {
-          const coin = shouldBePresent(
-            vaultCoins.find(coin => areEqualCoins(coin, key))
-          )
-
-          return coin
-        }),
-        areEqualCoins
-      ),
-    [value, vaultCoins]
-  )
-
-  const formatAmount = useFormatFiatAmount()
-
-  const pricesQuery = useTransformQueryData(
-    useCoinPricesQuery({ coins }),
-    useCallback(
-      prices => {
-        if (coins.length !== getRecordSize(prices)) {
-          throw new Error('Failed to load prices')
-        }
-
-        const total = sum(
-          value.map(({ amount, decimals, ...coinKey }) => {
-            const key = coinKeyToString(coinKey)
-            const price = prices[key]
-
-            return price * fromChainAmount(amount, decimals)
-          })
-        )
-
-        return formatAmount(total)
-      },
-      [coins.length, formatAmount, value]
-    )
-  )
+  const pricesQuery = useSwapFiatFeesQuery(value)
 
   return (
     <MatchEagerQuery
