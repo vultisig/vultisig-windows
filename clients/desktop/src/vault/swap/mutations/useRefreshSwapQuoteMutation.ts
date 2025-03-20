a5a8c2d5221ec14648910456b2076454d6aee538
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-20 07:51:31 +0200

    feat: add new refresh swap quote mutation hook

---
+++ clients/desktop/src/vault/swap/mutations/useRefreshSwapQuoteMutation.ts
@@ -0,0 +1,44 @@
+import { QueryKey, useMutation } from '@tanstack/react-query'
+
+import { getBalanceQueryKey } from '../../../coin/query/useBalancesQuery'
+import { useInvalidateQueries } from '../../../lib/ui/query/hooks/useInvalidateQueries'
+import { useCurrentVaultAddress } from '../../state/currentVault'
+import { getSwapQuoteQueryKey } from '../queries/useSwapQuoteQuery'
+import { useFromAmount } from '../state/fromAmount'
+import { useFromCoin } from '../state/fromCoin'
+import { useToCoin } from '../state/toCoin'
+
+export const useRefreshSwapQuoteMutation = () => {
+  const invalidateQueries = useInvalidateQueries()
+
+  const [fromCoinKey] = useFromCoin()
+  const [toCoinKey] = useToCoin()
+  const [fromAmount] = useFromAmount()
+
+  const address = useCurrentVaultAddress(fromCoinKey.chain)
+
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: () => {
+      const queryKeys: QueryKey[] = [
+        getBalanceQueryKey({
+          ...fromCoinKey,
+          address,
+        }),
+      ]
+
+      if (fromAmount) {
+        queryKeys.push(
+          getSwapQuoteQueryKey({
+            fromCoinKey,
+            toCoinKey,
+            fromAmount,
+          })
+        )
+      }
+
+      return invalidateQueries(queryKeys)
+    },
+  })
+
+  return { mutate, isPending, error }
+}
