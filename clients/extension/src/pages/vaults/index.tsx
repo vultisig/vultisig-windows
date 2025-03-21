db3d9f41e81f8236efdc70eec174f55b166de2e1
rcoderdev <radzionch@gmail.com>
2025-03-21 11:28:35 +0400

    Refactor i18n structure and remove unused translations
    - Updated the i18n configuration to include new language files and improved the structure for better maintainability.
    - Removed unused translation files and keys to streamline the localization process.
    - Enhanced the SelectItemModal component to utilize a title prop for better flexibility.
    - Adjusted TypeScript configurations to include additional type definitions for improved type safety.

--- clients/extension/src/pages/vaults/index.tsx
+++ clients/extension/src/pages/vaults/index.tsx
@@ -4,10 +4,8 @@ import '@clients/extension/src/pages/vaults/index.scss'
 import ConfigProvider from '@clients/extension/src/components/config-provider'
 import VultiError from '@clients/extension/src/components/vulti-error'
 import VultiLoading from '@clients/extension/src/components/vulti-loading'
-import i18n from '@clients/extension/src/i18n/config'
 import { Vultisig } from '@clients/extension/src/icons'
 import { VaultProps } from '@clients/extension/src/utils/interfaces'
-import messageKeys from '@clients/extension/src/utils/message-keys'
 import {
   getStoredLanguage,
   getStoredVaults,
@@ -59,17 +57,15 @@ const Component = () => {
   }
 
   const componentDidMount = (): void => {
-    getStoredLanguage().then(language => {
-      i18n.changeLanguage(language)
-
+    getStoredLanguage().then(() => {
       getStoredVaults().then(vaults => {
         if (vaults.length) {
           setState(prevState => ({ ...prevState, vaults, hasError: false }))
         } else {
           setState(prevState => ({
             ...prevState,
-            errorDescription: t(messageKeys.GET_VAULT_FAILED_DESCRIPTION),
-            errorTitle: t(messageKeys.GET_VAULT_FAILED),
+            errorDescription: t('get_vault_failed_description'),
+            errorTitle: t('get_vault_failed'),
             hasError: true,
           }))
         }
@@ -92,17 +88,13 @@ const Component = () => {
           <>
             <div className="header">
               <Vultisig className="logo" />
-              <span className="title">
-                {t(messageKeys.CONNECT_WITH_VULTISIG)}
-              </span>
+              <span className="title">{t('connect_with_vultisig')}</span>
             </div>
             <div className="content">
               <Form form={form} onFinish={handleSubmit}>
                 <Form.Item<FormProps>
                   name="uids"
-                  rules={[
-                    { required: true, message: t(messageKeys.SELECT_A_VAULT) },
-                  ]}
+                  rules={[{ required: true, message: t('select_vault') }]}
                 >
                   <Checkbox.Group>
                     {vaults.map(({ name, uid }) => (
@@ -117,10 +109,10 @@ const Component = () => {
             </div>
             <div className="footer">
               <Button onClick={handleClose} shape="round" block>
-                {t(messageKeys.CANCEL)}
+                {t('cancel')}
               </Button>
               <Button onClick={handleSubmit} type="primary" shape="round" block>
-                {t(messageKeys.CONNECT)}
+                {t('connect')}
               </Button>
             </div>
           </>
