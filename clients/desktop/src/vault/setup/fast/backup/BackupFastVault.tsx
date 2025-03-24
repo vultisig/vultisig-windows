a8325d3328246f89a9e60e87338578a40112f425
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-24 19:53:27 +0200

    fix: backup page

--- clients/desktop/src/vault/setup/fast/backup/BackupFastVault.tsx
+++ clients/desktop/src/vault/setup/fast/backup/BackupFastVault.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 
-import { FEATURE_FLAGS } from '../../../../constants'
 import { Match } from '../../../../lib/ui/base/Match'
 import { StepTransition } from '../../../../lib/ui/base/StepTransition'
 import { useStepNavigation } from '../../../../lib/ui/hooks/useStepNavigation'
@@ -10,7 +9,6 @@ import { SaveVaultStep } from '../../../keygen/shared/SaveVaultStep'
 import { useVaults } from '../../../queries/useVaultsQuery'
 import { useCurrentVault } from '../../../state/currentVault'
 import { SetupVaultSummaryStep } from '../../shared/SetupVaultSummaryStep'
-import { SetupVaultSummaryStepOld } from '../../shared/SetupVaultSummaryStepOld'
 import VaultBackupPage from '../../shared/vaultBackupSettings/VaultBackupPage'
 import { EmailConfirmation } from '.'
 import { BackupConfirmation } from './BackupConfirmation'
@@ -63,13 +61,9 @@ export const BackupFastVault = () => {
       backupSuccessfulSlideshow={() =>
         shouldShowBackupSummary ? (
           <StepTransition
-            from={({ onForward }) =>
-              FEATURE_FLAGS.ENABLE_NEW_SUMMARY_PAGES ? (
-                <SetupVaultSummaryStep onForward={onForward} />
-              ) : (
-                <SetupVaultSummaryStepOld onForward={onForward} />
-              )
-            }
+            from={({ onForward }) => (
+              <SetupVaultSummaryStep onForward={onForward} />
+            )}
             to={() => (
               <BackupSuccessSlide
                 onCompleted={() => navigate(appPaths.vault)}
