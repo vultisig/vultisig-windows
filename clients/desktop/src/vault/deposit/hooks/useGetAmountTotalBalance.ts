7cf931c4ba2afd64f48e95fc44b8cbaaec8f4888
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-03-27 08:18:42 +0200

    fix: RUNE balance in THORChain function

--- clients/desktop/src/vault/deposit/hooks/useGetAmountTotalBalance.ts
+++ clients/desktop/src/vault/deposit/hooks/useGetAmountTotalBalance.ts
@@ -1,31 +1,31 @@
 import { Chain } from '@core/chain/Chain'
-import { getCoinValue } from '@core/chain/coin/utils/getCoinValue'
-import { sum } from '@lib/utils/array/sum'
 import { useEffect, useState } from 'react'
 
 import { useVaultChainCoinsQuery } from '../../queries/useVaultChainCoinsQuery'
 
 export const useGetTotalAmountAvailableForChain = (chain: Chain) => {
-  const [totalAmountAvailable, setTotalAmountAvailable] = useState(0)
+  const [totalTokenAmount, setTotalTokenAmount] = useState(0)
+  const [totalCurrencyAmount, setTotalCurrencyAmount] = useState(0)
   const { data: coins } = useVaultChainCoinsQuery(chain)
 
   useEffect(() => {
-    if (coins && coins?.length > 0) {
-      setTotalAmountAvailable(
-        coins
-          ? sum(
-              coins.map(({ amount, decimals, price = 0 }) =>
-                getCoinValue({
-                  amount,
-                  decimals,
-                  price,
-                })
-              )
-            )
-          : 0
-      )
+    if (!coins || coins.length === 0) return
+
+    let tokenSum = 0
+    let currencySum = 0
+
+    for (const { amount, decimals, price = 0 } of coins) {
+      const tokenAmount = Number(amount) / 10 ** decimals
+      tokenSum += tokenAmount
+      currencySum += tokenAmount * price
     }
+
+    setTotalTokenAmount(tokenSum)
+    setTotalCurrencyAmount(currencySum)
   }, [coins])
 
-  return totalAmountAvailable
+  return {
+    totalTokenAmount,
+    totalCurrencyAmount,
+  }
 }
