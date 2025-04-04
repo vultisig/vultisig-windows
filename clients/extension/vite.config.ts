4faf00f6dbfeb1ce81c643758c52a74439819020
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-04-04 22:23:07 +0300

    feat: introduce hot reloading

--- clients/extension/vite.config.ts
+++ clients/extension/vite.config.ts
@@ -1,19 +1,78 @@
-import { defineConfig, mergeConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import webExtension from 'vite-plugin-web-extension'
 
-import appConfig from './vite.config.app'
-import baseConfig from './vite.config.base'
-import contentConfig from './vite.config.content'
-import relayConfig from './vite.config.relay'
-
-export default defineConfig(() => {
-  const chunk = process.env.CHUNK
-
-  switch (chunk) {
-    case 'content':
-      return mergeConfig(baseConfig, contentConfig)
-    case 'relay':
-      return mergeConfig(baseConfig, relayConfig)
-    default:
-      return mergeConfig(baseConfig, appConfig)
-  }
+export default defineConfig({
+  plugins: [
+    react(),
+    {
+      name: 'force-es-format-everywhere',
+      config(config) {
+        const output = config?.build?.rollupOptions?.output
+        if (Array.isArray(output)) {
+          output.forEach(o => (o.format = 'es'))
+        } else if (output) {
+          output.format = 'es'
+        }
+      },
+    },
+    webExtension({
+      manifest: path.resolve(__dirname, 'manifest.json'),
+      browser: 'chrome',
+      scriptViteConfig: defineConfig({
+        build: {
+          rollupOptions: {
+            output: {
+              format: 'es',
+              inlineDynamicImports: false,
+            },
+          },
+        },
+      }),
+      htmlViteConfig: defineConfig({
+        build: {
+          rollupOptions: {
+            output: {
+              format: 'es',
+              inlineDynamicImports: false,
+            },
+          },
+        },
+      }),
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '~': path.resolve(__dirname, 'src/styles'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'legacy',
+        includePaths: [path.resolve(__dirname, 'src/styles')],
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, 'src/pages/popup/popup.html'),
+        vault: path.resolve(__dirname, 'src/pages/vault/vault.html'),
+        vaults: path.resolve(__dirname, 'src/pages/vaults/vaults.html'),
+        transaction: path.resolve(
+          __dirname,
+          'src/pages/transaction/transaction.html'
+        ),
+        accounts: path.resolve(__dirname, 'src/pages/accounts/accounts.html'),
+        import: path.resolve(__dirname, 'src/pages/import/import.html'),
+      },
+      output: {
+        format: 'es',
+        inlineDynamicImports: false,
+      },
+    },
+  },
 })
