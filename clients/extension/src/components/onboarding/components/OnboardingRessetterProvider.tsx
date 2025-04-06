e6c58ec3a06682a20129f43d5f761dd11dccc1a4
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-04-06 19:12:02 +0300

    feat: onboarding steps logic

---
+++ clients/extension/src/components/onboarding/components/OnboardingRessetterProvider.tsx
@@ -0,0 +1,38 @@
+// TODO: discuss how to besst implement this for the extension
+
+// import { FC, PropsWithChildren, useEffect } from 'react'
+
+// import {
+//   CreateInstallMarker,
+//   IsFreshInstall,
+// } from '../../wailsjs/go/main/InstallMarkerService'
+// import {
+//   PersistentStateKey,
+//   usePersistentState,
+// } from '../state/persistentState'
+
+// const OnboardingResetter: FC<PropsWithChildren> = ({ children }) => {
+//   const [, setCompletedOnboarding] = usePersistentState(
+//     PersistentStateKey.HasFinishedOnboarding,
+//     false
+//   )
+
+//   useEffect(() => {
+//     const checkAndSetInstallMarker = async () => {
+//       try {
+//         const freshInstall = await IsFreshInstall()
+//         if (freshInstall) {
+//           setCompletedOnboarding(false)
+//           await CreateInstallMarker()
+//         }
+//       } catch (error) {
+//         console.error(error)
+//       }
+//     }
+//     checkAndSetInstallMarker()
+//   }, [setCompletedOnboarding])
+
+//   return <>{children}</>
+// }
+
+// export default OnboardingResetter
