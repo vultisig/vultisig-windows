77c13945a03347f7e945f0d42f308869ad32e3e3
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-04-10 14:19:36 +0330

    lint issue fixed

--- clients/extension/src/navigation/routes/index.tsx
+++ clients/extension/src/navigation/routes/index.tsx
@@ -2,6 +2,8 @@ import { SetupFastVaultPage } from '@clients/desktop/src/vault/setup/fast/SetupF
 import { SetupSecureVaultPage } from '@clients/desktop/src/vault/setup/secure/SetupSecureVaultPage'
 //import { SetupVaultPageController } from '@clients/desktop/src/vault/setup/SetupVaultPageController'
 import { NewVaultPage } from '@clients/desktop/src/vaults/components/NewVaultPage'
+import { IncompleteOnboardingOnly } from '@clients/extension/src/components/onboarding/components/IncompleteOnboardingOnly'
+import { OnboardingPage } from '@clients/extension/src/components/onboarding/components/OnboardingPage'
 import { appPaths } from '@clients/extension/src/navigation'
 import Layout from '@clients/extension/src/pages/popup/layout'
 import CurrencyPage from '@clients/extension/src/pages/popup/pages/currency'
@@ -11,13 +13,10 @@ import LanguagePage from '@clients/extension/src/pages/popup/pages/language'
 import MainPage from '@clients/extension/src/pages/popup/pages/main'
 import RenameVaultPage from '@clients/extension/src/pages/popup/pages/rename-vault'
 import SettingsPage from '@clients/extension/src/pages/popup/pages/settings'
+import { SetupVaultPageController } from '@clients/extension/src/pages/popup/pages/setup-vault/SetupVaultPageController'
 import VaultSettingsPage from '@clients/extension/src/pages/popup/pages/vault-settings'
 import VaultsPage from '@clients/extension/src/pages/popup/pages/vaults'
 import { createHashRouter, Navigate } from 'react-router-dom'
-
-import { IncompleteOnboardingOnly } from '@clients/extension/src/components/onboarding/components/IncompleteOnboardingOnly'
-import { OnboardingPage } from '@clients/extension/src/components/onboarding/components/OnboardingPage'
-import { SetupVaultPageController } from '@clients/extension/src/pages/popup/pages/setup-vault/SetupVaultPageController'
 
 const routes = [
   {
