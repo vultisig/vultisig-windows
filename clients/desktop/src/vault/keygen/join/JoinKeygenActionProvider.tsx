22d7f7adaff7fb8d0397192f658e3d8d2b48e92f
rcoderdev <radzionch@gmail.com>
2025-04-11 12:23:15 +0400

    feat: add JoinKeygenActionProvider for enhanced key generation flow
    - Introduced `JoinKeygenActionProvider` to manage different key generation actions based on the keygen type and library type.
    - Integrated the new provider into `JoinKeygenPage`, streamlining the component structure and improving modularity.
    - This addition supports the creation, reshare, and migration of vault key generation processes, enhancing overall user experience.

---
+++ clients/desktop/src/vault/keygen/join/JoinKeygenActionProvider.tsx
@@ -0,0 +1,57 @@
+import { fromLibType } from '@core/mpc/types/utils/libType'
+import { CreateVaultKeygenActionProvider as DKLSCreateKeygenActionProvider } from '@core/ui/mpc/keygen/create/CreateVaultKeygenActionProvider'
+import { ReshareVaultKeygenActionProvider as DKLSReshareKeygenActionProvider } from '@core/ui/mpc/keygen/reshare/ReshareVaultKeygenActionProvider'
+import { Match } from '@lib/ui/base/Match'
+import { ChildrenProp } from '@lib/ui/props'
+
+import { useAppPathState } from '../../../navigation/hooks/useAppPathState'
+import { MigrateVaultKeygenActionProvider } from '../../migrate/MigrateVaultKeygenActionProvider'
+import { CreateVaultLegacyKeygenActionProvider as GG20CreateKeygenActionProvider } from '../create/CreateVaultLegacyKeygenActionProvider'
+import { ReshareVaultLegacyKeygenActionProvider as GG20ReshareKeygenActionProvider } from '../reshare/ReshareVaultLegacyKeygenActionProvider'
+
+export const JoinKeygenActionProvider = ({ children }: ChildrenProp) => {
+  const { keygenType, keygenMsg } = useAppPathState<'joinKeygen'>()
+  const { libType } = keygenMsg
+  const mpcLib = fromLibType(libType)
+
+  return (
+    <Match
+      value={keygenType}
+      create={() => (
+        <Match
+          value={mpcLib}
+          DKLS={() => (
+            <DKLSCreateKeygenActionProvider>
+              {children}
+            </DKLSCreateKeygenActionProvider>
+          )}
+          GG20={() => (
+            <GG20CreateKeygenActionProvider>
+              {children}
+            </GG20CreateKeygenActionProvider>
+          )}
+        />
+      )}
+      reshare={() => (
+        <Match
+          value={mpcLib}
+          DKLS={() => (
+            <DKLSReshareKeygenActionProvider>
+              {children}
+            </DKLSReshareKeygenActionProvider>
+          )}
+          GG20={() => (
+            <GG20ReshareKeygenActionProvider>
+              {children}
+            </GG20ReshareKeygenActionProvider>
+          )}
+        />
+      )}
+      migrate={() => (
+        <MigrateVaultKeygenActionProvider>
+          {children}
+        </MigrateVaultKeygenActionProvider>
+      )}
+    />
+  )
+}
