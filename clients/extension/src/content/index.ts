982e45bd54288ebd7c0aacb821f024daa00c1b74
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-04-12 17:51:01 +0330

    Fix knip issue

--- clients/extension/src/content/index.ts
+++ clients/extension/src/content/index.ts
@@ -1,5 +1,13 @@
 import { setupBridgeMessengerRelay } from '../messengers/bridge'
 
+// This fake import is for Knip static analysis only.
+// It ensures that dependencies inside inpage.ts are not marked as unused.
+// It has no effect on runtime because the script is injected dynamically.
+// eslint-disable-next-line no-constant-condition
+if (false) {
+  import('../inpage/index.ts')
+}
+
 const insertInpageScript = () => {
   const script = document.createElement('script')
   script.src = chrome.runtime.getURL('inpage.js')
