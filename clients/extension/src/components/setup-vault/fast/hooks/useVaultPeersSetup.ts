8ee7d08c148617461463c5a4880189a1e107b1c2
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-04-15 22:30:06 +0300

    feat: add fast vault setup

---
+++ clients/extension/src/components/setup-vault/fast/hooks/useVaultPeersSetup.ts
@@ -0,0 +1,27 @@
+import { useMpcPeersSelectionRecord } from '@core/ui/mpc/state/mpcSelectedPeers'
+import { isEmpty } from '@lib/utils/array/isEmpty'
+import { recordFromKeys } from '@lib/utils/record/recordFromKeys'
+import { useEffect } from 'react'
+
+import { usePeerOptionsQuery } from '../../../keygen/shared/peerDiscovery/queries/usePeerOptionsQuery'
+
+export const useVaultPeersSetup = (enabled: boolean) => {
+  const peerOptionsQuery = usePeerOptionsQuery({
+    enabled: enabled,
+  })
+
+  const [peers, setPeers] = useMpcPeersSelectionRecord()
+  const numberOfPeers = Object.values(peers).length
+
+  useEffect(() => {
+    if (enabled && peerOptionsQuery.data && !isEmpty(peerOptionsQuery.data)) {
+      setPeers(recordFromKeys(peerOptionsQuery.data, () => true))
+    }
+  }, [enabled, peerOptionsQuery.data, setPeers])
+
+  return {
+    isPending: !enabled || numberOfPeers === 0 || peerOptionsQuery.isPending,
+    error: peerOptionsQuery.error,
+    hasPeers: numberOfPeers > 0,
+  }
+}
