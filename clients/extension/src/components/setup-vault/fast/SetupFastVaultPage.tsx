576e8380a10988e6eb37db56bdd7e060b169a83f
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-04-15 22:29:35 +0300

    feat: improve onboarding components

---
+++ clients/extension/src/components/setup-vault/fast/SetupFastVaultPage.tsx
@@ -0,0 +1,82 @@
+import { CreateVaultFlowProviders } from '@core/ui/mpc/keygen/create/CreateVaultFlowProviders'
+import { CreateVaultNameStep } from '@core/ui/mpc/keygen/create/CreateVaultNameStep'
+import { VaultSecurityTypeProvider } from '@core/ui/mpc/keygen/create/state/vaultSecurityType'
+import { EmailProvider } from '@core/ui/state/email'
+import { PasswordProvider } from '@core/ui/state/password'
+import { Match } from '@lib/ui/base/Match'
+import { useStepNavigation } from '@lib/ui/hooks/useStepNavigation'
+import { useNavigateBack } from '@lib/ui/navigation/hooks/useNavigateBack'
+
+import { MpcMediatorManager } from '../../../mpc/serverType/MpcMediatorManager'
+import { CreateVaultKeygenActionProvider } from '../../keygen/create/CreateVaultKeygenActionProvider'
+import { KeygenFlow } from '../../keygen/shared/KeygenFlow'
+import { ServerEmailStep } from '../../server/email/ServerEmailStep'
+import { SetServerPasswordStep } from '../../server/password/SetServerPasswordStep'
+import { ServerPasswordHintStep } from '../../server/password-hint/ServerPasswordHintStep'
+import { PasswordHintProvider } from '../../server/password-hint/state/password-hint'
+import { SetupVaultServerStep } from './SetupVaultServerStep'
+
+const steps = [
+  'name',
+  'email',
+  'password',
+  'hint',
+  'setupForCreateVault',
+  'createVault',
+] as const
+
+const lastEditableStep = 'password'
+
+export const SetupFastVaultPage = () => {
+  const { step, setStep, toPreviousStep, toNextStep } = useStepNavigation({
+    steps,
+    onExit: useNavigateBack(),
+  })
+
+  return (
+    <VaultSecurityTypeProvider value="fast">
+      <EmailProvider initialValue="">
+        <PasswordProvider initialValue="">
+          <PasswordHintProvider initialValue="">
+            <CreateVaultFlowProviders>
+              <CreateVaultKeygenActionProvider>
+                <MpcMediatorManager />
+                <Match
+                  value={step}
+                  name={() => <CreateVaultNameStep onFinish={toNextStep} />}
+                  email={() => (
+                    <ServerEmailStep
+                      onBack={toPreviousStep}
+                      onFinish={toNextStep}
+                    />
+                  )}
+                  password={() => (
+                    <SetServerPasswordStep
+                      onBack={toPreviousStep}
+                      onFinish={toNextStep}
+                    />
+                  )}
+                  hint={() => (
+                    <ServerPasswordHintStep
+                      onBack={toPreviousStep}
+                      onFinish={toNextStep}
+                    />
+                  )}
+                  setupForCreateVault={() => (
+                    <SetupVaultServerStep
+                      onBack={() => setStep(lastEditableStep)}
+                      onFinish={toNextStep}
+                    />
+                  )}
+                  createVault={() => (
+                    <KeygenFlow onBack={() => setStep(lastEditableStep)} />
+                  )}
+                />
+              </CreateVaultKeygenActionProvider>
+            </CreateVaultFlowProviders>
+          </PasswordHintProvider>
+        </PasswordProvider>
+      </EmailProvider>
+    </VaultSecurityTypeProvider>
+  )
+}
