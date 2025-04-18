72e696d26630c49916d10ac39f868ad9098dd389
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-04-18 15:41:51 +0300

    feat: finish off fast vault setup

--- core/ui/vault/backup/fast/BackupOverviewSlidesPartOne/VaultOverviewSlides.styles.tsx
+++ core/ui/vault/backup/fast/BackupOverviewSlidesPartOne/VaultOverviewSlides.styles.tsx
@@ -1,5 +1,7 @@
+import { VStack } from '@lib/ui/layout/Stack'
 import styled from 'styled-components'
 
-export const RiveWrapper = styled.div`
+export const RiveWrapper = styled(VStack)`
   flex: 1;
+  position: relative;
 `
--- core/ui/vault/backup/fast/BackupOverviewSlidesPartTwo/index.tsx
+++ core/ui/vault/backup/fast/BackupOverviewSlidesPartTwo/index.tsx
@@ -38,7 +38,11 @@ export const BackupOverviewSlidesPartTwo: FC<OnboardingStepsProps> = ({
       </ProgressWrapper>
       <VStack justifyContent="space-between" flexGrow>
         <RiveWrapper>
-          <AnimationComponent />
+          <AnimationComponent
+            style={{
+              flexGrow: 1,
+            }}
+          />
         </RiveWrapper>
         <VStack gap={12}>
           <AnimationDescription />
