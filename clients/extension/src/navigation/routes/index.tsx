570e972811d5b96bcfd8e3f58f3af6aa8ed37888
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-04-22 09:30:50 +0300

    refactor: use core paths when possible

--- clients/extension/src/navigation/routes/index.tsx
+++ clients/extension/src/navigation/routes/index.tsx
@@ -37,7 +37,7 @@ const routes = [
     element: <SetupVaultPageController />,
   },
   {
-    path: appPaths.setupSecureVault,
+    path: corePaths.setupSecureVault,
     element: <SetupSecureVaultPage />,
   },
   {
