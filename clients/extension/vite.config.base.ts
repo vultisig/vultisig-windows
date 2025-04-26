9cac1a20ee9a7e3da69668799d015b089fcff379
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-04-26 14:01:35 +0330

    remove toplevelAwait & wasm from plugins for now

--- clients/extension/vite.config.base.ts
+++ clients/extension/vite.config.base.ts
@@ -2,16 +2,9 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import topLevelAwait from 'vite-plugin-top-level-await'
-import wasm from 'vite-plugin-wasm'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({ exclude: ['fs'] }),
-    topLevelAwait(),
-    wasm(),
-  ],
+  plugins: [react(), nodePolyfills({ exclude: ['fs'] })],
   css: {
     preprocessorOptions: {
       scss: {
