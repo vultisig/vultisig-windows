67705e5edccabb77124e21a6f24deb3ae2de93c2
rcoderdev <radzionch@gmail.com>
2025-04-30 13:05:06 +0400

    feat: introduce TypeScript type checking rules and refactor delete vault folder mutation for improved code organization and type safety

--- core/ui/storage/vaultFolders.tsx
+++ core/ui/storage/vaultFolders.tsx
@@ -1,10 +1,17 @@
+import { useInvalidateQueries } from '@lib/ui/query/hooks/useInvalidateQueries'
+import { isEmpty } from '@lib/utils/array/isEmpty'
 import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
 import { sortEntitiesWithOrder } from '@lib/utils/entities/EntityWithOrder'
-import { useQuery } from '@tanstack/react-query'
+import { Entry } from '@lib/utils/entities/Entry'
+import { getLastItemOrder } from '@lib/utils/order/getLastItemOrder'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { useMemo } from 'react'
 
-import { vaultFoldersQueryKey } from '../query/keys'
+import { vaultFoldersQueryKey, vaultsQueryKey } from '../query/keys'
 import { useCoreStorage } from '../state/storage'
+import { useUpdateVaultMutation } from '../vault/mutations/useUpdateVaultMutation'
+import { getVaultId } from '../vault/Vault'
+import { useVaults } from './vaults'
 
 export const useVaultFoldersQuery = () => {
   const { getVaultFolders } = useCoreStorage()
@@ -29,4 +36,49 @@ export const useVaultFolder = (id: string) => {
   const folders = useVaultFolders()
 
   return useMemo(() => folders.find(folder => folder.id === id), [folders, id])
+}
+
+export const useDeleteVaultFolderMutation = () => {
+  const invalidateQueries = useInvalidateQueries()
+
+  const vaults = useVaults()
+
+  const { deleteVaultFolder } = useCoreStorage()
+
+  const { mutateAsync: updateVault } = useUpdateVaultMutation()
+
+  return useMutation({
+    mutationFn: async (folderId: string) => {
+      const folderVaults = vaults.filter(vault => vault.folderId === folderId)
+      const folderlessVaults = vaults.filter(vault => !vault.folderId)
+
+      await deleteVaultFolder(folderId)
+
+      if (!isEmpty(folderVaults)) {
+        const entries: Entry<string, number>[] = []
+        folderVaults.forEach(vault => {
+          const orders = [
+            ...entries.map(entry => entry.value),
+            ...folderlessVaults.map(vault => vault.order),
+          ]
+
+          const order = getLastItemOrder(orders)
+
+          entries.push({ key: getVaultId(vault), value: order })
+        })
+
+        await Promise.all(
+          entries.map(({ key, value }) =>
+            updateVault({
+              vaultId: key,
+              fields: { order: value },
+            })
+          )
+        )
+      }
+    },
+    onSuccess: () => {
+      invalidateQueries(vaultFoldersQueryKey, vaultsQueryKey)
+    },
+  })
 }
