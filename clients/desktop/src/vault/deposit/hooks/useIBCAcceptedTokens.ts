33fbbe2de42ce3e25f730e05b23029d28e6e5b00
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-05 10:46:25 +0300

    fix: small regression

--- clients/desktop/src/vault/deposit/hooks/useIBCAcceptedTokens.ts
+++ clients/desktop/src/vault/deposit/hooks/useIBCAcceptedTokens.ts
@@ -1,10 +1,15 @@
 import { Chain, CosmosChain } from '@core/chain/Chain'
+import { coinKeyFromString } from '@core/chain/coin/Coin'
 import { CHAINS_WITH_IBC_TOKENS, IBC_TOKENS } from '@core/chain/coin/ibc'
 import { useCurrentVaultCoins } from '@core/ui/vault/state/currentVaultCoins'
 import { withoutDuplicates } from '@lib/utils/array/withoutDuplicates'
 
+import { useAppPathParams } from '../../../navigation/hooks/useAppPathParams'
+
 export const useIBCAcceptedTokens = (destinationChain?: Chain) => {
   const coins = useCurrentVaultCoins()
+  const [{ coin: coinName }] = useAppPathParams<'deposit'>()
+  const { chain: chain } = coinKeyFromString(coinName)
 
   if (
     !destinationChain ||
@@ -18,7 +23,8 @@ export const useIBCAcceptedTokens = (destinationChain?: Chain) => {
       const isIbcToken = IBC_TOKENS.some(
         ibc =>
           ibc.ticker.toUpperCase() === coin.ticker.toUpperCase() &&
-          ibc.decimals === coin.decimals
+          ibc.decimals === coin.decimals &&
+          coin.chain === chain
       )
       return isIbcToken
     }),
