82bdb5822b127cf1506158967bfe2daa8cce70ef
rcoderdev <radzionch@gmail.com>
2025-05-05 09:45:10 +0400

    refactor: integrate CoreProvider into AppProviders to enhance state management and simplify component hierarchy

--- clients/extension/src/providers/AppProviders.tsx
+++ clients/extension/src/providers/AppProviders.tsx
@@ -4,8 +4,7 @@ import { MpcLib } from '@core/mpc/mpcLib'
 import { WalletCoreProvider } from '@core/ui/chain/providers/WalletCoreProvider'
 import { MpcLocalModeAvailabilityProvider } from '@core/ui/mpc/state/MpcLocalModeAvailability'
 import { VaultCreationMpcLibProvider } from '@core/ui/mpc/state/vaultCreationMpcLib'
-import { VersionProvider } from '@core/ui/product/state/version'
-import { CoreState } from '@core/ui/state/core'
+import { CoreProvider, CoreState } from '@core/ui/state/core'
 import { StorageDependant } from '@core/ui/storage/StorageDependant'
 import { GlobalStyle } from '@lib/ui/css/GlobalStyle'
 import { ChildrenProp } from '@lib/ui/props'
@@ -45,27 +44,19 @@ export const AppProviders = ({ children }: ChildrenProp) => {
     <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <ExtensionGlobalStyle />
-      <VersionProvider value="1.0.0">
-        <MpcLocalModeAvailabilityProvider value={false}>
-          <VaultCreationMpcLibProvider value={defaultMpcLib}>
-            <OpenUrlProvider value={openUrl}>
-              <SaveFileProvider value={saveFile}>
-                <MpcDeviceProvider value="extension">
-                  <QueryProvider>
-                    <I18nProvider>
-                      <WalletCoreProvider>
-                        <StorageProvider>
-                          <StorageDependant>{children}</StorageDependant>
-                        </StorageProvider>
-                      </WalletCoreProvider>
-                    </I18nProvider>
-                  </QueryProvider>
-                </MpcDeviceProvider>
-              </SaveFileProvider>
-            </OpenUrlProvider>
-          </VaultCreationMpcLibProvider>
-        </MpcLocalModeAvailabilityProvider>
-      </VersionProvider>
+      <MpcLocalModeAvailabilityProvider value={false}>
+        <VaultCreationMpcLibProvider value={defaultMpcLib}>
+          <CoreProvider value={coreState}>
+            <QueryProvider>
+              <I18nProvider>
+                <WalletCoreProvider>
+                  <StorageDependant>{children}</StorageDependant>
+                </WalletCoreProvider>
+              </I18nProvider>
+            </QueryProvider>
+          </CoreProvider>
+        </VaultCreationMpcLibProvider>
+      </MpcLocalModeAvailabilityProvider>
     </ThemeProvider>
   )
 }
