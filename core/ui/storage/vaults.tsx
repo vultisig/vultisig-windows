a2e7f6d6e2edbb101808100c30c83ea3ed6feeff
rcoderdev <radzionch@gmail.com>
2025-05-08 13:20:48 +0400

    refactor: remove console logs from mergeVaultsWithCoins function for cleaner code
    - Eliminated console log statements in the mergeVaultsWithCoins function to enhance code readability and maintainability.

--- core/ui/storage/vaults.tsx
+++ core/ui/storage/vaults.tsx
@@ -20,11 +20,8 @@ type MergeVaultsWithCoinsInput = {
 }
 
 const mergeVaultsWithCoins = ({ vaults, coins }: MergeVaultsWithCoinsInput) => {
-  console.log('mergeVaultsWithCoins', vaults, coins)
   return sortEntitiesWithOrder(vaults).map(vault => {
     const vaultCoins = coins[getVaultId(vault)] ?? []
-    console.log('vault: ', vault)
-    console.log('vaultCoins: ', vaultCoins)
     const vaultChains = vaultCoins.filter(isFeeCoin).map(coin => coin.chain)
 
     return {
