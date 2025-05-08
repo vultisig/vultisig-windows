3f167bf4ee34c0bf660113c47f8f06daa11dca56
rcoderdev <radzionch@gmail.com>
2025-05-08 14:30:06 +0400

    refactor: restructure keysigning flow components and introduce new action provider

--- core/ui/mpc/keysign/start/StartSecureKeysignFlow.tsx
+++ core/ui/mpc/keysign/start/StartSecureKeysignFlow.tsx
@@ -1,15 +1,17 @@
 import { KeysignSigningStep } from '@core/ui/mpc/keysign/KeysignSigningStep'
 import { KeysignPeerDiscoveryStep } from '@core/ui/mpc/keysign/peers/KeysignPeerDiscoveryStep'
+import { KeysignActionProviderProp } from '@core/ui/mpc/keysign/start/KeysignActionProviderProp'
 import { StartMpcSessionFlow } from '@core/ui/mpc/session/StartMpcSessionFlow'
 import { MpcPeersProvider } from '@core/ui/mpc/state/mpcPeers'
 import { MpcPeersSelectionProvider } from '@core/ui/mpc/state/mpcSelectedPeers'
 import { useCoreViewState } from '@core/ui/navigation/hooks/useCoreViewState'
 import { ValueTransfer } from '@lib/ui/base/ValueTransfer'
 
-import { KeysignActionProvider } from '../action/KeysignActionProvider'
 import { useRefreshedKeysignPayload } from '../hooks/useRefreshedKeysignPayload'
 
-export const StartSecureKeysignFlow = () => {
+export const StartSecureKeysignFlow = ({
+  keysignActionProvider: KeysignActionProvider,
+}: KeysignActionProviderProp) => {
   const [{ keysignPayload: potentiallyStaleKeysignPayload }] =
     useCoreViewState<'keysign'>()
   const keysignPayload = useRefreshedKeysignPayload(
