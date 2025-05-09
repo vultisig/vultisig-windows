0211f1aa63a6370963bf61938eff31739a6c6e89
zak <ledevzl@gmail.com>
2025-05-09 16:03:46 +0100

    fix: update nav routers

--- core/ui/vault/swap/form/info/SwapInfo.tsx
+++ core/ui/vault/swap/form/info/SwapInfo.tsx
@@ -1,10 +1,6 @@
 import { StrictInfoRow } from '@lib/ui/layout/StrictInfoRow'
 import { ActiveQueryOnly } from '@lib/ui/query/components/ActiveQueryOnly'
 
-<<<<<<< HEAD:core/ui/vault/swap/form/info/SwapInfo.tsx
-=======
-import { StrictInfoRow } from '@core/ui/layout/StrictInfoRow'
->>>>>>> c98b9877 (feat: use useCoreNavigate hook instead of useAppNavigate for vaults organisation):clients/desktop/src/vault/swap/form/info/SwapInfo.tsx
 import { useSwapQuoteQuery } from '../../queries/useSwapQuoteQuery'
 import { SwapFees } from './SwapFees'
 import { SwapProvider } from './SwapProvider'
