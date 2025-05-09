a803c6e274b4681ceb9483b074feef8296de3025
rcoderdev <radzionch@gmail.com>
2025-05-09 10:02:57 +0400

    feat: enhance MigrateIntro component with security type handling and update localization for upgrade shares info

--- clients/desktop/src/vault/migrate/MigrateIntro.tsx
+++ clients/desktop/src/vault/migrate/MigrateIntro.tsx
@@ -1,13 +1,22 @@
 import { MigrateInfoScreen } from '@core/ui/mpc/keygen/migrate/MigrateInfoScreen'
+import { useCurrentVaultSecurityType } from '@core/ui/vault/state/currentVault'
+import { Match } from '@lib/ui/base/Match'
 import { StepTransition } from '@lib/ui/base/StepTransition'
 import { Button } from '@lib/ui/buttons/Button'
-import { ChevronRightIcon } from '@lib/ui/icons/ChevronRightIcon'
-import { HStack } from '@lib/ui/layout/Stack'
 import { OnFinishProp } from '@lib/ui/props'
 import { Trans, useTranslation } from 'react-i18next'
 
 export const MigrateIntro = ({ onFinish }: OnFinishProp) => {
   const { t } = useTranslation()
+  const securityType = useCurrentVaultSecurityType()
+
+  const renderFinalScreen = () => (
+    <MigrateInfoScreen
+      animation="core/animations/upgrade_shares"
+      title={<Trans i18nKey="upgrade_shares_info" components={{ b: <b /> }} />}
+      action={<Button onClick={onFinish}>{t('next')}</Button>}
+    />
+  )
 
   return (
     <StepTransition
@@ -24,19 +33,26 @@ export const MigrateIntro = ({ onFinish }: OnFinishProp) => {
         />
       )}
       to={() => (
-        <MigrateInfoScreen
-          animation="/core/animations/choose-vault.riv"
-          title={
-            <Trans i18nKey="upgrade_all_devices" components={{ b: <b /> }} />
-          }
-          action={
-            <Button onClick={onFinish}>
-              <HStack alignItems="center" gap={8}>
-                {t('got_it')}
-                <ChevronRightIcon fontSize={20} />
-              </HStack>
-            </Button>
-          }
+        <Match
+          value={securityType}
+          fast={renderFinalScreen}
+          secure={() => (
+            <StepTransition
+              from={({ onFinish }) => (
+                <MigrateInfoScreen
+                  animation="/core/animations/choose-vault.riv"
+                  title={
+                    <Trans
+                      i18nKey="upgrade_all_devices"
+                      components={{ b: <b /> }}
+                    />
+                  }
+                  action={<Button onClick={onFinish}>{t('next')}</Button>}
+                />
+              )}
+              to={() => renderFinalScreen()}
+            />
+          )}
         />
       )}
     />
