e9de25caca343d8c87ec2399894d9e29ad47c5c8
rcoderdev <radzionch@gmail.com>
2025-05-10 10:28:24 +0400

    Refactor onboarding logic and navigation structure
    - Introduced the OnboardingResetter component to manage onboarding state and installation markers.
    - Updated App component to integrate OnboardingResetter within the ErrorBoundary for improved error handling.
    - Removed the obsolete NavigationProvider from the desktop client, streamlining the navigation structure.
    - Cleaned up unused parseUrlSearchString utility function to enhance codebase maintainability.

--- clients/desktop/src/App.tsx
+++ clients/desktop/src/App.tsx
@@ -1,4 +1,3 @@
-import { NavigationProvider } from '@core/ui/navigation/NavigationProvider'
 import { GlobalStyle } from '@lib/ui/css/GlobalStyle'
 import { VStack } from '@lib/ui/layout/Stack'
 import { ActiveRoute } from '@lib/ui/navigation/state'
@@ -8,23 +7,20 @@ import { ErrorBoundary } from './errors/components/ErrorBoundary'
 import { FullSizeErrorFallback } from './errors/components/FullSizeErrorFallback'
 import { LauncherObserver } from './launcher/components/LauncherObserver'
 import { routes } from './navigation/router'
-import OnboardingResetter from './onboarding/OnboardingRessetterProvider'
+import { OnboardingResetter } from './onboarding/OnboardingResetter'
 
 const App = () => {
   return (
     <AppProviders>
       <GlobalStyle />
       <VStack fullSize>
-        <OnboardingResetter>
-          <NavigationProvider>
-            <ErrorBoundary
-              renderFallback={props => <FullSizeErrorFallback {...props} />}
-            >
-              <LauncherObserver />
-              <ActiveRoute routes={routes} />
-            </ErrorBoundary>
-          </NavigationProvider>
-        </OnboardingResetter>
+        <ErrorBoundary
+          renderFallback={props => <FullSizeErrorFallback {...props} />}
+        >
+          <LauncherObserver />
+          <ActiveRoute routes={routes} />
+          <OnboardingResetter />
+        </ErrorBoundary>
       </VStack>
     </AppProviders>
   )
--- clients/extension/src/pages/index.tsx
+++ clients/extension/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { AppProviders } from '@clients/extension/src/providers/AppProviders'
-import { NavigationProvider } from '@core/ui/navigation/NavigationProvider'
 import { ActiveRoute } from '@lib/ui/navigation/state'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
@@ -9,9 +8,7 @@ import { routes } from '../navigation/router'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProviders>
-      <NavigationProvider>
-        <ActiveRoute routes={routes} />
-      </NavigationProvider>
+      <ActiveRoute routes={routes} />
     </AppProviders>
   </StrictMode>
 )
