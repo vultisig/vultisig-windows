934a8d6680eb884f4ae8c3d2822a594f9265cdda
rcoderdev <radzionch@gmail.com>
2025-05-12 12:14:57 +0400

    Enhance RootErrorFallback component with improved styling and layout
    - Added border radius and background color to the StackTrace for better visual appeal.
    - Adjusted the layout of error messages to ensure consistent styling and improved readability.
    - Updated navigation ID in the button click handler for accuracy.

--- core/ui/errors/RootErrorFallback.tsx
+++ core/ui/errors/RootErrorFallback.tsx
@@ -1,6 +1,7 @@
 import { useCoreNavigate } from '@core/ui/navigation/hooks/useCoreNavigate'
 import { useCore } from '@core/ui/state/core'
 import { Button } from '@lib/ui/buttons/Button'
+import { borderRadius } from '@lib/ui/css/borderRadius'
 import { UniformColumnGrid } from '@lib/ui/css/uniformColumnGrid'
 import { ErrorBoundaryFallbackProps } from '@lib/ui/errors/ErrorBoundary'
 import { FilledAlertIcon } from '@lib/ui/icons/FilledAlertIcon'
@@ -9,6 +10,7 @@ import { PageContent } from '@lib/ui/page/PageContent'
 import { PageHeader } from '@lib/ui/page/PageHeader'
 import { PageHeaderTitle } from '@lib/ui/page/PageHeaderTitle'
 import { Text, text } from '@lib/ui/text'
+import { getColor } from '@lib/ui/theme/getters'
 import { extractErrorMsg } from '@lib/utils/error/extractErrorMsg'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
@@ -22,6 +24,13 @@ const StackTrace = styled.pre`
     size: 12,
     weight: '400',
   })}
+  ${borderRadius.m};
+  background: ${getColor('foreground')};
+  padding: 20px;
+  max-height: 240px;
+  overflow: auto;
+  width: 100%;
+  max-width: 800px;
 `
 
 export const RootErrorFallback = ({
@@ -41,26 +50,19 @@ export const RootErrorFallback = ({
         title={<PageHeaderTitle>{t('something_went_wrong')}</PageHeaderTitle>}
       />
       <PageContent gap={20}>
-        <VStack
-          scrollable
-          flexGrow
-          gap={40}
-          alignItems="center"
-          justifyContent="center"
-        >
+        <VStack flexGrow gap={40} alignItems="center" justifyContent="center">
           <FilledAlertIcon style={{ fontSize: 66 }} />
-          <VStack style={{ maxWidth: 320 }} alignItems="center" gap={8}>
-            <Text
-              family="mono"
-              weight="700"
-              size={16}
-              color="contrast"
-              centerHorizontally
-            >
-              {extractErrorMsg(error)}
-            </Text>
-            {info && <StackTrace>{info.componentStack}</StackTrace>}
-          </VStack>
+          <Text
+            family="mono"
+            weight="700"
+            size={16}
+            color="contrast"
+            centerHorizontally
+            style={{ maxWidth: 320 }}
+          >
+            {extractErrorMsg(error)}
+          </Text>
+          {info && <StackTrace>{info.componentStack}</StackTrace>}
         </VStack>
         <UniformColumnGrid gap={20}>
           <Button
@@ -73,7 +75,7 @@ export const RootErrorFallback = ({
           </Button>
           <Button
             onClick={() => {
-              navigate({ id: 'vaults' })
+              navigate({ id: 'vault' })
               clearError()
             }}
           >
