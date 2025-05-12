26dc59010b36baeea0e942ecf9c6676322ba5e32
rcoderdev <radzionch@gmail.com>
2025-05-12 09:37:15 +0400

    force rerun

--- lib/ui/navigation/ActiveView.tsx
+++ lib/ui/navigation/ActiveView.tsx
@@ -11,7 +11,6 @@ export const ActiveView = ({ views }: ActiveViewProps) => {
   const [{ history }] = useNavigation()
 
   const { id } = getLastItem(history)
-
   const View = views[id]
 
   return <View />
