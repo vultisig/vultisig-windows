d17236f252578348ab36076f7230c58fdc136a76
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-13 09:06:50 +0300

    feat: add reusable address book hook

---
+++ core/ui/vault/send/addressSelector/hooks/useAddressSchema.ts
@@ -0,0 +1,58 @@
+import { useAssertWalletCore } from '@core/ui/chain/providers/WalletCoreProvider'
+import { useAddressBookItems } from '@core/ui/storage/addressBook'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect, useMemo } from 'react'
+import { useForm, useWatch } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import { z } from 'zod'
+
+import { getCoinOptions } from '../helpers/getCoinOptions'
+import {
+  getAddressSchema,
+  getModifyAddressSchema,
+} from '../schemas/addressSchema'
+
+export type AddressFormValues = z.infer<ReturnType<typeof getAddressSchema>>
+
+export const useAddressSchema = (type: 'add' | 'modify') => {
+  const addressBookItems = useAddressBookItems()
+  const walletCore = useAssertWalletCore()
+  const { t } = useTranslation()
+  const chainOptions = useMemo(() => getCoinOptions(), [])
+
+  const addressSchema = useMemo(
+    () =>
+      type === 'add'
+        ? getAddressSchema({
+            walletCore,
+            addressBookItems,
+            t,
+          })
+        : getModifyAddressSchema({
+            walletCore,
+            t,
+          }),
+    [addressBookItems, t, type, walletCore]
+  )
+
+  const form = useForm<AddressFormValues>({
+    resolver: zodResolver(addressSchema),
+    mode: 'onBlur',
+    defaultValues: {
+      chain: chainOptions[0].value,
+      title: '',
+      address: '',
+    },
+  })
+
+  const watchedChain = useWatch({ control: form.control, name: 'chain' })
+  const address = useWatch({ control: form.control, name: 'address' })
+
+  useEffect(() => {
+    if (watchedChain && address) {
+      form.trigger('address')
+    }
+  }, [watchedChain, address, form])
+
+  return form
+}
