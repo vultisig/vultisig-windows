1a8539f34a49eb13dbe80138d1e83eec9f732414
zak <ledevzl@gmail.com>
2025-05-14 14:41:45 +0100

    fix: remove duplicate emptyAddressView

--- core/ui/vault/vaultAddressBook/components/emptyAddressesView/EmptyAddressesView.tsx
+++ core/ui/vault/vaultAddressBook/components/emptyAddressesView/EmptyAddressesView.tsx
@@ -1,25 +1,19 @@
 import { Button } from '@lib/ui/buttons/Button'
-import { TriangleAlertIcon } from '@lib/ui/icons/TriangleAlertIcon'
 import { PageHeaderBackButton } from '@lib/ui/page/PageHeaderBackButton'
 import { PageHeaderTitle } from '@lib/ui/page/PageHeaderTitle'
-import { getColor } from '@lib/ui/theme/getters'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
 
 import { AddressBookPageHeader } from '../../AddressBookSettingsPage.styles'
 import {
   CenteredBox,
   Container,
   ResponsiveText,
+  StyledTriangleWarningIcon,
 } from './EmptyAddressView.styles'
 
 type EmptyAddressesViewProps = {
   onOpenAddAddressView: () => void
 }
-
-const StyledIcon = styled(TriangleAlertIcon)`
-  color: ${getColor('alertWarning')};
-`
 
 const EmptyAddressesView = ({
   onOpenAddAddressView,
@@ -37,7 +31,7 @@ const EmptyAddressesView = ({
       />
       <Container>
         <CenteredBox>
-          <StyledIcon width={120} height={120} />
+          <StyledTriangleWarningIcon width={120} height={120} />
           <ResponsiveText weight={700} color="contrast">
             {t('vault_settings_address_book_no_addresses_title')}
           </ResponsiveText>
