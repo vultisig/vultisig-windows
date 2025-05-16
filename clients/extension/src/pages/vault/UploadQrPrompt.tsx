69eb7b3c91464add1b3af00a1e506782a6ff2d1d
rcoderdev <radzionch@gmail.com>
2025-05-16 14:37:16 +0400

    feat: enhance UploadQrPrompt with mutation handling and loading state
    - Integrated useMutation for handling QR upload process in UploadQrPrompt component.
    - Updated button to display a spinner while the upload is pending, improving user feedback.
    - Replaced direct navigation with mutation to set the initial view and open the URL, streamlining the upload flow.

--- clients/extension/src/pages/vault/UploadQrPrompt.tsx
+++ clients/extension/src/pages/vault/UploadQrPrompt.tsx
@@ -1,15 +1,29 @@
 import { Button } from '@clients/extension/src/components/button'
-import { useCoreNavigate } from '@core/ui/navigation/hooks/useCoreNavigate'
+import { CoreView } from '@core/ui/navigation/CoreView'
+import { useCore } from '@core/ui/state/core'
 import { CameraIcon } from '@lib/ui/icons/CameraIcon'
+import { Spinner } from '@lib/ui/loaders/Spinner'
+import { useMutation } from '@tanstack/react-query'
+
+import { setInitialView } from '../../navigation/state'
 
 export const UploadQrPrompt = () => {
-  const navigate = useCoreNavigate()
+  const { openUrl } = useCore()
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      const view: CoreView = { id: 'uploadQr', state: {} }
+
+      await setInitialView(view)
+      openUrl(`chrome-extension://${chrome.runtime.id}/index.html`)
+    },
+  })
 
   return (
     <Button
       style={{ fontSize: 20 }}
-      icon={<CameraIcon />}
-      onClick={() => navigate({ id: 'uploadQr', state: {} })}
+      icon={isPending ? <Spinner /> : <CameraIcon />}
+      onClick={() => mutate()}
       size="sm"
       fitContent
     />
