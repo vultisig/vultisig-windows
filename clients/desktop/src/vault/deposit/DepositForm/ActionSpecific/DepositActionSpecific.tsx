6a8a142c7d96f259199665ac9bfa31797f6063df
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-16 10:10:58 +0300

    faet: unify stake and stake tcu

--- clients/desktop/src/vault/deposit/DepositForm/ActionSpecific/DepositActionSpecific.tsx
+++ clients/desktop/src/vault/deposit/DepositForm/ActionSpecific/DepositActionSpecific.tsx
@@ -1,38 +1,25 @@
 import { Chain } from '@core/chain/Chain'
 import { Coin } from '@core/chain/coin/Coin'
 import { Match } from '@lib/ui/base/Match'
-import {
-  UseFormGetValues,
-  UseFormSetValue,
-  UseFormWatch,
-} from 'react-hook-form'
 
 import { ChainAction } from '../../ChainAction'
 import { useGetMayaChainBondableAssetsQuery } from '../../hooks/useGetMayaChainBondableAssetsQuery'
-import { FormData } from '..'
+import { useDepositFormHandlers } from '../../providers/DepositFormHandlersProvider'
 import { BondUnbondLPSpecific } from './BondUnboldLPSpecific/BondUnbondLPSpecific'
 import { IBCTransferSpecific } from './IBCTransferSpecific/IBCTransferSpecific'
 import { MergeSpecific } from './MergeSpecific/MergeSpecific'
-import { StakeTCYSpecific } from './StakeTCYSpecific'
+import { StakeSpecific } from './StakeSpecific/StakeSpecific'
+import { UnstakeSpecific } from './StakeSpecific/UnstakeSpecific/UnstakeSpecific'
 import { SwitchSpecific } from './SwitchSpecific'
-import { UnstakeTCYSpecific } from './UnstakeTCYSpecific/UnstakeTCYSpecific'
 
 type Props = {
   chain: Chain
   action: ChainAction
-  setValue: UseFormSetValue<FormData>
-  getValues: UseFormGetValues<FormData>
-  watch: UseFormWatch<FormData>
 }
 
-export const DepositActionSpecific = ({
-  chain,
-  action,
-  setValue,
-  getValues,
-  watch,
-}: Props) => {
+export const DepositActionSpecific = ({ chain, action }: Props) => {
   const { data: bondableAssets = [] } = useGetMayaChainBondableAssetsQuery()
+  const [{ getValues }] = useDepositFormHandlers()
   const selectedBondableAsset = getValues('bondableAsset')
   const selectedCoin = getValues('selectedCoin') as Coin | null
 
@@ -43,44 +30,19 @@ export const DepositActionSpecific = ({
         <BondUnbondLPSpecific
           assets={bondableAssets}
           selectedAsset={selectedBondableAsset}
-          setValue={setValue}
-          watch={watch}
         />
       )}
       unbond_with_lp={() => (
         <BondUnbondLPSpecific
           assets={bondableAssets}
           selectedAsset={selectedBondableAsset}
-          setValue={setValue}
-          watch={watch}
         />
       )}
-      ibc_transfer={() => (
-        <IBCTransferSpecific
-          getValues={getValues}
-          setValue={setValue}
-          watch={watch}
-          chain={chain}
-        />
-      )}
-      merge={() => (
-        <MergeSpecific
-          selectedCoin={selectedCoin}
-          watch={watch}
-          setValue={setValue}
-        />
-      )}
-      switch={() => (
-        <SwitchSpecific
-          watch={watch}
-          setValue={setValue}
-          getValues={getValues}
-        />
-      )}
-      unstake_tcy={() => (
-        <UnstakeTCYSpecific setValue={setValue} getValues={getValues} />
-      )}
-      stake_tcy={() => <StakeTCYSpecific setValue={setValue} />}
+      ibc_transfer={() => <IBCTransferSpecific chain={chain} />}
+      merge={() => <MergeSpecific selectedCoin={selectedCoin} />}
+      switch={() => <SwitchSpecific />}
+      unstake_tcy={() => <UnstakeSpecific />}
+      stake_tcy={() => <StakeSpecific />}
       bond={() => null}
       unbond={() => null}
       leave={() => null}
