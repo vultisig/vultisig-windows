8123efbdbfad325a62f8ad438f10907ad14fed48
rcoderdev <radzionch@gmail.com>
2025-05-16 12:22:11 +0400

    chore: update yarn.lock and refactor VaultPage component
    - Removed unused dependencies from yarn.lock to clean up the project.
    - Introduced UploadQrPrompt component to replace ProvideQrPrompt in VaultPage for improved QR upload functionality.
    - Simplified VaultPage layout by removing unnecessary styled components.

--- clients/desktop/src/vault/components/VaultPage.tsx
+++ clients/desktop/src/vault/components/VaultPage.tsx
@@ -1,20 +1,17 @@
+import { UploadQrPrompt } from '@core/ui/qr/upload/UploadQrPrompt'
 import { useCurrentVault } from '@core/ui/vault/state/currentVault'
-import { toSizeUnit } from '@lib/ui/css/toSizeUnit'
 import { MenuIcon } from '@lib/ui/icons/MenuIcon'
 import { QrCodeIcon } from '@lib/ui/icons/QrCodeIcon'
 import { VStack } from '@lib/ui/layout/Stack'
-import { pageConfig } from '@lib/ui/page/config'
 import { PageHeader } from '@lib/ui/page/PageHeader'
 import { PageHeaderIconButton } from '@lib/ui/page/PageHeaderIconButton'
 import { PageHeaderIconButtons } from '@lib/ui/page/PageHeaderIconButtons'
 import { PageHeaderToggleTitle } from '@lib/ui/page/PageHeaderToggleTitle'
-import styled from 'styled-components'
 
 import UpdateAvailablePopup from '../../components/updateAvailablePopup/UpdateAvailablePopup'
 import { useAppNavigate } from '../../navigation/hooks/useAppNavigate'
 import { RefreshVaultBalance } from '../../vault/balance/RefreshVaultBalance'
 import { VaultOverview } from '../../vault/components/VaultOverview'
-import { ProvideQrPrompt } from '../../vault/qr/ProvideQrPrompt'
 
 export const VaultPage = () => {
   const { name } = useCurrentVault()
@@ -52,21 +49,10 @@ export const VaultPage = () => {
           }
         />
 
-        <PositionQrPrompt>
-          <ProvideQrPrompt />
-        </PositionQrPrompt>
+        <UploadQrPrompt />
         <VaultOverview />
       </VStack>
       <UpdateAvailablePopup />
     </>
   )
 }
-
-const PositionQrPrompt = styled.div`
-  position: fixed;
-  bottom: ${toSizeUnit(pageConfig.verticalPadding)};
-  left: 50%;
-  transform: translateX(-50%);
-  width: auto;
-  z-index: 1;
-`
--- core/ui/qr/upload/UploadQrPrompt.tsx
+++ core/ui/qr/upload/UploadQrPrompt.tsx
@@ -3,7 +3,9 @@ import { UnstyledButton } from '@lib/ui/buttons/UnstyledButton'
 import { centerContent } from '@lib/ui/css/centerContent'
 import { round } from '@lib/ui/css/round'
 import { sameDimensions } from '@lib/ui/css/sameDimensions'
+import { toSizeUnit } from '@lib/ui/css/toSizeUnit'
 import { CameraIcon } from '@lib/ui/icons/CameraIcon'
+import { pageConfig } from '@lib/ui/page/config'
 import { getColor } from '@lib/ui/theme/getters'
 import styled from 'styled-components'
 
@@ -16,11 +18,23 @@ const Container = styled(UnstyledButton)`
   font-size: 24px;
 `
 
-export const ProvideQrPrompt = () => {
+const Position = styled.div`
+  position: fixed;
+  bottom: ${toSizeUnit(pageConfig.verticalPadding)};
+  left: 50%;
+  transform: translateX(-50%);
+  width: auto;
+  z-index: 1;
+`
+
+export const UploadQrPrompt = () => {
   const navigate = useCoreNavigate()
+
   return (
-    <Container onClick={() => navigate({ id: 'uploadQr', state: {} })}>
-      <CameraIcon />
-    </Container>
+    <Position>
+      <Container onClick={() => navigate({ id: 'uploadQr', state: {} })}>
+        <CameraIcon />
+      </Container>
+    </Position>
   )
 }
