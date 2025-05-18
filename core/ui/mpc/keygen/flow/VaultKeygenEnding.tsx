6da97787e8961e8bf576f6f9c2b2883ef38691ba
rcoderdev <radzionch@gmail.com>
2025-05-18 13:28:53 +0400

    Add CreateVaultSuccessScreen component and integrate it into KeygenFlow for improved user feedback during vault creation process.

--- core/ui/mpc/keygen/flow/VaultKeygenEnding.tsx
+++ core/ui/mpc/keygen/flow/VaultKeygenEnding.tsx
@@ -1,30 +1,28 @@
 import { VaultKeygenBackupFlow } from '@core/ui/mpc/keygen/backup/VaultKeygenBackupFlow'
-import { MigrateSuccess } from '@core/ui/mpc/keygen/migrate/MigrateSuccess'
 import { useCurrentKeygenType } from '@core/ui/mpc/keygen/state/currentKeygenType'
-import { useCoreNavigate } from '@core/ui/navigation/hooks/useCoreNavigate'
+import { Match } from '@lib/ui/base/Match'
 import { StepTransition } from '@lib/ui/base/StepTransition'
 import { OnBackProp } from '@lib/ui/props'
+
+import { MigrateSuccess } from '../migrate/MigrateSuccess'
+import { KeygenFlowSuccess } from './KeygenFlowSuccess'
 
 export const KeygenFlowEnding = ({ onBack }: OnBackProp) => {
   const keygenType = useCurrentKeygenType()
 
-  const navigate = useCoreNavigate()
-
-  if (keygenType === 'migrate') {
-    return (
-      <StepTransition
-        from={({ onFinish }) => (
-          <VaultKeygenBackupFlow onFinish={onFinish} onBack={onBack} />
-        )}
-        to={() => <MigrateSuccess />}
-      />
-    )
-  }
-
   return (
-    <VaultKeygenBackupFlow
-      onFinish={() => navigate({ id: 'vault' })}
-      onBack={onBack}
+    <StepTransition
+      from={({ onFinish }) => (
+        <VaultKeygenBackupFlow onFinish={onFinish} onBack={onBack} />
+      )}
+      to={() => (
+        <Match
+          value={keygenType}
+          migrate={() => <MigrateSuccess />}
+          reshare={() => <KeygenFlowSuccess />}
+          create={() => <KeygenFlowSuccess />}
+        />
+      )}
     />
   )
 }
