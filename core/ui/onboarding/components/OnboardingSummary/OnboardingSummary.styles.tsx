74c6c220a1a8fb0b4426404e0fbe7db5de86fa1c
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-19 20:05:06 +0300

    feat: summary

---
+++ core/ui/onboarding/components/OnboardingSummary/OnboardingSummary.styles.tsx
@@ -0,0 +1,71 @@
+import { borderRadius } from '@lib/ui/css/borderRadius'
+import { Checkbox } from '@lib/ui/inputs/checkbox/Checkbox'
+import { HStack, VStack } from '@lib/ui/layout/Stack'
+import { PageContent } from '@lib/ui/page/PageContent'
+import { getColor } from '@lib/ui/theme/getters'
+import styled from 'styled-components'
+
+export const Wrapper = styled(PageContent)`
+  padding-inline: 12px;
+  margin-inline: auto;
+  padding-top: 20px;
+  justify-content: space-between;
+  overflow-y: hidden;
+`
+
+export const StyledCheckbox = styled(Checkbox)`
+  pointer-events: none;
+`
+
+export const ContentWrapper = styled(VStack)`
+  padding: 24px;
+  gap: 24px;
+  font-size: 24px;
+  ${borderRadius.s};
+`
+
+export const PillWrapper = styled.div`
+  position: relative;
+  padding: 8px 12px;
+  background-color: ${getColor('foreground')};
+  border: 1px solid ${getColor('foregroundExtra')};
+  border-radius: 0px 9999px 9999px 0px;
+  max-width: fit-content;
+  border-left: 2px solid ${getColor('foregroundDark')};
+
+  &:before {
+    content: '';
+    position: absolute;
+    left: -2px;
+    bottom: -470px;
+    height: 470px;
+    width: 2px;
+    background-color: ${getColor('foreground')};
+  }
+`
+
+export const SummaryListItem = styled(HStack)`
+  position: relative;
+  gap: 12px;
+  padding: 16px;
+  border-radius: 16px;
+  background-color: ${getColor('foreground')};
+  border: 1px solid ${getColor('foregroundExtra')};
+
+  &:before {
+    content: '';
+    position: absolute;
+    width: 23px;
+    height: 2px;
+    background-color: ${getColor('foregroundDark')};
+    top: 50%;
+    left: -24px;
+    transform: translateY(-50%);
+  }
+`
+
+export const IconWrapper = styled(VStack)`
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+`
---
+++ core/ui/onboarding/hooks/useOnboardingStepsAnimations.ts
@@ -0,0 +1,58 @@
+import { useStepNavigation } from '@lib/ui/hooks/useStepNavigation'
+import { useRive, useStateMachineInput } from '@rive-app/react-canvas'
+import { useCallback } from 'react'
+
+const STATE_MACHINE_NAME = 'State Machine 1'
+const INPUT_NAME = 'Index'
+
+export const ONBOARDING_ANIMATIONS = [0, 1, 2, 3, 4, 5] as const
+
+export const useOnboardingStepsAnimations = () => {
+  const {
+    step: currentAnimation,
+    toNextStep: toNextAnimation,
+    toPreviousStep: toPrevAnimation,
+  } = useStepNavigation({
+    steps: ONBOARDING_ANIMATIONS,
+  })
+
+  const { RiveComponent, rive } = useRive({
+    src: '/core/animations/onboarding.riv',
+    autoplay: true,
+    stateMachines: [STATE_MACHINE_NAME],
+  })
+
+  const stateMachineInput = useStateMachineInput(
+    rive,
+    STATE_MACHINE_NAME,
+    INPUT_NAME
+  )
+
+  const handleNextAnimation = useCallback(() => {
+    if (stateMachineInput && typeof stateMachineInput.value === 'number') {
+      stateMachineInput.value += 1
+      toNextAnimation()
+    }
+  }, [stateMachineInput, toNextAnimation])
+
+  // TODO: tony to refactor when the designer gives us the animations that work backwards
+  const handlePrevAnimation = useCallback(() => {
+    if (
+      stateMachineInput &&
+      typeof stateMachineInput.value === 'number' &&
+      stateMachineInput.value - 1 >= 0
+    ) {
+      stateMachineInput.value -= 1
+      toPrevAnimation()
+    }
+  }, [stateMachineInput, toPrevAnimation])
+
+  return {
+    animations: ONBOARDING_ANIMATIONS,
+    animationComponent: RiveComponent,
+    currentAnimation,
+    handlePrevAnimation,
+    handleNextAnimation,
+    isLoading: !rive,
+  }
+}
