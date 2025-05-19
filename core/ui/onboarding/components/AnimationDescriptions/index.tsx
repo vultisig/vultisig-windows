6cb476aae9cf92523a6b7b1601c54a5ab19d0a1d
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-19 20:03:10 +0300

    feat: vault backup

---
+++ core/ui/onboarding/components/AnimationDescriptions/index.tsx
@@ -0,0 +1,26 @@
+import { VStack } from '@lib/ui/layout/Stack'
+import { FC } from 'react'
+
+import { ONBOARDING_ANIMATIONS } from '../../hooks/useOnboardingStepsAnimations'
+import { VaultBackup } from './VaultBackup'
+import { VaultDevice } from './VaultDevice'
+import { VaultRecovery } from './VaultRecovery'
+import { VaultSharesInfo } from './VaultSharesInfo'
+import { VaultSharesIntro } from './VaultSharesIntro'
+import { VaultUnlock } from './VaultUnlock'
+
+const ANIMATIONS_CONTENT = [
+  () => <VaultSharesIntro />,
+  () => <VaultSharesInfo />,
+  () => <VaultDevice />,
+  () => <VaultRecovery />,
+  () => <VaultBackup />,
+  () => <VaultUnlock />,
+]
+
+type AnimationDescriptionProps = {
+  animation: (typeof ONBOARDING_ANIMATIONS)[number]
+}
+export const AnimationDescription: FC<AnimationDescriptionProps> = ({
+  animation,
+}) => <VStack justifyContent="center">{ANIMATIONS_CONTENT[animation]()}</VStack>
