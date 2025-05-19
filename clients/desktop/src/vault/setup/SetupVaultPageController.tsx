450a6867108abb004d7310845a7d37d99fde6187
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-19 20:00:45 +0300

    feat: move onboardin greeting

--- clients/desktop/src/vault/setup/SetupVaultPageController.tsx
+++ clients/desktop/src/vault/setup/SetupVaultPageController.tsx
@@ -1,7 +1,6 @@
+import { OnboardingController } from '@core/ui/onboarding/OnboardingController'
 import { useHasFinishedOnboarding } from '@core/ui/storage/onboarding'
 import { SetupVaultPage } from '@core/ui/vault/create/setup-vault'
-
-import { OnboardingController } from './onboarding/OnboardingController'
 
 export const SetupVaultPageController = () => {
   const hasFinishedOnboarding = useHasFinishedOnboarding()
