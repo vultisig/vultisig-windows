6246bef96aa3e5e6e9412bfbe517fb0f9468ee43
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-20 07:54:52 +0300

    fix: remove redundant wrapper

--- core/ui/onboarding/OnboardingController.tsx
+++ core/ui/onboarding/OnboardingController.tsx
@@ -2,7 +2,6 @@ import { Match } from '@lib/ui/base/Match'
 import { useStepNavigation } from '@lib/ui/hooks/useStepNavigation'
 import { VStack } from '@lib/ui/layout/Stack'
 
-import { ResponsivenessProvider } from '../providers/ResponsivenessProivder'
 import { OnboardingGreeting } from './components/OnboardingGreeting'
 import { OnboardingSteps } from './components/OnboardingSteps'
 import { OnboardingSummary } from './components/OnboardingSummary'
@@ -17,23 +16,19 @@ export const OnboardingController = () => {
   const { step, toNextStep } = useStepNavigation({ steps })
 
   return (
-    <ResponsivenessProvider>
-      <VStack
-        style={{
-          minHeight: '100%',
-        }}
-      >
-        <Match
-          value={step}
-          onboardingGreeting={() => (
-            <OnboardingGreeting onCompleteGreeting={toNextStep} />
-          )}
-          onboardingSteps={() => (
-            <OnboardingSteps onCompleteSteps={toNextStep} />
-          )}
-          onboardingSummary={() => <OnboardingSummary />}
-        />
-      </VStack>
-    </ResponsivenessProvider>
+    <VStack
+      style={{
+        minHeight: '100%',
+      }}
+    >
+      <Match
+        value={step}
+        onboardingGreeting={() => (
+          <OnboardingGreeting onCompleteGreeting={toNextStep} />
+        )}
+        onboardingSteps={() => <OnboardingSteps onCompleteSteps={toNextStep} />}
+        onboardingSummary={() => <OnboardingSummary />}
+      />
+    </VStack>
   )
 }
