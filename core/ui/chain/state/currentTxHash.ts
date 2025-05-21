c035ea323084fbf6fecf0440e84d1a9ff5f4c5b0
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-21 09:32:55 +0300

    feat: update current tx hash provider

--- core/ui/chain/state/currentTxHash.ts
+++ core/ui/chain/state/currentTxHash.ts
@@ -1,4 +1,6 @@
 import { getValueProviderSetup } from '@lib/ui/state/getValueProviderSetup'
 
+import { KeysignMutationResult } from '../../mpc/keysign/action/mutations/useKeysignMutation'
+
 export const { useValue: useCurrentTxHash, provider: CurrentTxHashProvider } =
-  getValueProviderSetup<string>('CurrentTxHash')
+  getValueProviderSetup<KeysignMutationResult>('CurrentTxHash')
