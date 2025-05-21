7639375e7510730bb3ef99c5404395640d353a15
zak <ledevzl@gmail.com>
2025-05-21 09:22:07 +0100

    update: add arrow back on import vault screen

--- core/ui/vault/new/index.tsx
+++ core/ui/vault/new/index.tsx
@@ -1,18 +1,29 @@
 import { useCoreNavigate } from '@core/ui/navigation/hooks/useCoreNavigate'
 import { ProductLogoBlock } from '@core/ui/product/ProductLogoBlock'
+import { useVaults } from '@core/ui/storage/vaults'
 import { Button } from '@lib/ui/buttons/Button'
 import { Divider } from '@lib/ui/divider'
 import { VStack } from '@lib/ui/layout/Stack'
 import { PageContent } from '@lib/ui/page/PageContent'
 import { PageFooter } from '@lib/ui/page/PageFooter'
+import { PageHeader } from '@lib/ui/page/PageHeader'
+import { PageHeaderBackButton } from '@lib/ui/page/PageHeaderBackButton'
 import { useTranslation } from 'react-i18next'
 
 export const NewVaultPage = () => {
   const { t } = useTranslation()
   const navigate = useCoreNavigate()
+  const vaults = useVaults()
 
   return (
     <VStack fullHeight>
+      {vaults.length > 0 && (
+        <PageHeader
+          hasBorder
+          style={{ padding: '16px' }}
+          primaryControls={<PageHeaderBackButton />}
+        />
+      )}
       <PageContent alignItems="center" justifyContent="center" flexGrow>
         <ProductLogoBlock />
       </PageContent>
