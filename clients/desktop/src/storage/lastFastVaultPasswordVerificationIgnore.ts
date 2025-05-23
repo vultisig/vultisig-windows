18d03a8e9167767d41d3277d1e8e1fc809afc5e6
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-23 10:06:06 +0300

    fix: linting

--- clients/desktop/src/storage/lastFastVaultPasswordVerificationIgnore.ts
+++ clients/desktop/src/storage/lastFastVaultPasswordVerificationIgnore.ts
@@ -10,30 +10,30 @@ import { StorageKey } from '@core/ui/storage/StorageKey'
 
 import { persistentStorage } from '../state/persistentState'
 
-export const getLastFastVaultPasswordVerification: GetLastFastVaultPasswordVerificationFunction =
+const getLastFastVaultPasswordVerification: GetLastFastVaultPasswordVerificationFunction =
   async () => {
     return (
-      (await persistentStorage.getItem<LastVerificationRecord>(
+      persistentStorage.getItem<LastVerificationRecord>(
         StorageKey.lastFastVaultPasswordVerification
-      )) ?? initialLastFastVaultPasswordVerification
+      ) ?? initialLastFastVaultPasswordVerification
     )
   }
 
-export const getLastFastVaultPasswordVerificationPerVault: GetLastFastVaultPasswordVerificationPerVaultFunction =
+const getLastFastVaultPasswordVerificationPerVault: GetLastFastVaultPasswordVerificationPerVaultFunction =
   async vaultId => {
     const all = await getLastFastVaultPasswordVerification()
     return all[vaultId] ?? 0
   }
 
-export const setLastFastVaultPasswordVerification: SetLastFastVaultPasswordVerificationFunction =
+const setLastFastVaultPasswordVerification: SetLastFastVaultPasswordVerificationFunction =
   async value => {
-    await persistentStorage.setItem(
+    persistentStorage.setItem(
       StorageKey.lastFastVaultPasswordVerification,
       value
     )
   }
 
-export const setLastFastVaultPasswordVerificationPerVault: SetLastFastVaultPasswordVerificationPerVaultFunction =
+const setLastFastVaultPasswordVerificationPerVault: SetLastFastVaultPasswordVerificationPerVaultFunction =
   async (vaultId, timestamp) => {
     const all = await getLastFastVaultPasswordVerification()
     const updated = { ...all, [vaultId]: timestamp }
