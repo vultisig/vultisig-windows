c0b0d81f8f1d4ec4162150c98e38c764e210e693
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-27 19:00:21 +0300

    feat: manage send coin

--- core/ui/vault/send/coin/ManageSendCoin.tsx
+++ core/ui/vault/send/coin/ManageSendCoin.tsx
@@ -1,68 +1,25 @@
-import { fromChainAmount } from '@core/chain/amount/fromChainAmount'
-import { CoinInputContainer } from '@core/ui/chain/coin/inputs/CoinInputContainer'
-import {
-  useCurrentVaultCoin,
-  useCurrentVaultCoins,
-} from '@core/ui/vault/state/currentVaultCoins'
-import { Opener } from '@lib/ui/base/Opener'
-import { InputContainer } from '@lib/ui/inputs/InputContainer'
+import { useCurrentVaultCoin } from '@core/ui/vault/state/currentVaultCoins'
 import { InputLabel } from '@lib/ui/inputs/InputLabel'
-import { SelectItemModal } from '@lib/ui/inputs/SelectItemModal'
-import { Text } from '@lib/ui/text'
-import { formatTokenAmount } from '@lib/utils/formatTokenAmount'
 import { useTranslation } from 'react-i18next'
 
-import { CoinOption } from '../../../chain/coin/inputs/CoinOption'
 import { useCoreViewState } from '../../../navigation/hooks/useCoreViewState'
-import { SendCoinBalanceDependant } from './balance/SendCoinBalanceDependant'
+import { HorizontalLine } from '../components/HorizontalLine'
+import { SendCoinInput } from '../components/SendCoinInput'
+import { SendInputContainer } from '../components/SendInputContainer'
 
 export const ManageSendCoin = () => {
   const [{ coin: coinKey }, setViewState] = useCoreViewState<'send'>()
   const coin = useCurrentVaultCoin(coinKey)
   const { t } = useTranslation()
-  const options = useCurrentVaultCoins()
 
   return (
-    <InputContainer>
-      <InputLabel>{t('asset')}</InputLabel>
-      <Opener
-        renderOpener={({ onOpen }) => (
-          <CoinInputContainer value={coin} onClick={onOpen} />
-        )}
-        renderContent={({ onClose }) => (
-          <SelectItemModal
-            title={t('choose_tokens')}
-            optionComponent={CoinOption}
-            filterFunction={(option, query) =>
-              option.ticker.toLowerCase().startsWith(query.toLowerCase())
-            }
-            onFinish={newValue => {
-              if (newValue) {
-                setViewState(prev => ({ ...prev, coin: newValue }))
-              }
-              onClose()
-            }}
-            options={options}
-          />
-        )}
+    <SendInputContainer>
+      <InputLabel>{t('asset_selection')}</InputLabel>
+      <HorizontalLine />
+      <SendCoinInput
+        value={coin}
+        onChange={coin => setViewState(prev => ({ ...prev, coin }))}
       />
-      <Text
-        centerVertically
-        weight="400"
-        size={14}
-        family="mono"
-        color="supporting"
-        style={{ gap: 8 }}
-      >
-        <span>{t('balance')}:</span>
-        <SendCoinBalanceDependant
-          success={amount => (
-            <span>
-              {formatTokenAmount(fromChainAmount(amount, coin.decimals))}
-            </span>
-          )}
-        />
-      </Text>
-    </InputContainer>
+    </SendInputContainer>
   )
 }
