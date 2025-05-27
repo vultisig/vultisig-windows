d7e4285027d6d964a40180e33953f5d98a91ac26
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-27 19:01:04 +0300

    feat: add horizontal line

---
+++ core/ui/vault/send/components/HorizontalLine.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const HorizontalLine = styled.div`
+  height: 1px;
+  width: 100%;
+  background: linear-gradient(90deg, #061b3a 0%, #284570 49.5%, #061b3a 100%);
+`
