ccd91ccea59186366d32c47c82d2d9a7b6283192
rcoderdev <radzionch@gmail.com>
2025-05-28 17:38:27 +0400

    Add PasscodeGuard component and integrate into AppProviders
    - Introduced the PasscodeGuard component to manage passcode entry and visibility based on passcode encryption status.
    - Updated AppProviders in both desktop and extension clients to wrap children components with PasscodeGuard, enhancing passcode management.
    - Added EnterPasscode component to prompt users for passcode input when necessary.

---
+++ core/ui/passcodeEncryption/guard/PasscodeGuard.tsx
@@ -0,0 +1,20 @@
+import { ChildrenProp } from '@lib/ui/props'
+
+import { useHasPasscodeEncryption } from '../../storage/passcodeEncryption'
+import { usePasscode } from '../state/passcode'
+import { EnterPasscode } from './EnterPasscode'
+
+export const PasscodeGuard = ({ children }: ChildrenProp) => {
+  const [passcode] = usePasscode()
+
+  const hasPasscodeEnabled = useHasPasscodeEncryption()
+
+  const isLocked = hasPasscodeEnabled && !passcode
+
+  return (
+    <>
+      {children}
+      {isLocked && <EnterPasscode />}
+    </>
+  )
+}
