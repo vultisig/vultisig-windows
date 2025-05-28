bd33dce6f8be11182c41ff1149ade4c4e9289da3
zak <ledevzl@gmail.com>
2025-05-29 00:53:16 +0100

    Add: paste and scanQR button

--- core/ui/address-book/form/index.tsx
+++ core/ui/address-book/form/index.tsx
@@ -6,23 +6,41 @@ import { AddressBookItem } from '@core/ui/address-book/model'
 import { ChainInput } from '@core/ui/chain/inputs/ChainInput'
 import { useAssertWalletCore } from '@core/ui/chain/providers/WalletCoreProvider'
 import { useCoreViewState } from '@core/ui/navigation/hooks/useCoreViewState'
+import { ScanQrView } from '@core/ui/qr/components/ScanQrView'
+import { useCore } from '@core/ui/state/core'
 import { useAddressBookItems } from '@core/ui/storage/addressBook'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { ActionInsideInteractiveElement } from '@lib/ui/base/ActionInsideInteractiveElement'
 import { Button } from '@lib/ui/buttons/Button'
+import { IconButton, iconButtonSizeRecord } from '@lib/ui/buttons/IconButton'
+import {
+  textInputHeight,
+  textInputHorizontalPadding,
+} from '@lib/ui/css/textInput'
+import { CameraIcon } from '@lib/ui/icons/CameraIcon'
+import { PasteIcon } from '@lib/ui/icons/PasteIcon'
 import { TextInput } from '@lib/ui/inputs/TextInput'
-import { VStack } from '@lib/ui/layout/Stack'
+import { HStack, VStack } from '@lib/ui/layout/Stack'
+import { Modal } from '@lib/ui/modal'
 import { PageContent } from '@lib/ui/page/PageContent'
 import { PageFooter } from '@lib/ui/page/PageFooter'
 import { PageHeader } from '@lib/ui/page/PageHeader'
 import { PageHeaderBackButton } from '@lib/ui/page/PageHeaderBackButton'
 import { PageHeaderTitle } from '@lib/ui/page/PageHeaderTitle'
 import { Text } from '@lib/ui/text'
+import { attempt } from '@lib/utils/attempt'
 import { extractErrorMsg } from '@lib/utils/error/extractErrorMsg'
 import { UseMutationResult } from '@tanstack/react-query'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 import { z } from 'zod'
+
+const FixedScanQRView = styled(ScanQrView)`
+  position: fixed;
+  inset: 0;
+`
 
 export type AddressBookFormValues = Pick<
   AddressBookItem,
@@ -48,6 +66,8 @@ export const AddressBookForm: FC<AddressBookFormProps> = ({
   const addressBookItems = useAddressBookItems()
   const walletCore = useAssertWalletCore()
   const [state] = useCoreViewState<'updateAddressBookItem'>()
+  const { getClipboardText } = useCore()
+  const [showScanner, setShowScanner] = useState(false)
   const {
     createAddressBookItem,
     error: createError,
@@ -122,6 +142,18 @@ export const AddressBookForm: FC<AddressBookFormProps> = ({
     onSubmit(values)
   }
 
+  const handlePaste = async () => {
+    const { data } = await attempt(getClipboardText)
+    if (data) {
+      setValue('address', data)
+    }
+  }
+
+  const handleScanSuccess = (address: string) => {
+    setValue('address', address)
+    setShowScanner(false)
+  }
+
   return (
     <VStack as="form" onSubmit={handleSubmit(handleSubmitForm)} fullHeight>
       <PageHeader
@@ -147,10 +179,30 @@ export const AddressBookForm: FC<AddressBookFormProps> = ({
           )}
         </VStack>
         <VStack gap={8}>
-          <TextInput
-            label={t('address')}
-            placeholder={t('type_here')}
-            {...register('address')}
+          <ActionInsideInteractiveElement
+            render={({ actionSize }) => (
+              <TextInput
+                label={t('address')}
+                placeholder={t('type_here')}
+                {...register('address')}
+                style={{
+                  paddingRight: actionSize.width + textInputHorizontalPadding,
+                }}
+              />
+            )}
+            action={
+              <HStack gap={8}>
+                <IconButton icon={<PasteIcon />} onClick={handlePaste} />
+                <IconButton
+                  icon={<CameraIcon fontSize={20} />}
+                  onClick={() => setShowScanner(true)}
+                />
+              </HStack>
+            }
+            actionPlacerStyles={{
+              right: textInputHorizontalPadding,
+              bottom: (textInputHeight - iconButtonSizeRecord.m) / 2,
+            }}
           />
           {errors.address && (
             <Text color="danger" size={12}>
@@ -179,6 +231,15 @@ export const AddressBookForm: FC<AddressBookFormProps> = ({
           {t('save')}
         </Button>
       </PageFooter>
+      {showScanner && (
+        <Modal
+          title=""
+          onClose={() => setShowScanner(false)}
+          withDefaultStructure={false}
+        >
+          <FixedScanQRView onFinish={handleScanSuccess} />
+        </Modal>
+      )}
     </VStack>
   )
 }
