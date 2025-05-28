b065469cec0b5bf3814f6cd13bdf4fe02e19eb38
rcoderdev <radzionch@gmail.com>
2025-05-28 13:59:32 +0400

    Enhance useDisablePasscodeMutation to update vaults keyshares
    - Integrated vaults keyshares management into the `useDisablePasscodeMutation` hook.
    - Added logic to decrypt and update vaults keyshares using the provided passcode before disabling passcode encryption.
    - Improved query invalidation to ensure consistent state across related components.

--- core/ui/passcodeEncryption/mutations/useDisablePasscodeMutation.ts
+++ core/ui/passcodeEncryption/mutations/useDisablePasscodeMutation.ts
@@ -1,16 +1,31 @@
 import { useInvalidateQueries } from '@lib/ui/query/hooks/useInvalidateQueries'
+import { recordFromItems } from '@lib/utils/record/recordFromItems'
+import { recordMap } from '@lib/utils/record/recordMap'
 import { useMutation } from '@tanstack/react-query'
 
 import { useCore } from '../../state/core'
 import { StorageKey } from '../../storage/StorageKey'
+import { useVaults } from '../../storage/vaults'
+import { getVaultId } from '../../vault/Vault'
+import { decryptVaultKeyShares } from '../core/vaultKeyShares'
+import { useAssertPasscode } from '../state/passcode'
 
 export const useDisablePasscodeMutation = () => {
-  const { setPasscodeEncryption } = useCore()
+  const { setPasscodeEncryption, updateVaultsKeyShares } = useCore()
   const invalidateQueries = useInvalidateQueries()
+  const passcode = useAssertPasscode()
+  const vaults = useVaults()
 
   return useMutation({
     mutationFn: async () => {
+      const vaultsKeyShares = recordMap(
+        recordFromItems(vaults, getVaultId),
+        ({ keyShares }) => decryptVaultKeyShares({ key: passcode, keyShares })
+      )
+
+      await updateVaultsKeyShares(vaultsKeyShares)
       await setPasscodeEncryption(null)
+
       await invalidateQueries(
         [StorageKey.passcodeEncryption],
         [StorageKey.vaults]
