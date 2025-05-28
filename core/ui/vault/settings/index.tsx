d43969e9ec54b21e9a5f51e742489679cc4da436
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-05-28 14:20:12 +0330

    Move vault settings page to core and reuse it in both clients (#1711)

--- core/ui/vault/settings/index.tsx
+++ core/ui/vault/settings/index.tsx
@@ -1,42 +1,30 @@
-import { Button } from '@clients/extension/src/components/button'
-import { useAppNavigate } from '@clients/extension/src/navigation/hooks/useAppNavigate'
+import { useCoreNavigate } from '@core/ui/navigation/hooks/useCoreNavigate'
 import { useCurrentVault } from '@core/ui/vault/state/currentVault'
-import { ChevronLeftIcon } from '@lib/ui/icons/ChevronLeftIcon'
 import { CircleInfoIcon } from '@lib/ui/icons/CircleInfoIcon'
+import { HardDriveDownloadIcon } from '@lib/ui/icons/HardDriveDownloadIcon'
 import { ShareIcon } from '@lib/ui/icons/ShareIcon'
+import { SignatureIcon } from '@lib/ui/icons/SignatureIcon'
 import { SquarePenIcon } from '@lib/ui/icons/SquarePenIcon'
 import { TrashIcon } from '@lib/ui/icons/TrashIcon'
 import { VStack } from '@lib/ui/layout/Stack'
 import { List } from '@lib/ui/list'
 import { ListItem } from '@lib/ui/list/item'
-import { useNavigateBack } from '@lib/ui/navigation/hooks/useNavigateBack'
 import { PageContent } from '@lib/ui/page/PageContent'
 import { PageHeader } from '@lib/ui/page/PageHeader'
-import { Text } from '@lib/ui/text'
+import { PageHeaderBackButton } from '@lib/ui/page/PageHeaderBackButton'
+import { PageHeaderTitle } from '@lib/ui/page/PageHeaderTitle'
 import { useTranslation } from 'react-i18next'
 
 export const VaultSettingsPage = () => {
   const { t } = useTranslation()
-  const navigate = useAppNavigate()
-  const navigateBack = useNavigateBack()
   const vault = useCurrentVault()
+  const navigate = useCoreNavigate()
 
   return (
     <VStack fullHeight>
       <PageHeader
-        primaryControls={
-          <Button
-            icon={<ChevronLeftIcon fontSize={20} />}
-            onClick={navigateBack}
-            size="sm"
-            fitContent
-          />
-        }
-        title={
-          <Text color="contrast" size={18} weight={500}>
-            {vault.name}
-          </Text>
-        }
+        primaryControls={<PageHeaderBackButton />}
+        title={<PageHeaderTitle>{vault.name}</PageHeaderTitle>}
         hasBorder
       />
       <PageContent flexGrow scrollable>
@@ -49,9 +37,30 @@ export const VaultSettingsPage = () => {
             showArrow
           />
           <ListItem
+            icon={<HardDriveDownloadIcon fontSize={20} />}
+            onClick={() => navigate({ id: 'vaultBackup' })}
+            title={t('backup')}
+            hoverable
+            showArrow
+          />
+          <ListItem
             icon={<SquarePenIcon fontSize={20} />}
             onClick={() => navigate({ id: 'renameVault' })}
             title={t('rename')}
+            hoverable
+            showArrow
+          />
+          <ListItem
+            icon={<ShareIcon fontSize={20} />}
+            onClick={() => navigate({ id: 'reshareVault' })}
+            title={t('reshare')}
+            hoverable
+            showArrow
+          />
+          <ListItem
+            icon={<SignatureIcon fontSize={20} />}
+            onClick={() => navigate({ id: 'signCustomMessage' })}
+            title={t('sign')}
             hoverable
             showArrow
           />
@@ -60,13 +69,6 @@ export const VaultSettingsPage = () => {
             onClick={() => navigate({ id: 'deleteVault' })}
             status="error"
             title={t('delete')}
-            hoverable
-            showArrow
-          />
-          <ListItem
-            icon={<ShareIcon fontSize={20} />}
-            onClick={() => navigate({ id: 'reshareVault' })}
-            title={t('reshare')}
             hoverable
             showArrow
           />
