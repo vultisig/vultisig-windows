8339bf86aecbba6261e53df659c5da2b6971933d
dev_zl <ledevzl@gmail.com>
2025-05-29 17:34:03 +0800

    Bug: include Sol base fee (#1709)
    * Bug: include Sol base fee
    
    * revert: send Form
    
    * fix: move base fee const to config file

--- core/chain/chains/solana/solanaConfig.ts
+++ core/chain/chains/solana/solanaConfig.ts
@@ -1,4 +1,8 @@
 export const solanaConfig = {
   priorityFeeLimit: Number(100_000), // Turbo fee in lamports, around 5 cents
   priorityFeePrice: 1_000_000, // Turbo fee in lamports, around 5 cents
+
+  // regardless of its complexity Solana charges a fixed base transaction fee of 5000 lamports per transaction.
+  // This base fee is separate from the priority fee.
+  baseFee: 5000, // 0.000005 SOL Base fee in lamports
 }
