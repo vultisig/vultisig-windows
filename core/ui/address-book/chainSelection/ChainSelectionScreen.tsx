8c2a9152fcb89330b44257f225bb553104dd2117
zak <ledevzl@gmail.com>
2025-05-29 14:28:20 +0100

    update: remove balance and list all chains

--- core/ui/address-book/chainSelection/ChainSelectionScreen.tsx
+++ core/ui/address-book/chainSelection/ChainSelectionScreen.tsx
@@ -1,12 +1,7 @@
-import { fromChainAmount } from '@core/chain/amount/fromChainAmount'
 import { Chain } from '@core/chain/Chain'
-import { getCoinValue } from '@core/chain/coin/utils/getCoinValue'
 import { ChainEntityIcon } from '@core/ui/chain/coin/icon/ChainEntityIcon'
 import { getChainLogoSrc } from '@core/ui/chain/metadata/getChainLogoSrc'
 import { useCoreViewState } from '@core/ui/navigation/hooks/useCoreViewState'
-import { useFiatCurrency } from '@core/ui/storage/fiatCurrency'
-import { BalanceVisibilityAware } from '@core/ui/vault/balance/visibility/BalanceVisibilityAware'
-import { useVaultChainsBalancesQuery } from '@core/ui/vault/queries/useVaultChainsBalancesQuery'
 import { TextInput } from '@lib/ui/inputs/TextInput'
 import { HStack, VStack } from '@lib/ui/layout/Stack'
 import { useNavigate } from '@lib/ui/navigation/hooks/useNavigate'
@@ -15,9 +10,6 @@ import { PageHeader } from '@lib/ui/page/PageHeader'
 import { PageHeaderBackButton } from '@lib/ui/page/PageHeaderBackButton'
 import { PageHeaderTitle } from '@lib/ui/page/PageHeaderTitle'
 import { Text } from '@lib/ui/text'
-import { sum } from '@lib/utils/array/sum'
-import { formatAmount } from '@lib/utils/formatAmount'
-import { formatTokenAmount } from '@lib/utils/formatTokenAmount'
 import { useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
@@ -39,16 +31,13 @@ export const ChainSelectionScreen = () => {
   const [selectedChain, setSelectedChain] = useState<Chain | null>(
     state?.selectedChain || null
   )
-  const fiatCurrency = useFiatCurrency()
-  const balancesQuery = useVaultChainsBalancesQuery()
 
   const chainOptions = useMemo(() => {
-    if (!balancesQuery.data) return []
-    return balancesQuery.data.map(balance => ({
-      value: balance.chain,
-      label: balance.chain,
+    return Object.values(Chain).map(chain => ({
+      value: chain,
+      label: chain,
     }))
-  }, [balancesQuery.data])
+  }, [])
 
   const filteredChains = useMemo(() => {
     if (!search) return chainOptions
@@ -74,31 +63,6 @@ export const ChainSelectionScreen = () => {
     })
   }
 
-  const getChainBalance = (chain: Chain) => {
-    if (!balancesQuery.data) return null
-    const chainBalance = balancesQuery.data.find(b => b.chain === chain)
-    if (!chainBalance) return null
-
-    const totalAmount = sum(
-      chainBalance.coins.map(coin =>
-        getCoinValue({
-          price: coin.price ?? 0,
-          amount: coin.amount,
-          decimals: coin.decimals,
-        })
-      )
-    )
-
-    const singleCoin =
-      chainBalance.coins.length === 1 ? chainBalance.coins[0] : null
-
-    return {
-      totalAmount,
-      singleCoin,
-      assets: chainBalance.coins.length,
-    }
-  }
-
   return (
     <Container>
       <PageHeader
@@ -116,7 +80,6 @@ export const ChainSelectionScreen = () => {
         </Text>
         <ChainList>
           {filteredChains.map(option => {
-            const balance = getChainBalance(option.value as Chain)
             const isSelected = selectedChain === option.value
             return (
               <ChainItem
@@ -133,29 +96,7 @@ export const ChainSelectionScreen = () => {
                     {option.value}
                   </Text>
                 </ChainContent>
-                <HStack gap={12} alignItems="center">
-                  {balance && (
-                    <VStack gap={4} alignItems="end">
-                      <Text color="contrast" size={14} weight="500">
-                        <BalanceVisibilityAware>
-                          {formatAmount(balance.totalAmount, fiatCurrency)}
-                        </BalanceVisibilityAware>
-                      </Text>
-                      <Text color="light" size={12} weight="500">
-                        {balance.assets > 1
-                          ? `${balance.assets} ${t('assets')}`
-                          : balance.singleCoin &&
-                            formatTokenAmount(
-                              fromChainAmount(
-                                balance.singleCoin.amount,
-                                balance.singleCoin.decimals
-                              )
-                            )}
-                      </Text>
-                    </VStack>
-                  )}
-                  <Checkbox checked={isSelected} />
-                </HStack>
+                <Checkbox checked={isSelected} />
               </ChainItem>
             )
           })}
