16a2c0fd7d3ca18f24fce5dfe9ac0d97da8148e9
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-30 13:12:14 +0300

    fix: send fiat fee wrapper

--- core/ui/vault/send/fee/SendFiatFeeWrapper.tsx
+++ core/ui/vault/send/fee/SendFiatFeeWrapper.tsx
@@ -1,3 +1,4 @@
+import { Text } from '@lib/ui/text'
 import { useTranslation } from 'react-i18next'
 
 import { SendChainSpecificProvider } from './SendChainSpecificProvider'
@@ -8,7 +9,9 @@ export const SendFiatFee = () => {
 
   return (
     <>
-      <span>{t('est_network_fee')}</span>
+      <Text size={13} color="shy">
+        {t('est_network_fee')}
+      </Text>
       <SendChainSpecificProvider>
         <SendFiatFeeValue />
       </SendChainSpecificProvider>
