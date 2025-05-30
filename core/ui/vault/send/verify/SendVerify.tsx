9c413e0c3185a37cdce3a30aa111f887e26b444f
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-05-30 13:12:36 +0300

    feat: send verify

--- core/ui/vault/send/verify/SendVerify.tsx
+++ core/ui/vault/send/verify/SendVerify.tsx
@@ -1,15 +1,10 @@
 import { fromChainAmount } from '@core/chain/amount/fromChainAmount'
-import { useCoinPriceQuery } from '@core/ui/chain/coin/price/queries/useCoinPriceQuery'
+import { isFeeCoin } from '@core/chain/coin/utils/isFeeCoin'
 import { TxOverviewMemo } from '@core/ui/chain/tx/TxOverviewMemo'
 import { TxOverviewPanel } from '@core/ui/chain/tx/TxOverviewPanel'
-import {
-  TxOverviewChainDataRow,
-  TxOverviewPrimaryRowTitle,
-  TxOverviewRow,
-} from '@core/ui/chain/tx/TxOverviewRow'
+import { TxOverviewRow } from '@core/ui/chain/tx/TxOverviewRow'
 import { useCurrentVaultCoin } from '@core/ui/vault/state/currentVaultCoins'
-import { WithProgressIndicator } from '@lib/ui/flow/WithProgressIndicator'
-import { VStack } from '@lib/ui/layout/Stack'
+import { HStack, VStack } from '@lib/ui/layout/Stack'
 import { Spinner } from '@lib/ui/loaders/Spinner'
 import { PageContent } from '@lib/ui/page/PageContent'
 import { PageHeader } from '@lib/ui/page/PageHeader'
@@ -18,13 +13,21 @@ import { PageHeaderTitle } from '@lib/ui/page/PageHeaderTitle'
 import { OnBackProp } from '@lib/ui/props'
 import { MatchQuery } from '@lib/ui/query/components/MatchQuery'
 import { Text } from '@lib/ui/text'
+import { getColor } from '@lib/ui/theme/getters'
+import { capitalizeFirstLetter } from '@lib/utils/capitalizeFirstLetter'
 import { formatTokenAmount } from '@lib/utils/formatTokenAmount'
+import { formatWalletAddress } from '@lib/utils/formatWalletAddress'
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 
-import { useFormatFiatAmount } from '../../../chain/hooks/useFormatFiatAmount'
+import { ChainCoinIcon } from '../../../chain/coin/icon/ChainCoinIcon'
+import { ChainEntityIcon } from '../../../chain/coin/icon/ChainEntityIcon'
+import { getCoinLogoSrc } from '../../../chain/coin/icon/utils/getCoinLogoSrc'
+import { shouldDisplayChainLogo } from '../../../chain/coin/icon/utils/shouldDisplayChainLogo'
+import { getChainLogoSrc } from '../../../chain/metadata/getChainLogoSrc'
+import { useCurrentVault } from '../../state/currentVault'
 import { SendFiatFee } from '../fee/SendFiatFeeWrapper'
-import { SendGasFeeWrapper } from '../fee/SendGasFeeWrapper'
 import { useSendCappedAmountQuery } from '../queries/useSendCappedAmountQuery'
 import { useSender } from '../sender/hooks/useSender'
 import { useSendMemo } from '../state/memo'
@@ -38,86 +41,94 @@ export const SendVerify: FC<OnBackProp> = ({ onBack }) => {
   const { t } = useTranslation()
   const [{ coin: coinKey }] = useCurrentSendCoin()
   const sender = useSender()
+  const { name } = useCurrentVault()
   const coin = useCurrentVaultCoin(coinKey)
   const [receiver] = useSendReceiver()
   const [memo] = useSendMemo()
-  const formatFiat = useFormatFiatAmount()
-
-  const coinPriceQuery = useCoinPriceQuery({
-    coin,
-  })
-
   const cappedAmountQuery = useSendCappedAmountQuery()
+
+  const { logo, chain, ticker, id } = coin
 
   return (
     <>
       <PageHeader
         primaryControls={<PageHeaderBackButton onClick={onBack} />}
-        title={<PageHeaderTitle>{t('verify')}</PageHeaderTitle>}
+        title={<PageHeaderTitle>{t('send_overview')}</PageHeaderTitle>}
       />
-      <PageContent gap={40}>
-        <WithProgressIndicator value={0.3}>
-          <TxOverviewPanel>
-            <TxOverviewChainDataRow>
-              <TxOverviewPrimaryRowTitle>{t('from')}</TxOverviewPrimaryRowTitle>
-              <span>{sender}</span>
-            </TxOverviewChainDataRow>
-            <TxOverviewChainDataRow>
-              <TxOverviewPrimaryRowTitle>{t('to')}</TxOverviewPrimaryRowTitle>
-              <span>{receiver}</span>
-            </TxOverviewChainDataRow>
-            {memo && <TxOverviewMemo value={memo} />}
-            <TxOverviewRow>
-              <span>{t('amount')}</span>
-              <span>
+      <PageContent gap={12}>
+        <TxOverviewPanel>
+          <AmountWrapper gap={24}>
+            <Text size={15} color="supporting">
+              {t('you_are_sending')}
+            </Text>
+            <HStack gap={8}>
+              <ChainCoinIcon
+                coinSrc={logo ? getCoinLogoSrc(logo) : undefined}
+                chainSrc={
+                  shouldDisplayChainLogo({
+                    ticker: ticker,
+                    chain: chain,
+                    isNative: isFeeCoin({
+                      id: id,
+                      chain: chain,
+                    }),
+                  })
+                    ? getChainLogoSrc(chain)
+                    : undefined
+                }
+                style={{ fontSize: 16 }}
+              />
+              <Text size={17}>
                 <MatchQuery
                   value={cappedAmountQuery}
                   error={() => <Text>{t('failed_to_load')}</Text>}
                   pending={() => <Spinner />}
                   success={({ amount, decimals }) =>
-                    formatTokenAmount(
-                      fromChainAmount(amount, decimals),
-                      coin.ticker
-                    )
+                    formatTokenAmount(fromChainAmount(amount, decimals))
                   }
                 />
-              </span>
-            </TxOverviewRow>
-
-            <TxOverviewRow>
-              <span>{t('value')}</span>
-              <span>
-                <MatchQuery
-                  value={cappedAmountQuery}
-                  error={() => <Text>{t('failed_to_load')}</Text>}
-                  pending={() => <Spinner />}
-                  success={({ amount, decimals }) => (
-                    <MatchQuery
-                      value={coinPriceQuery}
-                      error={() => <Text>{t('failed_to_load')}</Text>}
-                      pending={() => <Spinner />}
-                      success={coinPrice =>
-                        formatFiat(
-                          fromChainAmount(amount, decimals) * coinPrice
-                        )
-                      }
-                    />
-                  )}
-                />
-              </span>
-            </TxOverviewRow>
-
-            <TxOverviewRow>
-              <SendGasFeeWrapper />
-            </TxOverviewRow>
-            <TxOverviewRow>
-              <SendFiatFee />
-            </TxOverviewRow>
-          </TxOverviewPanel>
-        </WithProgressIndicator>
+                <Text as="span" color="shy">
+                  {ticker}
+                </Text>
+              </Text>
+            </HStack>
+          </AmountWrapper>
+          <TxOverviewRow>
+            <RowTitle>{t('from')}</RowTitle>
+            <Text size={14}>
+              {name}{' '}
+              <Text size={14} as="span" color="shy">
+                ({formatWalletAddress(sender)})
+              </Text>
+            </Text>
+          </TxOverviewRow>
+          <TxOverviewRow>
+            <RowTitle>{t('to')}</RowTitle>
+            <Text size={14}>{receiver}</Text>
+          </TxOverviewRow>
+          <TxOverviewRow>
+            <RowTitle>{capitalizeFirstLetter(t('network'))}</RowTitle>
+            <HStack gap={8}>
+              <ChainEntityIcon
+                value={getChainLogoSrc(chain)}
+                style={{ fontSize: 16 }}
+              />
+              <Text size={14}>{chain}</Text>
+            </HStack>
+          </TxOverviewRow>
+          {memo && <TxOverviewMemo value={memo} />}
+          <TxOverviewRow>
+            <SendFiatFee />
+          </TxOverviewRow>
+        </TxOverviewPanel>
         <SendTermsProvider initialValue={sendTerms.map(() => false)}>
-          <VStack gap={20}>
-            <SendTerms />
+          <SendTerms />
+          <VStack
+            style={{
+              marginTop: 'auto',
+            }}
+            gap={20}
+          >
             <SendConfirm />
           </VStack>
         </SendTermsProvider>
@@ -125,3 +136,13 @@ export const SendVerify: FC<OnBackProp> = ({ onBack }) => {
     </>
   )
 }
+
+const AmountWrapper = styled(VStack)`
+  padding-bottom: 20px !important;
+  margin-bottom: 12px;
+`
+
+const RowTitle = styled(Text)`
+  font-size: 13px;
+  color: ${getColor('textShy')};
+`
