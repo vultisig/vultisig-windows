ac99044b9bdc5d0c5b2cefbfe5b3f057001dc7e7
rcoderdev <radzionch@gmail.com>
2025-06-02 11:30:29 +0400

    Add Change Passcode feature
    - Introduced a new ChangePasscode component for managing passcode changes.
    - Updated ManagePasscode to include the ChangePasscode component.
    - Added necessary translation keys for passcode management.
    - Implemented change passcode mutation logic to handle passcode updates.
    - Created supporting components and utilities for UI layout and state management.

--- core/ui/passcodeEncryption/manage/ManagePasscode.tsx
+++ core/ui/passcodeEncryption/manage/ManagePasscode.tsx
@@ -1,7 +1,9 @@
+import { VStack } from '@lib/ui/layout/Stack'
 import { extractErrorMsg } from '@lib/utils/error/extractErrorMsg'
 import { useTranslation } from 'react-i18next'
 
 import { useDisablePasscodeMutation } from '../mutations/useDisablePasscodeMutation'
+import { ChangePasscode } from './change/ChangePasscode'
 import { EnablePasscodeInput } from './EnablePasscodeInput'
 
 export const ManagePasscode = () => {
@@ -14,11 +16,14 @@ export const ManagePasscode = () => {
   const { t } = useTranslation()
 
   return (
-    <EnablePasscodeInput
-      value={true}
-      onChange={() => disablePasscodeMutation()}
-      pendingMessage={isPending ? t('decrypting_vault_keyshares') : undefined}
-      errorMessage={error ? extractErrorMsg(error) : undefined}
-    />
+    <VStack gap={22}>
+      <EnablePasscodeInput
+        value={true}
+        onChange={() => disablePasscodeMutation()}
+        pendingMessage={isPending ? t('decrypting_vault_keyshares') : undefined}
+        errorMessage={error ? extractErrorMsg(error) : undefined}
+      />
+      {!isPending && <ChangePasscode />}
+    </VStack>
   )
 }
