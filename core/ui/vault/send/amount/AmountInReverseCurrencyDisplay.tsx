03771565e6f9f9931ff80ac313f60fb460b2c3b8
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-06-02 10:18:59 +0300

    feat: update amount component

--- core/ui/vault/send/amount/AmountInReverseCurrencyDisplay.tsx
+++ core/ui/vault/send/amount/AmountInReverseCurrencyDisplay.tsx
@@ -1,6 +1,7 @@
 import { useCoinPriceQuery } from '@core/ui/chain/coin/price/queries/useCoinPriceQuery'
 import { useFiatCurrency } from '@core/ui/storage/fiatCurrency'
 import { useCurrentVaultCoin } from '@core/ui/vault/state/currentVaultCoins'
+import { ValueProp } from '@lib/ui/props'
 import { MatchQuery } from '@lib/ui/query/components/MatchQuery'
 import { Text } from '@lib/ui/text'
 import { formatAmount } from '@lib/utils/formatAmount'
@@ -8,8 +9,11 @@ import { useTranslation } from 'react-i18next'
 
 import { useSendAmount } from '../state/amount'
 import { useCurrentSendCoin } from '../state/sendCoin'
+import { CurrencyInputMode } from './ManageAmountInputField'
 
-export const AmountInGlobalCurrencyDisplay = () => {
+export const AmountInReverseCurrencyDisplay = ({
+  value,
+}: ValueProp<CurrencyInputMode>) => {
   const { t } = useTranslation()
   const [sendAmount] = useSendAmount()
   const [{ coin: coinKey }] = useCurrentSendCoin()
@@ -20,12 +24,20 @@ export const AmountInGlobalCurrencyDisplay = () => {
     coin,
   })
 
+  const renderConverted = (price: number) => {
+    if (!sendAmount) return null
+
+    return value === 'base'
+      ? formatAmount(price * sendAmount, fiatCurrency)
+      : formatAmount(sendAmount, coin.ticker)
+  }
+
   return (
     <Text color="shy" size={14}>
-      {sendAmount && (
+      {!sendAmount ? null : (
         <MatchQuery
           value={priceQuery}
-          success={price => formatAmount(price * sendAmount, fiatCurrency)}
+          success={renderConverted}
           pending={() => t('loading')}
           error={() => t('failed_to_load')}
         />
