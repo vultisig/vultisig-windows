cbc0ca2a8cb7d4926ae101e660d4d68032db4b90
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-06-02 10:19:40 +0300

    feat: buttons

---
+++ lib/ui/icons/DollarIcon.tsx
@@ -0,0 +1,17 @@
+export const DollarIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    fill="none"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M8 1.333v13.333m3.333-11.333h-5a2.333 2.333 0 1 0 0 4.667h3.334a2.333 2.333 0 0 1 0 4.666H4"
+    />
+  </svg>
+)
