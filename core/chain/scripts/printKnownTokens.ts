0e506bd3d28b77de711c3da74cc39065935f8afa
rcoderdev <radzionch@gmail.com>
2025-06-08 13:31:29 +0400

    refactor: enhance token sorting in printKnownTokens script
    - Replaced manual sorting logic with a new utility function `sortRecordKeysAlphabetically` for improved readability and maintainability.
    - Updated the `printKnownTokens.ts` script to utilize the new sorting function, streamlining the output of known tokens.

--- core/chain/scripts/printKnownTokens.ts
+++ core/chain/scripts/printKnownTokens.ts
@@ -1,24 +1,19 @@
+import { recordMap } from '@lib/utils/record/recordMap'
+import { sortRecordKeysAlphabetically } from '@lib/utils/record/sortRecordKeysAlphabetically'
 import { execSync } from 'child_process'
 
+import { Chain } from '../Chain'
 import { chainTokens } from '../coin/chainTokens'
+import { KnownCoin } from '../coin/Coin'
 
 const main = () => {
-  const sortedChainTokens = Object.entries(chainTokens)
-    .sort(([chainA], [chainB]) => chainA.localeCompare(chainB))
-    .reduce(
-      (acc, [chain, coins]) => {
-        if (coins) {
-          const sortedCoins = [...coins].sort((a, b) =>
-            a.id.localeCompare(b.id)
-          )
-          ;(acc as any)[chain] = sortedCoins
-        }
-        return acc
-      },
-      {} as typeof chainTokens
+  const sorted = sortRecordKeysAlphabetically(
+    recordMap(chainTokens as Record<Chain, KnownCoin[]>, tokens =>
+      [...tokens].sort((a, b) => a.id.localeCompare(b.id))
     )
+  )
 
-  const output = JSON.stringify(sortedChainTokens, null, 2)
+  const output = JSON.stringify(sorted, null, 2)
 
   console.log(output)
 
