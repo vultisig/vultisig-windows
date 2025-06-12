70b85d4f4a1dd46a6fd62612ba9854aef7c68248
rcoderdev <radzionch@gmail.com>
2025-06-12 11:03:32 +0400

    Refactor StartFastKeysignFlow to remove PasswordProvider and streamline component structure. Update ValueTransfer components to include keys for better identification and maintainability.

--- core/ui/mpc/keysign/start/StartFastKeysignFlow.tsx
+++ core/ui/mpc/keysign/start/StartFastKeysignFlow.tsx
@@ -4,7 +4,6 @@ import { FastKeysignServerStep } from '@core/ui/mpc/keysign/fast/FastKeysignServ
 import { KeysignSigningStep } from '@core/ui/mpc/keysign/KeysignSigningStep'
 import { StartMpcSessionFlow } from '@core/ui/mpc/session/StartMpcSessionFlow'
 import { MpcPeersProvider } from '@core/ui/mpc/state/mpcPeers'
-import { PasswordProvider } from '@core/ui/state/password'
 import { Match } from '@lib/ui/base/Match'
 import { ValueTransfer } from '@lib/ui/base/ValueTransfer'
 import { useStepNavigation } from '@lib/ui/hooks/useStepNavigation'
@@ -29,48 +28,45 @@ export const StartFastKeysignFlow = ({
   })
 
   return (
-    <PasswordProvider initialValue="">
-      <Match
-        value={step}
-        server={() => (
-          <ValueTransfer<{ password: string }>
-            from={({ onFinish }) => <ServerPasswordStep onFinish={onFinish} />}
-            to={({ value: { password } }) => (
-              <FastKeysignServerStep
-                onFinish={toNextStep}
-                password={password}
+    <Match
+      value={step}
+      server={() => (
+        <ValueTransfer<{ password: string }>
+          key="password"
+          from={({ onFinish }) => <ServerPasswordStep onFinish={onFinish} />}
+          to={({ value: { password } }) => (
+            <FastKeysignServerStep onFinish={toNextStep} password={password} />
+          )}
+        />
+      )}
+      keysign={() => (
+        <ValueTransfer<string[]>
+          key="peers"
+          from={({ onFinish }) => {
+            return (
+              <WaitForServerStep
+                onBack={toPreviousStep}
+                onPeersChange={onFinish}
               />
-            )}
-          />
-        )}
-        keysign={() => (
-          <ValueTransfer<string[]>
-            from={({ onFinish }) => {
-              return (
-                <WaitForServerStep
-                  onBack={toPreviousStep}
-                  onPeersChange={onFinish}
-                />
-              )
-            }}
-            to={({ value }) => (
-              <MpcPeersProvider value={value}>
-                <StartMpcSessionFlow
-                  value="keysign"
-                  render={() => (
-                    <KeysignActionProvider>
-                      <KeysignSigningStep
-                        payload={keysignPayload}
-                        onFinish={onFinish}
-                      />
-                    </KeysignActionProvider>
-                  )}
-                />
-              </MpcPeersProvider>
-            )}
-          />
-        )}
-      />
-    </PasswordProvider>
+            )
+          }}
+          to={({ value }) => (
+            <MpcPeersProvider value={value}>
+              <StartMpcSessionFlow
+                value="keysign"
+                render={() => (
+                  <KeysignActionProvider>
+                    <KeysignSigningStep
+                      payload={keysignPayload}
+                      onFinish={onFinish}
+                    />
+                  </KeysignActionProvider>
+                )}
+              />
+            </MpcPeersProvider>
+          )}
+        />
+      )}
+    />
   )
 }
