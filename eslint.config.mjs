b1380eb694c822db9090771bac134a7d66c997d9
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-06-12 23:46:13 +0200

    feat: storybook setup part 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
@@ -22,100 +25,95 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 })
 
-export default [
-  {
-    ignores: [
-      '**/node_modules',
-      '**/dist',
-      '**/wailsjs',
-      '**/*_pb.ts',
-      'lib/dkls',
-      'lib/schnorr',
+export default [{
+  ignores: [
+    '**/node_modules',
+    '**/dist',
+    '**/wailsjs',
+    '**/*_pb.ts',
+    'lib/dkls',
+    'lib/schnorr',
+  ],
+}, ...compat.extends(
+  'eslint:recommended',
+  'plugin:react/recommended',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:jsx-a11y/recommended',
+  'prettier',
+  'plugin:prettier/recommended'
+), {
+  plugins: {
+    react,
+    '@typescript-eslint': typescriptEslint,
+    'jsx-a11y': jsxA11Y,
+    'react-hooks': fixupPluginRules(reactHooks),
+    'simple-import-sort': simpleImportSort,
+    'unused-imports': fixupPluginRules(unusedImportsPlugin),
+  },
+
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+    },
+
+    parser: tsParser,
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/no-autofocus': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-object-type': [
+      'off',
+      { allowEmptyObject: true },
     ],
-  },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
-    'prettier',
-    'plugin:prettier/recommended'
-  ),
-  {
-    plugins: {
-      react,
-      '@typescript-eslint': typescriptEslint,
-      'jsx-a11y': jsxA11Y,
-      'react-hooks': fixupPluginRules(reactHooks),
-      'simple-import-sort': simpleImportSort,
-      'unused-imports': fixupPluginRules(unusedImportsPlugin),
-    },
 
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
+    'unused-imports/no-unused-imports': 'error',
+
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
       },
+    ],
 
-      parser: tsParser,
-    },
-
-    settings: {
-      react: {
-        version: 'detect',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
       },
-    },
+    ],
 
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'jsx-a11y/no-autofocus': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-empty-object-type': [
-        'off',
-        { allowEmptyObject: true },
-      ],
-
-      'unused-imports/no-unused-imports': 'error',
-
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-        },
-      ],
-
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto',
-        },
-      ],
-
-      'react/prop-types': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'error',
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          selector: 'variable',
-          format: ['camelCase', 'PascalCase', 'snake_case'],
-          leadingUnderscore: 'allow',
-          trailingUnderscore: 'allow',
-        },
-      ],
-      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    },
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'snake_case'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
   },
-  // Override for declaration files where interfaces are required for module augmentation
-  {
-    files: ['**/*.d.ts'],
-    rules: {
-      '@typescript-eslint/consistent-type-definitions': 'off',
-    },
+}, // Override for declaration files where interfaces are required for module augmentation
+{
+  files: ['**/*.d.ts'],
+  rules: {
+    '@typescript-eslint/consistent-type-definitions': 'off',
   },
-]
+}, ...storybook.configs["flat/recommended"]];
