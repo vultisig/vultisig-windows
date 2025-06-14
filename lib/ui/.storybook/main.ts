70aa2917dc401b775ef16f61d70ae97ed30bd047
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-06-14 10:36:46 +0200

    feat: add stories for button

--- lib/ui/.storybook/main.ts
+++ lib/ui/.storybook/main.ts
@@ -10,10 +10,7 @@ function getAbsolutePath(value) {
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
+  stories: ['../**/*.stories.@(js|jsx|ts|tsx)', '../**/*.mdx'],
   addons: [
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-docs'),
