09c6f94415898cf9e6be420a23346caf35cb3c68
rcoderdev <radzionch@gmail.com>
2025-06-18 13:59:09 +0400

    Refactor CoreApp and PasscodeGuard: Replace VStack with styled Container and introduce TakeWholeSpaceAbsolutely for improved layout management

--- core/ui/passcodeEncryption/guard/PasscodeGuard.tsx
+++ core/ui/passcodeEncryption/guard/PasscodeGuard.tsx
@@ -1,26 +1,10 @@
-import { hideScrollbars } from '@lib/ui/css/hideScrollbars'
-import { takeWholeSpace } from '@lib/ui/css/takeWholeSpace'
+import { TakeWholeSpaceAbsolutely } from '@lib/ui/css/takeWholeSpaceAbsolutely'
 import { ChildrenProp } from '@lib/ui/props'
-import styled, { createGlobalStyle } from 'styled-components'
 
 import { useHasPasscodeEncryption } from '../../storage/passcodeEncryption'
 import { useAutoLock } from '../autoLock/core/autoLock'
 import { usePasscode } from '../state/passcode'
 import { EnterPasscode } from './EnterPasscode'
-
-const PasscodeOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  ${takeWholeSpace}
-  z-index: 9999;
-`
-
-const HideScrollbars = createGlobalStyle`
-  * {
-    ${hideScrollbars}
-  }
-`
 
 export const PasscodeGuard = ({ children }: ChildrenProp) => {
   const [passcode] = usePasscode()
@@ -36,10 +20,9 @@ export const PasscodeGuard = ({ children }: ChildrenProp) => {
       {children}
       {isLocked && (
         <>
-          <HideScrollbars />
-          <PasscodeOverlay>
+          <TakeWholeSpaceAbsolutely>
             <EnterPasscode />
-          </PasscodeOverlay>
+          </TakeWholeSpaceAbsolutely>
         </>
       )}
     </>
