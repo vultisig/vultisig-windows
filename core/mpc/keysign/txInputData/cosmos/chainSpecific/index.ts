7bb50ad84d6143d95fb93b2bfbc8c5a4073d0f4a
rcoderdev <radzionch@gmail.com>
2025-06-19 08:50:33 +0400

    Refactor Cosmos chain-specific data retrieval by simplifying the handling of vault-based chains and improving the clarity of the getCosmosChainSpecific function. This change enhances maintainability by streamlining the return structure and updating imports accordingly.

--- core/mpc/keysign/txInputData/cosmos/chainSpecific/index.ts
+++ core/mpc/keysign/txInputData/cosmos/chainSpecific/index.ts
@@ -1,30 +1,22 @@
-import {
-  Chain,
-  CosmosChain,
-  CosmosChainKind,
-  VaultBasedCosmosChain,
-} from '@core/chain/Chain'
+import { CosmosChain } from '@core/chain/Chain'
 import { getCosmosChainKind } from '@core/chain/chains/cosmos/utils/getCosmosChainKind'
 import { KeysignPayload } from '@core/mpc/types/vultisig/keysign/v1/keysign_message_pb'
-import { match } from '@lib/utils/match'
 
 import {
   CosmosSpecific,
   MAYAChainSpecific,
   THORChainSpecific,
 } from '../../../../types/vultisig/keysign/v1/blockchain_specific_pb'
-import { getBlockchainSpecificValue } from '../../../chainSpecific/KeysignChainSpecific'
-
-type VaultBasedCosmosChainSpecific = Omit<
-  THORChainSpecific | MAYAChainSpecific,
-  '$typeName'
->
+import {
+  chainSpecificRecord,
+  getBlockchainSpecificValue,
+} from '../../../chainSpecific/KeysignChainSpecific'
 
 type CosmosChainSpecific =
   | {
       ibcEnabled: CosmosSpecific
     }
-  | { vaultBased: VaultBasedCosmosChainSpecific }
+  | { vaultBased: THORChainSpecific | MAYAChainSpecific }
 
 export const getCosmosChainSpecific = (
   chain: CosmosChain,
@@ -32,27 +24,10 @@ export const getCosmosChainSpecific = (
 ) => {
   const chainKind = getCosmosChainKind(chain)
 
-  return match<CosmosChainKind, CosmosChainSpecific>(chainKind, {
-    ibcEnabled: () => ({
-      ibcEnabled: getBlockchainSpecificValue(
-        blockchainSpecific,
-        'cosmosSpecific'
-      ),
-    }),
-    vaultBased: () => {
-      const vaultChain = chain as VaultBasedCosmosChain
-
-      return {
-        vaultBased: match<
-          VaultBasedCosmosChain,
-          Omit<THORChainSpecific | MAYAChainSpecific, '$typeName'>
-        >(vaultChain, {
-          [Chain.THORChain]: () =>
-            getBlockchainSpecificValue(blockchainSpecific, 'thorchainSpecific'),
-          [Chain.MayaChain]: () =>
-            getBlockchainSpecificValue(blockchainSpecific, 'mayaSpecific'),
-        }),
-      }
-    },
-  })
+  return {
+    [chainKind]: getBlockchainSpecificValue(
+      blockchainSpecific,
+      chainSpecificRecord[chain]
+    ),
+  } as CosmosChainSpecific
 }
