2f8ab175c4b1ff2f0c158c444d39d2704440bfab
rcoderdev <radzionch@gmail.com>
2025-06-29 14:10:10 +0400

    Refactor key components to utilize CoreViewState for improved state management in Keysign and Send prompts. Streamline props handling in VaultPrimaryActions and SendPrompt components, enhancing code clarity and maintainability.

--- core/ui/mpc/keysign/StartKeysignPrompt.tsx
+++ core/ui/mpc/keysign/StartKeysignPrompt.tsx
@@ -1,4 +1,3 @@
-import { CoreView } from '@core/ui/navigation/CoreView'
 import { useCoreNavigate } from '@core/ui/navigation/hooks/useCoreNavigate'
 import { useCurrentVaultSecurityType } from '@core/ui/vault/state/currentVault'
 import { Button } from '@lib/ui/buttons/Button'
@@ -6,16 +5,14 @@ import { VStack } from '@lib/ui/layout/Stack'
 import { IsDisabledProp } from '@lib/ui/props'
 import { useTranslation } from 'react-i18next'
 
-type StartKeysignPromptProps = Omit<
-  Extract<CoreView, { id: 'keysign' }>['state'],
-  'securityType'
-> &
+import { CoreViewState } from '../../navigation/CoreView'
+
+type StartKeysignPromptProps = Omit<CoreViewState<'keysign'>, 'securityType'> &
   IsDisabledProp
 
 export const StartKeysignPrompt = ({
-  keysignPayload,
   isDisabled,
-  isDAppSigning,
+  ...coreViewState
 }: StartKeysignPromptProps) => {
   const { t } = useTranslation()
   const navigate = useCoreNavigate()
@@ -31,8 +28,7 @@ export const StartKeysignPrompt = ({
             id: 'keysign',
             state: {
               securityType: 'secure',
-              keysignPayload,
-              isDAppSigning,
+              ...coreViewState,
             },
           })
         }
@@ -47,8 +43,7 @@ export const StartKeysignPrompt = ({
               id: 'keysign',
               state: {
                 securityType: 'fast',
-                keysignPayload,
-                isDAppSigning,
+                ...coreViewState,
               },
             })
           }
