35e88adf382f55f887a764d0093573b970a74615
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-06-30 09:37:09 +0300

    feat: add new digit grpoup inpute part 1

--- core/ui/vault/backup/fast/index.tsx
+++ core/ui/vault/backup/fast/index.tsx
@@ -1,9 +1,11 @@
 import { verifyVaultEmailCode } from '@core/mpc/fast/api/verifyVaultEmailCode'
 import { useCurrentVault } from '@core/ui/vault/state/currentVault'
 import { getVaultId } from '@core/ui/vault/Vault'
-import { OTPInput } from '@lib/ui/inputs/OTPInput'
-import { HStack, VStack } from '@lib/ui/layout/Stack'
-import { Spinner } from '@lib/ui/loaders/Spinner'
+import {
+  DigitGroupInput,
+  DigitGroupInputProps,
+} from '@lib/ui/inputs/DigitGroupInput'
+import { VStack } from '@lib/ui/layout/Stack'
 import { PageContent } from '@lib/ui/page/PageContent'
 import { PageHeader } from '@lib/ui/page/PageHeader'
 import { PageHeaderBackButton } from '@lib/ui/page/PageHeaderBackButton'
@@ -11,13 +13,14 @@ import { OnFinishProp } from '@lib/ui/props'
 import { useIsTabletDeviceAndUp } from '@lib/ui/responsive/mediaQuery'
 import { Text } from '@lib/ui/text'
 import { useMutation } from '@tanstack/react-query'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
 
 const onCompleteDelay = 1000
 
 export const EmailConfirmation = ({ onFinish }: OnFinishProp) => {
+  const [input, setInput] = useState<string | null>('')
+
   const { t } = useTranslation()
   const vault = useCurrentVault()
   const { isPending, mutate, error, isSuccess } = useMutation({
@@ -36,6 +39,14 @@ export const EmailConfirmation = ({ onFinish }: OnFinishProp) => {
       return () => clearTimeout(timeoutId)
     }
   }, [isSuccess, onFinish])
+
+  const inputState: DigitGroupInputProps['validation'] = isSuccess
+    ? 'valid'
+    : isPending
+      ? 'loading'
+      : error
+        ? 'invalid'
+        : 'idle'
 
   return (
     <VStack fullHeight>
@@ -57,32 +68,14 @@ export const EmailConfirmation = ({ onFinish }: OnFinishProp) => {
           </Text>
         </VStack>
         <VStack gap={4}>
-          <OTPInput
-            validation={error ? 'invalid' : isSuccess ? 'valid' : undefined}
+          <DigitGroupInput
+            value={input}
+            onChange={value => setInput(value)}
+            validation={inputState}
             onCompleted={value => mutate(value)}
           />
-          {error ? (
-            <Text size={13} color="danger">
-              {t('email_confirmation_code_error')}
-            </Text>
-          ) : (
-            isPending && (
-              <HStack gap={4}>
-                <StyledAnimatedLoader />
-                <Text weight={500} as="span" color="contrast" size={13}>
-                  {t('fastVaultSetup.backup.verifyingCode')}
-                </Text>
-              </HStack>
-            )
-          )}
         </VStack>
       </PageContent>
     </VStack>
   )
 }
-
-const StyledAnimatedLoader = styled(Spinner)`
-  width: fit-content;
-  position: relative;
-  font-size: 20px;
-`
