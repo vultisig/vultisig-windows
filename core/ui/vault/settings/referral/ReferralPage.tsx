78f9b589d2a28b51c9cdf995c7517bb1768669b4
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-07-03 19:16:31 +0300

    feat: add additional elemetns for landing page

--- core/ui/vault/settings/referral/ReferralPage.tsx
+++ core/ui/vault/settings/referral/ReferralPage.tsx
@@ -1,8 +1,13 @@
 import { Match } from '@lib/ui/base/Match'
+import { IconButton } from '@lib/ui/buttons/IconButton'
 import { useStepNavigation } from '@lib/ui/hooks/useStepNavigation'
+import { InfoIcon } from '@lib/ui/icons/InfoIcon'
+import { VStack } from '@lib/ui/layout/Stack'
 import { PageContent } from '@lib/ui/page/PageContent'
 import { PageHeader } from '@lib/ui/page/PageHeader'
 import { PageHeaderBackButton } from '@lib/ui/page/PageHeaderBackButton'
+import { Text } from '@lib/ui/text'
+import { Tooltip } from '@lib/ui/tooltips/Tooltip'
 import { useTranslation } from 'react-i18next'
 
 import { ReferralLanding } from './components/ReferralLanding'
@@ -17,7 +22,26 @@ export const ReferralPage = () => {
     <>
       <PageHeader
         primaryControls={<PageHeaderBackButton />}
-        secondaryControls={<></>}
+        secondaryControls={
+          <Tooltip
+            renderOpener={() => (
+              <IconButton size="sm">
+                <InfoIcon />
+              </IconButton>
+            )}
+            content={
+              <VStack>
+                <Text size={16}>Referral Program</Text>
+                <Text color="supporting" size={13}>
+                  The referral programm is applied on THORChain swaps and is on
+                  a best effort basis. You need to register a THORName to use
+                  the Vultisig referral. The registration fee is 10 RUNE and 1
+                  RUNE for each year, which is paid to the THORChain network.
+                </Text>
+              </VStack>
+            }
+          />
+        }
         title={t('referral')}
         hasBorder
       />
