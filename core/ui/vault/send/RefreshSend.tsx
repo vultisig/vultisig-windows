a692182bcafc376d3f92c17830bbb463a03e871c
rcoderdev <radzionch@gmail.com>
2025-07-06 08:33:58 +0400

    Fix Send/Swap/Deposit navigation logic

--- core/ui/vault/send/RefreshSend.tsx
+++ core/ui/vault/send/RefreshSend.tsx
@@ -1,20 +1,19 @@
+import { extractAccountCoinKey } from '@core/chain/coin/AccountCoin'
 import { getBalanceQueryKey } from '@core/ui/chain/coin/queries/useBalancesQuery'
 import { chainSpecificQueryKeyPrefix } from '@core/ui/chain/coin/queries/useChainSpecificQuery'
 import { useCurrentSendCoin } from '@core/ui/vault/send/state/sendCoin'
-import { useCurrentVaultAddress } from '@core/ui/vault/state/currentVaultCoins'
 import { IconButton } from '@lib/ui/buttons/IconButton'
 import { RefreshCwIcon } from '@lib/ui/icons/RefreshCwIcon'
 import { useInvalidateQueries } from '@lib/ui/query/hooks/useInvalidateQueries'
 import { useMutation } from '@tanstack/react-query'
 
 export const RefreshSend = () => {
-  const [{ coin: coinKey }] = useCurrentSendCoin()
+  const coin = useCurrentSendCoin()
   const invalidateQueries = useInvalidateQueries()
-  const address = useCurrentVaultAddress(coinKey.chain)
 
   const { mutate: refresh, isPending } = useMutation({
     mutationFn: () =>
-      invalidateQueries(getBalanceQueryKey({ ...coinKey, address }), [
+      invalidateQueries(getBalanceQueryKey(extractAccountCoinKey(coin)), [
         chainSpecificQueryKeyPrefix,
       ]),
   })
