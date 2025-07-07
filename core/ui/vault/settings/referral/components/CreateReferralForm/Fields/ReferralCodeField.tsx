c79830bf62119244cde6c8bee045a3e15d84402a
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-07-07 16:08:50 +0300

    feat: referral form updates

---
+++ core/ui/vault/settings/referral/components/CreateReferralForm/Fields/ReferralCodeField.tsx
@@ -0,0 +1,74 @@
+import { Button } from '@lib/ui/buttons/Button'
+import { TextInput } from '@lib/ui/inputs/TextInput'
+import { HStack, VStack } from '@lib/ui/layout/Stack'
+import { Text } from '@lib/ui/text'
+import { getColor } from '@lib/ui/theme/getters'
+import { Controller, useFormContext } from 'react-hook-form'
+import styled from 'styled-components'
+
+import {
+  FormField,
+  FormFieldErrorText,
+  FormFieldLabel,
+} from '../../Referrals.styled'
+import { ReferralFormData } from '../config'
+
+export const ReferralCodeField = () => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext<ReferralFormData>()
+
+  const status = 'available'
+
+  return (
+    <VStack gap={14}>
+      <FormField>
+        <FormFieldLabel htmlFor="referral-name">
+          Pick Referral Code
+        </FormFieldLabel>
+        <HStack gap={8}>
+          <Controller
+            name="referralName"
+            control={control}
+            render={({ field }) => (
+              <TextInput
+                {...field}
+                id="referral-name"
+                placeholder="Enter up to 4 characters"
+              />
+            )}
+          />
+          <Button style={{ maxWidth: 97, fontSize: 14 }}>Search</Button>
+        </HStack>
+        {errors.referralName && (
+          <FormFieldErrorText>{errors.referralName.message}</FormFieldErrorText>
+        )}
+      </FormField>
+      <StatusWrapper justifyContent="space-between" alignItems="center">
+        <Text size={14} color="supporting">
+          Status
+        </Text>
+        <StatusPill>
+          <Text size={13} color={status === 'available' ? 'success' : 'danger'}>
+            Available
+          </Text>
+        </StatusPill>
+      </StatusWrapper>
+    </VStack>
+  )
+}
+
+const StatusWrapper = styled(HStack)`
+  height: 52px;
+`
+
+const StatusPill = styled.div`
+  display: flex;
+  padding: 8px 12px;
+  align-items: center;
+  gap: 4px;
+  border-radius: 99px;
+  border: 1.5px solid ${getColor('foregroundExtra')};
+  background: ${getColor('foreground')};
+`
