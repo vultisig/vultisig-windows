dd898f76acf3a829c722ed7b667a15278895d928
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-07-09 17:51:10 +0330

    Fix build issue

--- clients/extension/src/background/handlers/errorHandler.ts
+++ clients/extension/src/background/handlers/errorHandler.ts
@@ -1,19 +1,15 @@
-const codes = [4001, 4100, 4200, 4900, 4901] as const
-
-const errors: Record<Code, string> = {
+const errors = {
   4001: 'User rejected the request',
   4100: 'Unauthorized',
   4200: 'Unsupported method',
   4900: 'Provider disconnected',
   4901: 'Chain disconnected',
-}
-
-type Code = (typeof codes)[number]
+} as const
 
 export class EIP1193Error extends Error {
   code: number
 
-  constructor(code: Code) {
+  constructor(code: keyof typeof errors) {
     super(errors[code])
     this.code = code
   }
