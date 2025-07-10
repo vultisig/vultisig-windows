d9c221cd7905f4fd7642bebec6bcb2fb47a34daa
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-07-10 09:52:56 +0300

    fix: create referral form disabled state

--- core/ui/vault/settings/referral/components/CreateReferralForm/config.ts
+++ core/ui/vault/settings/referral/components/CreateReferralForm/config.ts
@@ -13,7 +13,7 @@ export const referralSchema = z.object({
     .max(4, 'Max 4 characters')
     .nonempty('Referral code is required'),
   expiration: z.number().min(1, 'Minimum 1 year').max(10, 'Maximum 10 years'),
-  payoutAsset: accountCoinSchema.optional(),
+  payoutAsset: accountCoinSchema,
 })
 
 export type ReferralFormData = z.infer<typeof referralSchema>
