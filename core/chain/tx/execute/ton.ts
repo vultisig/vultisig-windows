684a7d2ef6825c72855364b919f043be2f1ae276
rcoderdev <radzionch@gmail.com>
2025-07-11 12:50:22 +0400

    refactor: improve error handling and response validation in executeTonTx function

--- core/chain/tx/execute/ton.ts
+++ core/chain/tx/execute/ton.ts
@@ -1,5 +1,8 @@
 import { rootApiUrl } from '@core/config'
+import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
+import { attempt } from '@lib/utils/attempt'
 import { assertErrorMessage } from '@lib/utils/error/assertErrorMessage'
+import { isInError } from '@lib/utils/error/isInError'
 import { queryUrl } from '@lib/utils/query/queryUrl'
 import { TW } from '@trustwallet/wallet-core'
 
@@ -12,13 +15,23 @@ export const executeTonTx: ExecuteTxResolver = async ({ compiledTx }) => {
 
   const url = `${rootApiUrl}/ton/v2/sendBocReturnHash`
 
-  const response = await queryUrl<{ result: { hash: string } }>(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ boc: output.encoded }),
-  })
+  const response = await attempt(
+    queryUrl<{ result: { hash: string } }>(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ boc: output.encoded }),
+    })
+  )
 
-  return { txHash: Buffer.from(response.result.hash, 'base64').toString('hex') }
+  if ('error' in response && isInError(response.error, 'duplicate message')) {
+    return { txHash: '' }
+  }
+
+  const { result } = shouldBePresent(response.data)
+
+  return {
+    txHash: Buffer.from(result.hash, 'base64').toString('hex'),
+  }
 }
