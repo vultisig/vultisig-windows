afb0e70e955f46b1e68f8206214f46feb5fd7fe9
rcoderdev <radzionch@gmail.com>
2025-07-12 11:43:51 +0400

    refactor: enhance downloadSetupMessage function to utilize queryUrl and withoutUndefinedFields for cleaner request handling

--- lib/utils/query/queryUrl.ts
+++ lib/utils/query/queryUrl.ts
@@ -1,15 +1,22 @@
 import { assertFetchResponse } from '../fetch/assertFetchResponse'
 
+type ResponseType = 'json' | 'text'
+
+type QueryUrlOptions = RequestInit & {
+  responseType?: ResponseType
+}
+
 export const queryUrl = async <T>(
   url: string | URL,
-  options?: RequestInit
+  options: QueryUrlOptions = {}
 ): Promise<T> => {
+  const { responseType = 'json', ...rest } = options
   const response = await fetch(url, {
     method: 'GET',
-    ...options,
+    ...rest,
   })
 
   await assertFetchResponse(response)
 
-  return response.json()
+  return response[responseType]()
 }
