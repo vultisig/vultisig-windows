0d51b321a5590f67cbc75786befc9afedec84ab3
rcoderdev <radzionch@gmail.com>
2025-07-12 11:30:58 +0400

    refactor: standardize serverUrl naming and remove unused parameters in download and upload setup message functions

--- core/mpc/downloadSetupMessage.ts
+++ core/mpc/downloadSetupMessage.ts
@@ -2,22 +2,15 @@ import { assertFetchResponse } from '@lib/utils/fetch/assertFetchResponse'
 import { retry } from '@lib/utils/query/retry'
 
 type DownloadSetupMessageInput = {
-  serverURL: string
+  serverUrl: string
   sessionId: string
   messageId?: string
-  additionalHeaders?: string
 }
 
-/**
- *
- * @param {DownloadSetupMessageInput} input - the input parameters
- * @throws {Error} - if the response status is not between 200 and 300
- */
 const downloadSetupMessage = async ({
-  serverURL,
+  serverUrl,
   sessionId,
   messageId,
-  additionalHeaders,
 }: DownloadSetupMessageInput): Promise<string> => {
   const headers: Record<string, string> = {
     'Content-Type': 'application/json',
@@ -25,40 +18,20 @@ const downloadSetupMessage = async ({
   if (messageId) {
     headers['message_id'] = messageId
   }
-  // this one is only temporary right now
-  // when we do dkls/schnorr reshare will need it
-  if (additionalHeaders) {
-    headers['message-id'] = additionalHeaders
-  }
-  const response = await fetch(`${serverURL}/setup-message/${sessionId}`, {
+  const response = await fetch(`${serverUrl}/setup-message/${sessionId}`, {
     headers,
   })
 
   await assertFetchResponse(response)
-  if (response.status < 200 || response.status > 299) {
-    throw new Error(
-      `failed to download setup message from relay server, status: ${response.status}`
-    )
-  }
+
   return response.text()
 }
 
-export const waitForSetupMessage = async ({
-  serverURL,
-  sessionId,
-  messageId,
-  additionalHeaders,
-}: DownloadSetupMessageInput): Promise<string> => {
-  const setupMessage = await retry({
-    func: () =>
-      downloadSetupMessage({
-        serverURL,
-        sessionId,
-        messageId,
-        additionalHeaders,
-      }),
+export const waitForSetupMessage = async (
+  input: DownloadSetupMessageInput
+): Promise<string> =>
+  retry({
+    func: () => downloadSetupMessage(input),
     attempts: 10,
     delay: 1000,
   })
-  return setupMessage
-}
