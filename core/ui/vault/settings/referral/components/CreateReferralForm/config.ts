d7fb0ceae696147698b51da60095a2c114218471
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-07-13 10:45:03 +0300

    feat progress with verify part 2

--- core/ui/vault/settings/referral/components/CreateReferralForm/config.ts
+++ core/ui/vault/settings/referral/components/CreateReferralForm/config.ts
@@ -6,6 +6,7 @@ export const referralSchema = z.object({
     .max(4, 'Max 4 characters')
     .nonempty('Validated Referral code is required'),
   expiration: z.number().min(1, 'Minimum 1 year').max(10, 'Maximum 10 years'),
+  referralFeeAmount: z.number().min(0.1, 'Minimum 0.1 RUNE'),
 })
 
-export type ReferralFormData = z.infer<typeof referralSchema>
+export type CreateReferralFormData = z.infer<typeof referralSchema>
