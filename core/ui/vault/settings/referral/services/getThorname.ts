5df9425e9e6fe3fcd9c201ba6cf4672bb34a7179
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-07-14 21:57:21 +0300

    feat: add new services part 2

--- core/ui/vault/settings/referral/services/getThorname.ts
+++ core/ui/vault/settings/referral/services/getThorname.ts
@@ -1,20 +1,4 @@
-import { thorchainNodeBaseUrl } from '../constants'
-
-export type ThorNameRaw = {
-  name: string
-  expire_block_height: string
-  owner: string
-  preferred_asset?: string
-  affiliate_collector_rune: string
-}
-
-export const getNameInfo = async (name: string): Promise<ThorNameRaw> => {
-  const res = await fetch(`${thorchainNodeBaseUrl}/thorname/${name}`)
-  if (!res.ok) {
-    throw new Error(`Failed to fetch: ${res.status}`)
-  }
-  return res.json()
-}
+import { thorchainNodeBaseUrl } from '../config'
 
 export const checkAvailability = async (name: string): Promise<boolean> => {
   try {
