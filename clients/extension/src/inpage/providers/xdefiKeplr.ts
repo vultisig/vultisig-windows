52ac49f8a3702802dd3142bf184b790a376590f0
hippocampus-web3 <hippocampus.web3@proton.me>
2025-07-17 17:50:48 +0200

    Add feedback on signAmino fail

--- clients/extension/src/inpage/providers/xdefiKeplr.ts
+++ clients/extension/src/inpage/providers/xdefiKeplr.ts
@@ -194,7 +194,7 @@ export class XDEFIKeplrProvider extends Keplr {
     signDoc: StdSignDoc,
     _signOptions?: KeplrSignOptions
   ): Promise<AminoSignResponse> {
-    return new Promise<AminoSignResponse>(resolve => {
+    return new Promise<AminoSignResponse>((resolve, reject) => {
       const txDetails = signDoc.msgs.map(msg => {
         if (msg.type === CosmosMsgType.MSG_SEND) {
           return { txType: 'Keplr', ...msg.value } as TransactionType.Keplr
@@ -209,6 +209,7 @@ export class XDEFIKeplrProvider extends Keplr {
         .then(result => {
           resolve(result as any)
         })
+        .catch(reject)
     })
   }
   async signDirect(
