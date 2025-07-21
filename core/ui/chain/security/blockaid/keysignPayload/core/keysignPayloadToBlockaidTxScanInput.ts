d4896a4f3b7637b74ef0f7ef49379e52c040ddee
rcoderdev <radzionch@gmail.com>
2025-07-21 10:57:27 +0400

    refactor: remove outdated typecheck rule and add new rules for TypeScript codebase

--- core/ui/chain/security/blockaid/keysignPayload/core/keysignPayloadToBlockaidTxScanInput.ts
+++ core/ui/chain/security/blockaid/keysignPayload/core/keysignPayloadToBlockaidTxScanInput.ts
@@ -7,26 +7,26 @@ import { getKeysignCoin } from '@core/mpc/keysign/utils/getKeysignCoin'
 import { KeysignPayload } from '@core/mpc/types/vultisig/keysign/v1/keysign_message_pb'
 import { isOneOf } from '@lib/utils/array/isOneOf'
 import { bigIntToHex } from '@lib/utils/bigint/bigIntToHex'
+import { encodeFunctionData, erc20Abi } from 'viem'
 
 export const keysignPayloadToBlockaidTxScanInput = (
-  value: KeysignPayload
+  payload: KeysignPayload
 ): BlockaidTxScanInput | null => {
-  const coin = getKeysignCoin(value)
-  const chain = getKeysignChain(value)
+  const coin = getKeysignCoin(payload)
+  const chain = getKeysignChain(payload)
 
   if (!isOneOf(chain, Object.values(EvmChain))) {
     return null
   }
 
-  const swapPayload = getKeysignSwapPayload(value)
+  const swapPayload = getKeysignSwapPayload(payload)
 
   if (swapPayload) {
     return null
   }
 
-  if (!isFeeCoin(coin)) {
-    return null
-  }
+  const amount = BigInt(payload.toAmount)
+  const receiver = payload.toAddress as `0x${string}`
 
   return {
     chain,
@@ -35,9 +35,15 @@ export const keysignPayloadToBlockaidTxScanInput = (
       params: [
         {
           from: coin.address,
-          to: value.toAddress,
-          value: `0x${bigIntToHex(BigInt(value.toAmount))}`,
-          data: value.memo || '0x',
+          to: isFeeCoin(coin) ? receiver : coin.id,
+          value: `0x${isFeeCoin(coin) ? bigIntToHex(amount) : 0}`,
+          data: isFeeCoin(coin)
+            ? payload.memo || '0x'
+            : encodeFunctionData({
+                abi: erc20Abi,
+                functionName: 'transfer',
+                args: [receiver, amount],
+              }),
         },
       ],
     },
