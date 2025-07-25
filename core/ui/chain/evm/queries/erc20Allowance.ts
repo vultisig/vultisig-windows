91f254a9df22d54046f4fac96bfcff042931b46a
rcoderdev <radzionch@gmail.com>
2025-07-25 10:42:35 +0400

    refactor: streamline ERC20 allowance queries and introduce usePotentialQuery hook
    - Refactored the `useErc20Allowance` hook to utilize a new `getErc20AllowanceQuery` function for improved readability and maintainability.
    - Updated the `useErc20ApprovePayloadQuery` to leverage the new `usePotentialQuery` hook, enhancing its query handling logic.
    - Introduced `usePotentialQuery` to manage conditional queries based on input, simplifying the query structure across the codebase.

--- core/ui/chain/evm/queries/erc20Allowance.ts
+++ core/ui/chain/evm/queries/erc20Allowance.ts
@@ -4,9 +4,11 @@ import {
 } from '@core/chain/chains/evm/erc20/getErc20Allowance'
 import { useQuery } from '@tanstack/react-query'
 
+export const getErc20AllowanceQuery = (input: GetErc20AllowanceInput) => ({
+  queryKey: ['erc20Allowance', input],
+  queryFn: () => getErc20Allowance(input),
+})
+
 export const useErc20Allowance = (input: GetErc20AllowanceInput) => {
-  return useQuery({
-    queryKey: ['erc20Allowance', input],
-    queryFn: () => getErc20Allowance(input),
-  })
+  return useQuery(getErc20AllowanceQuery(input))
 }
