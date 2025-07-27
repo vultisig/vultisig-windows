335be5de01f8d33732df962fd99f85dbba6fed1d
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-07-27 16:21:01 +0330

    Fix lint issues

--- core/chain/tx/fee/format/formatFee.ts
+++ core/chain/tx/fee/format/formatFee.ts
@@ -1,17 +1,11 @@
 import { fromChainAmount } from '@core/chain/amount/fromChainAmount'
 import { Chain, EvmChain } from '@core/chain/Chain'
-import { cosmosGasLimitRecord } from '@core/chain/chains/cosmos/cosmosGasLimitRecord'
-import { polkadotConfig } from '@core/chain/chains/polkadot/config'
-import { solanaConfig } from '@core/chain/chains/solana/solanaConfig'
-import { tonConfig } from '@core/chain/chains/ton/config'
 import { chainFeeCoin } from '@core/chain/coin/chainFeeCoin'
 import { gwei } from '@core/chain/tx/fee/evm/gwei'
-import { rippleTxFee } from '@core/chain/tx/fee/ripple'
+import { getFeeAmount } from '@core/chain/tx/fee/getFeeAmount'
 import { KeysignChainSpecific } from '@core/mpc/keysign/chainSpecific/KeysignChainSpecific'
 import { isOneOf } from '@lib/utils/array/isOneOf'
 import { formatTokenAmount } from '@lib/utils/formatTokenAmount'
-import { matchDiscriminatedUnion } from '@lib/utils/matchDiscriminatedUnion'
-import { getFeeAmount } from '@core/chain/tx/fee/getFeeAmount'
 
 import { getFeeUnit } from './feeUnit'
 
