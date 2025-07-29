e47bab3ff0615efa02e5ae977250072399159415
hippocampus-web3 <hippocampus.web3@proton.me>
2025-07-29 17:31:07 +0200

    RUJI denoms

--- core/chain/coin/knownTokens/cosmos.ts
+++ core/chain/coin/knownTokens/cosmos.ts
@@ -181,6 +181,135 @@ export const knownCosmosTokens: Record<
       priceProviderId: 'ripple',
       hiddenFromUI: true,
     },
+    'x/staking-x/ruji': {
+      ticker: 'sRUJI',
+      logo: 'ruji',
+      decimals: 8,
+      priceProviderId: 'rujira',
+      hiddenFromUI: true,
+    },
+    'thor.nami': {
+      ticker: 'NAMI',
+      logo: 'nami.png',
+      decimals: 8,
+      priceProviderId: 'nami-protocol',
+      hiddenFromUI: true,
+    },
+    'thor.lqdy': {
+      ticker: 'LQDY',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'thor.auto': {
+      ticker: 'AUTO',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/nami-index-fixed-thor1yqf5spdv8c4088zmvqsg32eq63fzepsjvntahdk0ek0yjnkt3qdqftp3lc-rcpt':
+      {
+        ticker: 'RJI',
+        logo: 'nami.png', // TODO: Incorrect logo and missing price provider
+        decimals: 8,
+        hiddenFromUI: true,
+      },
+    'x/staking-thor.nami': {
+      ticker: 'sNAMI',
+      logo: 'nami.png', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-gaia-atom-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'ATOM/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-avax-avax-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'AVAX/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-btc-btc-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'BTC/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-bch-bch-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'BCH/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-bnb-bnb-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'BNB/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-doge-doge-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'DOGE/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-eth-eth-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'ETH/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-ltc-ltc-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'LTC/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-xrp-xrp-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'XRP/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-thor.lqdy-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'LQDY/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-thor.auto-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'AUTO/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-thor.nami-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'NAMI/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-x/ruji-eth-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+      ticker: 'RUJI/USDC.ETH',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-eth-eth-btc-btc': {
+      ticker: 'ETH.ETH/BTC.BTC',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
+    'x/bow-xyk-thor.lqdy-btc-btc': {
+      ticker: 'LQDY/BTC.BTC',
+      logo: 'ruji', // TODO: Incorrect logo and missing price provider
+      decimals: 8,
+      hiddenFromUI: true,
+    },
     ...kujiraCoinsOnThorChain,
   },
   [Chain.Cosmos]: {
