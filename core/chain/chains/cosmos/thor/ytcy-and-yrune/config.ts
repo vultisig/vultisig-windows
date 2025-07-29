911e5e8a28f136a7ebb0479504d59d136654c394
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-07-29 21:41:36 +0300

    feat: add constants

---
+++ core/chain/chains/cosmos/thor/ytcy-and-yrune/config.ts
@@ -0,0 +1,5 @@
+export const yRUNEContract = 'sthor1qv8n2kz3j4h7w9v5k5w2c5g9q2v7e6xw5g0n6y'
+export const yTCYContract = 'sthor1zv9m3kq2v6n8w4x8k8w3c6g0q3v8e7xw9g1n7z'
+export const affiliateContract =
+  'sthor1m4pk0kyc5xln5uznsur0d6frlvteghs0v6fyt8pw4vxfhfgskzts2g8ln6'
+export const affiliateAddress = 'thor1svfwxevnxtm4ltnw92hrqpqk4vzuzw9a4jzy04'
