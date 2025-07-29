064552d3238bc07b56188a9c041a58dd08a8d45d
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-07-29 07:56:57 +0330

    Add skipBroadcast to SignAmino & SignDirect (#2157)
    * Add skipBroadcast to SignAmino & SignDirect
    
    * fix dependency
    
    ---------
    
    Co-authored-by: rcoderdev <radzionch@gmail.com>

--- core/chain/tx/execute/sui.ts
+++ core/chain/tx/execute/sui.ts
@@ -4,13 +4,18 @@ import { TW } from '@trustwallet/wallet-core'
 
 import { ExecuteTxResolver } from './ExecuteTxResolver'
 
-export const executeSuiTx: ExecuteTxResolver = async ({ compiledTx }) => {
+export const executeSuiTx: ExecuteTxResolver = async ({
+  compiledTx,
+  skipBroadcast,
+}) => {
   const {
     unsignedTx,
     errorMessage: suiErrorMessage,
     signature: compiledSignature,
   } = TW.Sui.Proto.SigningOutput.decode(compiledTx)
-
+  if (skipBroadcast) {
+    throw 'Sui transactions cannot be executed without broadcasting'
+  }
   assertErrorMessage(suiErrorMessage)
 
   const rpcClient = getSuiClient()
