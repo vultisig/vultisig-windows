54c3eca16c44aa9d706e7fce322fcc408d74d341
rcoderdev <radzionch@gmail.com>
2025-07-30 15:45:14 +0400

    Update Polkadot dependencies to version 16.4.3 in package.json and yarn.lock. Clean up unused scripts in the Wails runtime package.json. Refactor Polkadot transaction hash generation by removing unnecessary hex prefix handling for improved clarity and maintainability.

--- core/chain/tx/hash/polkadot.ts
+++ core/chain/tx/hash/polkadot.ts
@@ -1,5 +1,4 @@
 import { OtherChain } from '@core/chain/Chain'
-import { ensureHexPrefix } from '@lib/utils/hex/ensureHexPrefix'
 
 import { getPolkadotClient } from '../../chains/polkadot/client'
 import { TxHashResolver } from './TxHashResolver'
@@ -10,13 +9,9 @@ export const getPolkadotTxHash: TxHashResolver<OtherChain.Polkadot> = async ({
   const client = await getPolkadotClient()
 
   return client
-    .createType(
-      'Extrinsic',
-      ensureHexPrefix(Buffer.from(encoded).toString('hex')),
-      {
-        isSigned: true,
-        version: 4,
-      }
-    )
+    .createType('Extrinsic', encoded, {
+      isSigned: true,
+      version: 4,
+    })
     .hash.toHex()
 }
