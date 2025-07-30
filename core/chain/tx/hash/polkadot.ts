47973e4c98f634407b0f3b160cc4a032e1834376
rcoderdev <radzionch@gmail.com>
2025-07-30 15:12:38 +0400

    Enhance Polkadot transaction hash generation by integrating the Polkadot client for asynchronous operations. Updated the `getPolkadotTxHash` function to improve accuracy and maintainability.

--- core/chain/tx/hash/polkadot.ts
+++ core/chain/tx/hash/polkadot.ts
@@ -1,14 +1,17 @@
 import { OtherChain } from '@core/chain/Chain'
-import { TypeRegistry } from '@polkadot/types'
 
+import { getPolkadotClient } from '../../chains/polkadot/client'
 import { TxHashResolver } from './TxHashResolver'
 
-export const getPolkadotTxHash: TxHashResolver<OtherChain.Polkadot> = ({
+export const getPolkadotTxHash: TxHashResolver<OtherChain.Polkadot> = async ({
   encoded,
-}) =>
-  new TypeRegistry()
+}) => {
+  const client = await getPolkadotClient()
+
+  return client
     .createType('Extrinsic', Buffer.from(encoded).toString('hex'), {
       isSigned: true,
       version: 4,
     })
     .hash.toHex()
+}
