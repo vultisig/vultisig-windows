7874a083e1399f4d347c44334ac7f09907ee1558
rcoderdev <radzionch@gmail.com>
2025-07-30 14:42:23 +0400

    Refactor Polkadot transaction hash generation to eliminate unnecessary variable assignment. Directly instantiate `TypeRegistry` within the function, enhancing clarity and maintainability.

--- core/chain/tx/hash/polkadot.ts
+++ core/chain/tx/hash/polkadot.ts
@@ -3,12 +3,10 @@ import { TypeRegistry } from '@polkadot/types'
 
 import { GetTxHashResolver } from './GetTxHashResolver'
 
-const registry = new TypeRegistry()
-
 export const getPolkadotTxHash: GetTxHashResolver<OtherChain.Polkadot> = ({
   encoded,
 }) =>
-  registry
+  new TypeRegistry()
     .createType('Extrinsic', Buffer.from(encoded).toString('hex'), {
       isSigned: true,
       version: 4,
