10a89ebbcb31ff5bcf1d058d6fca5f09060da074
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-07-31 17:51:31 +0300

    fix: logic in hook

--- core/ui/vault/deposit/DepositForm/ActionSpecific/hooks/useSelectedCoinCorrector.ts
+++ core/ui/vault/deposit/DepositForm/ActionSpecific/hooks/useSelectedCoinCorrector.ts
@@ -1,4 +1,8 @@
 import { Chain } from '@core/chain/Chain'
+import {
+  yRUNEReceiptDenom,
+  yTCYReceiptDenom,
+} from '@core/chain/chains/cosmos/thor/ytcy-and-yrune/config'
 import { chainFeeCoin } from '@core/chain/coin/chainFeeCoin'
 import { Coin } from '@core/chain/coin/Coin'
 import { knownCosmosTokens } from '@core/chain/coin/knownTokens/cosmos'
@@ -8,8 +12,8 @@ import { useCurrentVaultCoins } from '../../../../state/currentVaultCoins'
 import { ChainAction } from '../../../ChainAction'
 import { useDepositFormHandlers } from '../../../providers/DepositFormHandlersProvider'
 
-// Need to add these coins on the fly
-const makeCoinFromKnown = (denom: string): Coin => ({
+/** @tony: Required by product to build a Coin object from `knownCosmosTokens` on demand */
+const makeCoin = (denom: string): Coin => ({
   chain: Chain.THORChain,
   id: denom,
   ...knownCosmosTokens['THORChain'][denom],
@@ -18,34 +22,58 @@ const makeCoinFromKnown = (denom: string): Coin => ({
 export const useSelectedCoinCorrector = (action: ChainAction) => {
   const [{ watch, setValue }] = useDepositFormHandlers()
   const selectedCoin = watch('selectedCoin') as Coin | null
-
   const vaultCoins = useCurrentVaultCoins()
 
-  const { runeInVault, tcyInVault } = useMemo(() => {
-    const rune = vaultCoins.find(
-      c => c.chain === Chain.THORChain && c.ticker === 'RUNE'
-    )
-    const tcy = vaultCoins.find(
-      c => c.chain === Chain.THORChain && c.ticker === 'TCY'
-    )
-    return { runeInVault: rune, tcyInVault: tcy }
+  const { runeInVault, tcyInVault, yRuneInVault, yTcyInVault } = useMemo(() => {
+    const finder = (ticker: string) =>
+      vaultCoins.find(c => c.chain === Chain.THORChain && c.ticker === ticker)
+
+    return {
+      runeInVault: finder('RUNE'),
+      tcyInVault: finder('TCY'),
+      yRuneInVault: vaultCoins.find(c => c.id === yRUNEReceiptDenom),
+      yTcyInVault: vaultCoins.find(c => c.id === yTCYReceiptDenom),
+    }
   }, [vaultCoins])
 
   useEffect(() => {
     if (action === 'deposit_yRune') {
       const target = runeInVault ?? chainFeeCoin.THORChain
-      if (selectedCoin?.ticker !== target.ticker) {
+      if (selectedCoin?.id !== target.id) {
         setValue('selectedCoin', target, { shouldValidate: true })
       }
       return
     }
 
     if (action === 'deposit_yTcy') {
-      const target = tcyInVault ?? makeCoinFromKnown('tcy')
+      const target = tcyInVault ?? makeCoin('tcy')
+      if (selectedCoin?.id !== target.id) {
+        setValue('selectedCoin', target, { shouldValidate: true })
+      }
+      return
+    }
 
-      if (selectedCoin?.ticker !== target.ticker) {
+    if (action === 'withdraw_yRune') {
+      const target = yRuneInVault ?? makeCoin(yRUNEReceiptDenom)
+      if (selectedCoin?.id !== target.id) {
+        setValue('selectedCoin', target, { shouldValidate: true })
+      }
+      return
+    }
+
+    if (action === 'withdraw_yTcy') {
+      const target = yTcyInVault ?? makeCoin(yTCYReceiptDenom)
+      if (selectedCoin?.id !== target.id) {
         setValue('selectedCoin', target, { shouldValidate: true })
       }
     }
-  }, [action, selectedCoin, runeInVault, tcyInVault, setValue])
+  }, [
+    action,
+    selectedCoin,
+    runeInVault,
+    tcyInVault,
+    yRuneInVault,
+    yTcyInVault,
+    setValue,
+  ])
 }
