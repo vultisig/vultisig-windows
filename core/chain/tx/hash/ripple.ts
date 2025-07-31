fbab9416d7a02ba0a4370c907eb46b17562e22d5
rcoderdev <radzionch@gmail.com>
2025-07-31 13:27:44 +0400

    Refactor Ripple transaction hash generation to utilize the XRPL library for hashing. This change simplifies the function by directly returning the hash of the signed transaction, enhancing clarity and maintainability.

--- core/chain/tx/hash/ripple.ts
+++ core/chain/tx/hash/ripple.ts
@@ -1,12 +1,8 @@
 import { OtherChain } from '@core/chain/Chain'
-import { sha512 } from 'ethers'
+import { hashes } from 'xrpl'
 
 import { TxHashResolver } from './TxHashResolver'
 
 export const getRippleTxHash: TxHashResolver<OtherChain.Ripple> = ({
   encoded,
-}) => {
-  const fullHash = sha512(encoded)
-  const first256 = fullHash.slice(0, 32)
-  return Buffer.from(first256).toString('hex').toUpperCase()
-}
+}) => hashes.hashSignedTx(Buffer.from(encoded).toString('hex'))
