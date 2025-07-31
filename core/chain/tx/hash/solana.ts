1950a61796e778e3edb379b5db598986da9c8a51
rcoderdev <radzionch@gmail.com>
2025-07-31 12:40:24 +0400

    Refactor Solana transaction hash generation to directly return the signature from the first element of the signatures array. This change enhances clarity and maintainability by simplifying the function structure.

--- core/chain/tx/hash/solana.ts
+++ core/chain/tx/hash/solana.ts
@@ -1,11 +1,8 @@
-import { bs58 } from '@coral-xyz/anchor/dist/cjs/utils/bytes'
 import { OtherChain } from '@core/chain/Chain'
+import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
 
 import { TxHashResolver } from './TxHashResolver'
 
 export const getSolanaTxHash: TxHashResolver<OtherChain.Solana> = ({
-  encoded,
-}) =>
-  bs58.encode(
-    Uint8Array.prototype.slice.call(Buffer.from(encoded, 'base64'), 1, 65)
-  )
+  signatures,
+}) => shouldBePresent(signatures[0].signature)
