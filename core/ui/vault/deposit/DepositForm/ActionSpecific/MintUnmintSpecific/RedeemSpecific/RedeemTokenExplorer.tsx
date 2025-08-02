3914f1dac7dca3772f47815b9f2d46bcf63de034
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-08-02 11:03:05 +0300

    fix: redeem explorer

--- core/ui/vault/deposit/DepositForm/ActionSpecific/MintUnmintSpecific/RedeemSpecific/RedeemTokenExplorer.tsx
+++ core/ui/vault/deposit/DepositForm/ActionSpecific/MintUnmintSpecific/RedeemSpecific/RedeemTokenExplorer.tsx
@@ -1,10 +1,11 @@
-import { yAssetReceiptDenoms } from '@core/chain/chains/cosmos/thor/yAssets/config'
+import { yieldBearingAssetsIds } from '@core/chain/chains/cosmos/thor/yield-bearing-tokens/config'
+import { yieldBearingAssetsReceiptDenoms } from '@core/chain/chains/cosmos/thor/yield-bearing-tokens/config'
 import { Coin } from '@core/chain/coin/Coin'
 import { FormData } from '@core/ui/vault/deposit/DepositForm'
 import { DepositActionOption } from '@core/ui/vault/deposit/DepositForm/DepositActionOption'
 import { VStack } from '@lib/ui/layout/Stack'
 import { Modal } from '@lib/ui/modal'
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 import { UseFormSetValue } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 
@@ -25,17 +26,21 @@ export const RedeemTokenExplorer: FC<Props> = ({
 }) => {
   const { t } = useTranslation()
   const coins = useCurrentVaultCoins()
-  const yRUNE =
-    coins.find(c => c.id === yAssetReceiptDenoms['yRUNE']) ??
-    createMintUnmintCoin(yAssetReceiptDenoms['yRUNE'])
-  const yTCY =
-    coins.find(c => c.id === yAssetReceiptDenoms['yTCY']) ??
-    createMintUnmintCoin(yAssetReceiptDenoms['yTCY'])
+  const coinById = useMemo(() => new Map(coins.map(c => [c.id, c])), [coins])
+
+  const tokens = useMemo(
+    () =>
+      yieldBearingAssetsIds.map(asset => {
+        const denom = yieldBearingAssetsReceiptDenoms[asset]
+        return coinById.get(denom) ?? createMintUnmintCoin(denom)
+      }),
+    [coinById]
+  )
 
   return (
     <Modal onClose={onClose} title={t('select_token')}>
       <VStack gap={20}>
-        {[yRUNE, yTCY].map((token, index) => {
+        {tokens.map((token, index) => {
           return (
             <DepositActionOption
               key={index}
