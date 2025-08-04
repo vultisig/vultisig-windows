441dbadbcc3e01967fc46a6647c24ce5fdcf12bc
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-08-04 13:26:16 +0300

    Fix Bug with Unstake (#2204)
    * fix: unstake crash
    
    * fix: unstake crash
    
    * chore: remove logs

--- core/ui/vault/deposit/hooks/useDepositCoinCorrector.ts
+++ core/ui/vault/deposit/hooks/useDepositCoinCorrector.ts
@@ -4,17 +4,36 @@ import { useEffect } from 'react'
 import { useCoreNavigate } from '../../../navigation/hooks/useCoreNavigate'
 import { useCurrentVaultCoins } from '../../state/currentVaultCoins'
 import { ChainAction } from '../ChainAction'
+import { stakeableAssetsTickers, StakeableAssetTicker } from '../config'
 import { useDepositFormHandlers } from '../providers/DepositFormHandlersProvider'
 
 export const useDepositCoinCorrector = (selectedDepositAction: ChainAction) => {
   const [{ watch, setValue }] = useDepositFormHandlers()
   const selectedCoin = watch('selectedCoin') as Coin | null
   const navigate = useCoreNavigate()
-
   const runeCoin = useCurrentVaultCoins().find(coin => coin?.ticker === 'RUNE')
+  const defaultStakeableAssetTicker = stakeableAssetsTickers[0]
+  const defaultStakeableAsset = useCurrentVaultCoins().find(
+    coin => coin?.ticker === defaultStakeableAssetTicker
+  )
 
   useEffect(() => {
     if (
+      selectedDepositAction === 'unstake' &&
+      (!selectedCoin?.ticker ||
+        !stakeableAssetsTickers.includes(
+          selectedCoin.ticker as StakeableAssetTicker
+        ))
+    ) {
+      if (!defaultStakeableAsset) {
+        navigate({ id: 'vault' })
+        return
+      }
+
+      setValue('selectedCoin', defaultStakeableAsset, {
+        shouldValidate: true,
+      })
+    } else if (
       selectedDepositAction === 'bond' &&
       selectedCoin?.ticker !== runeCoin?.ticker
     ) {
@@ -28,6 +47,7 @@ export const useDepositCoinCorrector = (selectedDepositAction: ChainAction) => {
       })
     }
   }, [
+    defaultStakeableAsset,
     navigate,
     runeCoin,
     selectedCoin?.ticker,
