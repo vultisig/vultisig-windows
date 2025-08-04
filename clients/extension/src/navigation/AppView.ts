3050ef43c40fe273fe73b8b44cdfcafedbd3b335
rcoderdev <radzionch@gmail.com>
2025-08-04 08:26:59 +0400

    Add TODO for moving popupApi to dedicated app

--- clients/extension/src/navigation/AppView.ts
+++ clients/extension/src/navigation/AppView.ts
@@ -10,6 +10,8 @@ export type AppView =
   | { id: 'vaultsTab' }
   | { id: 'transactionTab' }
   | { id: 'pluginTab' }
+  // Most likely popupApi should be in a dedicated app(Extension's HTML page) e.g. popup-api.html
+  // TODO: @rcoderdev move popupApi to a dedicated app
   | { id: 'popupApi'; state: { call: PopupApiCall<any> } }
 
 export type AppViewId = AppView['id']
