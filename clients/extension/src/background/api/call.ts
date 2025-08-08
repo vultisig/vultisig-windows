c266413cb072b247034c872bd6e4fdd6f9579953
rcoderdev <radzionch@gmail.com>
2025-08-08 08:52:50 +0400

    Refactor API call methods to streamline source validation
    - Removed obsolete source validation logic from both background and popup API call methods.
    - Simplified the callBackgroundApi and callPopupApi functions by directly utilizing getProviderSource.
    - Enhanced code clarity and maintainability by eliminating unnecessary checks for supported sources.

--- clients/extension/src/background/api/call.ts
+++ clients/extension/src/background/api/call.ts
@@ -1,23 +1,13 @@
 import { sendToBackground } from '@core/inpage-provider/bridge/inpage'
-import { getProviderSource } from '@core/inpage-provider/source'
-import { isOneOf } from '@lib/utils/array/isOneOf'
 import { Result } from '@lib/utils/types/Result'
 
 import { ExtensionApiMessage } from '../../api'
 import { BackgroundApiCall } from './communication/core'
 import { BackgroundApiInterface, BackgroundApiMethodName } from './interface'
 
-const backgroundApiSupportedSources = ['inpage']
-
 export const callBackgroundApi = async <M extends BackgroundApiMethodName>(
   call: BackgroundApiCall<M>
 ): Promise<BackgroundApiInterface[M]['output']> => {
-  const source = getProviderSource()
-
-  if (!isOneOf(source, backgroundApiSupportedSources)) {
-    throw new Error(`Background API is not supported in ${source}`)
-  }
-
   const message: ExtensionApiMessage = {
     background: {
       call,
