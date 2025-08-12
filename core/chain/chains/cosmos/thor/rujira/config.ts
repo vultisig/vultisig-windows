6be154d10034ab98d7aaca93975ffdaefae1b03f
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-08-12 23:28:49 +0300

    fix: rujira config for staking

--- core/chain/chains/cosmos/thor/rujira/config.ts
+++ core/chain/chains/cosmos/thor/rujira/config.ts
@@ -2,7 +2,7 @@ export const rujiraGraphQlEndpoint = 'https://api.rujira.network/api/graphql'
 
 export const rujiraStakingConfig = {
   contract: 'thor13g83nn5ef4qzqeafp0508dnvkvm0zqr3sj7eefcn5umu65gqluusrml5cr',
-  bondDenom: 'ruji',
+  bondDenom: 'x/ruji',
   revenueDenom: 'uusdc',
   revenueDecimals: 6,
 } as const
