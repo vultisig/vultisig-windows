3cfeedd790a3830728f172908c399d8a6eae97d3
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-08-13 23:07:03 +0300

    yRUNE and yTCY (#2272)
    * feat: progress with ytcy and yrune
    
    * chore: fix types
    
    * fix: use depoyit keysign payload
    
    * fix: adust coin schema
    
    * fix: memos
    
    * chore: remove log

--- core/chain/chains/cosmos/thor/yield-bearing-tokens/yAssetsOnThorChain.ts
+++ core/chain/chains/cosmos/thor/yield-bearing-tokens/yAssetsOnThorChain.ts
@@ -3,13 +3,13 @@ import { yieldBearingAssetsReceiptDenoms } from './config'
 export const yieldBearingThorChainTokens = {
   [yieldBearingAssetsReceiptDenoms.yRUNE]: {
     ticker: 'yRUNE',
-    logo: 'ruji',
+    logo: 'yrune.png',
     decimals: 8,
     priceProviderId: 'thorchain',
   },
   [yieldBearingAssetsReceiptDenoms.yTCY]: {
     ticker: 'yTCY',
-    logo: 'tcy.png',
+    logo: 'ytcy.png',
     decimals: 8,
     priceProviderId: 'tcy',
   },
