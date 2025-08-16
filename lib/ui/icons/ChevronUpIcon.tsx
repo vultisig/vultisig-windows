033a272f306ea0d482bd7b475881747f79f1b8aa
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-08-16 17:30:29 +0300

    feat: add icon

---
+++ lib/ui/icons/ChevronUpIcon.tsx
@@ -0,0 +1,16 @@
+export const ChevronUpIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 12 12"
+    fill="none"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="square"
+      strokeWidth={1.5}
+      d="m2 7.5 4-4 4 4"
+    />
+  </svg>
+)
