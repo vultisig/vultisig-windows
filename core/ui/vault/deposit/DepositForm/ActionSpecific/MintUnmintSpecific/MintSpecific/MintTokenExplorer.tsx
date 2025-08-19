ea57369e1d74c11c08ddee94ee78f85d858b0f6f
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-08-19 20:56:59 +0300

    Improvements for yTCY and yRUNE (#2299)
    * feat: mint and redeem improvements
    
    * fix: scripts file
    
    * feat: add validation for unmerge ruji
    
    * feat: allocate component for unmerge ruji
    
    * feat: allocate component for unmerge ruji

--- core/ui/vault/deposit/DepositForm/ActionSpecific/MintUnmintSpecific/MintSpecific/MintTokenExplorer.tsx
+++ core/ui/vault/deposit/DepositForm/ActionSpecific/MintUnmintSpecific/MintSpecific/MintTokenExplorer.tsx
@@ -1,42 +1,29 @@
-import { Chain } from '@core/chain/Chain'
-import { chainFeeCoin } from '@core/chain/coin/chainFeeCoin'
 import { Coin } from '@core/chain/coin/Coin'
-import { FormData } from '@core/ui/vault/deposit/DepositForm'
 import { DepositActionOption } from '@core/ui/vault/deposit/DepositForm/DepositActionOption'
 import { VStack } from '@lib/ui/layout/Stack'
 import { Modal } from '@lib/ui/modal'
 import { FC } from 'react'
-import { UseFormSetValue } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
-
-import { useCurrentVaultCoin } from '../../../../../state/currentVaultCoins'
-import { createMintUnmintCoin } from '../utils'
 
 type Props = {
   activeOption?: Coin
   onOptionClick: (option: Coin) => void
   onClose: () => void
-  setValue: UseFormSetValue<FormData>
+  options: Coin[]
 }
 
 export const MintTokenExplorer: FC<Props> = ({
   onClose,
   onOptionClick,
   activeOption,
+  options,
 }) => {
   const { t } = useTranslation()
-  const tcyInVault = useCurrentVaultCoin({
-    id: 'tcy',
-    chain: Chain.THORChain,
-  })
 
   return (
     <Modal onClose={onClose} title={t('select_token')}>
       <VStack gap={20}>
-        {[
-          chainFeeCoin.THORChain,
-          tcyInVault ?? createMintUnmintCoin('tcy'),
-        ].map((token, index) => {
+        {options.map((token, index) => {
           return (
             <DepositActionOption
               key={index}
--- core/ui/vault/deposit/DepositForm/ActionSpecific/MintUnmintSpecific/RedeemSpecific/RedeemSpecific.tsx
+++ core/ui/vault/deposit/DepositForm/ActionSpecific/MintUnmintSpecific/RedeemSpecific/RedeemSpecific.tsx
@@ -1,19 +1,46 @@
+import {
+  yieldBearingAssetsIds,
+  yieldBearingAssetsReceiptDenoms,
+} from '@core/chain/chains/cosmos/thor/yield-bearing-tokens/config'
 import { Coin } from '@core/chain/coin/Coin'
 import { Opener } from '@lib/ui/base/Opener'
 import { ChevronRightIcon } from '@lib/ui/icons/ChevronRightIcon'
 import { IconWrapper } from '@lib/ui/icons/IconWrapper'
 import { HStack } from '@lib/ui/layout/Stack'
 import { Text } from '@lib/ui/text'
+import { useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
+import { useCurrentVaultCoins } from '../../../../../state/currentVaultCoins'
 import { useDepositFormHandlers } from '../../../../providers/DepositFormHandlersProvider'
 import { AssetRequiredLabel, Container } from '../../../DepositForm.styled'
+import { createMintUnmintCoin } from '../utils'
 import { RedeemTokenExplorer } from './RedeemTokenExplorer'
 
 export const RedeemSpecific = () => {
-  const [{ setValue, watch, getValues }] = useDepositFormHandlers()
+  const [{ setValue, getValues }] = useDepositFormHandlers()
+  const coins = useCurrentVaultCoins()
+
   const { t } = useTranslation()
   const selectedCoin = getValues('selectedCoin') as Coin | null
+  const coinById = useMemo(() => new Map(coins.map(c => [c.id, c])), [coins])
+
+  const tokens = useMemo(
+    () =>
+      yieldBearingAssetsIds.map(asset => {
+        const denom = yieldBearingAssetsReceiptDenoms[asset]
+        return coinById.get(denom) ?? createMintUnmintCoin(denom)
+      }),
+    [coinById]
+  )
+
+  useEffect(() => {
+    if (!tokens.some(token => token.ticker === selectedCoin?.ticker)) {
+      setValue('selectedCoin', tokens[0], {
+        shouldValidate: true,
+      })
+    }
+  }, [selectedCoin?.ticker, setValue, tokens])
 
   return (
     <Opener
@@ -36,8 +63,7 @@ export const RedeemSpecific = () => {
       )}
       renderContent={({ onClose }) => (
         <RedeemTokenExplorer
-          setValue={setValue}
-          activeOption={watch('selectedCoin')}
+          options={tokens}
           onOptionClick={token => {
             setValue('selectedCoin', token, {
               shouldValidate: true,
