4124d1e9d53990089ba8d124480859ff6770f44d
rcoderdev <radzionch@gmail.com>
2025-08-19 11:57:44 +0400

    Refactor chain handling and enhance background interface
    - Renamed `ChainsOfKind` to `ChainOfKind` for improved clarity in type definitions.
    - Introduced a new `getAppChain` resolver to retrieve application-specific chain data based on the provided chain kind.
    - Updated `BackgroundInterface` to include the new `getAppChain` method, enhancing the API's functionality.
    - Adjusted the `GetTxInputDataInput` type to utilize `ChainOfKind`, ensuring consistency across the codebase.

--- core/inpage-provider/background/interface.ts
+++ core/inpage-provider/background/interface.ts
@@ -1,11 +1,18 @@
+import { ChainOfKind } from '@core/chain/ChainKind'
 import { VaultExport } from '@core/ui/vault/export/core'
 import { Method } from '@lib/utils/types/Method'
 
 import { ActiveChainKind } from '../chain'
 
+type GetAppChainMethod<K extends ActiveChainKind = ActiveChainKind> = Method<
+  { chainKind: K },
+  ChainOfKind<K>
+>
+
 export type BackgroundInterface = {
   getVaults: Method<{}, VaultExport[]>
   getAppChainId: Method<{ chainKind: ActiveChainKind }, string>
+  getAppChain: GetAppChainMethod
 }
 
 export type BackgroundMethod = keyof BackgroundInterface
--- core/inpage-provider/background/resolvers/index.ts
+++ core/inpage-provider/background/resolvers/index.ts
@@ -1,6 +1,7 @@
 import { BackgroundMethod } from '@core/inpage-provider/background/interface'
 import { BackgroundResolver } from '@core/inpage-provider/background/resolver'
 
+import { getAppChain } from './getAppChain'
 import { getAppChainId } from './getAppChainId'
 import { getVaults } from './getVaults'
 
@@ -11,4 +12,5 @@ type BackgroundResolvers = {
 export const backgroundResolvers: BackgroundResolvers = {
   getVaults,
   getAppChainId,
+  getAppChain,
 }
