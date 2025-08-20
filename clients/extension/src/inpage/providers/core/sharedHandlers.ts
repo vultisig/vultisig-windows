4023e8cc1056d846930028d724512dd353f217fd
rcoderdev <radzionch@gmail.com>
2025-08-20 15:50:11 +0400

    Enhance getSharedHandlers to handle address retrieval for specific chains
    - Updated the getSharedHandlers function to include logic for returning a single address for certain chains, specifically Solana and IBC-enabled Cosmos chains.
    - Introduced the isOneOf utility to streamline the chain checking process, improving code clarity and maintainability.
    - Enhanced the overall request handling for accounts, ensuring more accurate responses based on the chain type.

--- clients/extension/src/inpage/providers/core/sharedHandlers.ts
+++ clients/extension/src/inpage/providers/core/sharedHandlers.ts
@@ -1,5 +1,6 @@
-import { Chain } from '@core/chain/Chain'
+import { Chain, IbcEnabledCosmosChain, OtherChain } from '@core/chain/Chain'
 import { callBackground } from '@core/inpage-provider/background'
+import { isOneOf } from '@lib/utils/array/isOneOf'
 import { attempt, withFallback } from '@lib/utils/attempt'
 
 // it's a temporary solution to handle shared requests before we completely migrate to the new communication layer
@@ -7,11 +8,23 @@ export const getSharedHandlers = (chain: Chain) => {
   return {
     get_accounts: async () =>
       withFallback(
-        attempt(async () => [
-          await callBackground({
+        attempt(async () => {
+          const address = await callBackground({
             getAddress: { chain },
-          }),
-        ]),
+          })
+
+          // for some reason get_accounts was returning a single address for some chains
+          // that's why there is this logic, but it worth double-checking that as it seems off
+          if (
+            isOneOf(chain, [
+              OtherChain.Solana,
+              ...Object.values(IbcEnabledCosmosChain),
+            ])
+          ) {
+            return address
+          }
+          return [address]
+        }),
         []
       ),
   } as const
