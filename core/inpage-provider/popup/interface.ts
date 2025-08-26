96502ce6818a6c62dbd857e5430d2f808f627d63
rcoderdev <radzionch@gmail.com>
2025-08-26 08:16:34 +0400

    feat: add signMessage resolver to PopupInterface
    - Introduced a new method, signMessage, to the PopupInterface for handling message signing requests.
    - Implemented the SignMessage resolver to provide a testing view for Popup authorization, displaying input and context data.
    - Updated PopupResolvers to include the new signMessage method, enhancing the functionality of the popup interface.

--- core/inpage-provider/popup/interface.ts
+++ core/inpage-provider/popup/interface.ts
@@ -1,3 +1,4 @@
+import { Chain } from '@core/chain/Chain'
 import { VaultAppSession } from '@core/extension/storage/appSessions'
 import { VaultExport } from '@core/ui/vault/export/core'
 import { Method } from '@lib/utils/types/Method'
@@ -6,6 +7,7 @@ export type PopupInterface = {
   grantVaultAccess: Method<{}, { appSession: VaultAppSession }>
   exportVaults: Method<{}, VaultExport[]>
   pluginReshare: Method<{ pluginId: string }, { joinUrl: string }>
+  signMessage: Method<{ message: string; chain: Chain }, string>
 }
 
 export type PopupMethod = keyof PopupInterface
