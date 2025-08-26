cfc86f6ce6f827d393546fce9a1ab333a961fc7d
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-08-26 20:52:50 +0330

    Fix lint

--- core/inpage-provider/background/resolvers/exportVault.ts
+++ core/inpage-provider/background/resolvers/exportVault.ts
@@ -1,11 +1,11 @@
 import { storage } from '@core/extension/storage'
 import { BackgroundResolver } from '@core/inpage-provider/background/resolver'
+import { hasServer } from '@core/mpc/devices/localPartyId'
 import { getVaultExportUid } from '@core/ui/vault/export/core/uid'
 import { getVaultId } from '@core/ui/vault/Vault'
 import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
 
 import { authorized } from '../middleware/authorized'
-import { hasServer } from '@core/mpc/devices/localPartyId'
 
 export const exportVault: BackgroundResolver<'exportVault'> = authorized(
   async () => {
