be1fe3d359bf7c20d0f7e2d70c9af5aba533c9df
rcoderdev <radzionch@gmail.com>
2025-08-26 14:14:45 +0400

    refactor: simplify popup call logic by removing bridge side check
    - Removed the check for bridge side in the callPopup function, streamlining the code.
    - Directly invoked callInpageProviderBridgeBackgroundAgent with the popup parameters, enhancing clarity and maintainability.

--- core/inpage-provider/popup/index.ts
+++ core/inpage-provider/popup/index.ts
@@ -3,20 +3,15 @@ import {
   PopupMethod,
 } from '@core/inpage-provider/popup/interface'
 import { PopupCall, PopupOptions } from '@core/inpage-provider/popup/resolver'
-import { getBridgeSide } from '@lib/extension/bridge/side'
 
 import { callInpageProviderBridgeBackgroundAgent } from '../bridge'
 
+// Call the popup from the inpage using this method.
+// For background context, use `callPopupFromBackground` and provide the appropriate `context`.
 export const callPopup = async <M extends PopupMethod>(
   call: PopupCall<M>,
   options: PopupOptions & { account?: string } = { closeOnFinish: true }
-): Promise<PopupInterface[M]['output']> => {
-  const side = getBridgeSide()
-  if (side !== 'inpage') {
-    throw new Error('Popup can only be called from inpage')
-  }
-
-  return callInpageProviderBridgeBackgroundAgent({
+): Promise<PopupInterface[M]['output']> =>
+  callInpageProviderBridgeBackgroundAgent({
     popup: { call, options },
   })
-}
