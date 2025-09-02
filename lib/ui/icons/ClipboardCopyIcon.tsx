1e779a805c1563788a6458b3077ba2ed6dec63fa
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-02 20:10:28 +0300

    Update Tx Complete Screen (#2352)
    * feat: improve txusccess screen
    
    * feat: add icon
    
    * feat: add icon
    
    * feat: improve list component
    
    * fix: inconsistency
    
    * fix: typo
    
    * feat: enhance button with attributes

---
+++ lib/ui/icons/ClipboardCopyIcon.tsx
@@ -0,0 +1,16 @@
+export const ClipboardCopyIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    fill="none"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5.334 2.666H4A1.333 1.333 0 0 0 2.667 4v9.333A1.333 1.333 0 0 0 4 14.666h8a1.333 1.333 0 0 0 1.334-1.333V12m-2.667-9.334H12A1.333 1.333 0 0 1 13.334 4v2.666M14 9.333H7.334m0 0L10 6.666M7.334 9.333 10 12M6 1.333h4c.368 0 .667.298.667.667v1.333A.667.667 0 0 1 10 4H6a.667.667 0 0 1-.666-.667V2c0-.369.298-.667.666-.667Z"
+    />
+  </svg>
+)
