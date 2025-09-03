48d4774ab7c58bc6112c22bde338cc9576a293a3
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-03 14:37:04 +0300

    fix: vault animations

--- core/ui/vault/backup/fast/BackupOverviewSlidesPartTwo/hooks/useBackupOverviewStepsAnimationsPartTwo.ts
+++ core/ui/vault/backup/fast/BackupOverviewSlidesPartTwo/hooks/useBackupOverviewStepsAnimationsPartTwo.ts
@@ -2,8 +2,6 @@ import { useRive } from '@rive-app/react-canvas'
 
 const stateMachineName = 'State Machine 1'
 
-// @antonio: done for UI purposes (we need to show as if this is the 3rd animation in sequence of 3)
-
 export const useBackupOverviewStepsAnimationsPartTwo = () => {
   const { RiveComponent, rive } = useRive({
     src: '/core/animations/fast-vault-backup-vault.riv',
