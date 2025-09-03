84af3c2aca7cf54ce06a300155b6a6c0b8e9aa1a
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-03 13:50:12 +0300

    feat: onboarding steps

--- core/ui/onboarding/components/OnboardingSteps.tsx
+++ core/ui/onboarding/components/OnboardingSteps.tsx
@@ -8,6 +8,7 @@ import { ChevronRightIcon } from '@lib/ui/icons/ChevronRightIcon'
 import { AnimatedVisibility } from '@lib/ui/layout/AnimatedVisibility'
 import { HStack, VStack } from '@lib/ui/layout/Stack'
 import { OnFinishProp } from '@lib/ui/props'
+import { mediaQuery } from '@lib/ui/responsive/mediaQuery'
 import { GradientText, Text } from '@lib/ui/text'
 import { match } from '@lib/utils/match'
 import { FC } from 'react'
@@ -28,93 +29,101 @@ export const OnboardingSteps: FC<OnFinishProp> = ({ onFinish }) => {
   const fontSize = 28
 
   return (
-    <StyledLayout maxWidth={400} fullSize>
-      <StyledHeader gap={16}>
-        <HStack justifyContent="space-between">
-          <StyledButton
-            icon={<ChevronLeftIcon fontSize={18} />}
-            kind="link"
-            onClick={!currentAnimation ? goBack : handlePrevAnimation}
-            size="sm"
-          >
-            {t('back')}
-          </StyledButton>
-          <StyledButton kind="link" onClick={onFinish} size="sm">
-            {t('skip')}
-          </StyledButton>
-        </HStack>
-        <MultistepProgressIndicator
-          markPreviousStepsAsCompleted
-          steps={animations.length}
-          value={animations.indexOf(currentAnimation) + 1}
-          variant="bars"
-        />
-      </StyledHeader>
-      <StyledContent alignItems="center">
-        <AnimationComponent />
-      </StyledContent>
-      <StyledFooter alignItems="center" gap={32}>
-        {match(currentAnimation, {
-          0: () => (
-            <AnimatedVisibility>
-              <Text as="div" size={fontSize} centerHorizontally>
-                {t('sayHelloTo')}{' '}
-                <GradientText as="span">{`${t('vaultShares')},`}</GradientText>{' '}
-                {t('yourNewRecoveryMethod')}
-              </Text>
-            </AnimatedVisibility>
-          ),
-          1: () => (
-            <AnimatedVisibility>
-              <Text as="div" size={fontSize} centerHorizontally>
-                {t('theyRe')}{' '}
-                <GradientText as="span">{t('splitIntoParts')}</GradientText>{' '}
-                {`${t('toIncreaseSecurity')},`}{' '}
-                <GradientText as="span">
-                  {t('removeSinglePointOfFailure')}
-                </GradientText>
-              </Text>
-            </AnimatedVisibility>
-          ),
-          2: () => (
-            <AnimatedVisibility>
-              <Text as="div" size={fontSize} centerHorizontally>
-                <GradientText as="span">{t('eachDevice')}</GradientText>{' '}
-                {t('inYourVaultHolds')}{' '}
-                <GradientText as="span">{t('oneVaultShare')}</GradientText>
-              </Text>
-            </AnimatedVisibility>
-          ),
-          3: () => (
-            <AnimatedVisibility>
-              <Text as="div" size={fontSize} centerHorizontally>
-                {t('recoverYourVault')}{' '}
-                <GradientText as="span">
-                  {t('deviceLostOrDamaged')}
-                </GradientText>
-              </Text>
-            </AnimatedVisibility>
-          ),
-          4: () => (
-            <AnimatedVisibility>
-              <Text as="div" size={28} centerHorizontally>
-                <GradientText as="span">
-                  {t('alwaysBackUpEachVaultShare')}
-                </GradientText>{' '}
-                {t('separatelyIna')}{' '}
-                <GradientText as="span">{t('differentLocation')}</GradientText>
-              </Text>
-            </AnimatedVisibility>
-          ),
-          5: () => (
-            <AnimatedVisibility>
-              <Text as="div" size={28} centerHorizontally>
-                {t('theseSharesCollaborate')}{' '}
-                <GradientText as="span">{t('unlockYourVault')}</GradientText>
-              </Text>
-            </AnimatedVisibility>
-          ),
-        })}
+    <StyledLayout fullSize justifyContent="space-between">
+      <VStack flexGrow>
+        <Header gap={16}>
+          <HStack justifyContent="space-between">
+            <StyledButton
+              icon={<ChevronLeftIcon fontSize={18} />}
+              kind="link"
+              onClick={!currentAnimation ? goBack : handlePrevAnimation}
+              size="sm"
+            >
+              {t('back')}
+            </StyledButton>
+            <StyledButton kind="link" onClick={onFinish} size="sm">
+              {t('skip')}
+            </StyledButton>
+          </HStack>
+          <MultistepProgressIndicator
+            markPreviousStepsAsCompleted
+            steps={animations.length}
+            value={animations.indexOf(currentAnimation) + 1}
+            variant="bars"
+          />
+        </Header>
+        <VStack justifyContent="center" flexGrow alignItems="center">
+          <AnimationWrapper>
+            <AnimationComponent />
+          </AnimationWrapper>
+        </VStack>
+      </VStack>
+      <Footer alignItems="center" gap={32}>
+        <AnimatedVisibility>
+          {match(currentAnimation, {
+            0: () => (
+              <>
+                <Text as="div" size={fontSize} centerHorizontally>
+                  {t('sayHelloTo')}{' '}
+                  <GradientText as="span">{`${t('vaultShares')},`}</GradientText>{' '}
+                  {t('yourNewRecoveryMethod')}
+                </Text>
+              </>
+            ),
+            1: () => (
+              <>
+                <Text as="div" size={fontSize} centerHorizontally>
+                  {t('theyRe')}{' '}
+                  <GradientText as="span">{t('splitIntoParts')}</GradientText>{' '}
+                  {`${t('toIncreaseSecurity')},`}{' '}
+                  <GradientText as="span">
+                    {t('removeSinglePointOfFailure')}
+                  </GradientText>
+                </Text>
+              </>
+            ),
+            2: () => (
+              <>
+                <Text as="div" size={fontSize} centerHorizontally>
+                  <GradientText as="span">{t('eachDevice')}</GradientText>{' '}
+                  {t('inYourVaultHolds')}{' '}
+                  <GradientText as="span">{t('oneVaultShare')}</GradientText>
+                </Text>
+              </>
+            ),
+            3: () => (
+              <>
+                <Text as="div" size={fontSize} centerHorizontally>
+                  {t('recoverYourVault')}{' '}
+                  <GradientText as="span">
+                    {t('deviceLostOrDamaged')}
+                  </GradientText>
+                </Text>
+              </>
+            ),
+            4: () => (
+              <>
+                <Text as="div" size={28} centerHorizontally>
+                  <GradientText as="span">
+                    {t('alwaysBackUpEachVaultShare')}
+                  </GradientText>{' '}
+                  {t('separatelyIna')}{' '}
+                  <GradientText as="span">
+                    {t('differentLocation')}
+                  </GradientText>
+                </Text>
+              </>
+            ),
+            5: () => (
+              <>
+                <Text as="div" size={28} centerHorizontally>
+                  {t('theseSharesCollaborate')}{' '}
+                  <GradientText as="span">{t('unlockYourVault')}</GradientText>
+                </Text>
+              </>
+            ),
+          })}
+        </AnimatedVisibility>
         <IconButton
           disabled={isLoading}
           kind="primary"
@@ -127,7 +136,7 @@ export const OnboardingSteps: FC<OnFinishProp> = ({ onFinish }) => {
         >
           <ChevronRightIcon />
         </IconButton>
-      </StyledFooter>
+      </Footer>
     </StyledLayout>
   )
 }
@@ -137,22 +146,45 @@ const StyledButton = styled(Button)`
   width: auto;
 `
 
-const StyledContent = styled(VStack)`
-  height: 400px;
-  padding: 0;
+const AnimationWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  flex: 1;
+
+  @media ${mediaQuery.mobileDeviceAndDown} {
+    max-height: 380px;
+  }
+
+  @media ${mediaQuery.tabletDeviceAndUp} {
+    max-height: 650px;
+  }
+
+  @media ${mediaQuery.desktopDeviceAndUp} {
+    max-height: 750px;
+  }
 `
 
-const StyledFooter = styled(VStack)`
-  bottom: 0px;
+const Footer = styled(VStack)`
   padding: 0 24px 36px;
-  position: absolute;
 `
 
-const StyledHeader = styled(VStack)`
+const Header = styled(VStack)`
   padding: 36px 24px 0;
 `
 
 const StyledLayout = styled(VStack)`
   margin: 0 auto;
   position: relative;
+
+  @media ${mediaQuery.mobileDeviceAndDown} {
+    width: calc(100% - 48px);
+  }
+
+  @media ${mediaQuery.tabletDeviceAndUp} {
+    width: 750px;
+  }
+
+  @media ${mediaQuery.desktopDeviceAndUp} {
+    width: 800px;
+  }
 `
