92150e989fdc9fb0af95d016776b3370a7a82295
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-06 19:01:04 +0300

    feat: progress on adjusting the forms

--- core/ui/vault/deposit/DepositForm/ActionSpecific/StakeSpecific/UnstakeSpecific/UnstakeSpecific.tsx
+++ core/ui/vault/deposit/DepositForm/ActionSpecific/StakeSpecific/UnstakeSpecific/UnstakeSpecific.tsx
@@ -1,18 +1,63 @@
 import { Match } from '@lib/ui/base/Match'
+import { Opener } from '@lib/ui/base/Opener'
+import { ChevronRightIcon } from '@lib/ui/icons/ChevronRightIcon'
+import { IconWrapper } from '@lib/ui/icons/IconWrapper'
+import { HStack, VStack } from '@lib/ui/layout/Stack'
+import { Text } from '@lib/ui/text'
+import { useTranslation } from 'react-i18next'
 
 import { StakeableAssetTicker, StakeableChain } from '../../../../config'
 import { useDepositCoin } from '../../../../providers/DepositCoinProvider'
 import { useDepositFormHandlers } from '../../../../providers/DepositFormHandlersProvider'
-import { StakeSpecific } from '../StakeSpecific'
+import { AssetRequiredLabel, Container } from '../../../DepositForm.styled'
+import { StakeTokenExplorer } from '../StakeTokenExplorer'
 import { UnstakeTCYSpecific } from './UnstakeTCYSpecific'
 
 export const UnstakeSpecific = () => {
-  const [{ chain }] = useDepositFormHandlers()
   const [{ ticker: selectedCoinTicker }] = useDepositCoin()
+  const [{ chain, register }] = useDepositFormHandlers()
+  const { t } = useTranslation()
+  const [selectedCoin, setSelectedCoin] = useDepositCoin()
 
   return (
     <>
-      <StakeSpecific />
+      <Match
+        value={chain as StakeableChain}
+        THORChain={() => (
+          <Opener
+            renderOpener={({ onOpen }) => (
+              <VStack gap={8}>
+                <Container onClick={onOpen}>
+                  <HStack alignItems="center" gap={4}>
+                    <Text weight="400" family="mono" size={16}>
+                      {selectedCoin.ticker || t('select_token')}
+                    </Text>
+                    {!selectedCoin && (
+                      <AssetRequiredLabel as="span" color="danger" size={14}>
+                        *
+                      </AssetRequiredLabel>
+                    )}
+                  </HStack>
+                  <IconWrapper style={{ fontSize: 20 }}>
+                    <ChevronRightIcon />
+                  </IconWrapper>
+                </Container>
+              </VStack>
+            )}
+            renderContent={({ onClose }) => (
+              <StakeTokenExplorer
+                activeOption={selectedCoin}
+                onOptionClick={token => {
+                  setSelectedCoin(token)
+                  onClose()
+                }}
+                onClose={onClose}
+              />
+            )}
+          />
+        )}
+        Ton={() => null}
+      />
       <Match
         value={chain as StakeableChain}
         THORChain={() => (
