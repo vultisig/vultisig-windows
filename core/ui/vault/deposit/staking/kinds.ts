afeb297283526d1ec75c207d22c45b70e3fcab71
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-07 11:11:11 +0300

    fix: adjust stake kinds

--- core/ui/vault/deposit/staking/kinds.ts
+++ core/ui/vault/deposit/staking/kinds.ts
@@ -4,10 +4,8 @@ import { StakeKind } from './types'
 export function toStakeKind(action: ChainAction): StakeKind | null {
   switch (action) {
     case 'stake':
-    case 'stake_ruji':
       return 'stake'
     case 'unstake':
-    case 'unstake_ruji':
       return 'unstake'
     case 'withdraw_ruji_rewards':
       return 'claim'
