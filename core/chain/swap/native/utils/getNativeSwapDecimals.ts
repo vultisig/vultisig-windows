44f7112a1a0ce94e9bebca7ea3cd5091d1a7368b
rcoderdev <radzionch@gmail.com>
2025-09-08 13:56:39 +0400

    Fix logic in getNativeSwapDecimals function to return THORChain's decimals for fee coins on MayaChain. This change corrects the condition for determining the appropriate decimal value used in native swaps.

--- core/chain/swap/native/utils/getNativeSwapDecimals.ts
+++ core/chain/swap/native/utils/getNativeSwapDecimals.ts
@@ -7,7 +7,7 @@ import { Chain } from '../../../Chain'
 // Use THORChain's decimals for all native swaps, except for the CACAO asset,
 // which is the only asset that uses 10 decimals.
 export const getNativeSwapDecimals = (coin: CoinKey) => {
-  if (coin.chain === Chain.MayaChain && !isFeeCoin(coin)) {
+  if (coin.chain === Chain.MayaChain && isFeeCoin(coin)) {
     return chainFeeCoin[coin.chain].decimals
   }
 
