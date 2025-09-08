800c82f87eae5da7beb935e9941b9a59595d5090
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-08 09:12:35 +0300

    feat: unstake specific component refactoring

--- core/ui/vault/deposit/DepositForm/ActionSpecific/StakeSpecific/UnstakeSpecific/UnstakeTCYSpecific.tsx
+++ core/ui/vault/deposit/DepositForm/ActionSpecific/StakeSpecific/UnstakeSpecific/UnstakeTCYSpecific.tsx
@@ -9,29 +9,40 @@ import {
 import { ActionInsideInteractiveElement } from '@lib/ui/base/ActionInsideInteractiveElement'
 import { MaxButton, maxButtonOffset } from '@lib/ui/buttons/MaxButton'
 import { AmountTextInput } from '@lib/ui/inputs/AmountTextInput'
+import { Checkbox } from '@lib/ui/inputs/checkbox/Checkbox'
 import { InputContainer } from '@lib/ui/inputs/InputContainer'
 import { HStack, VStack } from '@lib/ui/layout/Stack'
 import { Text } from '@lib/ui/text'
+import { Controller } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 
 import { useDepositFormHandlers } from '../../../../providers/DepositFormHandlersProvider'
-import { useUnstakableTcyQuery } from './hooks/useUnstakableTcyQuery'
+import { useUnstakableStakeAssetQuery } from './hooks/useUnstakableStakeAssetQuery'
 
 const suggestions = [0.25, 0.5, 0.75]
 
 export const UnstakeTCYSpecific = () => {
-  const [{ setValue, getValues, register }] = useDepositFormHandlers()
-
+  const [{ setValue, getValues, control, watch }] = useDepositFormHandlers()
+  const autoCompound = watch('autoCompound')
   const { t } = useTranslation()
   const address = useCurrentVaultAddress(Chain.THORChain)
-  const { data: unstakableTcy = 0n } = useUnstakableTcyQuery(address)
+
+  const { data: { tcyBalance = 0n, stcyBalance = 0n } = {} } =
+    useUnstakableStakeAssetQuery({
+      address,
+      autoCompound,
+    })
+
   const coins = useCurrentVaultCoins()
   const percentage = getValues('percentage')
 
   const tcyCoin = coins.find(c => c.id === 'tcy')
   const decimals = tcyCoin?.decimals ?? 8
   const ticker = tcyCoin?.ticker ?? 'TCY'
-  const maxDisplayAmount = fromChainAmount(unstakableTcy, decimals)
+  const maxDisplayAmount = fromChainAmount(
+    autoCompound ? stcyBalance : tcyBalance,
+    decimals
+  )
 
   return (
     <VStack gap={12}>
@@ -90,19 +101,20 @@ export const UnstakeTCYSpecific = () => {
           }}
         />
       </InputContainer>
-      <InputContainer>
-        <label
-          style={{
-            display: 'flex',
-            gap: 8,
-            alignItems: 'center',
-            marginTop: 8,
-          }}
-        >
-          <input type="checkbox" {...register('autoCompound')} />
-          <Text>Unstake {tcyAutoCompounderConfig.shareTicker}</Text>
-        </label>
-      </InputContainer>
+
+      <Controller
+        name="autoCompound"
+        control={control}
+        render={({ field }) => (
+          <Checkbox
+            value={field.value}
+            onChange={field.onChange}
+            label={t('unstake_share_token_label', {
+              ticker: tcyAutoCompounderConfig.shareTicker,
+            })}
+          />
+        )}
+      />
     </VStack>
   )
 }
