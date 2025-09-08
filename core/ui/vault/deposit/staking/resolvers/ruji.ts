5dc74d2a6f9829e4793159bd714d97eecb8af309
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-08 10:54:41 +0300

    feat: adapt ruji module for staking

--- core/ui/vault/deposit/staking/resolvers/ruji.ts
+++ core/ui/vault/deposit/staking/resolvers/ruji.ts
@@ -2,59 +2,47 @@ import { toChainAmount } from '@core/chain/amount/toChainAmount'
 import { CosmosChain } from '@core/chain/Chain'
 import { rujiraStakingConfig } from '@core/chain/chains/cosmos/thor/rujira/config'
 import { CoinKey } from '@core/chain/coin/Coin'
-import { knownCosmosTokens } from '@core/chain/coin/knownTokens/cosmos'
 import { getDenom } from '@core/chain/coin/utils/getDenom'
+import { match } from '@lib/utils/match'
 
-import { StakeInput, StakeKind, StakeResolver, StakeSpecific } from '../types'
+import { RujiPayload, StakeSpecific } from '../types'
 
-export const rujiSpecific: StakeResolver = {
-  id: 'ruji',
+export function getRujiSpecific({ coin, input }: RujiPayload): StakeSpecific {
+  const denom =
+    getDenom(coin as CoinKey<CosmosChain>) ?? rujiraStakingConfig.bondDenom
 
-  supports(coin) {
-    // We key off ticker or denom (be robust if the ticker casing changes)
-    return (
-      coin.ticker?.toUpperCase() ===
-      knownCosmosTokens['THORChain']['x/ruji'].ticker.toUpperCase()
-    )
-  },
+  return match<RujiPayload['input']['kind'], StakeSpecific>(input.kind, {
+    stake: () => {
+      if (!('amount' in input)) {
+        throw new Error('Invalid amount')
+      }
 
-  buildIntent(verb: StakeKind, input: StakeInput, { coin }): StakeSpecific {
-    const denom =
-      getDenom(coin as CoinKey<CosmosChain>) ?? rujiraStakingConfig.bondDenom
-
-    if (verb === 'stake') {
-      const amountUnits = toChainAmount(
-        (input as any).amount,
-        coin.decimals
-      ).toString()
-      // CosmWasm execute: { account: { bond: {} } } with funds RUJI denom
+      const amountUnits = toChainAmount(input.amount, coin.decimals).toString()
       return {
         kind: 'wasm',
         contract: rujiraStakingConfig.contract,
         executeMsg: { account: { bond: {} } },
         funds: [{ denom, amount: amountUnits }],
       }
-    }
+    },
+    unstake: () => {
+      if (!('amount' in input)) {
+        throw new Error('Invalid amount')
+      }
 
-    if (verb === 'unstake') {
-      const amountUnits = toChainAmount(
-        (input as any).amount,
-        coin.decimals
-      ).toString()
+      const amountUnits = toChainAmount(input.amount, coin.decimals).toString()
       return {
         kind: 'wasm',
         contract: rujiraStakingConfig.contract,
         executeMsg: { account: { withdraw: { amount: amountUnits } } },
         funds: [],
       }
-    }
-
-    // claim rewards
-    return {
+    },
+    claim: () => ({
       kind: 'wasm',
       contract: rujiraStakingConfig.contract,
       executeMsg: { account: { claim: {} } },
       funds: [],
-    }
-  },
+    }),
+  })
 }
