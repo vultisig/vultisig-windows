c6d54e9106155d8143a5019572ac411aa162d24d
Johnny Luo <johnnyluo1980@gmail.com>
2025-09-08 20:32:58 +1000

    Update @trustwallet/wallet-core dependency to version 4.3.6 and remove empty scripts from package.json

--- lib/utils/hex/numberToHex.ts
+++ lib/utils/hex/numberToHex.ts
@@ -1,1 +1,9 @@
 export const numberToHex = (num: number) => `0x${num.toString(16)}`
+
+export const numberToEvenHex = (amount: number | Long) => {
+  let hex = amount.toString(16)
+  if (hex.length % 2 !== 0) {
+    hex = '0' + hex
+  }
+  return hex
+}
