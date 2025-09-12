5b38ba50569748f50d2a4350576cae1bdf969a07
rcoderdev <radzionch@gmail.com>
2025-09-12 08:51:52 +0400

    Refactor fee quote handling to utilize bigint and improve structure
    - Updated Solana and EVM fee quote resolvers to use bigint for fee calculations, enhancing accuracy.
    - Refactored Tron fee quote resolver to accept AccountCoinKey and streamlined its logic.
    - Removed deprecated fee quote resolver files, consolidating fee quote logic for better maintainability.
    - Introduced a new query for transaction fee quotes, integrating various chain fee resolvers for improved consistency.

--- core/chain/chains/solana/solanaConfig.ts
+++ core/chain/chains/solana/solanaConfig.ts
@@ -1,8 +1,8 @@
 export const solanaConfig = {
-  priorityFeeLimit: Number(100_000), // Turbo fee in lamports, around 5 cents
-  priorityFeePrice: 1_000_000, // Turbo fee in lamports, around 5 cents
+  priorityFeeLimit: 100_000n, // Turbo fee in lamports, around 5 cents
+  priorityFeePrice: 1_000_000n, // Turbo fee in lamports, around 5 cents
 
   // regardless of its complexity Solana charges a fixed base transaction fee of 5000 lamports per transaction.
   // This base fee is separate from the priority fee.
-  baseFee: 5000, // 0.000005 SOL Base fee in lamports
+  baseFee: 5000n, // 0.000005 SOL Base fee in lamports
 }
--- core/chain/tx/fee/quote/resolvers/sui.ts
+++ core/chain/tx/fee/quote/resolvers/sui.ts
@@ -1,11 +1,7 @@
 import { getSuiClient } from '../../../../chains/sui/client'
-import { FeeQuote } from '../core'
-import { FeeQuoteInput, FeeQuoteResolver } from '../resolver'
 
-export const getSuiFeeQuote: FeeQuoteResolver<'Sui'> = async (
-  _input: FeeQuoteInput<'Sui'>
-): Promise<FeeQuote<'sui'>> => {
+export const getSuiFeeQuote = async () => {
   const client = getSuiClient()
-  const gasPrice = await client.getReferenceGasPrice()
-  return BigInt(gasPrice)
+
+  return client.getReferenceGasPrice()
 }
