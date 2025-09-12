06508d0fab6b4e2eea5564640ed7a961626ea55d
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-09-12 20:13:53 +0300

    Update deprecated Jupiter token endpoint  (#2407)
    * refactor: update Solana Jupiter token structure and metadata handling
    
    - Changed token property `address` to `id` and updated related references.
    - Replaced `logoURI` with `icon` in the token type and adjusted filtering logic accordingly.
    - Updated the token metadata resolver to fetch and return the new `icon` property.
    - Modified the whitelisted coins query URL to use the new API endpoint for fetching verified tokens.
    
    * refactor: update Coingecko ID handling and streamline API URLs
    
    - Changed the return value of `getSolanaCoingeckoId` to return `undefined` instead of an empty string when no ID is found.
    - Introduced a constant for the base Jupiter tokens URL to improve code readability and maintainability in the token metadata resolver and whitelisted coins query.

--- core/chain/coin/token/metadata/resolvers/solana.ts
+++ core/chain/coin/token/metadata/resolvers/solana.ts
@@ -1,16 +1,21 @@
 import { SolanaJupiterToken } from '@core/chain/coin/jupiter/token'
 import { queryUrl } from '@lib/utils/query/queryUrl'
 
+import { getSolanaCoingeckoId } from '../../../coingecko/getCoingeckoId'
 import { TokenMetadataResolver } from '../resolver'
 
+export const baseJupiterTokensUrl = 'https://lite-api.jup.ag/tokens/v2'
+
 export const getSolanaTokenMetadata: TokenMetadataResolver = async ({ id }) => {
-  const { decimals, logoURI, symbol, extensions } =
-    await queryUrl<SolanaJupiterToken>(`https://tokens.jup.ag/token/${id}`)
+  const [{ decimals, icon, symbol }] = await queryUrl<SolanaJupiterToken[]>(
+    `${baseJupiterTokensUrl}/search?query=${id}`
+  )
+  const coingeckoId = await getSolanaCoingeckoId({ id })
 
   return {
     decimals,
-    logo: logoURI,
+    logo: icon,
     ticker: symbol,
-    priceProviderId: extensions?.coingeckoId,
+    priceProviderId: coingeckoId,
   }
 }
