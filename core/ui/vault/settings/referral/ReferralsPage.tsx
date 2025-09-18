7bde19324c9ecbae38934da92ae7c7c99ea8d951
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-18 10:10:01 +0300

    feat: refactor referrals page

--- core/ui/vault/settings/referral/ReferralsPage.tsx
+++ core/ui/vault/settings/referral/ReferralsPage.tsx
@@ -57,7 +57,7 @@ export const ReferralPage = () => {
             <>
               <PageHeader
                 primaryControls={<PageHeaderBackButton />}
-                title={t('title_1')}
+                title={t('referrals_default_title')}
               />
               <ContentWrapper>
                 <Match
