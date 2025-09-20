790163001d2cc9b1d61576f4ebc1f585621a6a73
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-20 12:31:35 +0300

    feat: cleanup config

--- core/chain/chains/cosmos/thor/yield-bearing-tokens/config.ts
+++ core/chain/chains/cosmos/thor/yield-bearing-tokens/config.ts
@@ -1,24 +1,18 @@
-export const yieldBearingAssetsIds = ['yRUNE', 'yTCY'] as const
-type YieldBearingAsset = (typeof yieldBearingAssetsIds)[number]
+export const yieldBearingTokensIdToContractMap: Record<string, string> = {
+  'x/nami-index-nav-thor1mlphkryw5g54yfkrp6xpqzlpv4f8wh6hyw27yyg4z2els8a9gxpqhfhekt-rcpt':
+    'thor1mlphkryw5g54yfkrp6xpqzlpv4f8wh6hyw27yyg4z2els8a9gxpqhfhekt',
 
-// MAINNET contracts
-export const yieldBearingAssetsContracts: Record<YieldBearingAsset, string> = {
-  yRUNE: 'thor1mlphkryw5g54yfkrp6xpqzlpv4f8wh6hyw27yyg4z2els8a9gxpqhfhekt',
-  yTCY: 'thor1h0hr0rm3dawkedh44hlrmgvya6plsryehcr46yda2vj0wfwgq5xqrs86px',
-}
+  'x/nami-index-nav-thor1h0hr0rm3dawkedh44hlrmgvya6plsryehcr46yda2vj0wfwgq5xqrs86px-rcpt':
+    'thor1h0hr0rm3dawkedh44hlrmgvya6plsryehcr46yda2vj0wfwgq5xqrs86px',
+} as const
+
+export const yieldContractsByBaseDenom = {
+  rune: 'thor1mlphkryw5g54yfkrp6xpqzlpv4f8wh6hyw27yyg4z2els8a9gxpqhfhekt',
+  tcy: 'thor1h0hr0rm3dawkedh44hlrmgvya6plsryehcr46yda2vj0wfwgq5xqrs86px',
+} as const
 
 // MAINNET affiliate contract + your affiliate address
 export const yieldBearingAssetsAffiliateContract =
   'thor1v3f7h384r8hw6r3dtcgfq6d5fq842u6cjzeuu8nr0cp93j7zfxyquyrfl8'
 export const yieldBearingAssetsAffiliateAddress =
   'thor1svfwxevnxtm4ltnw92hrqpqk4vzuzw9a4jzy04'
-
-// MAINNET receipt denoms
-export const yieldBearingAssetsReceiptDenoms: Record<
-  YieldBearingAsset,
-  string
-> = {
-  yRUNE:
-    'x/nami-index-nav-thor1mlphkryw5g54yfkrp6xpqzlpv4f8wh6hyw27yyg4z2els8a9gxpqhfhekt-rcpt',
-  yTCY: 'x/nami-index-nav-thor1h0hr0rm3dawkedh44hlrmgvya6plsryehcr46yda2vj0wfwgq5xqrs86px-rcpt',
-}
