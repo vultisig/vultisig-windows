22f13a2c3a5235e4cb430b1b9f22170c691a60a6
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-09-22 19:01:05 +0300

    fix: deposit coin provider

--- core/ui/vault/deposit/providers/DepositCoinProvider.tsx
+++ core/ui/vault/deposit/providers/DepositCoinProvider.tsx
@@ -1,5 +1,43 @@
 import { AccountCoin } from '@core/chain/coin/AccountCoin'
-import { getStateProviderSetup } from '@lib/ui/state/getStateProviderSetup'
+import { ChildrenProp } from '@lib/ui/props'
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useState,
+} from 'react'
 
-export const { useState: useDepositCoin, provider: DepositCoinProvider } =
-  getStateProviderSetup<AccountCoin>('DepositCoin')
+import { useCoreViewState } from '../../../navigation/hooks/useCoreViewState'
+import { useCurrentVaultCoin } from '../../state/currentVaultCoins'
+import { useCorrectSelectedCoin } from '../hooks/useCorrectSelectedCoin'
+
+const DepositCoinContext = createContext<
+  [AccountCoin, Dispatch<SetStateAction<AccountCoin>>]
+>(null!)
+
+export const DepositCoinProvider = ({ children }: ChildrenProp) => {
+  const [{ coin: coinKey }] = useCoreViewState<'deposit'>()
+  const initialCoin = useCurrentVaultCoin(coinKey)
+
+  const state = useState(initialCoin)
+
+  const [coin, setCoin] = state
+
+  const correctedCoin = useCorrectSelectedCoin({
+    currentDepositCoin: coin,
+  })
+
+  if (correctedCoin && coin !== correctedCoin) {
+    setCoin(correctedCoin)
+    return
+  }
+
+  return (
+    <DepositCoinContext.Provider value={state}>
+      {children}
+    </DepositCoinContext.Provider>
+  )
+}
+
+export const useDepositCoin = () => useContext(DepositCoinContext)
