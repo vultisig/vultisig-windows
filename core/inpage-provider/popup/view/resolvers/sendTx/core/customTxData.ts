765332238908109ed232759a4ac35e713e75d620
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-09-23 11:44:29 +0330

    Refactor transaction handling in customTxData to incorporate vault and improve PSBT processing. Update knip.json to remove obsolete ignore pattern for restrictPsbt file.

--- core/inpage-provider/popup/view/resolvers/sendTx/core/customTxData.ts
+++ core/inpage-provider/popup/view/resolvers/sendTx/core/customTxData.ts
@@ -5,6 +5,9 @@ import { getEvmContractCallHexSignature } from '@core/chain/chains/evm/contract/
 import { getEvmContractCallSignatures } from '@core/chain/chains/evm/contract/call/signatures'
 import { chainFeeCoin } from '@core/chain/coin/chainFeeCoin'
 import { Coin, CoinKey } from '@core/chain/coin/Coin'
+import { deriveAddress } from '@core/chain/publicKey/address/deriveAddress'
+import { getPublicKey } from '@core/chain/publicKey/getPublicKey'
+import { Vault } from '@core/ui/vault/Vault'
 import { attempt } from '@lib/utils/attempt'
 import { matchRecordUnion } from '@lib/utils/matchRecordUnion'
 import { areLowerCaseEqual } from '@lib/utils/string/areLowerCaseEqual'
@@ -15,6 +18,7 @@ import { Psbt } from 'bitcoinjs-lib'
 import { IKeysignTransactionPayload, ITransactionPayload } from '../interfaces'
 import { parseSolanaTx } from './solana/parser'
 import { SolanaTxData } from './solana/types/types'
+import { restrictPsbtToInputs } from './utxo/restrictPsbt'
 
 type RegularTxData = IKeysignTransactionPayload & {
   isEvmContractCall?: boolean
@@ -34,6 +38,7 @@ export type CustomTxData =
 
 type GetCustomTxDataInput = {
   walletCore: WalletCore
+  vault: Vault
   transactionPayload: ITransactionPayload
   getCoin: (coinKey: CoinKey) => Promise<Coin>
   requestOrigin: string
@@ -41,6 +46,7 @@ type GetCustomTxDataInput = {
 
 export const getCustomTxData = ({
   walletCore,
+  vault,
   transactionPayload,
   getCoin,
   requestOrigin,
@@ -100,11 +106,37 @@ export const getCustomTxData = ({
           },
         }
       },
-      serialized: async ({ data, chain }) => {
+      serialized: async ({ data, chain, params }) => {
         if (chain === Chain.Bitcoin) {
           const dataBuffer = Buffer.from(data, 'base64')
-          const psbt = Psbt.fromBuffer(Buffer.from(dataBuffer))
-
+          let psbt = Psbt.fromBuffer(Buffer.from(dataBuffer))
+          if (params && params.length > 0) {
+            const publicKey = getPublicKey({
+              chain: Chain.Bitcoin,
+              walletCore,
+              hexChainCode: vault.hexChainCode,
+              publicKeys: vault.publicKeys,
+            })
+            const address = deriveAddress({
+              chain,
+              publicKey,
+              walletCore,
+            })
+            const currentWalletEntries = params.filter(e =>
+              areLowerCaseEqual(e.address, address)
+            )
+            if (currentWalletEntries.length === 0) {
+              throw new Error('No entries for wallet address')
+            }
+            psbt = restrictPsbtToInputs(
+              psbt,
+              currentWalletEntries.map(p => ({
+                signingIndexes: p.signingIndexes,
+                sigHash: p.sigHash,
+              })),
+              Buffer.from(publicKey.data())
+            )
+          }
           return { psbt }
         }
 
