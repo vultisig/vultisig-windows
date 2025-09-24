4da0068df304092c8e5cdc0f26f418860b7d5429
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-09-24 15:28:54 +0330

    Refactor error handling in SaveImportedVaultStep

--- core/ui/vault/import/components/SaveImportedVaultStep.tsx
+++ core/ui/vault/import/components/SaveImportedVaultStep.tsx
@@ -1,24 +1,24 @@
+import { FlowErrorPageContent } from '@core/ui/flow/FlowErrorPageContent'
 import { useCoreNavigate } from '@core/ui/navigation/hooks/useCoreNavigate'
 import { useCore } from '@core/ui/state/core'
 import { useVaultOrders, useVaults } from '@core/ui/storage/vaults'
 import { useVaultBackupOverride } from '@core/ui/vault/import/state/vaultBackupOverride'
 import { SaveVaultStep } from '@core/ui/vault/save/SaveVaultStep'
 import { getVaultId, Vault } from '@core/ui/vault/Vault'
+import { Button } from '@lib/ui/buttons/Button'
 import { ValueProp } from '@lib/ui/props'
 import { getLastItemOrder } from '@lib/utils/order/getLastItemOrder'
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-
-import { FlowErrorPageContent } from '../../../flow/FlowErrorPageContent'
 
 export const SaveImportedVaultStep = ({ value }: ValueProp<Vault>) => {
   const { t } = useTranslation()
+  const [state, setState] = useState({ errorMessage: '', isValidated: false })
+  const { errorMessage, isValidated } = state
   const { client } = useCore()
   const navigate = useCoreNavigate()
   const override = useVaultBackupOverride()
-
   const vaults = useVaults()
-
   const vaultOrders = useVaultOrders()
 
   const finalValue = useMemo(
@@ -29,27 +29,40 @@ export const SaveImportedVaultStep = ({ value }: ValueProp<Vault>) => {
     [override, value, vaultOrders]
   )
 
-  const error = useMemo(() => {
-    if (vaults.find(v => getVaultId(v) === getVaultId(finalValue))) {
-      return t('vault_already_exists')
-    }
-    if (client === 'extension' && value.libType === 'GG20') {
-      return t('extension_vault_import_restriction')
-    }
-  }, [client, finalValue, t, value.libType, vaults])
+  useEffect(() => {
+    if (!isValidated) {
+      let errorMessage = ''
 
-  if (error) {
-    return (
-      <FlowErrorPageContent title={t('failed_to_save_vault')} error={error} />
+      if (vaults.find(v => getVaultId(v) === getVaultId(finalValue))) {
+        errorMessage = t('vault_already_exists')
+      }
+
+      if (client === 'extension' && value.libType === 'GG20') {
+        errorMessage = t('extension_vault_import_restriction')
+      }
+
+      setState({ errorMessage, isValidated: true })
+    }
+  }, [client, finalValue, isValidated, t, value.libType, vaults])
+
+  return isValidated ? (
+    errorMessage ? (
+      <FlowErrorPageContent
+        title={t('failed_to_save_vault')}
+        error={errorMessage}
+        action={
+          <Button onClick={() => navigate({ id: 'vault' })}>{t('back')}</Button>
+        }
+      />
+    ) : (
+      <SaveVaultStep
+        onBack={() => navigate({ id: 'vault' })}
+        onFinish={() => navigate({ id: 'vault' })}
+        value={finalValue}
+        title={t('import_vault')}
+      />
     )
-  }
-
-  return (
-    <SaveVaultStep
-      onBack={() => navigate({ id: 'vault' })}
-      onFinish={() => navigate({ id: 'vault' })}
-      value={finalValue}
-      title={t('import_vault')}
-    />
+  ) : (
+    <></>
   )
 }
