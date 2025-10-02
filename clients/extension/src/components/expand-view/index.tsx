66de40e088df22cec067b092a123c8d76f8d7021
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-10-02 11:16:43 +0330

    Update ExpandView visibility logic

--- clients/extension/src/components/expand-view/index.tsx
+++ clients/extension/src/components/expand-view/index.tsx
@@ -1,16 +1,14 @@
-import { isPopupView } from '@clients/extension/src/utils/functions'
+import { useOpenInExpandedViewMutation } from '@clients/extension/src/expanded-view/mutations/openInExpandedView'
 import { initialCoreView } from '@core/ui/navigation/CoreView'
+import { useIsScreenWidthLessThan } from '@lib/ui/hooks/useIsScreenWidthLessThan'
 import { ExpandIcon } from '@lib/ui/icons/ExpandIcon'
 import { ListItem } from '@lib/ui/list/item'
 import { useTranslation } from 'react-i18next'
 
-import { useOpenInExpandedViewMutation } from '../../expanded-view/mutations/openInExpandedView'
-
 export const ExpandView = () => {
   const { t } = useTranslation()
-  const visible = isPopupView()
-
   const { mutate: openInExpandedView } = useOpenInExpandedViewMutation()
+  const visible = useIsScreenWidthLessThan(481)
 
   return (
     visible && (
