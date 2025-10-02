50fbbe26c270a6f4455cc8ac357fff08500798d0
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-02 23:09:23 +0300

    feat: add settings constants

---
+++ core/ui/settings/constants.ts
@@ -0,0 +1,9 @@
+export const vultUrl = 'https://vult.network'
+export const vultisigWindowsGithubUrl =
+  'https://github.com/vultisig/vultisig-windows'
+
+export const discordReferralUrl = 'https://discord.gg/ngvW8tRRfB'
+export const vultisigTwitterUrl = 'https://x.com/vultisig'
+export const vultisigWebsite = 'https://vultisig.com/'
+export const vultisigPrivacyPolicyUrl = 'https://vultisig.com/privacy'
+export const vultisigTermsOfServiceUrl = 'https://vultisig.com/termofservice'
