d5058a388d1be5112202a88f4d551bfb72f0a3a4
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-04 10:21:47 +0300

    fix: duplicated components

--- core/ui/settings copy/constants.ts
+++
@@ -1,9 +0,0 @@
-export const vultUrl = 'https://vult.network'
-export const vultisigWindowsGithubUrl =
-  'https://github.com/vultisig/vultisig-windows'
-
-export const discordReferralUrl = 'https://discord.gg/ngvW8tRRfB'
-export const vultisigTwitterUrl = 'https://x.com/vultisig'
-export const vultisigWebsite = 'https://vultisig.com/'
-export const vultisigPrivacyPolicyUrl = 'https://vultisig.com/privacy'
-export const vultisigTermsOfServiceUrl = 'https://vultisig.com/termofservice'
