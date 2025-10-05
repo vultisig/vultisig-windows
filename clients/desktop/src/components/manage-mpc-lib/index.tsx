efe61d83d2fcc16454e3db40dcf58c034431b938
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-05 11:57:43 +0300

    feat: add click gate

--- clients/desktop/src/components/manage-mpc-lib/index.tsx
+++ clients/desktop/src/components/manage-mpc-lib/index.tsx
@@ -2,33 +2,23 @@ import { useVaultCreationMpcLib } from '@clients/desktop/src/mpc/state/vaultCrea
 import { useCore } from '@core/ui/state/core'
 import { Opener } from '@lib/ui/base/Opener'
 import { UnstyledButton } from '@lib/ui/buttons/UnstyledButton'
+import { useClickGate } from '@lib/ui/hooks/useClickGate'
 import { Switch } from '@lib/ui/inputs/switch'
 import { Modal } from '@lib/ui/modal'
 import { Text } from '@lib/ui/text'
-import { useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 
 export const ManageMpcLib = () => {
   const { t } = useTranslation()
   const { version } = useCore()
   const [value, setValue] = useVaultCreationMpcLib()
-  const clickCount = useRef(0)
+  const gate = useClickGate()
   const isDKLS = value === 'DKLS'
 
   return (
     <Opener
       renderOpener={({ onOpen }) => (
-        <UnstyledButton
-          onClick={() => {
-            if (clickCount.current < 2) {
-              clickCount.current += 1
-              return
-            }
-
-            onOpen()
-            clickCount.current = 0
-          }}
-        >
+        <UnstyledButton onClick={() => gate(onOpen)}>
           <Text
             as="span"
             color="shy"
