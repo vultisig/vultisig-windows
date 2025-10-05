8d089b443ed4ef280091611d50a64721e72f8141
rcoderdev <radzionch@gmail.com>
2025-10-05 08:37:01 +0400

    Implement RequestFastVaultBackup component with header and content layout
    - Added RequestFastVaultBackup component to provide a structured layout for quick backup requests.
    - Integrated PageHeader and FitPageContent for improved UI presentation.
    - Included translation support for the server backup title.

--- core/ui/vault/backup/fast/request/index.tsx
+++ core/ui/vault/backup/fast/request/index.tsx
@@ -1,3 +1,20 @@
+import { FitPageContent } from '@lib/ui/page/PageContent'
+import { PageHeader } from '@lib/ui/page/PageHeader'
+import { useTranslation } from 'react-i18next'
+
+import { PageHeaderBackButton } from '../../../../flow/PageHeaderBackButton'
+
 export const RequestFastVaultBackup = () => {
-  return <p>todo</p>
+  const { t } = useTranslation()
+  return (
+    <>
+      <PageHeader
+        title={t('server_backup')}
+        primaryControls={<PageHeaderBackButton />}
+      />
+      <FitPageContent contentMaxWidth={468}>
+        <p>Form will be here!</p>
+      </FitPageContent>
+    </>
+  )
 }
