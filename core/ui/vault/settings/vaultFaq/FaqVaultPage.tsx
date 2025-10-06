567dcac1a9446e786a2e49acda064a006247be08
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-06 20:14:51 +0300

    fix: failing build

--- core/ui/vault/settings/vaultFaq/FaqVaultPage.tsx
+++ core/ui/vault/settings/vaultFaq/FaqVaultPage.tsx
@@ -138,7 +138,7 @@ const RowsWrapper = styled.div`
   }
 `
 
-export const Divider = styled.div`
+const Divider = styled.div`
   height: 1px;
   width: 100%;
   background: linear-gradient(90deg, #061b3a 0%, #284570 49.5%, #061b3a 100%);
