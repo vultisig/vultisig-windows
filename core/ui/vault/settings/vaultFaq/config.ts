093098ed8cef581d691695d0fc7d8198947b2b15
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-06 20:02:31 +0300

    chore: fix faq page

---
+++ core/ui/vault/settings/vaultFaq/config.ts
@@ -0,0 +1,55 @@
+import type { TFunction } from 'i18next'
+
+type FaqItem = {
+  id: number
+  title: string
+  content: string
+}
+
+export const getFaqData = (t: TFunction): FaqItem[] => [
+  {
+    id: 1,
+    title: t('faq_setup_crypto_vault_title'),
+    content: t('faq_setup_crypto_vault_content'),
+  },
+  {
+    id: 2,
+    title: t('faq_supported_cryptocurrencies_title'),
+    content: t('faq_supported_cryptocurrencies_content'),
+  },
+  {
+    id: 3,
+    title: t('faq_secure_crypto_vault_title'),
+    content: t('faq_secure_crypto_vault_content'),
+  },
+  {
+    id: 4,
+    title: t('faq_vultisig_make_money_title'),
+    content: t('faq_vultisig_make_money_content'),
+  },
+  {
+    id: 5,
+    title: t('faq_recover_assets_title'),
+    content: t('faq_recover_assets_content'),
+  },
+  {
+    id: 6,
+    title: t('faq_need_to_register_title'),
+    content: t('faq_need_to_register_content'),
+  },
+  {
+    id: 7,
+    title: t('faq_is_open_source_title'),
+    content: t('faq_is_open_source_content'),
+  },
+  {
+    id: 8,
+    title: t('faq_data_protection_title'),
+    content: t('faq_data_protection_content'),
+  },
+  {
+    id: 9,
+    title: t('faq_comparison_title'),
+    content: t('faq_comparison_content'),
+  },
+]
