f9e33197a4e67d09ef4a1e5a5d5db68d1795cbbc
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-09 15:45:38 +0300

    feat: add new button variant

--- lib/ui/buttons/ButtonProps.tsx
+++ lib/ui/buttons/ButtonProps.tsx
@@ -5,7 +5,7 @@ export type ButtonProps = Omit<
   'disabled'
 > & {
   disabled?: boolean | string
-  kind?: 'primary' | 'secondary' | 'link'
+  kind?: 'primary' | 'secondary' | 'link' | 'action'
   loading?: boolean
   status?: 'default' | 'danger' | 'success' | 'warning'
 }
