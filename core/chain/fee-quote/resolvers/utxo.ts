369051cc08041e4da61f4fbcdffac4045101bc51
rcoderdev <radzionch@gmail.com>
2025-10-10 11:13:47 +0400

    Refactor UTXO fee quote resolver to enhance fee calculation logic
    * Introduced a byteFeeMultiplier function to streamline the calculation of transaction fees based on suggested values.
    * Updated the getByteFee function to return BigInt values for improved consistency and accuracy in fee estimation.
    * Simplified the getUtxoFeeQuote resolver by directly using the calculated byte fee, enhancing code clarity and maintainability.

--- core/chain/fee-quote/resolvers/utxo.ts
+++ core/chain/fee-quote/resolvers/utxo.ts
@@ -1,18 +1,22 @@
 import { UtxoChain } from '@core/chain/Chain'
 import { getUtxoStats } from '@core/chain/chains/utxo/client/getUtxoStats'
-import { adjustByteFee } from '@core/chain/tx/fee/utxo/adjustByteFee'
 
 import { FeeQuoteResolver } from '../resolver'
 
+const byteFeeMultiplier = (value: bigint) => (value * 25n) / 10n
+
 const getByteFee = async (chain: UtxoChain) => {
-  if (chain === UtxoChain.Zcash) return 1000
-  const { data } = await getUtxoStats(chain)
-  const raw = data.suggested_transaction_fee_per_byte_sat
-  return Math.floor(raw * 2.5)
+  if (chain === UtxoChain.Zcash) return 1000n
+
+  const {
+    data: { suggested_transaction_fee_per_byte_sat },
+  } = await getUtxoStats(chain)
+
+  return byteFeeMultiplier(BigInt(suggested_transaction_fee_per_byte_sat))
 }
 
 export const getUtxoFeeQuote: FeeQuoteResolver<'utxo'> = async ({ coin }) => {
-  const chain = coin.chain as UtxoChain
-  const byteFee = adjustByteFee(await getByteFee(chain), 1)
-  return { byteFee: BigInt(byteFee), byteFeeMultiplier: 1 }
+  const byteFee = await getByteFee(coin.chain)
+
+  return { byteFee, byteFeeMultiplier: 1 }
 }
