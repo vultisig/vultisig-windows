de00dc27025c0fc114552b87467e175c0c375f50
rcoderdev <radzionch@gmail.com>
2025-10-11 08:37:12 +0400

    Export feeQuoteQueryKeyPrefix for improved accessibility in related components and refactor RefreshSend to utilize the new invalidateQueriesMutation for enhanced query management.

--- core/ui/chain/fee-quote/query.ts
+++ core/ui/chain/fee-quote/query.ts
@@ -2,7 +2,7 @@ import { getFeeQuote } from '@core/chain/fee-quote'
 import { ChainSpecificResolverInput } from '@core/mpc/keysign/chainSpecific/resolver'
 import { useQuery } from '@tanstack/react-query'
 
-const feeQuoteQueryKeyPrefix = 'feeQuote'
+export const feeQuoteQueryKeyPrefix = 'feeQuote'
 
 export const useFeeQuoteQuery = (input: ChainSpecificResolverInput) => {
   return useQuery({
