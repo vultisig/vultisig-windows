ce27d8f57ad5da45c75afe30c55654e295cfb775
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-11 09:30:32 +0300

    feat: add dedicated modadl for backup for desktop and for mobile

--- core/ui/vault/settings/backup/index.tsx
+++ core/ui/vault/settings/backup/index.tsx
@@ -1,11 +1,7 @@
 import { hasServer } from '@core/mpc/devices/localPartyId'
 import { Opener } from '@lib/ui/base/Opener'
-import { CloudIcon } from '@lib/ui/icons/CloudIcon'
 import { FolderUploadIcon } from '@lib/ui/icons/FolderUploadIcon'
-import { TabletSmartphoneIcon } from '@lib/ui/icons/TabletSmartphoneIcon'
-import { VStack } from '@lib/ui/layout/Stack'
 import { ListItem } from '@lib/ui/list/item'
-import { Modal } from '@lib/ui/modal'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
@@ -13,17 +9,9 @@ import { CoreView } from '../../../navigation/CoreView'
 import { useCoreNavigate } from '../../../navigation/hooks/useCoreNavigate'
 import { useCurrentVault } from '../../state/currentVault'
 import { DescriptionText, ListItemIconWrapper } from '..'
-import { BackupOption } from './BackupOption'
+import { BackupModal, BackupOptionType } from './BackupModal'
 
-const backupOptionTypes = ['device', 'server'] as const
-type BackupOptionType = (typeof backupOptionTypes)[number]
-
-const backupOptionIcon: Record<BackupOptionType, React.FC> = {
-  device: TabletSmartphoneIcon,
-  server: CloudIcon,
-}
-
-const backupOptionView: Record<BackupOptionType, CoreView> = {
+export const backupOptionView: Record<BackupOptionType, CoreView> = {
   device: { id: 'selectVaultsBackup' },
   server: { id: 'requestFastVaultBackup' },
 }
@@ -57,25 +45,7 @@ export const VaultSettingsBackup = () => {
           showArrow
         />
       )}
-      renderContent={({ onClose }) => (
-        <Modal title={t('choose_backup_method')} onClose={onClose}>
-          <VStack gap={16}>
-            {backupOptionTypes.map(option => {
-              const Icon = backupOptionIcon[option]
-              return (
-                <BackupOption
-                  title={t(`${option}_backup`)}
-                  key={option}
-                  icon={<Icon />}
-                  onClick={() => navigate(backupOptionView[option])}
-                >
-                  {t(`${option}_backup_description`)}
-                </BackupOption>
-              )
-            })}
-          </VStack>
-        </Modal>
-      )}
+      renderContent={({ onClose }) => <BackupModal onClose={onClose} />}
     />
   )
 }
