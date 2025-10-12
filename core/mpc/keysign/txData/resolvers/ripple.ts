1db04771e7893bfb7b5f85d40dbeed0fb4a23f7c
rcoderdev <radzionch@gmail.com>
2025-10-12 11:08:54 +0400

    Refactor Ripple transaction data resolver for improved clarity and type safety
    * Updated the `getRippleTxData` resolver to destructure the `coin` object, enhancing readability.
    * Simplified account information retrieval by directly accessing the `address` property, promoting better type management.
    * Improved the handling of account data and ledger index for more accurate transaction data representation.

--- core/mpc/keysign/txData/resolvers/ripple.ts
+++ core/mpc/keysign/txData/resolvers/ripple.ts
@@ -3,11 +3,13 @@ import { getRippleAccountInfo } from '@core/chain/chains/ripple/account/info'
 import { KeysignTxDataResolver } from '../resolver'
 
 export const getRippleTxData: KeysignTxDataResolver<'ripple'> = async ({
-  coin,
+  coin: { address },
 }) => {
-  const rippleAccount = await getRippleAccountInfo(coin.address)
+  const { account_data, ledger_current_index } =
+    await getRippleAccountInfo(address)
+
   return {
-    sequence: BigInt(rippleAccount.account_data.Sequence),
-    lastLedgerSequence: BigInt((rippleAccount.ledger_current_index ?? 0) + 60),
-  } as any
+    sequence: BigInt(account_data.Sequence),
+    lastLedgerSequence: BigInt((ledger_current_index ?? 0) + 60),
+  }
 }
