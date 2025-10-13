1f18e451e41f5453ea118137179c65ef81266a8d
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-13 20:07:14 +0300

    fix: icon

--- lib/ui/icons/FileTextIcon.tsx
+++ lib/ui/icons/FileTextIcon.tsx
@@ -1,18 +1,20 @@
 import { SvgProps } from '@lib/ui/props'
 import { FC } from 'react'
 
-export const FileTextIcon: FC<SvgProps> = props => (
+export const FileTextIcon: FC<SvgProps> = () => (
   <svg
-    fill="none"
-    height="1em"
-    stroke="currentColor"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="1.5"
-    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
     width="1em"
-    {...props}
+    height="1em"
+    viewBox="0 0 20 20"
+    fill="none"
   >
-    <path d="M14 2V6C14 6.53043 14.2107 7.03914 14.5858 7.41421C14.9609 7.78929 15.4696 8 16 8H20M10 9H8M16 13H8M16 17H8M15 2H6C5.46957 2 4.96086 2.21071 4.58579 2.58579C4.21071 2.96086 4 3.46957 4 4V20C4 20.5304 4.21071 21.0391 4.58579 21.4142C4.96086 21.7893 5.46957 22 6 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V7L15 2Z" />
+    <path
+      d="M2.70831 10.0007H4.37498M8.12498 10.0007H10.2083M8.12498 6.87565H11.875M2.70831 6.87565H4.37498M2.70831 13.1257H4.37498M5.20831 17.2923H14.7916C15.7121 17.2923 16.4583 16.5462 16.4583 15.6257V4.37565C16.4583 3.45518 15.7121 2.70898 14.7916 2.70898H5.20831C4.28784 2.70898 3.54165 3.45518 3.54165 4.37565V15.6257C3.54165 16.5462 4.28784 17.2923 5.20831 17.2923Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </svg>
 )
