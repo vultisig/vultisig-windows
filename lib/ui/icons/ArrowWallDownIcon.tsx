7411a678d8297fb5009f681582e695bf6906581c
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-14 19:12:44 +0300

    faet: add icons pt1

---
+++ lib/ui/icons/ArrowWallDownIcon.tsx
@@ -0,0 +1,33 @@
+export const ArrowWallDownIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="0.923em"
+      viewBox="0 0 13 12"
+      fill="none"
+    >
+      <path
+        d="M8.625 6.625L6.85355 8.39645C6.6583 8.5917 6.3417 8.5917 6.14645 8.39645L4.375 6.625"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.5 1.375V8.375"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.125 10.625H2.875"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
