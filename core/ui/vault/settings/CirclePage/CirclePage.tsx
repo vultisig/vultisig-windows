6bf7963da9fad50a03c1ffd65ada8be652dd5c21
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-14 19:12:07 +0300

    feat: add circle page

--- core/ui/vault/settings/CirclePage/CirclePage.tsx
+++ core/ui/vault/settings/CirclePage/CirclePage.tsx
@@ -3,13 +3,22 @@ import { PageHeaderBackButton } from '@core/ui/flow/PageHeaderBackButton'
 import { ShareVaultCard } from '@core/ui/vault/share/ShareVaultCard'
 import { useCurrentVault } from '@core/ui/vault/state/currentVault'
 import { Button } from '@lib/ui/buttons/Button'
+import { IconButton } from '@lib/ui/buttons/IconButton'
+import { IconWrapper } from '@lib/ui/icons/IconWrapper'
+import { NavigationXIcon } from '@lib/ui/icons/NavigationXIcon'
 import { Image } from '@lib/ui/image/Image'
-import { VStack } from '@lib/ui/layout/Stack'
+import { hStack, VStack, vStack } from '@lib/ui/layout/Stack'
 import { PageContent } from '@lib/ui/page/PageContent'
 import { PageFooter } from '@lib/ui/page/PageFooter'
 import { PageHeader } from '@lib/ui/page/PageHeader'
+import { Text } from '@lib/ui/text'
+import { getColor } from '@lib/ui/theme/getters'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
+
+import { APYOverview } from './components/APYOverview'
+import { BalanceOverviewTable } from './components/BalanceOverviewTable'
+import { TransactionActions } from './components/TransactionActions'
 
 export const CirclePage = () => {
   const { t } = useTranslation()
@@ -21,15 +30,37 @@ export const CirclePage = () => {
         primaryControls={<PageHeaderBackButton />}
         title={t('circle_title')}
       />
-      <StyledPageContent alignItems="center" flexGrow>
-        <ImageWrapper>
-          <Image
-            src="/core/images/register-vault-bg.png"
-            alt="register vault"
-            width={400}
-            height={330}
-          />
-        </ImageWrapper>
+      <StyledPageContent>
+        <Image
+          src="/core/images/circle-screen-bg.png"
+          alt="register vault"
+          width={353}
+          height={143}
+        />
+        <Text size={14} color="shyExtra">
+          Deposit your $USDC into a Circle account and earn yield. Securely
+          within your Vultisig vault.
+        </Text>
+        <MainWrapper>
+          <Text weight={600}>Balance Overview</Text>
+          <BalanceOverviewTable />
+          <APYOverview />
+          <div />
+          <Button>CLaim Rewards</Button>
+          <TransactionActions />
+          <BannerWrapper>
+            <Text size={12} color="shyExtra">
+              Funds remain fully under your vault’s control. Circle yield is
+              generated through secure off-chain treasuries. Withdraw anytime
+              after settlement.
+            </Text>
+            <IconButton>
+              <IconWrapper size={8}>
+                <NavigationXIcon />
+              </IconWrapper>
+            </IconButton>
+          </BannerWrapper>
+        </MainWrapper>
       </StyledPageContent>
       <PageFooter>
         <SaveAsImage
@@ -46,16 +77,38 @@ export const CirclePage = () => {
   )
 }
 
-const ImageWrapper = styled.div`
-  position: relative;
-`
-
 const StyledHeader = styled(PageHeader)`
   position: relative;
   z-index: 1;
 `
 
 const StyledPageContent = styled(PageContent)`
-  position: relative;
-  top: -75px;
+  max-width: 353px;
+  margin-inline: auto;
+
+  ${vStack({
+    gap: 12,
+    flexGrow: true,
+  })};
+`
+
+const MainWrapper = styled.div`
+  padding: 16px;
+  border-radius: 16px;
+  border: 1px solid ${getColor('foregroundSuper')};
+  background: ${getColor('foreground')};
+
+  ${vStack({
+    gap: 15,
+  })};
+`
+
+const BannerWrapper = styled.div`
+  ${hStack({
+    justifyContent: 'space-between',
+  })};
+
+  padding: 13px;
+  border: 1px solid ${getColor('foregroundExtra')};
+  background: ${getColor('foreground')};
 `
