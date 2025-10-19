4a9cf7a48a428902b399cb96716fbbe2398777e8
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-10-19 18:05:25 +0330

    Enhance inNewWindow function to adjust window dimensions based on OS. Added platform detection for macOS to set appropriate height and width, ensuring better user experience across different systems.

--- core/inpage-provider/popup/resolvers/background/inNewWindow.ts
+++ core/inpage-provider/popup/resolvers/background/inNewWindow.ts
@@ -1,6 +1,8 @@
 import { pick } from '@lib/utils/record/pick'
+import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
 
 import { PopupOptions } from '../../resolver'
+import { UAParser } from 'ua-parser-js'
 
 type ExecuteInput = {
   abortSignal: AbortSignal
@@ -17,13 +19,26 @@ export const inNewWindow = async <T>({
   execute,
 }: Input<T>): Promise<T> => {
   const currentWindow = await chrome.windows.getCurrent()
+
+  const platform =
+    'userAgentData' in navigator
+      ? (navigator.userAgentData as any).platform
+      : navigator.platform
+
+  const isMacOs = platform.toLowerCase().includes('mac')
+
   const newWindow = await new Promise<chrome.windows.Window | undefined>(
     resolve =>
       chrome.windows.create(
         {
           url,
           type: 'panel',
-          ...pick(currentWindow, ['height', 'left', 'top', 'width']),
+          top: currentWindow.top,
+          height: isMacOs ? currentWindow.height : 600,
+          width: isMacOs ? currentWindow.width : 480,
+          left: isMacOs
+            ? currentWindow.left
+            : shouldBePresent(currentWindow.width) - 500,
         },
         resolve
       )
