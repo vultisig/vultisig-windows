89f4edda267204db88c8f56bc49adf516425741e
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-20 09:05:26 +0300

    feat: add the new icon button kind in settings page

--- core/ui/settings/index.tsx
+++ core/ui/settings/index.tsx
@@ -83,7 +83,10 @@ export const SettingsPage: FC<Props> = props => {
             />
           }
           secondaryControls={
-            <IconButton onClick={() => navigate({ id: 'shareVault' })}>
+            <IconButton
+              kind="action"
+              onClick={() => navigate({ id: 'shareVault' })}
+            >
               <QrCodeIcon />
             </IconButton>
           }
