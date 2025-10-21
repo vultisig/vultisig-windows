652fa436c77a48e9737392a87f1302a1c70dde45
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-21 09:41:00 +0300

    feat: replace refresh icon with calendar clock and move header controls to separate component

--- clients/desktop/src/vault/components/VaultPage.tsx
+++ clients/desktop/src/vault/components/VaultPage.tsx
@@ -6,19 +6,17 @@ import { hasServer } from '@core/mpc/devices/localPartyId'
 import { FastVaultPasswordVerification } from '@core/ui/mpc/fast/FastVaultPasswordVerification'
 import { useCurrentVault } from '@core/ui/vault/state/currentVault'
 import { getVaultId } from '@core/ui/vault/Vault'
-import { IconButton } from '@lib/ui/buttons/IconButton'
-import { MenuIcon } from '@lib/ui/icons/MenuIcon'
 import { VStack } from '@lib/ui/layout/Stack'
 import { PageHeader } from '@lib/ui/page/PageHeader'
 
 import { UpdatePrompt } from '../../versioning/UpdatePrompt'
+import { VaultPageHeaderControls } from './VaultPageHeaderControls'
 import { VaultSelector } from './VaultSelector'
 
 export const VaultPage = () => {
   const vault = useCurrentVault()
   const { signers } = vault
   const isFastVault = hasServer(signers)
-  const navigate = useAppNavigate()
   const vaultId = getVaultId(vault)
 
   return (
@@ -26,12 +24,7 @@ export const VaultPage = () => {
       <VStack flexGrow>
         <PageHeader
           hasBorder
-          primaryControls={
-            <IconButton onClick={() => navigate({ id: 'settings' })}>
-              <MenuIcon />
-            </IconButton>
-          }
-          secondaryControls={<RefreshVaultBalance />}
+          secondaryControls={<VaultPageHeaderControls />}
           title={<VaultSelector value={vault} />}
         />
 
