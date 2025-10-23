2488f6298fd4fb3c8b4b95cf4819593fda43d982
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-23 10:20:07 +0300

    feat: add new arrow icon components for UI library

---
+++ lib/ui/icons/ArrowUpRightIcon.tsx
@@ -0,0 +1,17 @@
+export const ArrowUpRightIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 20 20"
+    fill="none"
+  >
+    <path
+      d="M15.2083 12.7082V4.7915M15.2083 4.7915H7.29167M15.2083 4.7915L5 14.9998"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
