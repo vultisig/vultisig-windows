705343d6731772209870a796df535302adc3c83e
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-10-23 10:19:10 +0300

    feat: prioritize deposit enabled chains in vault primary actions

--- clients/desktop/src/vault/components/VaultOverviewPrimaryActions.tsx
+++ clients/desktop/src/vault/components/VaultOverviewPrimaryActions.tsx
@@ -1,13 +1,18 @@
 import { VaultPrimaryActions } from '@core/ui/vault/components/VaultPrimaryActions'
+import { depositEnabledChains } from '@core/ui/vault/deposit/DepositEnabledChain'
 import { useCurrentVaultChains } from '@core/ui/vault/state/currentVaultCoins'
 import { isEmpty } from '@lib/utils/array/isEmpty'
 
 export const VaultOverviewPrimaryActions = () => {
   const chains = useCurrentVaultChains()
+  // @tony: favour deposit enabled chain as a default to show more primary actions
+  const depositEnabledChain = chains.find(chain =>
+    depositEnabledChains.includes(chain)
+  )
 
   if (isEmpty(chains)) {
     return null
   }
 
-  return <VaultPrimaryActions fromChain={chains[0]} />
+  return <VaultPrimaryActions fromChain={depositEnabledChain || chains[0]} />
 }
