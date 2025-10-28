7075906590a1043e3b97b4c64afcc082a2eb5b2a
rcoderdev <radzionch@gmail.com>
2025-10-28 07:45:14 +0400

    refactor: enhance TextInputWithPasteAction component for better paste handling
    - Integrated useRef to manage input value directly within TextInputWithPasteAction, improving paste functionality.
    - Updated DepositForm to utilize TextInputWithPasteAction, replacing the previous input field for enhanced reusability and maintainability.

--- core/ui/components/TextInputWithPasteAction.tsx
+++ core/ui/components/TextInputWithPasteAction.tsx
@@ -5,29 +5,39 @@ import {
   textInputHorizontalPadding,
 } from '@lib/ui/css/textInput'
 import { TextInput, TextInputProps } from '@lib/ui/inputs/TextInput'
+import { useRef } from 'react'
 
 import { InputPasteAction } from './InputPasteAction'
 
-export const TextInputWithPasteAction = (props: TextInputProps) => (
-  <ActionInsideInteractiveElement
-    render={({ actionSize }) => (
-      <TextInput
-        {...props}
-        style={{
-          paddingRight: actionSize.width + textInputHorizontalPadding,
-        }}
-      />
-    )}
-    action={
-      <InputPasteAction
-        onPaste={value => {
-          props.onValueChange?.(value)
-        }}
-      />
-    }
-    actionPlacerStyles={{
-      bottom: (textInputHeight - iconButtonSize.md) / 2,
-      right: textInputHorizontalPadding,
-    }}
-  />
-)
+export const TextInputWithPasteAction = (props: TextInputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  return (
+    <ActionInsideInteractiveElement
+      render={({ actionSize }) => (
+        <TextInput
+          {...props}
+          ref={inputRef}
+          style={{
+            paddingRight: actionSize.width + textInputHorizontalPadding,
+          }}
+        />
+      )}
+      action={
+        <InputPasteAction
+          onPaste={value => {
+            if (inputRef.current) {
+              inputRef.current.value = value
+              const event = new Event('input', { bubbles: true })
+              inputRef.current.dispatchEvent(event)
+            }
+          }}
+        />
+      }
+      actionPlacerStyles={{
+        bottom: (textInputHeight - iconButtonSize.md) / 2,
+        right: textInputHorizontalPadding,
+      }}
+    />
+  )
+}
