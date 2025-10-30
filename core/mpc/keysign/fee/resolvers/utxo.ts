9945dfd0e7e7d962af63446bb76a360aadc58dd7
rcoderdev <radzionch@gmail.com>
2025-10-30 11:14:22 +0400

    refactor: streamline UTXO fee calculation by enhancing input handling
    - Simplified the `getUtxoFeeAmount` function by directly passing the input object to `getUtxoSigningInputs`, improving code clarity.
    - Removed unnecessary destructuring of parameters, maintaining focus on essential logic for fee retrieval.

--- core/mpc/keysign/fee/resolvers/utxo.ts
+++ core/mpc/keysign/fee/resolvers/utxo.ts
@@ -3,16 +3,8 @@ import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
 import { getUtxoSigningInputs } from '../../signingInputs/resolvers/utxo'
 import { GetFeeAmountResolver } from '../resolver'
 
-export const getUtxoFeeAmount: GetFeeAmountResolver = ({
-  keysignPayload,
-  walletCore,
-  publicKey,
-}) => {
-  const [{ plan }] = getUtxoSigningInputs({
-    keysignPayload,
-    walletCore,
-    publicKey,
-  })
+export const getUtxoFeeAmount: GetFeeAmountResolver = input => {
+  const [{ plan }] = getUtxoSigningInputs(input)
 
   return shouldBePresent(plan?.fee, `UTXO signing input plan fee`).toBigInt()
 }
