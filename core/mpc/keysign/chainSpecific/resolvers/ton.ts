7304dd6f354c092d09992d96bb20907315392e59
rcoderdev <radzionch@gmail.com>
2025-11-01 12:40:34 +0400

    refactor: simplify coin retrieval in Ton keysign resolver
    - Updated getTonChainSpecific to streamline coin extraction by directly using keysignPayload, enhancing code clarity and maintainability.
    - Removed unnecessary type specification for coin retrieval, improving overall readability.

--- core/mpc/keysign/chainSpecific/resolvers/ton.ts
+++ core/mpc/keysign/chainSpecific/resolvers/ton.ts
@@ -1,5 +1,4 @@
 import { create } from '@bufbuild/protobuf'
-import { OtherChain } from '@core/chain/Chain'
 import { getTonAccountInfo } from '@core/chain/chains/ton/account/getTonAccountInfo'
 import { TonSpecificSchema } from '@core/mpc/types/vultisig/keysign/v1/blockchain_specific_pb'
 
@@ -9,8 +8,8 @@ import { GetChainSpecificResolver } from '../resolver'
 export const getTonChainSpecific: GetChainSpecificResolver<
   'tonSpecific'
 > = async ({ keysignPayload }) => {
-  const coin = getKeysignCoin<OtherChain.Ton>(keysignPayload)
-  const { account_state } = await getTonAccountInfo(coin.address)
+  const { address } = getKeysignCoin(keysignPayload)
+  const { account_state } = await getTonAccountInfo(address)
   const sequenceNumber = BigInt(account_state.seqno || 0)
 
   return create(TonSpecificSchema, {
