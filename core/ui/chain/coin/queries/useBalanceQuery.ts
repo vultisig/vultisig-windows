4380a0005a4a794b7fe29ae8f32e6ff2e1818a32
rcoderdev <radzionch@gmail.com>
2025-11-01 10:22:09 +0400

    refactor: remove unused useBalance hook and related logic
    - Deleted the useBalance hook from useBalanceQuery.ts, simplifying the balance retrieval process.
    - Removed the shouldBePresent utility call, streamlining the code and enhancing readability.

--- core/ui/chain/coin/queries/useBalanceQuery.ts
+++ core/ui/chain/coin/queries/useBalanceQuery.ts
@@ -1,6 +1,5 @@
 import { CoinBalanceResolverInput } from '@core/chain/coin/balance/resolver'
 import { coinKeyToString } from '@core/chain/coin/Coin'
-import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
 import { Exact } from '@lib/utils/types/Exact'
 import { useMemo } from 'react'
 
@@ -22,12 +21,4 @@ export const useBalanceQuery = <T extends CoinBalanceResolverInput>(
       error,
     }
   }, [query, input])
-}
-
-export const useBalance = <T extends CoinBalanceResolverInput>(
-  input: Exact<CoinBalanceResolverInput, T>
-) => {
-  const { data } = useBalanceQuery(input)
-
-  return shouldBePresent(data, `${coinKeyToString(input)} balance`)
 }
