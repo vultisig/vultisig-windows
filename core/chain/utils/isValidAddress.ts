fb04a855ec6fb1e6cbd4327bbb8c362a698d00e1
rcoderdev <radzionch@gmail.com>
2025-11-10 07:14:01 +0400

    Enhance address validation logic in isValidAddress function to support custom prefixes for MayaChain and Zcash. This update allows for more accurate Bech32 address validation based on chain-specific prefixes, improving overall address handling.

--- core/chain/utils/isValidAddress.ts
+++ core/chain/utils/isValidAddress.ts
@@ -8,14 +8,20 @@ type Input = {
   walletCore: WalletCore
 }
 
+const customPrefixes: Partial<Record<Chain, string>> = {
+  [Chain.MayaChain]: 'maya',
+  [Chain.Zcash]: 'tex',
+}
+
 export const isValidAddress = ({ chain, address, walletCore }: Input) => {
   const coinType = getCoinType({
     walletCore,
     chain,
   })
 
-  if (chain === Chain.MayaChain) {
-    return walletCore.AnyAddress.isValidBech32(address, coinType, 'maya')
+  const customPrefix = customPrefixes[chain]
+  if (customPrefix && address.toLowerCase().startsWith(customPrefix)) {
+    return walletCore.AnyAddress.isValidBech32(address, coinType, customPrefix)
   }
 
   return walletCore.AnyAddress.isValid(address, coinType)
