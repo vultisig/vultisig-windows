9c99d4bd9d17f74437c98e07745cf59cfe01549d
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-11-11 17:25:54 +0200

    feat: add block explorer link to vault chain page
    - Replaced refresh button with block explorer icon button in header
    - Added navigation to chain-specific block explorer for current vault address

--- core/ui/vault/chain/VaultChainPage.tsx
+++ core/ui/vault/chain/VaultChainPage.tsx
@@ -1,19 +1,35 @@
+import { getBlockExplorerUrl } from '@core/chain/utils/getBlockExplorerUrl'
 import { PageHeaderBackButton } from '@core/ui/flow/PageHeaderBackButton'
+import { useCore } from '@core/ui/state/core'
 import {
   BottomNavigation,
   bottomNavigationHeight,
 } from '@core/ui/vault/components/BottomNavigation'
 import { VaultHeader } from '@core/ui/vault/components/VaultHeader'
+import { IconButton } from '@lib/ui/buttons/IconButton'
 import { hideScrollbars } from '@lib/ui/css/hideScrollbars'
+import { ArCubeIcon } from '@lib/ui/icons/ArCubeIcon'
+import { IconWrapper } from '@lib/ui/icons/IconWrapper'
 import { VStack } from '@lib/ui/layout/Stack'
 import { PageContent } from '@lib/ui/page/PageContent'
 import styled from 'styled-components'
 
-import { RefreshVaultBalance } from '../page/balance/RefreshVaultBalance'
+import { useCurrentVaultAddress } from '../state/currentVaultCoins'
 import { VaultChainTabs } from './tabs/VaultChainTabs'
+import { useCurrentVaultChain } from './useCurrentVaultChain'
 import { VaultChainOverview } from './VaultChainOverview'
 
 export const VaultChainPage = () => {
+  const { openUrl } = useCore()
+  const chain = useCurrentVaultChain()
+  const address = useCurrentVaultAddress(chain)
+
+  const blockExplorerUrl = getBlockExplorerUrl({
+    chain,
+    entity: 'address',
+    value: address,
+  })
+
   return (
     <Wrapper
       data-testid="VaultChainPage-Wrapper"
@@ -23,8 +39,13 @@ export const VaultChainPage = () => {
       <VStack flexGrow>
         <VaultHeader
           primaryControls={<PageHeaderBackButton />}
-          showRefresh
-          refreshButton={<RefreshVaultBalance />}
+          secondaryControls={
+            <IconButton onClick={() => openUrl(blockExplorerUrl)}>
+              <IconWrapper size={24}>
+                <ArCubeIcon />
+              </IconWrapper>
+            </IconButton>
+          }
         />
         <StyledPageContent scrollable gap={32} flexGrow>
           <VaultChainOverview />
