a358c02282bdf43fbb7d50ca9cd1bf1f3f6b11b8
rcoderdev <radzionch@gmail.com>
2025-11-11 07:16:10 +0400

    Refactor UTXO handling in keysigning process to improve balance checks and payload updates. Removed unnecessary calculations and streamlined the conversion of UTXOs to UTXO info. Set sendMaxAmount to false by default for clarity in transaction handling.

--- core/mpc/keysign/refine/utxo.ts
+++ core/mpc/keysign/refine/utxo.ts
@@ -1,10 +1,14 @@
 import { create } from '@bufbuild/protobuf'
 import { shouldBePresent } from '@lib/utils/assert/shouldBePresent'
+import { bigIntSum } from '@lib/utils/bigint/bigIntSum'
 import { WalletCore } from '@trustwallet/wallet-core'
+import { TW } from '@trustwallet/wallet-core'
 import { PublicKey } from '@trustwallet/wallet-core/dist/src/wallet-core'
 
+import { UTXOSpecificSchema } from '../../types/vultisig/keysign/v1/blockchain_specific_pb'
 import { KeysignPayload } from '../../types/vultisig/keysign/v1/keysign_message_pb'
 import { UtxoInfoSchema } from '../../types/vultisig/keysign/v1/utxo_info_pb'
+import { getBlockchainSpecificValue } from '../chainSpecific/KeysignChainSpecific'
 import { getUtxoSigningInputs } from '../signingInputs/resolvers/utxo'
 
 type RefineKeysignUtxoInput = {
@@ -13,26 +17,96 @@ type RefineKeysignUtxoInput = {
   publicKey: PublicKey
 }
 
+const dustStats = 600n
+
+type ConvertPlanUtxosToUtxoInfoInput = {
+  utxos: Array<TW.Bitcoin.Proto.IUnspentTransaction>
+  walletCore: WalletCore
+}
+
+const convertPlanUtxosToUtxoInfo = ({
+  utxos,
+  walletCore,
+}: ConvertPlanUtxosToUtxoInfoInput) =>
+  utxos.map(({ outPoint, amount }) => {
+    const hash = shouldBePresent(outPoint?.hash, 'UTXO outPoint hash')
+    const index = shouldBePresent(outPoint?.index, 'UTXO outPoint index')
+
+    return create(UtxoInfoSchema, {
+      hash: walletCore.HexCoding.encode(Uint8Array.from(hash).reverse()),
+      amount: BigInt(shouldBePresent(amount, 'UTXO amount').toString()),
+      index,
+    })
+  })
+
 export const refineKeysignUtxo = (input: RefineKeysignUtxoInput) => {
   const [signingInput] = getUtxoSigningInputs(input)
 
-  if (signingInput.plan?.utxos) {
-    return {
-      ...input.keysignPayload,
-      utxoInfo: signingInput.plan.utxos.map(({ outPoint, amount }) => {
-        const hash = shouldBePresent(outPoint?.hash, 'UTXO outPoint hash')
-        const index = shouldBePresent(outPoint?.index, 'UTXO outPoint index')
+  const utxoSpecific = getBlockchainSpecificValue(
+    input.keysignPayload.blockchainSpecific,
+    'utxoSpecific'
+  )
 
-        return create(UtxoInfoSchema, {
-          hash: input.walletCore.HexCoding.encode(
-            Uint8Array.from(hash).reverse()
-          ),
-          amount: BigInt(shouldBePresent(amount, 'UTXO amount').toString()),
-          index,
-        })
-      }),
+  const plan = shouldBePresent(signingInput.plan, 'UTXO signing input plan')
+  const actualFee = BigInt(
+    shouldBePresent(plan.fee, 'UTXO signing input plan fee').toString()
+  )
+  const utxos = shouldBePresent(plan.utxos, 'UTXO signing input plan utxos')
+
+  let updatedKeysignPayload = input.keysignPayload
+  let updatedBlockchainSpecific = input.keysignPayload.blockchainSpecific
+
+  if (input.keysignPayload.toAmount && !utxoSpecific.sendMaxAmount) {
+    const amount = BigInt(input.keysignPayload.toAmount)
+    const balance = bigIntSum(
+      input.keysignPayload.utxoInfo.map(({ amount }) => amount)
+    )
+    const remainingBalance = balance - amount
+
+    if (remainingBalance <= actualFee + dustStats) {
+      updatedBlockchainSpecific = {
+        case: 'utxoSpecific',
+        value: create(UTXOSpecificSchema, {
+          ...utxoSpecific,
+          sendMaxAmount: true,
+        }),
+      }
+
+      updatedKeysignPayload = {
+        ...input.keysignPayload,
+        blockchainSpecific: updatedBlockchainSpecific,
+      }
+
+      const [updatedSigningInput] = getUtxoSigningInputs({
+        ...input,
+        keysignPayload: updatedKeysignPayload,
+      })
+
+      const updatedPlan = shouldBePresent(
+        updatedSigningInput.plan,
+        'UTXO updated signing input plan'
+      )
+      const updatedUtxos = shouldBePresent(
+        updatedPlan.utxos,
+        'UTXO updated signing input plan utxos'
+      )
+
+      return {
+        ...updatedKeysignPayload,
+        utxoInfo: convertPlanUtxosToUtxoInfo({
+          utxos: updatedUtxos,
+          walletCore: input.walletCore,
+        }),
+      }
     }
   }
 
-  return input.keysignPayload
+  return {
+    ...updatedKeysignPayload,
+    blockchainSpecific: updatedBlockchainSpecific,
+    utxoInfo: convertPlanUtxosToUtxoInfo({
+      utxos,
+      walletCore: input.walletCore,
+    }),
+  }
 }
