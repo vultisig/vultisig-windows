b9ff377415c7ac61797d2d54fc306b2434188b8c
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-11-14 09:07:45 +0200

    refactor: reorder imports in ReadBackupFileStep component

--- clients/desktop/src/vault/import/components/ReadBackupFileStep.tsx
+++ clients/desktop/src/vault/import/components/ReadBackupFileStep.tsx
@@ -1,15 +1,14 @@
 import { useAppViewState } from '@clients/desktop/src/navigation/hooks/useAppViewState'
-import { Buffer } from 'buffer'
-
 import { ReadFileBase64 } from '@clients/desktop/wailsjs/go/main/App'
 import { FlowErrorPageContent } from '@core/ui/flow/FlowErrorPageContent'
 import { FlowPageHeader } from '@core/ui/flow/FlowPageHeader'
-import { FileBasedVaultBackupResult } from '@core/ui/vault/import/VaultBackupResult'
 import { vaultBackupResultFromFileBytes } from '@core/ui/vault/import/utils/vaultBackupResultFromFile'
+import { FileBasedVaultBackupResult } from '@core/ui/vault/import/VaultBackupResult'
 import { FlowPendingPageContent } from '@lib/ui/flow/FlowPendingPageContent'
 import { OnFinishProp } from '@lib/ui/props'
 import { MatchQuery } from '@lib/ui/query/components/MatchQuery'
 import { useMutation } from '@tanstack/react-query'
+import { Buffer } from 'buffer'
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
