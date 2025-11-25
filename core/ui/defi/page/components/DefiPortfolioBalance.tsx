9d0f2e37f5995428c90996ba41b6fef7c241c0e8
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-11-25 11:43:02 +0200

    feat: replace DefiPortfolioIcons with banner image and update layout styling
    - Replaced DefiPortfolioIcons component with Image component displaying homepage-banner.svg
    - Added ImageWrapper with absolute positioning and object-fit cover for banner image
    - Increased DeFi Portfolio text size from 14 to 18
    - Added fixed container height of 136px mobile, 360px tablet and up
    - Created Content wrapper with relative positioning and centered flex layout
    - Removed DefiPortfolioIcons import and added Image an

--- core/ui/defi/page/components/DefiPortfolioBalance.tsx
+++ core/ui/defi/page/components/DefiPortfolioBalance.tsx
@@ -2,15 +2,15 @@ import { useFormatFiatAmount } from '@core/ui/chain/hooks/useFormatFiatAmount'
 import { BalanceVisibilityAware } from '@core/ui/vault/balance/visibility/BalanceVisibilityAware'
 import { ManageVaultBalanceVisibility } from '@core/ui/vault/page/balance/visibility/ManageVaultBalanceVisibility'
 import { useVaultTotalBalanceQuery } from '@core/ui/vault/queries/useVaultTotalBalanceQuery'
+import { Image } from '@lib/ui/image/Image'
 import { HStack, VStack } from '@lib/ui/layout/Stack'
 import { Spinner } from '@lib/ui/loaders/Spinner'
 import { MatchQuery } from '@lib/ui/query/components/MatchQuery'
+import { mediaQuery } from '@lib/ui/responsive/mediaQuery'
 import { Text } from '@lib/ui/text'
 import { getColor } from '@lib/ui/theme/getters'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
-
-import { DefiPortfolioIcons } from './DefiPortfolioIcons'
 
 export const DefiPortfolioBalance = () => {
   const query = useVaultTotalBalanceQuery()
@@ -19,9 +19,15 @@ export const DefiPortfolioBalance = () => {
 
   return (
     <Container>
-      <DefiPortfolioIcons />
-      <VStack alignItems="center" gap={12}>
-        <Text size={14} color="contrast" weight="500">
+      <ImageWrapper>
+        <Image
+          src="/core/images/defi/homepage-banner.svg"
+          width="100%"
+          height="100%"
+        />
+      </ImageWrapper>
+      <Content alignItems="center" gap={12}>
+        <Text size={18} color="contrast" weight="500">
           DeFi Portfolio
         </Text>
         <MatchQuery
@@ -41,7 +47,7 @@ export const DefiPortfolioBalance = () => {
           )}
         />
         <ManageVaultBalanceVisibility />
-      </VStack>
+      </Content>
     </Container>
   )
 }
@@ -56,4 +62,25 @@ const Container = styled.div`
   border-radius: 16px;
   background: ${getColor('foreground')};
   margin: 0 20px;
+  height: 136px;
+
+  @media ${mediaQuery.tabletDeviceAndUp} {
+    height: 360px;
+  }
+`
+
+const Content = styled(VStack)`
+  position: relative;
+  flex: 1;
+  justify-content: center;
+`
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  inset: 0;
+
+  & > img {
+    object-fit: cover;
+  }
 `
