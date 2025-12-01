677d2c2a9298075bf0cd765e03819ad5bf0ba765
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-12-01 08:46:36 +0200

    Refactor StakedPositions to use FilteredPositions component
    - Replaced position filtering logic with FilteredPositions component
    - Removed direct imports of useDefiPositions, getAvailablePositionsForChain, useCurrentDefiChain, DefiPositionEmptyState, and PositionsList
    - Simplified component to pass positionType="stake" to FilteredPositions

--- core/ui/defi/chain/tabs/StakedPositions.tsx
+++ core/ui/defi/chain/tabs/StakedPositions.tsx
@@ -1,26 +1,5 @@
-import {
-  getAvailablePositionsForChain,
-  useDefiPositions,
-} from '@core/ui/storage/defiPositions'
-
-import { useCurrentDefiChain } from '../useCurrentDefiChain'
-import { DefiPositionEmptyState } from './DefiPositionEmptyState'
-import { PositionsList } from './PositionsList'
+import { FilteredPositions } from './FilteredPositions'
 
 export const StakedPositions = () => {
-  const chain = useCurrentDefiChain()
-  const selectedPositionIds = useDefiPositions(chain)
-
-  const availablePositions = getAvailablePositionsForChain(chain)
-  const stakePositions = availablePositions.filter(p => p.type === 'stake')
-
-  const selectedStakePositions = stakePositions.filter(p =>
-    selectedPositionIds.includes(p.id)
-  )
-
-  if (selectedStakePositions.length === 0) {
-    return <DefiPositionEmptyState />
-  }
-
-  return <PositionsList positions={selectedStakePositions} />
+  return <FilteredPositions positionType="stake" />
 }
