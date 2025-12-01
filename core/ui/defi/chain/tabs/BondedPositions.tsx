09efaafea1d2fa1c1105c9c61beae2b9b112f8f6
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-12-01 08:45:47 +0200

    Refactor BondedPositions to use FilteredPositions component
    - Replaced position filtering logic with FilteredPositions component
    - Removed direct imports of useDefiPositions, getAvailablePositionsForChain, useCurrentDefiChain, DefiPositionEmptyState, and PositionsList
    - Simplified component to pass positionType="bond" to FilteredPositions

--- core/ui/defi/chain/tabs/BondedPositions.tsx
+++ core/ui/defi/chain/tabs/BondedPositions.tsx
@@ -1,26 +1,5 @@
-import {
-  getAvailablePositionsForChain,
-  useDefiPositions,
-} from '@core/ui/storage/defiPositions'
-
-import { useCurrentDefiChain } from '../useCurrentDefiChain'
-import { DefiPositionEmptyState } from './DefiPositionEmptyState'
-import { PositionsList } from './PositionsList'
+import { FilteredPositions } from './FilteredPositions'
 
 export const BondedPositions = () => {
-  const chain = useCurrentDefiChain()
-  const selectedPositionIds = useDefiPositions(chain)
-
-  const availablePositions = getAvailablePositionsForChain(chain)
-  const bondPositions = availablePositions.filter(p => p.type === 'bond')
-
-  const selectedBondPositions = bondPositions.filter(p =>
-    selectedPositionIds.includes(p.id)
-  )
-
-  if (selectedBondPositions.length === 0) {
-    return <DefiPositionEmptyState />
-  }
-
-  return <PositionsList positions={selectedBondPositions} />
+  return <FilteredPositions positionType="bond" />
 }
