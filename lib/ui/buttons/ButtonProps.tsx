ef818bf592ed7c7841cf8d4dc47ddad5b4dc7900
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2025-12-15 08:33:13 +0200

    ui(button): remove action kind

--- lib/ui/buttons/ButtonProps.tsx
+++ lib/ui/buttons/ButtonProps.tsx
@@ -5,7 +5,7 @@ export type ButtonProps = Omit<
   'disabled'
 > & {
   disabled?: boolean | string
-  kind?: 'primary' | 'secondary' | 'link' | 'action' | 'outlined'
+  kind?: 'primary' | 'secondary' | 'link' | 'outlined'
   loading?: boolean
   status?: 'default' | 'danger' | 'success' | 'warning'
 }
