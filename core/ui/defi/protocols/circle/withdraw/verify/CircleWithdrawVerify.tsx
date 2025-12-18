c506bd911d698bb9529ffbb39db5fef8e83ad7c6
rcoderdev <radzionch@gmail.com>
2025-12-18 08:20:45 +0400

    Add Circle withdraw keysign functionality
    - Introduced build.ts to create keysign payload for Circle withdrawals.
    - Implemented useCircleWithdrawKeysignPayloadQuery for managing keysign payload queries.
    - Updated CircleWithdrawVerify component to utilize the new keysign payload query, enhancing the withdrawal verification process.

--- core/ui/defi/protocols/circle/withdraw/verify/CircleWithdrawVerify.tsx
+++ core/ui/defi/protocols/circle/withdraw/verify/CircleWithdrawVerify.tsx
@@ -1,24 +1,41 @@
+import { Chain } from '@core/chain/Chain'
+import { usdc } from '@core/chain/coin/knownTokens'
 import { TitleHeader } from '@core/ui/flow/TitleHeader'
-import { VStack } from '@lib/ui/layout/Stack'
+import { VerifyKeysignStart } from '@core/ui/mpc/keysign/start/VerifyKeysignStart'
+import { VerifyTransactionOverview } from '@core/ui/mpc/keysign/verify/VerifyTransactionOverview'
+import { useCurrentVaultAddress } from '@core/ui/vault/state/currentVaultCoins'
 import { OnBackProp, ValueProp } from '@lib/ui/props'
-import { Text } from '@lib/ui/text'
 import { useTranslation } from 'react-i18next'
 
-import { CirclePageContainer } from '../../CirclePageContainer'
+import { circleName } from '../../core/config'
+import { useCircleAccount } from '../../queries/circleAccount'
+import { useCircleWithdrawKeysignPayloadQuery } from '../keysignPayload/useCircleWithdrawKeysignPayloadQuery'
 
 type CircleWithdrawVerifyProps = ValueProp<bigint> & OnBackProp
 
-export const CircleWithdrawVerify = ({ onBack }: CircleWithdrawVerifyProps) => {
+export const CircleWithdrawVerify = ({
+  value: amount,
+  onBack,
+}: CircleWithdrawVerifyProps) => {
   const { t } = useTranslation()
+  const vaultAddress = useCurrentVaultAddress(Chain.Ethereum)
+  const { address: senderAddress } = useCircleAccount()
+  const keysignPayloadQuery = useCircleWithdrawKeysignPayloadQuery({ amount })
 
   return (
     <>
       <TitleHeader title={t('circle.verify_withdraw')} onBack={onBack} />
-      <CirclePageContainer>
-        <VStack alignItems="center" justifyContent="center" flexGrow>
-          <Text color="shy">{t('coming_soon')}</Text>
-        </VStack>
-      </CirclePageContainer>
+      <VerifyKeysignStart keysignPayloadQuery={keysignPayloadQuery}>
+        <VerifyTransactionOverview
+          coin={usdc}
+          amount={amount}
+          senderName={circleName}
+          senderAddress={senderAddress}
+          receiver={vaultAddress}
+          chain={Chain.Ethereum}
+          keysignPayloadQuery={keysignPayloadQuery}
+        />
+      </VerifyKeysignStart>
     </>
   )
 }
