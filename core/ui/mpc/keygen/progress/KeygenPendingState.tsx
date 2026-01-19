be44806579bf701c0bff0962a69860a02961563a
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2026-01-19 12:36:20 +0200

    feat: add keygen pending state

--- core/ui/mpc/keygen/progress/KeygenPendingState.tsx
+++ core/ui/mpc/keygen/progress/KeygenPendingState.tsx
@@ -1,24 +1,35 @@
 import { KeygenStep } from '@core/mpc/keygen/KeygenStep'
-import { KeygenProductEducation } from '@core/ui/mpc/keygen/education/product/KeygenProductEducation'
+import { KeygenLoadingAnimation } from '@core/ui/mpc/keygen/progress/KeygenLoadingAnimation'
 import { KeygenProgressIndicator } from '@core/ui/mpc/keygen/progress/KeygenProgressIndicator'
 import { VStack } from '@lib/ui/layout/Stack'
-import { PageContent } from '@lib/ui/page/PageContent'
 import { PageFooter } from '@lib/ui/page/PageFooter'
 import { ValueProp } from '@lib/ui/props'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow: hidden;
+`
+
+const ContentArea = styled.div`
+  position: relative;
+  flex: 1;
+  z-index: 1;
+`
 
 export const KeygenPendingState = ({ value }: ValueProp<KeygenStep | null>) => {
   return (
-    <>
-      <PageContent alignItems="center" justifyContent="center" scrollable>
-        <VStack maxWidth={576} fullWidth>
-          <KeygenProductEducation />
-        </VStack>
-      </PageContent>
+    <Container>
+      <KeygenLoadingAnimation isConnected={value !== null} />
+      <ContentArea />
       <PageFooter alignItems="center">
         <VStack alignItems="center" maxWidth={576} fullWidth>
           <KeygenProgressIndicator value={value} />
         </VStack>
       </PageFooter>
-    </>
+    </Container>
   )
 }
