b7feb415a55f1849248de4e5e3630186421c7a1f
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2026-01-21 10:03:48 +0200

    feat: update config

--- core/ui/vault/settings/referral/components/CreateReferral/CreateReferralForm/config.ts
+++ core/ui/vault/settings/referral/components/CreateReferral/CreateReferralForm/config.ts
@@ -4,6 +4,7 @@ export const referralSchema = z.object({
   referralName: z
     .string()
     .max(4, 'Max 4 characters')
+    .regex(/^\S+$/, 'Whitespace not allowed')
     .nonempty('Validated Referral code is required'),
   expiration: z
     .number()
