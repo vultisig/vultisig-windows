6534bc778ac1d0c1dfdfbd33c856e9a591e8dd09
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2026-01-30 14:38:24 +0200

    fix: gradient

--- core/ui/vult/discount/tier/container.tsx
+++ core/ui/vult/discount/tier/container.tsx
@@ -10,7 +10,7 @@ type TierStyles = {
 
 const tierStyles: Record<VultDiscountTier, TierStyles> = {
   bronze: {
-    background: `radial-gradient(77.32% 73.64% at 57.64% 132.91%, rgba(219, 87, 39, 0.40) 0%, rgba(6, 27, 58, 0.00) 100%),
+    background: `radial-gradient(77.32% 55% at 57.64% 132.91%, rgba(219, 87, 39, 0.40) 0%, rgba(6, 27, 58, 0.00) 100%),
       linear-gradient(218deg, rgba(6, 27, 58, 0.00) 55%, rgba(219, 87, 39, 0.40) 104.56%),
       linear-gradient(164deg, rgba(6, 27, 58, 0.00) 47.12%, rgba(219, 87, 39, 0.40) 126.16%),
       linear-gradient(0deg, rgba(6, 27, 58, 0.50) 0%, rgba(6, 27, 58, 0.50) 100%), #061B3A`,
@@ -19,14 +19,14 @@ const tierStyles: Record<VultDiscountTier, TierStyles> = {
   },
   silver: {
     background: `linear-gradient(204deg, rgba(6, 27, 58, 0.00) 42.72%, rgba(201, 214, 232, 0.40) 122.24%),
-      radial-gradient(77.52% 74.04% at 54.43% 138.19%, rgba(201, 214, 232, 0.40) 0%, rgba(6, 27, 58, 0.00) 100%),
+      radial-gradient(77.52% 55% at 54.43% 138.19%, rgba(201, 214, 232, 0.40) 0%, rgba(6, 27, 58, 0.00) 100%),
       linear-gradient(164deg, rgba(6, 27, 58, 0.00) 47.12%, rgba(201, 214, 232, 0.40) 126.16%),
       linear-gradient(0deg, rgba(6, 27, 58, 0.50) 0%, rgba(6, 27, 58, 0.50) 100%), #061B3A`,
     borderBottom: '2px solid rgba(51, 119, 217, 0.22)',
     boxShadow: '0 4px 16.6px 0 rgba(201, 214, 232, 0.14) inset',
   },
   gold: {
-    background: `radial-gradient(89.11% 84.51% at 72.66% 127.89%, rgba(255, 194, 92, 0.60) 0%, rgba(6, 27, 58, 0.00) 100%),
+    background: `radial-gradient(89.11% 60% at 72.66% 127.89%, rgba(255, 194, 92, 0.60) 0%, rgba(6, 27, 58, 0.00) 100%),
       linear-gradient(204deg, rgba(6, 27, 58, 0.00) 42.72%, rgba(255, 194, 92, 0.50) 122.24%),
       linear-gradient(164deg, rgba(6, 27, 58, 0.00) 47.12%, rgba(255, 194, 92, 0.50) 126.16%),
       linear-gradient(0deg, rgba(6, 27, 58, 0.50) 0%, rgba(6, 27, 58, 0.50) 100%), #061B3A`,
@@ -34,7 +34,7 @@ const tierStyles: Record<VultDiscountTier, TierStyles> = {
     boxShadow: '0 4px 16.6px 0 rgba(255, 194, 92, 0.15) inset',
   },
   platinum: {
-    background: `radial-gradient(67.3% 63.79% at 54.43% 117.84%, rgba(51, 230, 191, 0.60) 0%, rgba(6, 27, 58, 0.00) 100%),
+    background: `radial-gradient(67.3% 48% at 54.43% 117.84%, rgba(51, 230, 191, 0.60) 0%, rgba(6, 27, 58, 0.00) 100%),
       linear-gradient(204deg, rgba(6, 27, 58, 0.00) 42.72%, rgba(51, 230, 191, 0.60) 122.24%),
       linear-gradient(150deg, rgba(6, 27, 58, 0.00) 49.38%, rgba(72, 121, 253, 0.60) 109.62%),
       linear-gradient(0deg, rgba(6, 27, 58, 0.50) 0%, rgba(6, 27, 58, 0.50) 100%), #061B3A`,
@@ -42,7 +42,7 @@ const tierStyles: Record<VultDiscountTier, TierStyles> = {
     boxShadow: '0 8px 26px 0 rgba(19, 200, 157, 0.26) inset',
   },
   diamond: {
-    background: `radial-gradient(82.33% 78.08% at 66.75% 125.38%, rgba(0, 204, 255, 0.60) 0%, rgba(6, 27, 58, 0.00) 100%),
+    background: `radial-gradient(82.33% 58% at 66.75% 125.38%, rgba(0, 204, 255, 0.60) 0%, rgba(6, 27, 58, 0.00) 100%),
       linear-gradient(205deg, rgba(6, 27, 58, 0.00) 35.49%, rgba(151, 71, 255, 0.60) 121.65%),
       linear-gradient(150deg, rgba(6, 27, 58, 0.00) 49.38%, rgba(151, 71, 255, 0.60) 109.62%),
       linear-gradient(0deg, rgba(6, 27, 58, 0.50) 0%, rgba(6, 27, 58, 0.50) 100%), #061B3A`,
@@ -50,7 +50,7 @@ const tierStyles: Record<VultDiscountTier, TierStyles> = {
     boxShadow: '0 8px 26px 0 rgba(19, 61, 200, 0.36) inset',
   },
   ultimate: {
-    background: `radial-gradient(95.09% 93.37% at 35.96% 118.34%, rgba(255, 194, 92, 0.60) 0%, rgba(6, 27, 58, 0.00) 100%),
+    background: `radial-gradient(95.09% 70% at 35.96% 118.34%, rgba(255, 194, 92, 0.60) 0%, rgba(6, 27, 58, 0.00) 100%),
       linear-gradient(205deg, rgba(6, 27, 58, 0.00) 35.49%, rgba(72, 121, 253, 0.70) 121.65%),
       linear-gradient(133deg, rgba(6, 27, 58, 0.00) 51.68%, rgba(249, 195, 105, 0.70) 99.67%),
       linear-gradient(0deg, rgba(6, 27, 58, 0.50) 0%, rgba(6, 27, 58, 0.50) 100%), #061B3A`,
