f528cfef60f1bb6b93c7ac94c1d06c14c0760e2b
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2026-02-05 12:36:05 +0200

    feat(deposit): remove autocompound toggle for TCY deposits
    Remove the autocompound checkbox when staking TCY since sTCY
    is now managed through the DeFi section instead of the deposit flow.

--- core/ui/vault/deposit/DepositForm/ActionSpecific/StakeSpecific/StakeSpecific.tsx
+++ core/ui/vault/deposit/DepositForm/ActionSpecific/StakeSpecific/StakeSpecific.tsx
@@ -1,12 +1,9 @@
-import { tcyAutoCompounderConfig } from '@core/chain/chains/cosmos/thor/tcy-autocompound/config'
 import { Match } from '@lib/ui/base/Match'
 import { Opener } from '@lib/ui/base/Opener'
 import { ChevronRightIcon } from '@lib/ui/icons/ChevronRightIcon'
 import { IconWrapper } from '@lib/ui/icons/IconWrapper'
-import { Checkbox } from '@lib/ui/inputs/checkbox/Checkbox'
-import { HStack, VStack } from '@lib/ui/layout/Stack'
+import { HStack } from '@lib/ui/layout/Stack'
 import { Text } from '@lib/ui/text'
-import { Controller } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 
 import { StakeableChain } from '../../../config'
@@ -16,7 +13,7 @@ import { AssetRequiredLabel, Container } from '../../DepositForm.styled'
 import { StakeTokenExplorer } from './StakeTokenExplorer'
 
 export const StakeSpecific = () => {
-  const [{ chain, control }] = useDepositFormHandlers()
+  const [{ chain }] = useDepositFormHandlers()
   const { t } = useTranslation()
   const [selectedCoin, setSelectedCoin] = useDepositCoin()
 
@@ -26,38 +23,21 @@ export const StakeSpecific = () => {
       THORChain={() => (
         <Opener
           renderOpener={({ onOpen }) => (
-            <VStack gap={8}>
-              <Container onClick={onOpen}>
-                <HStack alignItems="center" gap={4}>
-                  <Text weight="400" family="mono" size={16}>
-                    {selectedCoin.ticker || t('select_token')}
-                  </Text>
-                  {!selectedCoin && (
-                    <AssetRequiredLabel as="span" color="danger" size={14}>
-                      *
-                    </AssetRequiredLabel>
-                  )}
-                </HStack>
-                <IconWrapper style={{ fontSize: 20 }}>
-                  <ChevronRightIcon />
-                </IconWrapper>
-              </Container>
-              {selectedCoin?.id === tcyAutoCompounderConfig.depositDenom && (
-                <Controller
-                  name="autoCompound"
-                  control={control}
-                  render={({ field: { onChange, value } }) => (
-                    <Checkbox
-                      value={value}
-                      onChange={onChange}
-                      label={t('auto_compound_into_label', {
-                        ticker: tcyAutoCompounderConfig.shareTicker,
-                      })}
-                    />
-                  )}
-                />
-              )}
-            </VStack>
+            <Container onClick={onOpen}>
+              <HStack alignItems="center" gap={4}>
+                <Text weight="400" family="mono" size={16}>
+                  {selectedCoin.ticker || t('select_token')}
+                </Text>
+                {!selectedCoin && (
+                  <AssetRequiredLabel as="span" color="danger" size={14}>
+                    *
+                  </AssetRequiredLabel>
+                )}
+              </HStack>
+              <IconWrapper style={{ fontSize: 20 }}>
+                <ChevronRightIcon />
+              </IconWrapper>
+            </Container>
           )}
           renderContent={({ onClose }) => (
             <StakeTokenExplorer
