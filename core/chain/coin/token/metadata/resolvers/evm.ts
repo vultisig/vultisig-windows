aa3e3c5641cb1ae85e2f35defaf51ebfda9a5971
Ehsan-saradar <ehsan.saradar@gmail.com>
2026-02-08 23:19:42 +0330

    Fetch token logo from 1inch API for EVM

--- core/chain/coin/token/metadata/resolvers/evm.ts
+++ core/chain/coin/token/metadata/resolvers/evm.ts
@@ -1,6 +1,11 @@
 import { EvmChain } from '@core/chain/Chain'
+import { getEvmChainId } from '@core/chain/chains/evm/chainInfo'
 import { getEvmClient } from '@core/chain/chains/evm/client'
+import { queryOneInch } from '@core/chain/coin/find/resolvers/evm/queryOneInch'
+import { OneInchToken } from '@core/chain/coin/oneInch/token'
 import { TokenMetadataResolver } from '@core/chain/coin/token/metadata/resolver'
+import { attempt } from '@lib/utils/attempt'
+import { hexToNumber } from '@lib/utils/hex/hexToNumber'
 import { Address, erc20Abi } from 'viem'
 
 export const getEvmTokenMetadata: TokenMetadataResolver<EvmChain> = async ({
@@ -22,8 +27,25 @@ export const getEvmTokenMetadata: TokenMetadataResolver<EvmChain> = async ({
     }),
   ])
 
-  return {
+  const result: ReturnType<TokenMetadataResolver<EvmChain>> = {
     ticker,
     decimals,
   }
+
+  // Try to fetch logo from 1inch API
+  const oneInchChainId = hexToNumber(getEvmChainId(chain))
+  const logoResult = await attempt(() =>
+    queryOneInch<Record<string, OneInchToken>>(
+      `/token/v1.2/${oneInchChainId}/custom?addresses=${id}`
+    )
+  )
+
+  if ('data' in logoResult) {
+    const tokenData = logoResult.data[id]
+    if (tokenData?.logoURI) {
+      result.logo = tokenData.logoURI
+    }
+  }
+
+  return result
 }
