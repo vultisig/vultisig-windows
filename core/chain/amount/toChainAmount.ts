aae19db15a8cedb2784be3325f3000e74f655491
Ehsan-saradar <ehsan.saradar@gmail.com>
2026-02-10 00:14:39 +0330

    fix issue with scientific notation

--- core/chain/amount/toChainAmount.ts
+++ core/chain/amount/toChainAmount.ts
@@ -1,4 +1,7 @@
 import { parseUnits } from 'viem'
 
-export const toChainAmount = (amount: number, decimals: number) =>
-  parseUnits(amount.toFixed(decimals), decimals)
+export const toChainAmount = (amount: number, decimals: number) => {
+  const str = amount.toString()
+  const value = /[eE]/.test(str) ? amount.toFixed(decimals) : str
+  return parseUnits(value, decimals)
+}
