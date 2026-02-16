6bde24b5b4131e2b1086d328e394cdb16db2601e
Ehsan-saradar <ehsan.saradar@gmail.com>
2026-02-16 12:55:55 +0330

    Update memo typography

--- core/ui/chain/tx/TxOverviewPlainMemo.tsx
+++ core/ui/chain/tx/TxOverviewPlainMemo.tsx
@@ -1,4 +1,4 @@
-import { HStack } from '@lib/ui/layout/Stack'
+import { VStack } from '@lib/ui/layout/Stack'
 import { ValueProp } from '@lib/ui/props'
 import { Text } from '@lib/ui/text'
 import { useTranslation } from 'react-i18next'
@@ -7,13 +7,11 @@ export const TxOverviewPlainMemo = ({ value }: ValueProp<string>) => {
   const { t } = useTranslation()
 
   return (
-    <HStack alignItems="center" gap={4} justifyContent="space-between">
-      <Text size={14} color="shy" weight="500">
-        {t('memo')}
-      </Text>
-      <Text color="primary" size={14}>
+    <VStack gap={4}>
+      <Text color="shy">{t('memo')}</Text>
+      <Text color="primary" family="mono" size={14} weight="700">
         {value}
       </Text>
-    </HStack>
+    </VStack>
   )
 }
