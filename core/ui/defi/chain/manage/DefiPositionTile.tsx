52417ef9bd0a48ac9b9dcbb1eef76698619e79c8
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2026-02-16 10:21:54 +0200

    fix: stretch position icon wrapper to fill parent width in DefiPositionTile

--- core/ui/defi/chain/manage/DefiPositionTile.tsx
+++ core/ui/defi/chain/manage/DefiPositionTile.tsx
@@ -38,6 +38,7 @@ const PositionIconWrapper = styled.div<IsActiveProp>`
     justifyContent: 'center',
   })};
   position: relative;
+  align-self: stretch;
   border-radius: 24px;
   background: rgba(11, 26, 58, 0.5);
   height: 74px;
