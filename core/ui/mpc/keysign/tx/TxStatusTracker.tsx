118e7872bde3b9a0593a5a4e178633aa2cd4b2e9
rcoderdev <radzionch@gmail.com>
2026-02-19 08:07:23 +0400

    Squashed commits for feature-transaction-status-animation (#3377)

---
+++ core/ui/mpc/keysign/tx/TxStatusTracker.tsx
@@ -0,0 +1,15 @@
+import { Chain } from '@core/chain/Chain'
+import { useTxStatusQuery } from '@core/ui/chain/tx/status/useTxStatusQuery'
+
+import { TransactionStatusAnimation } from './TransactionStatusAnimation'
+
+type TxStatusTrackerProps = {
+  chain: Chain
+  hash: string
+}
+
+export const TxStatusTracker = ({ chain, hash }: TxStatusTrackerProps) => {
+  const { data: status } = useTxStatusQuery({ chain, hash })
+
+  return <TransactionStatusAnimation status={status ?? 'pending'} />
+}
