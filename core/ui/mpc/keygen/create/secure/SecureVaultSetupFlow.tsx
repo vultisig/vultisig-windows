368cdeb777ea3d4954c3e9f35e8a94711e571b4f
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2026-02-21 20:43:17 +0200

    [Onboarding v3][PR2] Add referral support and step indicator to SecureVaultSetupFlow

--- core/ui/mpc/keygen/create/secure/SecureVaultSetupFlow.tsx
+++ core/ui/mpc/keygen/create/secure/SecureVaultSetupFlow.tsx
@@ -1,5 +1,12 @@
+import { useBoolean } from '@lib/ui/hooks/useBoolean'
 import { OnBackProp, OnFinishProp } from '@lib/ui/props'
+import { useToast } from '@lib/ui/toast/ToastProvider'
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
+import { ReferralHeaderButton } from '../steps/ReferralHeaderButton'
+import { ReferralModal } from '../steps/ReferralModal'
+import { secureVaultSetupSteps } from '../steps/vault-setup-steps'
 import { VaultNameStep } from '../steps/VaultNameStep'
 import { SecureVaultCreationInput } from '../VaultCreationInput'
 
@@ -9,6 +16,45 @@ type SecureVaultSetupFlowProps = OnFinishProp<SecureVaultCreationInput> &
 export const SecureVaultSetupFlow = ({
   onFinish,
   onBack,
-}: SecureVaultSetupFlowProps) => (
-  <VaultNameStep onFinish={onFinish} onBack={onBack} />
-)
+}: SecureVaultSetupFlowProps) => {
+  const { t } = useTranslation()
+  const [referralCode, setReferralCode] = useState('')
+  const [
+    isReferralModalOpen,
+    { set: openReferralModal, unset: closeReferralModal },
+  ] = useBoolean(false)
+  const { addToast } = useToast()
+
+  const handleApplyReferral = () => {
+    closeReferralModal()
+    addToast({ message: t('fastVaultSetup.referralAdded') })
+  }
+
+  const headerRight = (
+    <ReferralHeaderButton
+      hasReferral={!!referralCode}
+      onClick={openReferralModal}
+    />
+  )
+
+  return (
+    <>
+      <ReferralModal
+        isOpen={isReferralModalOpen}
+        onClose={closeReferralModal}
+        referralCode={referralCode}
+        onReferralCodeChange={setReferralCode}
+        onApply={handleApplyReferral}
+      />
+      <VaultNameStep
+        onFinish={({ name }) =>
+          onFinish({ name, referral: referralCode || undefined })
+        }
+        onBack={onBack}
+        steps={secureVaultSetupSteps}
+        stepIndex={0}
+        headerRight={headerRight}
+      />
+    </>
+  )
+}
