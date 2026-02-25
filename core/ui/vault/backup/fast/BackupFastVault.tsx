6bf5cacd1d2efa5ba53e5482bf2f3c4495327995
Antonio Simeonov <63366707+antoni0dev@users.noreply.github.com>
2026-02-25 11:08:14 +0200

    feat(backup): wire VaultCreatedSuccessScreen into fast vault flow

--- core/ui/vault/backup/fast/BackupFastVault.tsx
+++ core/ui/vault/backup/fast/BackupFastVault.tsx
@@ -1,9 +1,7 @@
 import { isServer } from '@core/mpc/devices/localPartyId'
 import { useVaultCreationInput } from '@core/ui/mpc/keygen/create/state/vaultCreationInput'
-import { useVaults } from '@core/ui/storage/vaults'
 import { BackupOverviewScreen } from '@core/ui/vault/backup/BackupOverviewScreen'
 import { EmailConfirmation } from '@core/ui/vault/backup/fast'
-import { VaultBackupSummaryStep } from '@core/ui/vault/backup/VaultBackupSummaryStep'
 import { SaveVaultStep } from '@core/ui/vault/save/SaveVaultStep'
 import { useCurrentVault } from '@core/ui/vault/state/currentVault'
 import { Match } from '@lib/ui/base/Match'
@@ -12,6 +10,7 @@ import { OnBackProp, OnFinishProp } from '@lib/ui/props'
 import { useTranslation } from 'react-i18next'
 
 import { InitiateFastVaultBackup } from './InitiateFastVaultBackup'
+import { VaultCreatedSuccessScreen } from './VaultCreatedSuccessScreen'
 
 const steps = [
   'backupOverview',
@@ -38,15 +37,12 @@ export const BackupFastVault = ({
   const { step, toNextStep, toPreviousStep } = useStepNavigation({
     steps,
   })
-  const vaults = useVaults()
   const vault = useCurrentVault()
   const vaultCreationInput = useVaultCreationInput()
   const email =
     vaultCreationInput && 'fast' in vaultCreationInput
       ? vaultCreationInput.fast.email
       : ''
-  // @antonio: by design we only need to show the summary step if user has more than 2 vaults
-  const shouldShowBackupSummary = vaults.length > 1
 
   return (
     <Match
@@ -77,18 +73,12 @@ export const BackupFastVault = ({
       backupPage={() => (
         <InitiateFastVaultBackup
           password={password}
-          onFinish={() => {
-            if (shouldShowBackupSummary) {
-              toNextStep()
-            } else {
-              onFinish()
-            }
-          }}
+          onFinish={toNextStep}
           onBack={toPreviousStep}
         />
       )}
       backupSuccessfulSlideshow={() => (
-        <VaultBackupSummaryStep onFinish={onFinish} />
+        <VaultCreatedSuccessScreen onFinish={onFinish} />
       )}
     />
   )
