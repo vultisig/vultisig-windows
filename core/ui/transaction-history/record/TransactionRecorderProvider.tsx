2754d4861762cdaca3ad7ce60d9021ba087cef74
Ehsan-saradar <ehsan.saradar@gmail.com>
2026-03-29 07:53:43 +0330

    Fix TransactionRecorderProvider to handle mutation listener errors and successes (#3636)
    * fix: update TransactionRecorderProvider to handle mutation listener errors and successes
    
    - Integrated `useKeysignMutationListener` to manage success and error callbacks.
    - Adjusted the mutation listener to call the parent listener's methods on success and error events.
    
    * fix: remove unnecessary async from onSuccess callback in TransactionRecorderProvider

--- core/ui/transaction-history/record/TransactionRecorderProvider.tsx
+++ core/ui/transaction-history/record/TransactionRecorderProvider.tsx
@@ -1,5 +1,8 @@
 import { getVaultId } from '@core/mpc/vault/Vault'
-import { KeysignMutationListenerProvider } from '@core/ui/mpc/keysign/action/state/keysignMutationListener'
+import {
+  KeysignMutationListenerProvider,
+  useKeysignMutationListener,
+} from '@core/ui/mpc/keysign/action/state/keysignMutationListener'
 import { useSaveTransactionRecordMutation } from '@core/ui/storage/transactionHistory'
 import { useCurrentVault } from '@core/ui/vault/state/currentVault'
 import { ChildrenProp } from '@lib/ui/props'
@@ -9,6 +12,7 @@ import { useKeysignMessagePayload } from '../../mpc/keysign/state/keysignMessage
 import { createTransactionRecord } from './createTransactionRecord'
 
 export const TransactionRecorderProvider = ({ children }: ChildrenProp) => {
+  const parentListener = useKeysignMutationListener()
   const payload = useKeysignMessagePayload()
   const vault = useCurrentVault()
   const vaultId = getVaultId(vault)
@@ -17,7 +21,12 @@ export const TransactionRecorderProvider = ({ children }: ChildrenProp) => {
   return (
     <KeysignMutationListenerProvider
       value={{
+        onError: error => {
+          parentListener.onError?.(error)
+        },
         onSuccess: result => {
+          parentListener.onSuccess?.(result)
+
           if (!('txs' in result)) return
           if (!('keysign' in payload)) return
 
