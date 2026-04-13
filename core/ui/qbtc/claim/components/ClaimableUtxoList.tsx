6d4e8339519aadfb5ae88c783d2f7ce78845d6c0
Ehsan-saradar <ehsan.saradar@gmail.com>
2026-04-13 10:57:28 +0330

    feat(qbtc): add ClaimableUtxoList component with empty state

---
+++ core/ui/qbtc/claim/components/ClaimableUtxoList.tsx
@@ -0,0 +1,34 @@
+import { VStack } from '@lib/ui/layout/Stack'
+import { List } from '@lib/ui/list'
+import { Text } from '@lib/ui/text'
+import { useTranslation } from 'react-i18next'
+
+import { QbtcUtxo } from '../types'
+import { ClaimableUtxoItem } from './ClaimableUtxoItem'
+
+type ClaimableUtxoListProps = {
+  utxos: QbtcUtxo[]
+}
+
+/** Renders a list of claimable QBTC UTXOs or an empty-state message. */
+export const ClaimableUtxoList = ({ utxos }: ClaimableUtxoListProps) => {
+  const { t } = useTranslation()
+
+  if (utxos.length === 0) {
+    return (
+      <VStack alignItems="center" gap={8} style={{ paddingTop: 32 }}>
+        <Text color="supporting" size={14}>
+          {t('qbtc_claim_no_claimable_utxos')}
+        </Text>
+      </VStack>
+    )
+  }
+
+  return (
+    <List>
+      {utxos.map(utxo => (
+        <ClaimableUtxoItem key={`${utxo.txid}-${utxo.vout}`} value={utxo} />
+      ))}
+    </List>
+  )
+}
