21d35a27f4a7a850cdf34fcb646925c69365a898
Ehsan-saradar <ehsan.saradar@gmail.com>
2026-04-13 11:37:58 +0330

    fix(qbtc): use qbtcTendermintRpcUrl export for extension compatibility

--- core/ui/qbtc/claim/getClaimWithProofDisabled.ts
+++ core/ui/qbtc/claim/getClaimWithProofDisabled.ts
@@ -1,11 +1,11 @@
-import { qbtcRestUrl } from '@vultisig/core-chain/chains/cosmos/qbtc/tendermintRpcUrl'
+import { qbtcTendermintRpcUrl } from '@vultisig/core-chain/chains/cosmos/qbtc/tendermintRpcUrl'
 import { queryUrl } from '@vultisig/lib-utils/query/queryUrl'
 
 import { QbtcParamsResponse } from './types'
 
 /** Checks whether the ClaimWithProof feature is disabled on the QBTC chain. */
 export const getClaimWithProofDisabled = async (): Promise<boolean> => {
-  const url = `${qbtcRestUrl}/qbtc/v1/params/ClaimWithProofDisabled`
+  const url = `${qbtcTendermintRpcUrl}/qbtc/v1/params/ClaimWithProofDisabled`
 
   const { param } = await queryUrl<QbtcParamsResponse>(url)
 
