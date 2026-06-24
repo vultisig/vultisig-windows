d70cae7ef7d79203aada0e7aef47eb3e8a9e5254
rcoderdev <radzionch@gmail.com>
2026-06-24 04:45:43 +0400

    Implement Ethereum wallet permissions (#4188)

--- clients/extension/src/inpage/providers/ethereum/resolvers/wallet_requestPermissions.ts
+++ clients/extension/src/inpage/providers/ethereum/resolvers/wallet_requestPermissions.ts
@@ -1,1 +1,23 @@
-export const requestWalletPermissions = async (): Promise<any[]> => []
+import { requestEthAccounts } from './eth_requestAccounts'
+import {
+  getEthAccountsPermission,
+  type Web3WalletPermission,
+} from './wallet_getPermissions'
+
+type WalletRequestPermissionsInput = Array<
+  Partial<Record<Web3WalletPermission['parentCapability'], unknown>>
+>
+
+export const requestWalletPermissions = async (
+  params?: WalletRequestPermissionsInput
+): Promise<Web3WalletPermission[]> => {
+  const requestedPermissions = params?.[0]
+
+  if (!requestedPermissions || !('eth_accounts' in requestedPermissions)) {
+    return []
+  }
+
+  const accounts = await requestEthAccounts(undefined)
+
+  return [getEthAccountsPermission(accounts)]
+}
