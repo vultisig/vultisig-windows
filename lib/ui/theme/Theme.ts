3d1b5c4c359b3c29d97b60aefbb0ef2cec8ea0d6
rcoderdev <radzionch@gmail.com>
2026-07-01 19:04:21 +0400

    Fix theme icon style export

--- lib/ui/theme/Theme.ts
+++ lib/ui/theme/Theme.ts
@@ -1,7 +1,7 @@
 import { ThemeColors } from './ThemeColors'
 import { ThemeName } from './ThemeName'
 
-export const iconStyles = ['vultisig', 'station'] as const
+const iconStyles = ['vultisig', 'station'] as const
 export type IconStyle = (typeof iconStyles)[number]
 
 export type Theme = {
