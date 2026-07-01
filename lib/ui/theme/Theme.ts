4999268defbe3a6f07e1aa3cadc058cb421fb02c
rcoderdev <radzionch@gmail.com>
2026-07-01 19:01:31 +0400

    Address theme icon style typing

--- lib/ui/theme/Theme.ts
+++ lib/ui/theme/Theme.ts
@@ -1,9 +1,12 @@
 import { ThemeColors } from './ThemeColors'
 import { ThemeName } from './ThemeName'
 
+export const iconStyles = ['vultisig', 'station'] as const
+export type IconStyle = (typeof iconStyles)[number]
+
 export type Theme = {
   name: ThemeName
   fontFamily: string
-  iconStyle: 'vultisig' | 'station'
+  iconStyle: IconStyle
   colors: ThemeColors
 }
