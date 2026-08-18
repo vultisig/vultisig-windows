27d91fb98f68b567f315aca7149a786e54c43cbc
rcoderdev <radzionch@gmail.com>
2026-08-18 10:56:29 +0400

    fix(tron): include memo fee in estimates

--- core/inpage-provider/popup/view/resolvers/sendTx/SendTxOverview.tsx
+++ core/inpage-provider/popup/view/resolvers/sendTx/SendTxOverview.tsx
@@ -23,6 +23,7 @@ import { useAssertWalletCore } from '@core/ui/chain/providers/WalletCoreProvider
 import { BlockaidEvmSimulationView } from '@core/ui/chain/security/blockaid/tx/blockaidEvmSimulationView'
 import { useEvmContractCallInfoQuery } from '@core/ui/chain/tx/utils/useEvmContractCallInfoQuery'
 import { useUniversalRouterSwap } from '@core/ui/chain/tx/utils/useUniversalRouterSwap'
+import { getKeysignFeeAmount } from '@core/ui/mpc/keysign/fee/tronMemoFee'
 import { VerifyKeysignStart } from '@core/ui/mpc/keysign/start/VerifyKeysignStart'
 import { SignAminoDisplay } from '@core/ui/mpc/keysign/tx/components/SignAminoDisplay'
 import { SignDirectDisplay } from '@core/ui/mpc/keysign/tx/components/SignDirectDisplay'
@@ -68,7 +69,6 @@ import { parseBlockaidSuiSimulation } from '@vultisig/core-chain/security/blocka
 import { BlockaidSolanaSimulationInfo } from '@vultisig/core-chain/security/blockaid/tx/simulation/core'
 import { FeeSettings } from '@vultisig/core-mpc/keysign/chainSpecific/FeeSettings'
 import { getBlockchainSpecificValue } from '@vultisig/core-mpc/keysign/chainSpecific/KeysignChainSpecific'
-import { getFeeAmount } from '@vultisig/core-mpc/keysign/fee'
 import { getKeysignChain } from '@vultisig/core-mpc/keysign/utils/getKeysignChain'
 import { KeysignPayload } from '@vultisig/core-mpc/types/vultisig/keysign/v1/keysign_message_pb'
 import { shouldBePresent } from '@vultisig/lib-utils/assert/shouldBePresent'
@@ -135,7 +135,7 @@ export const SendTxOverview = ({
       walletCore,
     ],
     queryFn: () =>
-      getFeeAmount({
+      getKeysignFeeAmount({
         keysignPayload: shouldBePresent(keysignPayloadQuery.data),
         walletCore,
         publicKey: shouldBePresent(publicKey),
---
+++ core/ui/mpc/keysign/fee/tronMemoFee.ts
@@ -0,0 +1,121 @@
+import { getTronStakingDisplay } from '@core/ui/chain/tx/getTronStakingDisplay'
+import { Chain } from '@vultisig/core-chain/Chain'
+import { tronRpcUrl } from '@vultisig/core-chain/chains/tron/config'
+import { isFeeCoin } from '@vultisig/core-chain/coin/utils/isFeeCoin'
+import { getFeeAmount } from '@vultisig/core-mpc/keysign/fee'
+import { getSendFeeEstimate } from '@vultisig/core-mpc/keysign/send/getSendFeeEstimate'
+import { getKeysignChain } from '@vultisig/core-mpc/keysign/utils/getKeysignChain'
+
+const defaultTronMemoFee = 1_000_000n
+
+type TronChainParameter = {
+  key?: unknown
+  value?: unknown
+}
+
+type TronChainParametersResponse = {
+  chainParameter?: unknown
+}
+
+const parseMemoFee = (value: unknown): bigint | undefined => {
+  if (typeof value === 'bigint') return value >= 0n ? value : undefined
+
+  if (typeof value === 'number') {
+    return Number.isSafeInteger(value) && value >= 0 ? BigInt(value) : undefined
+  }
+
+  if (typeof value === 'string' && /^\d+$/.test(value)) {
+    return BigInt(value)
+  }
+
+  return undefined
+}
+
+export const fetchTronMemoFee = async (
+  fetcher: typeof fetch = fetch
+): Promise<bigint> => {
+  try {
+    const response = await fetcher(`${tronRpcUrl}/wallet/getchainparameters`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: '{}',
+    })
+
+    if (!response.ok) return defaultTronMemoFee
+
+    const data = (await response.json()) as TronChainParametersResponse
+    if (!Array.isArray(data.chainParameter)) return defaultTronMemoFee
+
+    const memoParameter = (data.chainParameter as TronChainParameter[]).find(
+      parameter => parameter.key === 'getMemoFee'
+    )
+
+    return parseMemoFee(memoParameter?.value) ?? defaultTronMemoFee
+  } catch {
+    return defaultTronMemoFee
+  }
+}
+
+let tronMemoFeePromise: Promise<bigint> | undefined
+
+const getTronMemoFee = () => {
+  tronMemoFeePromise ??= fetchTronMemoFee()
+  return tronMemoFeePromise
+}
+
+type TronMemoFeeInput = {
+  chain: Chain
+  isNativeToken: boolean
+  memo?: string
+}
+
+export const paysTronMemoFee = ({
+  chain,
+  isNativeToken,
+  memo,
+}: TronMemoFeeInput) =>
+  chain === Chain.Tron &&
+  isNativeToken &&
+  !!memo &&
+  !getTronStakingDisplay({ chain, memo })
+
+type AddTronMemoFeeInput = TronMemoFeeInput & {
+  fee: bigint
+  memoFee?: () => Promise<bigint>
+}
+
+export const addTronMemoFee = async ({
+  fee,
+  memoFee = getTronMemoFee,
+  ...input
+}: AddTronMemoFeeInput) =>
+  paysTronMemoFee(input) ? fee + (await memoFee()) : fee
+
+type GetKeysignFeeAmountInput = Parameters<typeof getFeeAmount>[0]
+
+export const getKeysignFeeAmount = async (input: GetKeysignFeeAmountInput) => {
+  const fee = await getFeeAmount(input)
+  const { keysignPayload } = input
+
+  return addTronMemoFee({
+    fee,
+    chain: getKeysignChain(keysignPayload),
+    isNativeToken: keysignPayload.coin?.isNativeToken ?? false,
+    memo: keysignPayload.memo,
+  })
+}
+
+type GetSendFeeEstimateInput = Parameters<typeof getSendFeeEstimate>[0]
+
+export const getSendFeeEstimateWithTronMemo = async (
+  input: GetSendFeeEstimateInput
+) => {
+  const fee = await getSendFeeEstimate(input)
+
+  return addTronMemoFee({
+    fee,
+    chain: input.coin.chain,
+    isNativeToken: isFeeCoin(input.coin),
+    memo: input.memo,
+  })
+}
